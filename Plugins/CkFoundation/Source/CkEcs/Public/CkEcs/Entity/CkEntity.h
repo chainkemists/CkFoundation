c21db244c9128488c2fe98dadefbe67b9f3d538d
Saad Khattak <sulfur@chainkemists.com>
2023-07-31 19:33:23 -0700

    feat: added ecs Entity
    - a wrapper around the concept of an 'ID' in entt/ecs
    - the wrapper helps with debugging in the editor

---
+++ Plugins/CkFoundation/Source/CkEcs/Public/CkEcs/Entity/CkEntity.h
@@ -0,0 +1,97 @@
+#pragma once
+
+#include "CkMacros/CkMacros.h"
+#include "CkFormat/CkFormat.h"
+
+#include "CkThirdParty/entt-3.12.2/src/entt/entity/entity.hpp"
+
+#include <CoreMinimal.h>
+
+#include <CkEntity.generated.h>
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType, meta=(HasNativeMake, HasNativeBreak="CkEcs.CkUtilsEntityUE.Break_Entity"))
+struct CKECS_API FCk_Entity
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Entity);
+
+public:
+    using IdType = entt::entity;
+    using TombstoneType = entt::tombstone_t;
+    using EntityNumberType = entt::entt_traits<IdType>::entity_type;
+    using VersionNumberType = entt::entt_traits<IdType>::version_type;
+
+public:
+    FCk_Entity(IdType InID = entt::tombstone);
+    FCk_Entity(TombstoneType);
+
+    auto operator=(IdType InOther) noexcept -> ThisType&;
+    auto operator=(TombstoneType InOther) noexcept -> ThisType&;
+
+    auto operator==(ThisType InOther) const -> bool;
+    auto operator<(ThisType InOther) const -> bool;
+    CK_DECL_AND_DEF_OPERATORS(ThisType);
+
+public:
+    auto Get_IsTombstone() const -> bool;
+
+    auto Get_EntityNumber() const -> EntityNumberType;
+    auto Get_VersionNumber() const -> VersionNumberType;
+
+public:
+    static auto Tombstone() -> FCk_Entity;
+
+private:
+    IdType _ID;
+
+private:
+    static FCk_Entity _Tombstone;
+
+public:
+    CK_PROPERTY_GET(_ID)
+
+#if WITH_EDITORONLY_DATA
+private:
+    UPROPERTY(VisibleAnywhere)
+    int32 _EntityID;
+
+    UPROPERTY(VisibleAnywhere)
+    int32 _EntityNumber;
+
+    UPROPERTY(VisibleAnywhere)
+    int32 _EntityVersion;
+#endif
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+#if NOT WITH_EDITORONLY_DATA
+static_assert
+(
+    sizeof(FCk_Entity) == sizeof(FCk_Entity::IdType),
+    "Entity should be nothing more than an ID"
+);
+#endif
+
+// --------------------------------------------------------------------------------------------------------------------
+
+CK_DEFINE_CUSTOM_FORMATTER(FCk_Entity, [&]()
+{
+    return ck::Format
+    (
+        TEXT("{}|{}({})"),
+        InObj.Get_ID(),
+        InObj.Get_EntityNumber(),
+        InObj.Get_VersionNumber()
+    );
+});
+
+// --------------------------------------------------------------------------------------------------------------------
+
+CK_DELETE_CUSTOM_IS_VALID(FCk_Entity);
+
+// --------------------------------------------------------------------------------------------------------------------
