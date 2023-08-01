5be606378b588f12c34291f4603998cc03a42a08
Saad Khattak <sulfur@chainkemists.com>
2023-07-31 19:29:14 -0700

    feat: added extract_value_type and make_new_ptr type traits
    - extract_value_type stores the value_type of pointer type
    - make_new_ptr can return a constructed complex pointer such as TUniquePtr or TSharedPtr
    - added All and Any policies

--- Plugins/CkFoundation/Source/CkCore/Public/CkTypeTraits/CkTypeTraits.h
+++ Plugins/CkFoundation/Source/CkCore/Public/CkTypeTraits/CkTypeTraits.h
@@ -1,7 +1,60 @@
 #pragma once
 
+#include <CoreMinimal.h>
+
 namespace ck::type_traits
 {
     struct as_array {};
     struct as_string {};
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    template <typename T>
+    struct extract_value_type;
+
+    template <typename T>
+    struct extract_value_type<T*> { using type = T; };
+
+    template <typename T>
+    struct extract_value_type<TUniquePtr<T>> { using type = T; };
+
+    template <typename T>
+    struct extract_value_type<TSharedPtr<T>> { using type = T; };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    template <typename T>
+    struct make_new_ptr;
+
+    template <typename T>
+    struct make_new_ptr<TSharedPtr<T>>
+    {
+        using type = T;
+
+        template <typename... T_Args>
+        auto operator()(T_Args&&... InArgs)
+        {
+            return MakeShared<T>(std::forward<T_Args>(InArgs)...);
+        }
+    };
+
+    template <typename T>
+    struct make_new_ptr<TUniquePtr<T>>
+    {
+        using type = T;
+
+        template <typename... T_Args>
+        auto operator()(T_Args&&... InArgs)
+        {
+            return MakeUnique<T>(std::forward<T_Args>(InArgs)...);
+        }
+    };
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ck::policy
+{
+    struct All {};
+    struct Any {};
 }
