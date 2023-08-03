3bcf27fd4f31feb2be5341fd77c4679a2ace2a64
Saad Khattak <sulfur@chainkemists.com>
2023-08-02 22:05:37 -0700

    chore: minor fixes and name change of FPtrWrapper to TPtrWrapper

--- Plugins/CkFoundation/Source/CkCore/Public/CkTypes/CkConstWrapper.h
+++ Plugins/CkFoundation/Source/CkCore/Public/CkTypes/CkConstWrapper.h
@@ -6,17 +6,24 @@ namespace ck
     // Intended to be used in another class to force const-correctness on types
     // that are stored as pointers
     template <typename T_ComplexPtrType>
-    class FPtrWrapper
+    class TPtrWrapper
     {
     public:
         using ValueType = T_ComplexPtrType;
+        using ThisType = TPtrWrapper<ValueType>;
         using StoredValueType = typename type_traits::extract_value_type<T_ComplexPtrType>::type;
 
     public:
-        FPtrWrapper();
+        TPtrWrapper();
+        TPtrWrapper(const ThisType& InOther);
+        TPtrWrapper(ThisType&& InOther) = default;
 
         template <typename... T_Args>
-        FPtrWrapper(T_Args&&... InArgs);
+        TPtrWrapper(T_Args&&... InArgs);
+
+    public:
+        auto operator=(const ThisType& InOther) -> ThisType& ;
+        auto operator=(ThisType&& InOther) -> ThisType & = default;
 
     public:
         auto operator->() -> StoredValueType*;
@@ -32,38 +39,51 @@ namespace ck
     // --------------------------------------------------------------------------------------------------------------------
 
     template <typename T_ComplexPtrType>
-    FPtrWrapper<T_ComplexPtrType>::FPtrWrapper()
+    TPtrWrapper<T_ComplexPtrType>::TPtrWrapper()
         :  _Ptr(std::move(type_traits::make_new_ptr<ValueType>{}()))
     {
     }
 
     template <typename T_ComplexPtrType>
+    TPtrWrapper<T_ComplexPtrType>::TPtrWrapper(const ThisType& InOther)
+        : _Ptr(ck::type_traits::move_or_copy_ptr<ValueType>{}(InOther._Ptr))
+    {
+    }
+
+    template <typename T_ComplexPtrType>
     template <typename ... T_Args>
-    FPtrWrapper<T_ComplexPtrType>::FPtrWrapper(T_Args&&... InArgs)
+    TPtrWrapper<T_ComplexPtrType>::TPtrWrapper(T_Args&&... InArgs)
         :  _Ptr(std::move(type_traits::make_new_ptr<ValueType>{}(std::forward<T_Args>(InArgs)...)))
     {
     }
 
     template <typename T_ComplexPtrType>
-    auto FPtrWrapper<T_ComplexPtrType>::operator->() -> StoredValueType*
+    auto TPtrWrapper<T_ComplexPtrType>::operator=(const ThisType& InOther) -> ThisType&
+    {
+        _Ptr = ck::type_traits::move_or_copy_ptr<ValueType>{}(InOther._Ptr);
+        return *this;
+    }
+
+    template <typename T_ComplexPtrType>
+    auto TPtrWrapper<T_ComplexPtrType>::operator->() -> StoredValueType*
     {
         return _Ptr.Get();
     }
 
     template <typename T_ComplexPtrType>
-    auto FPtrWrapper<T_ComplexPtrType>::operator->() const -> const StoredValueType*
+    auto TPtrWrapper<T_ComplexPtrType>::operator->() const -> const StoredValueType*
     {
         return _Ptr.Get();
     }
 
     template <typename T_ComplexPtrType>
-    auto FPtrWrapper<T_ComplexPtrType>::operator*() -> StoredValueType&
+    auto TPtrWrapper<T_ComplexPtrType>::operator*() -> StoredValueType&
     {
         return *_Ptr;
     }
 
     template <typename T_ComplexPtrType>
-    auto FPtrWrapper<T_ComplexPtrType>::operator*() const -> const StoredValueType&
+    auto TPtrWrapper<T_ComplexPtrType>::operator*() const -> const StoredValueType&
     {
         return *_Ptr;
     }
