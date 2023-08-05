49e699374afafd24541e8cf4522f0cf070371c4f
Saad Khattak <sulfur@chainkemists.com>
2023-08-04 21:08:30 -0700

    feat: Registry can now create and destroy Entities
    - all functions private since our 'Lifetime' module will handle creation/destruction of Entities
    - can create an Entity with an ID hint

--- Plugins/CkFoundation/Source/CkEcs/Public/CkEcs/Registry/CkRegistry.cpp
+++ Plugins/CkFoundation/Source/CkEcs/Public/CkEcs/Registry/CkRegistry.cpp
@@ -11,6 +11,21 @@ void foo()
 
 // --------------------------------------------------------------------------------------------------------------------
 
+auto FCk_Registry::CreateEntity() -> EntityType
+{
+    return _InternalRegistry->create();
+}
+
+auto FCk_Registry::CreateEntity(EntityType InEntityHint) -> EntityType
+{
+    return _InternalRegistry->create(InEntityHint.Get_ID());
+}
+
+auto FCk_Registry::DestroyEntity(EntityType InEntity) -> void
+{
+    _InternalRegistry->destroy(InEntity.Get_ID());
+}
+
 auto FCk_Registry::IsValid(EntityType InEntity) -> bool
 {
     return _InternalRegistry->valid(InEntity.Get_ID());
