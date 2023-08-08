57edace9aa04d9d913c0debb1988abfebc7de535
Saad Khattak <sulfur@chainkemists.com>
2023-08-07 21:16:50 -0700

    feat, wip: base Processor that provides a nicer interface
    - it is not necessary to use this processor as a base
    - this base processor provides a nicer interface for deriving systems
    - uses template polymorphism to drive the compile-time deduced fragments/tags for `View` preparation

---
+++ Plugins/CkFoundation/Source/CkEcs/Public/CkEcs/Processor/CkProcessor.h
@@ -0,0 +1,69 @@
+#pragma once
+#include "CkEcs/Handle/CkHandle.h"
+#include "CkEcs/Registry/CkRegistry.h"
+
+
+namespace ck
+{
+    template <typename T_DerivedProcessor, typename... T_Fragments>
+    class TProcessor
+    {
+        CK_GENERATED_BODY(TProcessor<T_DerivedProcessor COMMA T_Fragments...>);
+
+    public:
+        using FEntitType = FCk_Entity;
+        using FTimeType = float;
+        using FHandleType = FCk_Handle;
+        using FRegistryType = FCk_Registry;
+        using FDerivedType = T_DerivedProcessor;
+
+    public:
+        explicit TProcessor(const FRegistryType& InRegistry);
+
+    public:
+        // TODO: replace InDeltaT with type-safe Time once ready
+        auto Tick(FTimeType InDeltaT) -> void;
+
+    private:
+        template <typename... T_ComponentsOnly>
+        auto DoTick(FTimeType InDeltaT, entt::type_list<T_ComponentsOnly...>) -> void;
+
+    private:
+        CK_ENABLE_SFINAE_THIS(FDerivedType);
+
+    protected:
+        FRegistryType _Registry;
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    template <typename T_DerivedProcessor, typename ... T_Fragments>
+    TProcessor<T_DerivedProcessor, T_Fragments...>::TProcessor(const FRegistryType& InRegistry)
+        : _Registry(InRegistry)
+    {
+    }
+
+    template <typename T_DerivedProcessor, typename ... T_Fragments>
+    auto TProcessor<T_DerivedProcessor, T_Fragments...>::Tick(FTimeType InDeltaT) -> void
+    {
+        using FViewType = decltype(_Registry.View<T_Fragments...>());
+        using FComponentsOnly = typename FViewType::template TComponentsOnly<T_Fragments...>;
+
+        DoTick(InDeltaT, FComponentsOnly{});
+    }
+
+    template <typename T_DerivedProcessor, typename ... T_Fragments>
+    template <typename ... T_ComponentsOnly>
+    auto TProcessor<T_DerivedProcessor, T_Fragments...>::DoTick(FTimeType InDeltaT,
+        entt::type_list<T_ComponentsOnly...>) -> void
+    {
+        _Registry.View<T_Fragments...>().Each([&](FEntitType InEntity, T_ComponentsOnly&... InComponents)
+        {
+            auto Handle = FHandleType{InEntity, _Registry};
+            This()->ForEachEntity(InDeltaT, Handle, InComponents...);
+        });
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------
+}
+
