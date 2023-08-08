efed00396e5953d60397e229687d19e4785cefa9
Saad Khattak <sulfur@chainkemists.com>
2023-08-07 21:20:30 -0700

    feat: added the Lifetime fragment, Processor and Utils
    - the Lifetime utils it the main (only) way to create Entities (Registry.Create() is `private`)
    - although the processors are untested, they compile while using the new base CkProcessor
    
    notes: we do not have owership semantics with EntityLifetime yet which need to come in sooner rather than later to avoid leaked Entities

---
+++ Plugins/CkFoundation/Source/CkLifetime/Public/CkLifetime/EntityLifetime/CkEntityLifetime_Fragment_Utils.cpp
@@ -0,0 +1,37 @@
+#include "CkEntityLifetime_Fragment_Utils.h"
+
+#include "CkLifetime/EntityLifetime/CkEntityLifetime_Fragment.h"
+
+auto UCk_Utils_EntityLifetime_UE::
+Request_DestroyEntity(FCk_Handle InHandle) -> void
+{
+    InHandle.Add<ck::FCk_Tag_TriggerDestroyEntity>();
+}
+
+auto UCk_Utils_EntityLifetime_UE::
+Request_CreateEntity(FCk_Handle InHandle) -> FCk_Handle
+{
+    const auto& NewEntity = (*InHandle)->CreateEntity();
+    InHandle.Add<ck::FCk_Tag_EntityJustCreated>(NewEntity);
+
+    return HandleType{ NewEntity, **InHandle };
+}
+
+auto UCk_Utils_EntityLifetime_UE::
+Request_CreateEntity(RegistryType& InRegistry) -> HandleType
+{
+    const auto& NewEntity = InRegistry.CreateEntity();
+    InRegistry.Add<ck::FCk_Tag_EntityJustCreated>(NewEntity);
+
+    return HandleType{ NewEntity, InRegistry };
+}
+
+auto UCk_Utils_EntityLifetime_UE::
+Request_CreateEntity(RegistryType& InRegistry,
+    const EntityIdHint& InEntityHint) -> HandleType
+{
+    const auto& NewEntity = InRegistry.CreateEntity(InEntityHint.Entity);
+    InRegistry.Add<ck::FCk_Tag_EntityJustCreated>(NewEntity);
+
+    return HandleType{ NewEntity, InRegistry };
+}
---
+++ Plugins/CkFoundation/Source/CkLifetime/Public/CkLifetime/EntityLifetime/CkEntityLifetime_Processor.h
@@ -0,0 +1,54 @@
+#pragma once
+#include "CkEntityLifetime_Fragment.h"
+
+#include "CkEcs/Processor/CkProcessor.h"
+#include "CkEcs/Registry/CkRegistry.h"
+
+namespace ck
+{
+    // --------------------------------------------------------------------------------------------------------------------
+
+    class CKLIFETIME_API FCk_Processor_EntityLifetime_EntityJustCreated
+    {
+    public:
+        using FTimeType = float;
+        using FRegistryType = FCk_Registry;
+
+    public:
+        explicit FCk_Processor_EntityLifetime_EntityJustCreated(const FRegistryType& InRegistry);
+
+    private:
+        FCk_Registry _Registry;
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    class CKLIFETIME_API FCk_Processor_EntityLifetime_TriggerDestroyEntity
+        : public TProcessor<FCk_Processor_EntityLifetime_TriggerDestroyEntity, FCk_Tag_TriggerDestroyEntity>
+    {
+    public:
+        using Super = TProcessor;
+
+    public:
+        using TProcessor::TProcessor;
+
+    public:
+        auto Tick(FTimeType InDeltaT) -> void;
+        auto ForEachEntity(FTimeType InDeltaT, FHandleType InHandle) const -> void;
+
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    class CKLIFETIME_API FCk_Processor_EntityLifetime_PendingDestroyEntity
+        : public TProcessor<FCk_Processor_EntityLifetime_PendingDestroyEntity, FCk_Tag_PendingDestroyEntity>
+    {
+    public:
+        using TProcessor::TProcessor;
+
+    public:
+        auto ForEachEntity(FTimeType InDeltaT, FHandleType InHandle) const -> void;
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+}
