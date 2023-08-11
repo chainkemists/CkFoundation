af6fac76901c348b0efd8cfce75304b67f12d15e
Saad Khattak <sulfur@chainkemists.com>
2023-08-10 23:12:48 -0700

    feat, wip: added the `CkActor` module

---
+++ Plugins/CkFoundation/Source/CkActor/CkActor.cpp
@@ -0,0 +1,15 @@
+#include "CkActor.h"
+
+#define LOCTEXT_NAMESPACE "FCkActorModule"
+
+void FCkActorModule::StartupModule()
+{
+}
+
+void FCkActorModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkActorModule, CkActor)
