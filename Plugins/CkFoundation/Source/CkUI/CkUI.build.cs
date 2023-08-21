52d5af2161d268928927b25ad19d4ecd749d0dbe
Nitrogen-CK <nitrogen@chainkemists.com>
2023-08-20 21:29:59 -0400

    feat: Added CkUI Module alongside some Screen utils
    # Conflicts:
    #	Plugins/CkFoundation/CkFoundation.uplugin

---
+++ Plugins/CkFoundation/Source/CkUI/CkUI.build.cs
@@ -0,0 +1,49 @@
+using System;
+using UnrealBuildTool;
+
+public class CkUI : CkModuleRules
+{
+    public CkUI(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PublicIncludePaths.AddRange(
+            new string[] {
+                // ... add public include paths required here ...
+            }
+            );
+
+
+        PrivateIncludePaths.AddRange(
+            new string[] {
+                // ... add other private include paths required here ...
+            }
+            );
+
+
+        PublicDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "Core",
+                // ... add other public dependencies that you statically link with here ...
+            }
+            );
+
+
+        PrivateDependencyModuleNames.AddRange(
+            new string[]
+            {
+                "CoreUObject",
+                "Engine",
+                "Slate",
+                "SlateCore",
+                "GameplayTags",
+                "DeveloperSettings",
+                "UMG",
+
+                "CkThirdParty",
+                "CkCore",
+                "CkLog",
+                // ... add private dependencies that you statically link with here ...
+            }
+            );
+    }
+}
---
+++ Plugins/CkFoundation/Source/CkUI/Public/CkUI/CkScreen_Utils.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <Kismet/BlueprintFunctionLibrary.h>
+
+#include "CkMacros/CkMacros.h"
+
+#include "CkScreen_Utils.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable)
+class CKUI_API UCk_Utils_Screen_UE : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_Screen_UE);
+
+public:
+    UFUNCTION(BlueprintPure)
+    static bool ProjectWorldLocationToWidgetPositionCoords(APlayerController* InPlayerController, const FVector& InWorldLocation, FVector2D& OutViewportPosition);
+
+    /**
+    * Converts a world location to screen position for HUD drawing. This differs from the results of FSceneView::WorldToScreen in that it returns a position along the edge of the screen for offscreen locations
+    *
+    * @param InLocation - The world space location to be converted to screen space
+    * @param InEdgePercent - How close to the edge of the screen, 1.0 = at edge, 0.0 = at center of screen. .9 or .95 is usually desirable
+    * @param InViewportCenterLoc - for offsetting center of the screen, leave at (0.5, 0.5) for no offset
+    * @param OutScreenPosition - the screen coordinates for HUD drawing
+    * @param OutRotationAngleDegrees - The angle to rotate a hud element if you want it pointing toward the offscreen indicator, 0° if onscreen
+    * @param OutIsOnScreen - True if the specified location is in the camera view (may be obstructed)
+    */
+    UFUNCTION( BlueprintPure, meta=( WorldContext = "InWorldContextObject", CallableWithoutWorldContext))
+    static void FindScreenEdgeLocationForWorldLocation(
+        UObject* InWorldContextObject,
+        const FVector& InLocation,
+        float InEdgePercent,
+        FVector2D InViewportCenterLoc,
+        FVector2D& OutScreenPosition,
+        float& OutRotationAngleDegrees,
+        bool &OutIsOnScreen);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
