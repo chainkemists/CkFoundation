e1696e3cef30422e9a580681cacf42d855e8993e
Sulfur-CK <sulfur@chainkemists.com>
2023-08-20 19:29:36 -0700

    chore: exposed all ActorModifier functions to Blueprints
    - we may 'un-expose' them again in the future, mainly because the future 'Transform' fragment should be the one that is replicated

--- Plugins/CkFoundation/Source/CkActor/Public/CkActor/ActorModifier/CkActorModifier_Utils.h
+++ Plugins/CkFoundation/Source/CkActor/Public/CkActor/ActorModifier/CkActorModifier_Utils.h
@@ -16,43 +16,61 @@ public:
     CK_GENERATED_BODY(UCk_Utils_ActorModifier_UE);
 
 public:
-    static auto
+    UFUNCTION(BlueprintCallable,
+        Category = "Ck|Utils|ActorModifier|Requests",
+        meta = (AutoCreateRefTerm = "InDelegate"))
+    static void
     Request_SetLocation(
         FCk_Handle                                   InHandle,
         const FCk_Request_ActorModifier_SetLocation& InRequest,
-        const FCk_Delegate_Transform_OnUpdate&       InDelegate) -> void;
+        const FCk_Delegate_Transform_OnUpdate&       InDelegate);
 
-    static auto
+    UFUNCTION(BlueprintCallable,
+        Category = "Ck|Utils|ActorModifier|Requests",
+        meta = (AutoCreateRefTerm = "InDelegate"))
+    static void
     Request_AddLocationOffset(
         FCk_Handle                                         InHandle,
         const FCk_Request_ActorModifier_AddLocationOffset& InRequest,
-        const FCk_Delegate_Transform_OnUpdate&             InDelegate) -> void;
+        const FCk_Delegate_Transform_OnUpdate&             InDelegate);
 
 public:
-    static auto
+    UFUNCTION(BlueprintCallable,
+        Category = "Ck|Utils|ActorModifier|Requests",
+        meta = (AutoCreateRefTerm = "InDelegate"))
+    static void
     Request_SetRotation(
         FCk_Handle                                   InHandle,
         const FCk_Request_ActorModifier_SetRotation& InRequest,
-        const FCk_Delegate_Transform_OnUpdate&       InDelegate) -> void;
+        const FCk_Delegate_Transform_OnUpdate&       InDelegate);
 
-    static auto
+    UFUNCTION(BlueprintCallable,
+        Category = "Ck|Utils|ActorModifier|Requests",
+        meta = (AutoCreateRefTerm = "InDelegate"))
+    static void
     Request_AddRotationOffset(
         FCk_Handle                                         InHandle,
         const FCk_Request_ActorModifier_AddRotationOffset& InRequest,
-        const FCk_Delegate_Transform_OnUpdate&             InDelegate) -> void;
+        const FCk_Delegate_Transform_OnUpdate&             InDelegate);
 
 public:
-    static auto
+    UFUNCTION(BlueprintCallable,
+        Category = "Ck|Utils|ActorModifier|Requests",
+        meta = (AutoCreateRefTerm = "InDelegate"))
+    static void
     Request_SetScale(
         FCk_Handle InHandle,
         const FCk_Request_ActorModifier_SetScale& InRequest,
-        const FCk_Delegate_Transform_OnUpdate& InDelegate) -> void;
+        const FCk_Delegate_Transform_OnUpdate& InDelegate);
 
-    static auto
+    UFUNCTION(BlueprintCallable,
+        Category = "Ck|Utils|ActorModifier|Requests",
+        meta = (AutoCreateRefTerm = "InDelegate"))
+    static void
     Request_SetTransform(
         FCk_Handle InHandle,
         const FCk_Request_ActorModifier_SetTransform& InRequest,
-        const FCk_Delegate_Transform_OnUpdate&        InDelegate) -> void;
+        const FCk_Delegate_Transform_OnUpdate&        InDelegate);
 
 public:
     UFUNCTION(BlueprintCallable,
