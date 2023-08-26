830015a2043420c9200e45c24ec2eb2d57b50eba
Sulfur-CK <sulfur@chainkemists.com>
2023-08-25 20:30:03 -0700

    feat: entity lifetime processor now correctly destroys remote Entities
    - if the Entity is replicated and we have replicated objects, we trigger the destruction mechanism on the replicated objects
    
    buddy: Nitrogen

--- Plugins/CkFoundation/Source/CkEcs/Public/CkEcs/EntityLifetime/CkEntityLifetime_Processor.cpp
+++ Plugins/CkFoundation/Source/CkEcs/Public/CkEcs/EntityLifetime/CkEntityLifetime_Processor.cpp
@@ -1,6 +1,12 @@
 #include "CkEntityLifetime_Processor.h"
 
+#include <CkCore/Actor/CkActor_Utils.h>
+
+#include "CkCore/Algorithms/CkAlgorithms.h"
+
 #include "CkEcs/CkEcsLog.h"
+#include "CkEcs/Fragments/ReplicatedObjects/CkReplicatedObjects_Fragment.h"
+#include "CkEcs/Fragments/ReplicatedObjects/CkReplicatedObjects_Utils.h"
 
 namespace ck
 {
@@ -44,6 +50,23 @@ namespace ck
     {
         ecs::VeryVerbose(TEXT("Entity [{}] set to 'Pending Destroy'"), InHandle);
         InHandle.Add<FCk_Tag_PendingDestroyEntity>(InHandle);
+
+        if (InHandle.Has<FCk_Tag_Replicated>())
+        {
+            ck::algo::ForEachIsValid(UCk_Utils_ReplicatedObjects_UE::Get_ReplicatedObjects(InHandle).Get_ReplicatedObjects(),
+                [&](UCk_ReplicatedObject_UE* InRO)
+                {
+                    const auto OutermostActor = UCk_Utils_Actor_UE::Get_OutermostActor_RemoteAuthority(InRO);
+
+                    // In this case, we are one of the clients and we do NOT need to go any further
+                    if (ck::Is_NOT_Valid(OutermostActor))
+                    { return; }
+
+                    const auto EcsReplicatedObject = Cast<UCk_Ecs_ReplicatedObject_UE>(InRO);
+
+                    EcsReplicatedObject->Request_TriggerDestroyAssociatedEntity();
+                });
+        }
 
         // TODO: Invoke Signal when Signals are ready
     }
