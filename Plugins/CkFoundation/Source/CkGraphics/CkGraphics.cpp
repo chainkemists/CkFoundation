00bb5424d06b7cbdbf453fb968aabc6660c1cb55
Nitrogen-CK <nitrogen@chainkemists.com>
2023-08-29 12:39:56 -0400

    feat: Added CkGraphics module

---
+++ Plugins/CkFoundation/Source/CkGraphics/CkGraphics.cpp
@@ -0,0 +1,15 @@
+#include "CkGraphics.h"
+
+#define LOCTEXT_NAMESPACE "FCkGraphicsModule"
+
+void FCkGraphicsModule::StartupModule()
+{
+}
+
+void FCkGraphicsModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkGraphicsModule, CkGraphics)
