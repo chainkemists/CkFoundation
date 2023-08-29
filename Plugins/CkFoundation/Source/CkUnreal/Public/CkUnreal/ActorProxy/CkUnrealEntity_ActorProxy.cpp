aaf34b1bf107814790c4ed2a2fbfcf61c483c489
Sulfur-CK <sulfur@chainkemists.com>
2023-08-28 18:55:51 -0700

    refactor [OBS-575]: ActorProxy has been refactored
    - it's now much simpler as it no longer requires the bootstrapper

--- Plugins/CkFoundation/Source/CkUnreal/Public/CkUnreal/ActorProxy/CkUnrealEntity_ActorProxy.cpp
+++ Plugins/CkFoundation/Source/CkUnreal/Public/CkUnreal/ActorProxy/CkUnrealEntity_ActorProxy.cpp
@@ -1,87 +1,67 @@
-//#include "CkUnrealEntity_ActorProxy.h"
-//
-//#include "CkCore/Actor/CkActor_Utils.h"
-//#include "CkCore/ObjectReplication/CkObjectReplicatorComponent.h"
-//
-//#include "CkEcs/EntityLifetime/CkEntityLifetime_Fragment_Utils.h"
-//#include "CkEcs/OwningActor/CkOwningActor_Fragment_Params.h"
-//
-//#include "CkUnreal/CkUnreal_Log.h"
-//#include "CkUnreal/EcsBootstrapper/CkEcsBootstrapper.h"
-//#include "CkUnreal/Entity/CkUnrealEntity_ConstructionScript.h"
-//#include "CkUnreal/Entity/CkUnrealEntity_Utils.h"
-//
-//#include "Net/UnrealNetwork.h"
-//
-//// --------------------------------------------------------------------------------------------------------------------
-//
-//ACk_UnrealEntity_ActorProxy_UE::
-//ACk_UnrealEntity_ActorProxy_UE()
-//{
-//    bReplicates = false;
-//    bAlwaysRelevant = false;
-//
-//#if WITH_EDITORONLY_DATA
-//    _ChildActorComponent = CreateEditorOnlyDefaultSubobject<UChildActorComponent>(TEXT("Proxy Actor Comp"));
-//#endif
-//}
-//
-//auto
-//    ACk_UnrealEntity_ActorProxy_UE::
-//    DoInvokeOnEntityCreated(
-//        const FCk_Handle& InCreatedEntity)
-//    -> void
-//{
-//    OnEntityCreated(InCreatedEntity);
-//}
-//
-//#if WITH_EDITOR
-//auto
-//    ACk_UnrealEntity_ActorProxy_UE::
-//    PostEditChangeProperty(
-//        FPropertyChangedEvent& InPropertyChangedEvent)
-//    -> void
-//{
-//    Super::PostEditChangeProperty(InPropertyChangedEvent);
-//
-//    if (ck::Is_NOT_Valid(InPropertyChangedEvent.Property))
-//    {
-//        // TODO: does this need to be logged?
-//        return;
-//    }
-//
-//    if (InPropertyChangedEvent.Property->GetFName() != GET_MEMBER_NAME_CHECKED(ACk_UnrealEntity_ActorProxy_UE, _ActorToSpawn))
-//    { return; }
-//
-//    _ChildActorComponent->DestroyChildActor();
-//    _ChildActorComponent->SetChildActorClass(_ActorToSpawn);
-//}
-//
-//auto
-//    ACk_UnrealEntity_ActorProxy_UE::
-//    BeginPlay() -> void
-//{
-//    Super::BeginPlay();
-//
-//    if (ck::Is_NOT_Valid(_ActorToSpawn))
-//    { return; }
-//
-//    if (HasAuthority())
-//    {
-//        const auto NewHandle = UCk_Utils_EntityLifetime_UE::Request_CreateEntity(Get_TransientHandle());
-//        GetComponentByClass<UCk_ActorInfo_ActorComponent_UE>()->_EntityHandle = NewHandle;
-//        UCk_Utils_OwningActor_UE::Link(this, NewHandle);
-//        UCk_Utils_OwningActor_UE::Add(NewHandle, this->GetOwner(), nullptr);
-//
-//        UCk_EcsBootstrapper_UE::Create(FCk_Bootstrapper_Construction_Params{this, _UnrealEntity});
-//
-//    }
-//
-//#if WITH_EDITORONLY_DATA
-//    _ChildActorComponent->DestroyChildActor();
-//#endif
-//}
-//
-//#endif
-//
-//// --------------------------------------------------------------------------------------------------------------------
+#include "CkUnrealEntity_ActorProxy.h"
+
+#include "CkCore/Actor/CkActor_Utils.h"
+#include "CkCore/ObjectReplication/CkObjectReplicatorComponent.h"
+
+#include "CkEcs/EntityLifetime/CkEntityLifetime_Fragment_Utils.h"
+#include "CkEcs/OwningActor/CkOwningActor_Fragment_Params.h"
+
+#include "CkUnreal/CkUnreal_Log.h"
+#include "CkUnreal/EcsBootstrapper/CkEcsBootstrapper.h"
+#include "CkUnreal/Entity/CkUnrealEntity_ConstructionScript.h"
+#include "CkUnreal/Entity/CkUnrealEntity_Utils.h"
+
+#include "Net/UnrealNetwork.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+ACk_UnrealEntity_ActorProxy_UE::
+ACk_UnrealEntity_ActorProxy_UE()
+{
+    bReplicates = false;
+    bAlwaysRelevant = false;
+}
+
+#if WITH_EDITOR
+auto
+    ACk_UnrealEntity_ActorProxy_UE::
+    PostEditChangeProperty(
+        FPropertyChangedEvent& InPropertyChangedEvent)
+    -> void
+{
+    Super::PostEditChangeProperty(InPropertyChangedEvent);
+
+    if (ck::Is_NOT_Valid(InPropertyChangedEvent.Property))
+    {
+        // TODO: does this need to be logged?
+        return;
+    }
+
+    if (InPropertyChangedEvent.Property->GetFName() != GET_MEMBER_NAME_CHECKED(ACk_UnrealEntity_ActorProxy_UE, _ActorToSpawn))
+    { return; }
+
+    _TransientActor = UCk_Utils_Actor_UE::Request_SpawnActor
+    (
+        UCk_Utils_Actor_UE::SpawnActorParamsType{this, _ActorToSpawn}.Set_SpawnTransform(GetActorTransform())
+    );
+    _TransientActor->bIsEditorOnlyActor = true;
+}
+
+auto
+    ACk_UnrealEntity_ActorProxy_UE::
+    BeginPlay() -> void
+{
+    Super::BeginPlay();
+
+    if (ck::Is_NOT_Valid(_ActorToSpawn))
+    { return; }
+
+    const auto NewActor = UCk_Utils_Actor_UE::Request_SpawnActor
+    (
+        UCk_Utils_Actor_UE::SpawnActorParamsType{this, _ActorToSpawn}.Set_SpawnTransform(GetActorTransform())
+    );
+}
+
+#endif
+
+// --------------------------------------------------------------------------------------------------------------------
--- Plugins/CkFoundation/Source/CkUnreal/Public/CkUnreal/ActorProxy/CkUnrealEntity_ActorProxy.h
+++ Plugins/CkFoundation/Source/CkUnreal/Public/CkUnreal/ActorProxy/CkUnrealEntity_ActorProxy.h
@@ -1,54 +1,51 @@
-//#pragma once
-//
-//#include "CkEcs/Handle/CkHandle.h"
-//
-//#include "CkMacros/CkMacros.h"
-//
-//#include "CkUnreal/Entity/CkUnrealEntity_Fragment_Params.h"
-//
-//#include "CkUnrealEntity_ActorProxy.generated.h"
-//
-//// --------------------------------------------------------------------------------------------------------------------
-//
-//UCLASS(Abstract, Blueprintable, BlueprintType)
-//class CKUNREAL_API ACk_UnrealEntity_ActorProxy_UE : public AActor
-//{
-//    GENERATED_BODY()
-//
-//public:
-//    CK_GENERATED_BODY(ACk_UnrealEntity_ActorProxy_UE);
-//
-//public:
-//    ACk_UnrealEntity_ActorProxy_UE();
-//
-//private:
-//    auto DoInvokeOnEntityCreated(const FCk_Handle& InCreatedEntity) -> void;
-//
-//protected:
-//    UFUNCTION(BlueprintImplementableEvent)
-//    FCk_Handle
-//    Get_TransientHandle() const;
-//
-//    UFUNCTION(BlueprintImplementableEvent)
-//    void
-//    OnEntityCreated(const FCk_Handle& InCreatedEntity) const;
-//
-//protected:
-//#if WITH_EDITOR
-//    virtual auto PostEditChangeProperty(FPropertyChangedEvent& InPropertyChangedEvent) -> void override;
-//#endif
-//
-//public:
-//    virtual auto BeginPlay() -> void override;
-//
-//public:
-//    UPROPERTY(EditInstanceOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
-//    TSubclassOf<AActor> _ActorToSpawn;
-//
-//#if WITH_EDITORONLY_DATA
-//    UPROPERTY(Transient)
-//    TObjectPtr<UChildActorComponent> _ChildActorComponent;
-//#endif
-//};
-//
-//// --------------------------------------------------------------------------------------------------------------------
+#pragma once
+
+#include "CkEcs/Handle/CkHandle.h"
+
+#include "CkMacros/CkMacros.h"
+
+#include "CkUnreal/Entity/CkUnrealEntity_Fragment_Params.h"
+
+#include "CkUnrealEntity_ActorProxy.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(Abstract, Blueprintable, BlueprintType)
+class CKUNREAL_API ACk_UnrealEntity_ActorProxy_UE : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(ACk_UnrealEntity_ActorProxy_UE);
+
+public:
+    ACk_UnrealEntity_ActorProxy_UE();
+
+protected:
+    UFUNCTION(BlueprintImplementableEvent)
+    FCk_Handle
+    Get_TransientHandle() const;
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void
+    OnEntityCreated(const FCk_Handle& InCreatedEntity) const;
+
+protected:
+#if WITH_EDITOR
+    virtual auto PostEditChangeProperty(FPropertyChangedEvent& InPropertyChangedEvent) -> void override;
+#endif
+
+public:
+    virtual auto BeginPlay() -> void override;
+
+public:
+    UPROPERTY(EditInstanceOnly, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+    TSubclassOf<AActor> _ActorToSpawn;
+
+#if WITH_EDITORONLY_DATA
+    UPROPERTY(Transient)
+    TObjectPtr<AActor> _TransientActor;
+#endif
+};
+
+// --------------------------------------------------------------------------------------------------------------------
