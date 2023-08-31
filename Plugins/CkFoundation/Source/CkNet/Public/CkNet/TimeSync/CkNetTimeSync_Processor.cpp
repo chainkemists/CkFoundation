8ec7b3752ba4a391319b4ddc72e0bad9bf71bbd4
Sulfur-CK <sulfur@chainkemists.com>
2023-08-30 20:38:03 -0700

    feat, wip: [UNTESTED] added NetTimeSync Fragment, Processors and Utilities
    - NetTimeSync is a Fragment that can be added to any Entity that requires latency information for all players
    - The replicated component is intended to be added by one Entity per Registry, although we are not currently imposing that restriction strictly

---
+++ Plugins/CkFoundation/Source/CkNet/Public/CkNet/TimeSync/CkNetTimeSync_Processor.cpp
@@ -0,0 +1,94 @@
+#include "CkNetTimeSync_Processor.h"
+
+#include "NetworkTimeSubsystem.h"
+
+#include "CkCore/Algorithms/CkAlgorithms.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ck
+{
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    FCk_Processor_TimeSync_OnNetworkClockSynchronized::
+    ForEachEntity(
+        TimeType InDeltaT,
+        HandleType InHandle,
+        const TObjectPtr<UCk_Fragment_TimeSync_Rep>& InTimeSync_Rep)
+{
+    if (_DelegateHandle.IsValid())
+    { return; }
+
+    _NetworkTimeSubsystem = UNetworkTimeSubsystem::Get(InTimeSync_Rep.Get());
+
+    CK_ENSURE_IF_NOT(ck::IsValid(_NetworkTimeSubsystem),
+        TEXT("Could not get a valid NetworkTimeSubsystem from [{}] on Entity [{}]"), InTimeSync_Rep, InHandle)
+    { return; }
+
+    _DelegateHandle = _NetworkTimeSubsystem->OnNetworkClockSynchronized_Cpp.AddRaw(
+        this, &FCk_Processor_TimeSync_OnNetworkClockSynchronized::OnNetworkClockSynchronized);
+}
+
+FCk_Processor_TimeSync_OnNetworkClockSynchronized::
+    ~FCk_Processor_TimeSync_OnNetworkClockSynchronized()
+{
+    if (NOT _DelegateHandle.IsValid())
+    { return; }
+
+    _NetworkTimeSubsystem->OnNetworkClockSynchronized_Cpp.Remove(_DelegateHandle);
+}
+
+auto
+    FCk_Processor_TimeSync_OnNetworkClockSynchronized::
+    OnNetworkClockSynchronized(
+        float OldServerDelta,
+        float NewServerDelta,
+        float RoundTripTime)
+    -> void
+{
+    _Registry.View<TObjectPtr<UCk_Fragment_TimeSync_Rep>>().ForEach(
+    [&](EntityType InEntity, TObjectPtr<UCk_Fragment_TimeSync_Rep> InRep)
+    {
+        auto Handle = HandleType{InEntity, _Registry};
+        InRep->DoBroadcast_TimeSync(RoundTripTime);
+    });
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    FCk_Processor_TimeSync_HandleRequests::
+    Tick(
+        TimeType InDeltaT)
+    -> void
+{
+    TProcessor::Tick(InDeltaT);
+
+    _Registry.Clear<MarkedDirtyBy>();
+}
+
+auto
+    FCk_Processor_TimeSync_HandleRequests::
+    ForEachEntity(
+        TimeType InDeltaT,
+        HandleType InHandle,
+        FFragment_TimeSync_Requests& InRequests)
+{
+    ck::algo::ForEachRequest(InRequests._TimeSyncRequests,
+    [&](const FCk_Request_NetTimeSync_NewSync& InNewSync)
+    {
+        _Registry.View<FFragment_TimeSync>().ForEach([&](EntityType InEntity, FFragment_TimeSync& InTimeSync)
+        {
+            InTimeSync._PlayerRoundTripTimes.FindOrAdd(InNewSync.Get_PlayerController(), InNewSync.Get_RoundTripTime());
+        });
+    });
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+}
+
+// --------------------------------------------------------------------------------------------------------------------
---
+++ Plugins/CkFoundation/Source/CkNet/Public/CkNet/TimeSync/CkNetTimeSync_Utils.cpp
@@ -0,0 +1,106 @@
+#include "CkNetTimeSync_Utils.h"
+
+#include "CkNetTimeSync_Fragment.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Utils_NetTimeSync_UE::
+    Add(
+        FCk_Handle InHandle)
+    -> void
+{
+    InHandle.Add<ck::FFragment_TimeSync>();
+}
+
+auto
+    UCk_Utils_NetTimeSync_UE::
+    Has(
+        FCk_Handle InHandle)
+    -> bool
+{
+    return InHandle.Has<ck::FFragment_TimeSync>();
+}
+
+auto
+    UCk_Utils_NetTimeSync_UE::
+    Ensure(FCk_Handle InHandle)
+    -> bool
+{
+    CK_ENSURE_IF_NOT(Has(InHandle), TEXT("Handle [{}] does NOT have [{}]"), InHandle, ctti::nameof_v<ck::FFragment_TimeSync>)
+    { return false; }
+
+    return true;
+}
+
+auto
+UCk_Utils_NetTimeSync_UE::
+    Request_NewTimeSync(
+        FCk_Handle InHandle,
+        FCk_Request_NetTimeSync_NewSync InRequest) -> void
+{
+    auto& RequestsFragment = InHandle.AddOrGet<ck::FFragment_TimeSync_Requests>();
+    RequestsFragment._TimeSyncRequests.Emplace(InRequest);
+}
+
+auto
+    UCk_Utils_NetTimeSync_UE::
+    Get_RoundTripTime(
+        FCk_Handle InHandle)
+    -> float
+{
+    if (NOT Ensure(InHandle))
+    { return {}; }
+
+    return InHandle.Get<ck::FFragment_TimeSync>().Get_RoundTripTime();
+}
+
+auto
+    UCk_Utils_NetTimeSync_UE::
+    Get_Latency(
+        FCk_Handle InHandle)
+    -> float
+{
+    return Get_RoundTripTime(InHandle) / 2.0f;
+}
+
+auto
+    UCk_Utils_NetTimeSync_UE::
+    Get_PlayerRoundTripTime(
+        APlayerController* InPlayerController,
+        FCk_Handle InHandle)
+    -> float
+{
+    if (NOT Ensure(InHandle))
+    { return {}; }
+
+    const auto ValueFound = InHandle.Get<ck::FFragment_TimeSync>().Get_PlayerRoundTripTimes().Find(InPlayerController);
+
+    CK_ENSURE_IF_NOT(ck::IsValid(ValueFound, ck::IsValid_Policy_NullptrOnly{}),
+        TEXT("Could not find PlayerController [{}] using Entity [{}]. Possible syncing error OR Player disconnected"))
+    { return {}; }
+
+    return *ValueFound;
+}
+
+auto
+    UCk_Utils_NetTimeSync_UE::
+    Get_PlayerLatency(
+        APlayerController* InPlayerController,
+        FCk_Handle InHandle)
+    -> float
+{
+    return Get_PlayerRoundTripTime(InPlayerController, InHandle) / 2.0f;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Utils_NetTimeSync_UE::
+    DoAdd_TimeSync_Rep(FCk_Handle InHandle)
+    -> void
+{
+    TryAddReplicatedFragment<UCk_Fragment_TimeSync_Rep>(InHandle);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
