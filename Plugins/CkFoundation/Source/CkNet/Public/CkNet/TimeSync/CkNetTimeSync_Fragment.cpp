a8e7b961255b8ab35e14b9cee4124546ada2e598
Sulfur-CK <sulfur@chainkemists.com>
2023-08-31 20:33:50 -0700

    fix: NetTimeSync now using the new CK_REP_OBJ macros
    - Server is now broadcasting/relaying the NewTimeSync

--- Plugins/CkFoundation/Source/CkNet/Public/CkNet/TimeSync/CkNetTimeSync_Fragment.cpp
+++ Plugins/CkFoundation/Source/CkNet/Public/CkNet/TimeSync/CkNetTimeSync_Fragment.cpp
@@ -11,18 +11,34 @@ auto
         FCk_Time InRoundTripTime)
     -> void
 {
-    if (NOT Get_AssociatedEntity().IsValid()) { return; }
+    CK_REP_OBJ_EXECUTE_IF_VALID([&]()
+    {
+        UCk_Utils_NetTimeSync_UE::Request_NewTimeSync
+        (
+            Get_AssociatedEntity(),
+            FCk_Request_NetTimeSync_NewSync{}.Set_PlayerController(InPlayerController).Set_RoundTripTime(InRoundTripTime)
+        );
+    });
+}
 
-    CK_ENSURE_VALID_UNREAL_WORLD_IF_NOT(this) { return; }
+auto
+    UCk_Fragment_NetTimeSync_Rep::
+    Broadcast_TimeSync_Clients_Implementation(
+        APlayerController* InPlayerController,
+        FCk_Time InRoundTripTime)
+    -> void
+{
+    CK_REP_OBJ_EXECUTE_IF_VALID_IGNORE_SERVER([&]()
+    {
+        if (GetWorld()->GetFirstPlayerController() == InPlayerController)
+        { return; }
 
-    if (GetWorld()->IsNetMode(NM_Client))
-    { return; }
-
-    UCk_Utils_NetTimeSync_UE::Request_NewTimeSync
-    (
-        Get_AssociatedEntity(),
-        FCk_Request_NetTimeSync_NewSync{}.Set_PlayerController(InPlayerController).Set_RoundTripTime(InRoundTripTime)
-    );
+        UCk_Utils_NetTimeSync_UE::Request_NewTimeSync
+        (
+            Get_AssociatedEntity(),
+            FCk_Request_NetTimeSync_NewSync{}.Set_PlayerController(InPlayerController).Set_RoundTripTime(InRoundTripTime)
+        );
+    });
 }
 
 auto
