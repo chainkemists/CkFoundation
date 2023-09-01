8465b440cfe1d1af199d759f97eb222d4409029d
Nitrogen-CK <nitrogen@chainkemists.com>
2023-08-31 22:36:07 -0400

    feat: Added user setting to enable disable transform smoothing

--- Plugins/CkFoundation/Source/CkEcsBasics/CkEcsBasics.Build.cs
+++ Plugins/CkFoundation/Source/CkEcsBasics/CkEcsBasics.Build.cs
@@ -14,6 +14,7 @@ public class CkEcsBasics : CkModuleRules
             "Core",
             "CoreUObject",
             "Engine",
+            "DeveloperSettings",
 
             "CkActor",
             "CkCore",
---
+++ Plugins/CkFoundation/Source/CkEcsBasics/Public/CkEcsBasics/Transform/CkTransform_Settings.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <CoreMinimal.h>
+#include <Engine/DeveloperSettings.h>
+
+#include "CkMacros/CkMacros.h"
+#include "CkTransform_Settings.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(config = EditorPerProjectUserSettings, meta = (DisplayName = "Transform"))
+class CKECSBASICS_API UCk_Transform_UserSettings_UE : public UDeveloperSettings
+{
+    GENERATED_BODY()
+
+public:
+    explicit UCk_Transform_UserSettings_UE(const FObjectInitializer& ObjectInitializer);
+
+private:
+    UPROPERTY(Config, EditAnywhere, BlueprintReadOnly, Category = "General", meta = (AllowPrivateAccess = true))
+    bool _EnableTransformSmoothing = true;
+
+public:
+    CK_PROPERTY_GET(_EnableTransformSmoothing);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+class CKECSBASICS_API UCk_Utils_Transform_UserSettings_UE
+{
+public:
+    static auto Get_EnableTransformSmoothing() -> bool;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
