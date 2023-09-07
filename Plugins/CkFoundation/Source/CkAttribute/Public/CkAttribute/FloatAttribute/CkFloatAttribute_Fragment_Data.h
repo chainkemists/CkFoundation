338d82c2bd74a83aa3867257c69b61333a4fbc7c
Nitrogen-CK <nitrogen@chainkemists.com>
2023-09-07 15:08:59 -0400

    chore: FCk_Fragment_FloatAttributes_ParamsData map property now uses ForceInlineRow meta

--- Plugins/CkFoundation/Source/CkAttribute/Public/CkAttribute/FloatAttribute/CkFloatAttribute_Fragment_Data.h
+++ Plugins/CkFoundation/Source/CkAttribute/Public/CkAttribute/FloatAttribute/CkFloatAttribute_Fragment_Data.h
@@ -19,7 +19,7 @@ public:
 
 private:
     UPROPERTY(EditAnywhere, BlueprintReadWrite,
-              meta = (AllowPrivateAccess = true))
+              meta = (AllowPrivateAccess = true, ForceInlineRow))
     TMap<FGameplayTag, float> _AttributeBaseValues;
 
 public:
