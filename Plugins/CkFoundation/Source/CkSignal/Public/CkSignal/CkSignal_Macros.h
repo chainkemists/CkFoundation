c411c16054ba7cd118fb5c70991bfa567914c1d6
Sulfur-CK <sulfur@chainkemists.com>
2023-09-07 20:18:03 -0700

    feat, refactor: adjusted the Signal macros - added 'meta' macros
    - the meta macros are the ones that are intended for use majority of the time
    - the more granular macros can be used to construct other 'meta' macros, if needed, or as a starting point for more complex setups (e.g. templated Attributes)

--- Plugins/CkFoundation/Source/CkSignal/Public/CkSignal/CkSignal_Macros.h
+++ Plugins/CkFoundation/Source/CkSignal/Public/CkSignal/CkSignal_Macros.h
@@ -1,11 +1,11 @@
 #pragma once
 
-#include "CkCore/Payload/CkPayload.h"
-
 #include "CkSignal/CkSignal_Fragment.h"
 #include "CkSignal/CkSignal_Utils.h"
 
-#include "CkEcs/Handle/CkHandle.h"
+// --------------------------------------------------------------------------------------------------------------------
+// Granular macros for defining Signals and Utilities individually.
+// Consider using the 'meta' macros found below
 
 #define CK_DEFINE_SIGNAL(_API_, _SignalName_, ...)\
     _API_ struct FFragment_Signal_##_SignalName_ \
@@ -20,5 +20,21 @@
     : public ck::TFragment_Signal_UnrealMulticast<FFragment_Signal_##_SignalName_, _MulticastName_, __VA_ARGS__> {}
 
 #define CK_DEFINE_SIGNAL_WITH_DELEGATE_UTILS(_API_, _SignalName_)\
-    _API_ class UUtils_Signal_UnrealMulticast_##_SignalName_ \
+    _API_ class UUtils_Signal_##_SignalName_ \
     : public ck::TUtils_Signal_UnrealMulticast<FFragment_Signal_##_SignalName_, FFragment_Signal_UnrealMulticast_##_SignalName_> {}
+
+// --------------------------------------------------------------------------------------------------------------------
+// 'Meta' Macros for defining Signals and Utilities
+
+// Define Signal that works ONLY within C++
+#define CK_DEFINE_SIGNAL_AND_UTILS(_API_, _SignalName_, ...)\
+    CK_DEFINE_SIGNAL(_API_, _SignalName_, __VA_ARGS__)\
+    CK_DEFINE_SIGNAL_UTILS(_API_, _SignalName_)\
+
+// Define Signal that works in C++ AND supports Unreal Delegates with the same interface
+#define CK_DEFINE_SIGNAL_AND_UTILS_WITH_DELEGATE(_API_, _SignalName_, _MulticastDelegate_, ...)\
+    CK_DEFINE_SIGNAL(_API_, _SignalName_, __VA_ARGS__);\
+    CK_DEFINE_SIGNAL_WITH_DELEGATE(_API_, _SignalName_, _MulticastDelegate_, __VA_ARGS__);\
+    CK_DEFINE_SIGNAL_WITH_DELEGATE_UTILS(_API_, _SignalName_)
+
+// --------------------------------------------------------------------------------------------------------------------
