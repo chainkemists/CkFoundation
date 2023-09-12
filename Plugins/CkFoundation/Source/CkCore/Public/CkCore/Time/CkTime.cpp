1943e751573f18359919a451e35a5f168c8c2702
Sulfur-CK <sulfur@chainkemists.com>
2023-09-11 20:42:18 -0700

    feat: added Unreal Time type
    - this is a type-safe version of the various 'types' of time we get from Unreal
    - the upcoming utils with Get_WorldTime returns this type with the correct (baked in) ECk_Time_WorldTimeType

--- Plugins/CkFoundation/Source/CkCore/Public/CkCore/Time/CkTime.cpp
+++ Plugins/CkFoundation/Source/CkCore/Public/CkCore/Time/CkTime.cpp
@@ -1,5 +1,7 @@
 #include "CkTime.h"
 
+#include "CkEnsure/CkEnsure.h"
+
 // --------------------------------------------------------------------------------------------------------------------
 
 const FCk_Time FCk_Time::Zero = FCk_Time{0.0f};
@@ -52,6 +54,125 @@ auto FCk_Time::
 Get_Milliseconds() const -> float
 {
     return _Seconds * 1000.0f;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+FCk_Time_Unreal::
+    FCk_Time_Unreal(
+        const TimeType& InTime,
+        WorldTimeType  InTimeType)
+    : _Time(InTime)
+    , _TimeType(InTimeType)
+{
+}
+
+auto
+    FCk_Time_Unreal::
+    operator==(const ThisType& InOther) const
+    -> bool
+{
+    const auto& IsSameWorldTimeType = Get_TimeType() == InOther.Get_TimeType();
+
+    if (NOT CK_ENSURE(IsSameWorldTimeType,
+        TEXT("WorldTimeTypes [{}] and [{}] do not match"),
+        Get_TimeType(),
+        InOther.Get_TimeType()))
+    {
+        return false;
+    }
+
+    return Get_Time() == InOther.Get_Time();
+}
+
+auto
+    FCk_Time_Unreal::
+    operator<(const ThisType& InOther) const
+    -> bool
+{
+    const auto& IsSameWorldTimeType = Get_TimeType() == InOther.Get_TimeType();
+
+    if (NOT CK_ENSURE(IsSameWorldTimeType,
+        TEXT("WorldTimeTypes [{}] and [{}] do not match"),
+        Get_TimeType(),
+        InOther.Get_TimeType()))
+    {
+        return false;
+    }
+
+    return Get_Time() < InOther.Get_Time();
+}
+
+auto
+    FCk_Time_Unreal::
+    operator-(const ThisType& InOther) const
+    -> ThisType
+{
+    const auto& IsSameWorldTimeType = Get_TimeType() == InOther.Get_TimeType();
+
+    CK_ENSURE
+    (
+        IsSameWorldTimeType,
+        TEXT("WorldTimeTypes [{}] and [{}] do not match"),
+        Get_TimeType(),
+        InOther.Get_TimeType()
+    );
+
+    return ThisType{ Get_Time() - InOther.Get_Time(), Get_TimeType() };
+}
+
+auto
+    FCk_Time_Unreal::
+    operator+(const ThisType& InOther) const
+    -> ThisType
+{
+    const auto& IsSameWorldTimeType = Get_TimeType() == InOther.Get_TimeType();
+
+    CK_ENSURE
+    (
+        IsSameWorldTimeType,
+        TEXT("WorldTimeTypes [{}] and [{}] do not match"),
+        Get_TimeType(),
+        InOther.Get_TimeType()
+    );
+
+    return ThisType{ Get_Time() + InOther.Get_Time(), Get_TimeType() };
+}
+
+auto
+    FCk_Time_Unreal::
+    operator*(const ThisType& InOther) const
+    -> ThisType
+{
+    const auto& IsSameWorldTimeType = Get_TimeType() == InOther.Get_TimeType();
+
+    CK_ENSURE
+    (
+        IsSameWorldTimeType,
+        TEXT("WorldTimeTypes [{}] and [{}] do not match"),
+        Get_TimeType(),
+        InOther.Get_TimeType()
+    );
+
+    return ThisType{ Get_Time() * InOther.Get_Time(), Get_TimeType() };
+}
+
+auto
+    FCk_Time_Unreal::
+    operator/(const ThisType& InOther) const
+    -> ThisType
+{
+    const auto& IsSameWorldTimeType = Get_TimeType() == InOther.Get_TimeType();
+
+    CK_ENSURE
+    (
+        IsSameWorldTimeType,
+        TEXT("WorldTimeTypes [{}] and [{}] do not match"),
+        Get_TimeType(),
+        InOther.Get_TimeType()
+    );
+
+    return ThisType{ Get_Time() / InOther.Get_Time(), Get_TimeType() };
 }
 
 // --------------------------------------------------------------------------------------------------------------------
--- Plugins/CkFoundation/Source/CkCore/Public/CkCore/Time/CkTime.h
+++ Plugins/CkFoundation/Source/CkCore/Public/CkCore/Time/CkTime.h
@@ -7,6 +7,20 @@
 
 // --------------------------------------------------------------------------------------------------------------------
 
+UENUM(BlueprintType)
+enum class ECk_Time_WorldTimeType : uint8
+{
+    PausedAndDilatedAndClamped,
+    DilatedAndClampedOnly,
+    RealTime,
+    AudioTime,
+    DeltaTime,
+};
+
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_Time_WorldTimeType);
+
+// --------------------------------------------------------------------------------------------------------------------
+
 USTRUCT(BlueprintType)
 struct CKCORE_API FCk_Time
 {
@@ -53,3 +67,62 @@ CK_DEFINE_CUSTOM_FORMATTER(FCk_Time, [&]()
 });
 
 // --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKCORE_API FCk_Time_Unreal
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Time_Unreal);
+
+    friend class UCk_Utils_Time_UE;
+
+    using TimeType       = FCk_Time;
+    using WorldTimeType = ECk_Time_WorldTimeType;
+
+public:
+    FCk_Time_Unreal() = default;
+
+private:
+    FCk_Time_Unreal(
+        const TimeType& InTime,
+        WorldTimeType  InTimeType);
+
+public:
+    auto operator==(const ThisType& InOther) const -> bool;
+    auto operator< (const ThisType& InOther) const -> bool;
+    CK_DECL_AND_DEF_OPERATORS(ThisType);
+
+    auto operator-(const ThisType& InOther) const  -> ThisType;
+    auto operator+(const ThisType& InOther) const  -> ThisType;
+    auto operator*(const ThisType& InOther) const  -> ThisType;
+    auto operator/(const ThisType& InOther) const  -> ThisType;
+    CK_DECL_AND_DEF_ADD_SUBTRACT_ASSIGNMENT_OPERATORS(ThisType);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    FCk_Time _Time;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    ECk_Time_WorldTimeType _TimeType = ECk_Time_WorldTimeType::PausedAndDilatedAndClamped;
+
+public:
+    CK_PROPERTY_GET(_Time)
+    CK_PROPERTY_GET(_TimeType)
+};
+
+CK_DEFINE_CUSTOM_FORMATTER(FCk_Time_Unreal, [&]()
+{
+    return ck::Format
+    (
+        TEXT("Time: [{}], TimeType: [{}]"),
+        InObj.Get_Time(),
+        InObj.Get_TimeType()
+    );
+});
+
+// --------------------------------------------------------------------------------------------------------------------
+
