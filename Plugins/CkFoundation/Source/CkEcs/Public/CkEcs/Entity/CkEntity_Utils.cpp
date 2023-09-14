c3c78750f718061c01320dfc0bdc0bd3868c9014
Nitrogen-CK <nitrogen@chainkemists.com>
2023-09-14 18:44:13 -0400

    chore: Added additional BP utils functions for FCk_Entity

--- Plugins/CkFoundation/Source/CkEcs/Public/CkEcs/Entity/CkEntity_Utils.cpp
+++ Plugins/CkFoundation/Source/CkEcs/Public/CkEcs/Entity/CkEntity_Utils.cpp
@@ -3,14 +3,55 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 auto
-UCk_Utils_Entity_UE::
-Break_Entity(const FCk_Entity& InEntity,
-    int32& OutEntityID,
-    int32& OutEntityNumber,
-    int32& OutEntityVersion)
--> void
+    UCk_Utils_Entity_UE::
+    Get_Entity_IsEqual(
+        const FCk_Entity& InEntityA,
+        const FCk_Entity& InEntityB)
+    -> bool
+{
+    return InEntityA == InEntityB;
+}
+
+auto
+    UCk_Utils_Entity_UE::
+    Get_Entity_IsNotEqual(
+        const FCk_Entity& InEntityA,
+        const FCk_Entity& InEntityB)
+    -> bool
+{
+    return InEntityA != InEntityB;
+}
+
+auto
+    UCk_Utils_Entity_UE::
+    Conv_EntityToText(
+        const FCk_Entity& InEntity)
+    -> FText
+{
+    return FText::FromString(Conv_EntityToString(InEntity));
+}
+
+auto
+    UCk_Utils_Entity_UE::
+    Conv_EntityToString(
+        const FCk_Entity& InEntity)
+    -> FString
+{
+    return ck::Format_UE(TEXT("{}"), InEntity);
+}
+
+auto
+    UCk_Utils_Entity_UE::
+    Break_Entity(
+        const FCk_Entity& InEntity,
+        int32& OutEntityID,
+        int32& OutEntityNumber,
+        int32& OutEntityVersion)
+    -> void
 {
     OutEntityID = static_cast<int32>(InEntity.Get_ID());
     OutEntityNumber = InEntity.Get_EntityNumber();
     OutEntityVersion = InEntity.Get_VersionNumber();
 }
+
+// --------------------------------------------------------------------------------------------------------------------
--- Plugins/CkFoundation/Source/CkEcs/Public/CkEcs/Entity/CkEntity_Utils.h
+++ Plugins/CkFoundation/Source/CkEcs/Public/CkEcs/Entity/CkEntity_Utils.h
@@ -5,6 +5,8 @@
 
 #include "CkEntity_Utils.generated.h"
 
+// --------------------------------------------------------------------------------------------------------------------
+
 UCLASS(NotBlueprintable)
 class CKECS_API UCk_Utils_Entity_UE : public UBlueprintFunctionLibrary
 {
@@ -15,6 +17,38 @@ public:
 
 private:
     UFUNCTION(BlueprintPure,
+              Category = "Ck|Utils|Entity",
+              meta = (CompactNodeTitle = "==", KeyWords = "==,equal"))
+    static bool
+    Get_Entity_IsEqual(
+        const FCk_Entity& InEntityA,
+        const FCk_Entity& InEntityB);
+
+    UFUNCTION(BlueprintPure,
+           Category = "Ck|Utils|Entity",
+           meta = (CompactNodeTitle = "!=", KeyWords = "!=,not,equal"))
+    static bool
+    Get_Entity_IsNotEqual(
+        const FCk_Entity& InEntityA,
+        const FCk_Entity& InEntityB);
+
+public:
+    UFUNCTION(BlueprintPure,
+              Category = "Ck|Utils|Entity",
+              meta = (DisplayName = "Entity To Text", CompactNodeTitle = "->", BlueprintAutocast))
+    static FText
+    Conv_EntityToText(
+        const FCk_Entity& InEntity);
+
+    UFUNCTION(BlueprintPure,
+              Category = "Ck|Utils|Entity",
+              meta = (DisplayName = "Entity To String", CompactNodeTitle = "->", BlueprintAutocast))
+    static FString
+    Conv_EntityToString(
+        const FCk_Entity& InEntity);
+
+private:
+    UFUNCTION(BlueprintPure,
         Category = "Ck|Utils|Entity",
         meta = (NativeBreakFunc))
     static void
@@ -22,5 +56,6 @@ private:
         int32& OutEntityID,
         int32& OutEntityNumber,
         int32& OutEntityVersion);
-
 };
+
+// --------------------------------------------------------------------------------------------------------------------
