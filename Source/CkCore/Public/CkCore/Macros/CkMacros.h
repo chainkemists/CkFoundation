e0cf2bf0c9a54eb3725e62a348dce170f697a66e
Nitrogen-CK <nitrogen@chainkemists.com>
2023-09-18 18:41:20 -0400

    fix: Fix issue where the syntax for the CK_DEFINE_CONSTRUCTORS was incorrect for case with 5-9 parameters

--- Source/CkCore/Public/CkCore/Macros/CkMacros.h
+++ Source/CkCore/Public/CkCore/Macros/CkMacros.h
@@ -109,23 +109,23 @@ namespace ck
 
 #define CK_DEFINE_CONSTRUCTOR_5(_ClassType_, _1, _2, _3, _4, _5)\
     _ClassType_(decltype(_1) _1, decltype(_2) _2, decltype(_3) _3, decltype(_4) _4, decltype(_5) _5)\
-        : _1(std::move(_1)), _2(std::move(_2)), _3(std::move(_3)), _4(std::move(_4)), _5(std::move(_5){}
+        : _1(std::move(_1)), _2(std::move(_2)), _3(std::move(_3)), _4(std::move(_4)), _5(std::move(_5)){}
 
 #define CK_DEFINE_CONSTRUCTOR_6(_ClassType_, _1, _2, _3, _4, _5, _6)\
     _ClassType_(decltype(_1) _1, decltype(_2) _2, decltype(_3) _3, decltype(_4) _4, decltype(_5) _5, decltype(_6) _6)\
-        : _1(std::move(_1)), _2(std::move(_2)), _3(std::move(_3)), _4(std::move(_4)), _5(std::move(_5), _6(std::move(_6){}
+        : _1(std::move(_1)), _2(std::move(_2)), _3(std::move(_3)), _4(std::move(_4)), _5(std::move(_5)), _6(std::move(_6)){}
 
 #define CK_DEFINE_CONSTRUCTOR_7(_ClassType_, _1, _2, _3, _4, _5, _6, _7)\
     _ClassType_(decltype(_1) _1, decltype(_2) _2, decltype(_3) _3, decltype(_4) _4, decltype(_5) _5, decltype(_6) _6, decltype(_7) _7)\
-        : _1(std::move(_1)), _2(std::move(_2)), _3(std::move(_3)), _4(std::move(_4)), _5(std::move(_5), _6(std::move(_6), _7(std::move(_7){}
+        : _1(std::move(_1)), _2(std::move(_2)), _3(std::move(_3)), _4(std::move(_4)), _5(std::move(_5)), _6(std::move(_6)), _7(std::move(_7)){}
 
 #define CK_DEFINE_CONSTRUCTOR_8(_ClassType_, _1, _2, _3, _4, _5, _6, _7, _8)\
     _ClassType_(decltype(_1) _1, decltype(_2) _2, decltype(_3) _3, decltype(_4) _4, decltype(_5) _5, decltype(_6) _6, decltype(_7) _7, decltype(_8) _8)\
-        : _1(std::move(_1)), _2(std::move(_2)), _3(std::move(_3)), _4(std::move(_4)), _5(std::move(_5), _6(std::move(_6), _7(std::move(_7), _8(std::move(_8)){}
+        : _1(std::move(_1)), _2(std::move(_2)), _3(std::move(_3)), _4(std::move(_4)), _5(std::move(_5)), _6(std::move(_6)), _7(std::move(_7)), _8(std::move(_8)){}
 
 #define CK_DEFINE_CONSTRUCTOR_9(_ClassType_, _1, _2, _3, _4, _5, _6, _7, _8, _9)\
     _ClassType_(decltype(_1) _1, decltype(_2) _2, decltype(_3) _3, decltype(_4) _4, decltype(_5) _5, decltype(_6) _6, decltype(_7) _7, decltype(_8) _8, decltype(_9) _9)\
-        : _1(std::move(_1)), _2(std::move(_2)), _3(std::move(_3)), _4(std::move(_4)), _5(std::move(_5), _6(std::move(_6), _7(std::move(_7), _8(std::move(_8)), _9(std::move(_9)){}
+        : _1(std::move(_1)), _2(std::move(_2)), _3(std::move(_3)), _4(std::move(_4)), _5(std::move(_5)), _6(std::move(_6)), _7(std::move(_7)), _8(std::move(_8)), _9(std::move(_9)){}
 
 #define CK_DEFINE_CONSTRUCTOR_VARIADIC(_ClassType_, M, ...)\
     EXPAND(M(_ClassType_, __VA_ARGS__))
