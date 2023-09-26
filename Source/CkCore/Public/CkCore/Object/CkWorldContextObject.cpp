4ce94eba8f38807e5e31c92180108d011e823b8a
Nitrogen-CK <nitrogen@chainkemists.com>
2023-09-25 23:33:46 -0400

    feat: Added CkWorldContextObject that acts as a base UObject class that can retrieve the active world

---
+++ Source/CkCore/Public/CkCore/Object/CkWorldContextObject.cpp
@@ -0,0 +1,40 @@
+#include "CkWorldContextObject.h"
+
+#include "CkCore/Validation/CkIsValid.h"
+
+#include <Engine/Engine.h>
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_GameWorldContextObject_UE::
+    GetWorld() const
+    -> UWorld*
+{
+    const auto& world = [&]() -> UWorld*
+    {
+        if (ck::Is_NOT_Valid(GEngine))
+        { return {}; }
+
+        for (auto& context : GEngine->GetWorldContexts())
+        {
+            const auto& contextWorld = context.World();
+
+            if (ck::Is_NOT_Valid(contextWorld))
+            { continue; }
+
+            const auto& gameInstance = contextWorld->GetGameInstance();
+
+            if (ck::IsValid(gameInstance))
+            {
+                return gameInstance->GetWorld();
+            }
+        }
+
+        return {};
+    }();
+
+    return world;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
