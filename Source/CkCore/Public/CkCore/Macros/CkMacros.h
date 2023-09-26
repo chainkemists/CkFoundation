ec3b59bb89ace784649374ca5cfff626e93620b2
Sulfur-CK <sulfur@chainkemists.com>
2023-09-26 12:05:53 -0700

    refactor: renamed some macros to follow the current guidelines

--- Source/CkCore/Public/CkCore/Macros/CkMacros.h
+++ Source/CkCore/Public/CkCore/Macros/CkMacros.h
@@ -24,15 +24,17 @@ namespace ck
 
 // --------------------------------------------------------------------------------------------------------------------
 
-#define Paste(a,b) a ## b
-#define XPASTE(a,b) Paste(a,b)
+#define PASTE(a,b) a ## b
+#define XPASTE(a,b) PASTE(a,b)
 
 // --------------------------------------------------------------------------------------------------------------------
 
 // Taken from: https://stackoverflow.com/a/26408195/368599 and modified to work with MSVC
-#define __NARG__(...)  __NARG_I_(__VA_ARGS__,__RSEQ_N())
-#define __NARG_I_(...) EXPAND(__ARG_N(__VA_ARGS__))
-#define __ARG_N( \
+// ReSharper disable once IdentifierTypo
+#define NARG_(...)  NARG_I_(__VA_ARGS__,RSEQ_N())
+// ReSharper disable once IdentifierTypo
+#define NARG_I_(...) EXPAND(ARG_N(__VA_ARGS__))
+#define ARG_N( \
       _1, _2, _3, _4, _5, _6, _7, _8, _9,_10, \
      _11,_12,_13,_14,_15,_16,_17,_18,_19,_20, \
      _21,_22,_23,_24,_25,_26,_27,_28,_29,_30, \
@@ -40,7 +42,8 @@ namespace ck
      _41,_42,_43,_44,_45,_46,_47,_48,_49,_50, \
      _51,_52,_53,_54,_55,_56,_57,_58,_59,_60, \
      _61,_62,_63,N,...) N
-#define __RSEQ_N() \
+// ReSharper disable once IdentifierTypo
+#define RSEQ_N() \
      63,62,61,60,                   \
      59,58,57,56,55,54,53,52,51,50, \
      49,48,47,46,45,44,43,42,41,40, \
@@ -131,7 +134,7 @@ namespace ck
     EXPAND(M(_ClassType_, __VA_ARGS__))
 
 #define CK_DEFINE_CONSTRUCTOR(_ClassType_, ...)\
-    CK_DEFINE_CONSTRUCTOR_VARIADIC(_ClassType_, XPASTE(CK_DEFINE_CONSTRUCTOR_, EXPAND(__NARG__(__VA_ARGS__))), __VA_ARGS__)
+    CK_DEFINE_CONSTRUCTOR_VARIADIC(_ClassType_, XPASTE(CK_DEFINE_CONSTRUCTOR_, EXPAND(NARG_(__VA_ARGS__))), __VA_ARGS__)
 
 #define CK_DEFINE_CONSTRUCTORS(_ClassType_, ...)\
     _ClassType_() = default;\
