d579a35cdb65d3300e91b20358d41eb73a93699f
Nitrogen-CK <nitrogen@chainkemists.com>
2023-09-26 15:01:42 -0400

    feat: CkOverlapBody user settings are now using CkSettings new base classes

--- Source/CkOverlapBody/Public/CkOverlapBody/Settings/CkOverlapBody_Settings.h
+++ Source/CkOverlapBody/Public/CkOverlapBody/Settings/CkOverlapBody_Settings.h
@@ -2,20 +2,18 @@
 
 #include "CkCore/Macros/CkMacros.h"
 
+#include "CkSettings/Public/CkSettings/UserSettings/CkUserSettings.h"
+
 #include <CoreMinimal.h>
-#include <Engine/DeveloperSettingsBackedByCVars.h>
 
 #include "CkOverlapBody_Settings.generated.h"
 
 // --------------------------------------------------------------------------------------------------------------------
 
-UCLASS(config = EditorPerProjectUserSettings, meta = (DisplayName = "OverlapBody"))
-class CKOVERLAPBODY_API UCk_OverlapBody_UserSettings_UE : public UDeveloperSettingsBackedByCVars
+UCLASS(meta = (DisplayName = "OverlapBody"))
+class CKOVERLAPBODY_API UCk_OverlapBody_UserSettings_UE : public UCk_EditorPerProject_UserSettings_UE
 {
     GENERATED_BODY()
-
-public:
-    explicit UCk_OverlapBody_UserSettings_UE(const FObjectInitializer& ObjectInitializer);
 
 private:
     // Should draw the debug information of all existing Sensor Fragments
