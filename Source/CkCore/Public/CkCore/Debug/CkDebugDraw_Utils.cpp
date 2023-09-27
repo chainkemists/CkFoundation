24e424e1b9c1f53afa289f17d9153d4f9a32648a
Nitrogen-CK <nitrogen@chainkemists.com>
2023-09-27 17:16:30 -0400

    feat: Added new DebugDraw utils lib with a function to draw an ASCII progress bar

---
+++ Source/CkCore/Public/CkCore/Debug/CkDebugDraw_Utils.cpp
@@ -0,0 +1,39 @@
+﻿#include "CkDebugDraw_Utils.h"
+
+#include "CkCore/Ensure/CkEnsure.h"
+#include "CkCore/Format/CkFormat.h"
+#include "CkCore/Validation/CkIsValid.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Utils_DebugDraw_UE::
+    Create_ASCII_ProgressBar(
+        const FCk_FloatRange_0to1& InProgressValue,
+        int32                      InProgressBarCharacterLength)
+    -> FString
+{
+    CK_ENSURE_IF_NOT(InProgressBarCharacterLength > 0, TEXT("ASCII ProgressBar character length needs to be > 0"))
+    { return {}; }
+
+    FStringBuilderBase StringBuilder;
+
+    const auto& progressValue = InProgressValue.Get_Value();
+    const auto& numberOfCharacters = FMath::RoundToInt(progressValue * static_cast<float>(InProgressBarCharacterLength));
+
+    for (auto i = 0; i < InProgressBarCharacterLength; ++i)
+    {
+        if (i < numberOfCharacters)
+        {
+            StringBuilder.Append(TEXT("▯"));
+        }
+        else
+        {
+            StringBuilder.Append(TEXT(" "));
+        }
+    }
+
+    return StringBuilder.ToString();
+}
+
+// --------------------------------------------------------------------------------------------------------------------
