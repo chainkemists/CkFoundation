ec3b33304a2e0282b1e955108743020fe11e71f6
Nitrogen-CK <nitrogen@chainkemists.com>
2023-09-29 19:53:18 -0400

    ref: Simply some struts by using CK_DEFINE_CONSTRUCTORS macro

--- Source/CkOverlapBody/Public/CkOverlapBody/Marker/CkMarker_Fragment.cpp
+++ Source/CkOverlapBody/Public/CkOverlapBody/Marker/CkMarker_Fragment.cpp
@@ -2,26 +2,4 @@
 
 // --------------------------------------------------------------------------------------------------------------------
 
-namespace ck
-{
-    FFragment_Marker_Current::
-        FFragment_Marker_Current(
-            ECk_EnableDisable InEnableDisable)
-        : _AttachedEntityAndActor()
-        , _EnableDisable(InEnableDisable)
-    {
-    }
-
-    // --------------------------------------------------------------------------------------------------------------------
-
-    FFragment_Marker_Params::
-        FFragment_Marker_Params(
-            ParamsType InParams)
-        : _Params(InParams)
-    {
-    }
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
 
