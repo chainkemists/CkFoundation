f8194ae4585609c9e37766b549b361140b18c36b
Sulfur-CK <sulfur@chainkemists.com>
2023-10-01 15:43:22 -0700

    refactor: UnrealEntity_ConstructionScript is now Entity_ConstructionScript and is part of the Ecs Module

---
+++ Source/CkEcs/Public/CkEcs/EntityConstructionScript/CkEntity_ConstructionScript.cpp
@@ -0,0 +1,23 @@
+#include "CkEntity_ConstructionScript.h"
+
+#include "CkCore/Ensure/CkEnsure.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Entity_ConstructionScript_PDA::Construct(
+        const FCk_Handle& InHandle) -> void
+{
+    DoConstruct(InHandle);
+}
+
+auto
+    UCk_Entity_ConstructionScript_PDA::
+    DoConstruct_Implementation(
+        const FCk_Handle& InHandle)
+    -> void
+{
+    CK_ENSURE_FALSE(TEXT("The function in the BASE class [{}] should have been overridden"), ck::TypeToString<ThisType>);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
