47551fd603bb6d7f3bebb5889db3b0d81dc7169c
Sulfur-CK <sulfur@chainkemists.com>
2023-10-01 15:43:22 -0700

    refactor: UnrealEntity_ConstructionScript is now Entity_ConstructionScript and is part of the Ecs Module

---
+++ Source/CkEcs/Public/CkEcs/EntityConstructionScript/CkEntity_ConstructionScript.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "CkCore/Types/DataAsset/CkDataAsset.h"
+
+#include "CkEntity_ConstructionScript.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(Abstract, BlueprintType, Blueprintable, EditInlineNew)
+class CKECS_API UCk_Entity_ConstructionScript_PDA : public UCk_DataAsset_PDA
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Entity_ConstructionScript_PDA);
+
+public:
+    auto
+    Construct(
+        const FCk_Handle& InHandle) -> void;
+
+protected:
+    UFUNCTION(BlueprintNativeEvent,
+              DisplayName = "Construct")
+    void
+    DoConstruct(
+        const FCk_Handle& InHandle);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
