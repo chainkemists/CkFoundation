b77eefab1dad7d31f3a9f47a4ce2ff3bc486ea18
Nitrogen-CK <nitrogen@chainkemists.com>
2023-10-03 11:26:38 -0400

    feat: UCk_DataAsset_PDA now has access to the world by virtue of overriding GetWorld and retrieving the (only) valid one from the current GameInstance

--- Source/CkCore/Public/CkCore/Types/DataAsset/CkDataAsset.cpp
+++ Source/CkCore/Public/CkCore/Types/DataAsset/CkDataAsset.cpp
@@ -1,1 +1,40 @@
 #include "CkDataAsset.h"
+
+#include "CkCore/Validation/CkIsValid.h"
+
+#include <Engine/Engine.h>
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_DataAsset_PDA::
+    GetWorld() const
+    -> UWorld*
+{
+    const auto& world = [&]() -> UWorld*
+    {
+        if (ck::Is_NOT_Valid(GEngine))
+        { return {}; }
+
+        for (auto& context : GEngine->GetWorldContexts())
+        {
+            const auto& contextWorld = context.World();
+
+            if (ck::Is_NOT_Valid(contextWorld))
+            { continue; }
+
+            const auto& gameInstance = contextWorld->GetGameInstance();
+
+            if (ck::IsValid(gameInstance))
+            {
+                return gameInstance->GetWorld();
+            }
+        }
+
+        return {};
+    }();
+
+    return world;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
