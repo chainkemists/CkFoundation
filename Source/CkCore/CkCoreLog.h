5a35d37fbf38b2e93189c65db11598d493219a7c
Sulfur-CK <sulfur@chainkemists.com>
2023-10-08 16:34:39 -0700

    feat: added core log (see caveat)
    - CkLog depends on CkCore, once [OBS-859] is completed, we'll have a CkCore log

---
+++ Source/CkCore/CkCoreLog.h
@@ -0,0 +1,21 @@
+#pragma once
+
+// TODO: uncomment once [OBS-859] is resolved
+// #include "CkLog/CkLog_Utils.h"
+
+#include <CoreMinimal.h>
+#include <Modules/ModuleManager.h>
+
+// --------------------------------------------------------------------------------------------------------------------
+
+CKCORE_API DECLARE_LOG_CATEGORY_EXTERN(CkCore, Log, All);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ck::core
+{
+    // TODO: uncomment once [OBS-859] is resolved
+    // CK_DEFINE_LOG_FUNCTIONS(CkEcs);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
