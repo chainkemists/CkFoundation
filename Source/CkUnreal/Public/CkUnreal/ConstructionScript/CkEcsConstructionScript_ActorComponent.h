9cb92184caf3e0b42cfaf690ec70d993f5edf81d
Sulfur-CK <sulfur@chainkemists.com>
2023-10-10 23:40:02 -0700

    feat: EcsConstructionScript now has logic to correctly invoke the ReplicationComplete multicast
    notes: OnReplicationComplete is an event that can be added to the event-graph of an Actor with this component. This is the 'true' BeginPlay where we can be sure that we have our Entity and all the Fragments have been replicated.
    
    future improvement: _OnReplicationComplete_MC should probably exist on the base class (where the base class version of TryInvoke_OnReplicationComplete should invoke the MC without any additional logic)

--- Source/CkUnreal/Public/CkUnreal/ConstructionScript/CkEcsConstructionScript_ActorComponent.h
+++ Source/CkUnreal/Public/CkUnreal/ConstructionScript/CkEcsConstructionScript_ActorComponent.h
@@ -3,12 +3,13 @@
 #include "CkCore/Component/CkActorComponent.h"
 #include "CkCore/Enums/CkEnums.h"
 
-#include "CkEcs/Fragments/ReplicatedObjects/CkReplicatedObjects_Fragment_Params.h"
-#include "CkEcs/Subsystem/CkEcsWorld_Subsystem.h"
-
-#include "CkUnreal/Entity/CkUnrealEntity_Fragment_Params.h"
+#include "CkEcs/OwningActor/CkOwningActor_Fragment_Params.h"
 
 #include "CkEcsConstructionScript_ActorComponent.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FCk_Delegate_OnReplicationComplete_MC);
 
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -17,7 +18,7 @@ UCLASS(Abstract,
        BlueprintType,
        HideCategories("Replication", "ComponentTick", "Rendering", "Activation", "Tags", "ComponentReplication", "Mobile", "RayTracing",
                       "Collision", "AssetUserData", "Cooking", "Sockets", "Variable", "Navigation", "HLOD", "Physics"))
-class CKUNREAL_API UCk_EcsConstructionScript_ActorComponent_UE : public UCk_ActorComponent_UE
+class CKUNREAL_API UCk_EcsConstructionScript_ActorComponent_UE : public UCk_EcsConstructionScript_ActorComponent_Base_UE
 {
     GENERATED_BODY()
 
@@ -27,12 +28,17 @@ public:
 public:
     UCk_EcsConstructionScript_ActorComponent_UE();
 
+    friend class UCk_Fragment_EntityReplicationDriver_Rep;
+
 protected:
     auto OnUnregister() -> void override;
 
 protected:
     auto Do_Construct_Implementation(
         const FCk_ActorComponent_DoConstruct_Params& InParams) -> void override;
+
+private:
+    auto TryInvoke_OnReplicationComplete(EInvoke_Caller InCaller) -> void override;
 
 public:
     // Temporary function for Obsidian Toggle
@@ -41,13 +47,29 @@ public:
 
 public:
     UPROPERTY(EditDefaultsOnly)
-    TSubclassOf<UCk_EcsWorld_Subsystem_UE> _EcsWorldSubsystem;
+    TSubclassOf<class UCk_EcsWorld_Subsystem_UE> _EcsWorldSubsystem;
 
     UPROPERTY(EditDefaultsOnly)
     ECk_Replication _Replication = ECk_Replication::Replicates;
 
     UPROPERTY(EditDefaultsOnly, Instanced)
-    TObjectPtr<UCk_UnrealEntity_WithActor_PDA> _UnrealEntity;
+    TObjectPtr<class UCk_UnrealEntity_WithActor_PDA> _UnrealEntity;
+
+private:
+    UPROPERTY(BlueprintAssignable, Category = "Public",
+        meta = (AllowPrivateAccess))
+    FCk_Delegate_OnReplicationComplete_MC _OnReplicationComplete_MC;
+
+private:
+    enum class EOnReplicationCompleteBroadcastStep
+    {
+        Wait,
+        WaitOnReplicationDriver,
+        WaitOnConstructionScript,
+        Broadcast
+    };
+
+    EOnReplicationCompleteBroadcastStep _ReplicationComplete_BroadcastStep = EOnReplicationCompleteBroadcastStep::Wait;
 
 public:
     CK_PROPERTY(_UnrealEntity);
