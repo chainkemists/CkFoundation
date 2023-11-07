152ecd2228ab64393d80f01cbc455d6fbde6de18
Nitrogen-CK <nitrogen@chainkemists.com>
2023-11-06 20:00:15 -0500

    feat: Added Providers for AnimAsset ParamsData

--- Source/CkAnimation/Public/CkAnimation/AnimAsset/CkAnimAsset_Fragment_Data.cpp
+++ Source/CkAnimation/Public/CkAnimation/AnimAsset/CkAnimAsset_Fragment_Data.cpp
@@ -12,3 +12,47 @@ FCk_AnimAsset_Animation::
 }
 
 // --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Provider_AnimAsset_ParamsData_PDA::
+    Get_Value_Implementation(
+        FCk_Handle InHandle) const
+    -> FCk_Fragment_AnimAsset_ParamsData
+{
+    return {};
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Provider_AnimAsset_ParamsData_Literal_PDA::
+    Get_Value_Implementation(
+        FCk_Handle InHandle) const
+    -> FCk_Fragment_AnimAsset_ParamsData
+{
+    return _Value;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Provider_MultipleAnimAsset_ParamsData_PDA::
+    Get_Value_Implementation(
+        FCk_Handle InHandle) const
+    -> FCk_Fragment_MultipleAnimAsset_ParamsData
+{
+    return {};
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Provider_MultipleAnimAsset_ParamsData_Literal_PDA::
+    Get_Value_Implementation(
+        FCk_Handle InHandle) const
+    -> FCk_Fragment_MultipleAnimAsset_ParamsData
+{
+    return _Value;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
