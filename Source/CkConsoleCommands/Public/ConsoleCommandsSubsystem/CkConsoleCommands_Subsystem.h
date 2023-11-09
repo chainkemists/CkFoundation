9643a31e5ecde84d85c3a392715d0a1fe6641e0a
Sulfur-CK <sulfur@chainkemists.com>
2023-11-08 21:59:35 -0800

    feat: added the ConsoleCommands submodule with the ConsoleCommands Subsystem
    - the subsystem (and its helper Actor) allows running console commands on the Server
    - future task [OBS-1068] will add support for running the same commands on the Client

---
+++ Source/CkConsoleCommands/Public/ConsoleCommandsSubsystem/CkConsoleCommands_Subsystem.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "CkCore/Macros/CkMacros.h"
+
+#include "CkEcs/World/CkEcsWorld.h"
+
+#include <Subsystems/WorldSubsystem.h>
+
+#include "CkConsoleCommands_Subsystem.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS()
+class CKCONSOLECOMMANDS_API ACk_ConsoleCommandsHelper_UE : public AActor
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(ACk_ConsoleCommandsHelper_UE);
+
+public:
+    ACk_ConsoleCommandsHelper_UE();
+
+protected:
+    auto BeginPlay() -> void override;
+
+public:
+    UFUNCTION(Server, Reliable)
+    void Server_Request_RunConsoleCommand(
+        const FString&  InCommand);
+
+private:
+    UFUNCTION(NetMulticast, Reliable)
+    void MC_Request_OutputOnAll(
+        const FString& InCommandOutput);
+};
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable, BlueprintType)
+class CKCONSOLECOMMANDS_API UCk_ConsoleCommands_Subsystem_UE : public UWorldSubsystem
+{
+    GENERATED_BODY()
+
+    friend ACk_ConsoleCommandsHelper_UE;
+
+public:
+    CK_GENERATED_BODY(UCk_ConsoleCommands_Subsystem_UE);
+
+public:
+    auto Initialize(FSubsystemCollectionBase& Collection) -> void override;
+    auto ShouldCreateSubsystem(UObject* InOuter) const -> bool override;
+
+public:
+    UFUNCTION()
+    void
+    OnNewPlayerControllerAdded(
+        AGameModeBase* InGameModeBase,
+        APlayerController* InPlayerController);
+
+    UFUNCTION()
+    void
+    RunConsoleCommand_OnServer(
+        const TArray<FString>& InCommands);
+
+    UFUNCTION(BlueprintCallable)
+    void
+    Request_RunConsoleCommand_OnServer(
+        const FString& InCommand);
+
+private:
+    auto DoSpawnConsoleCommandsActor(APlayerController* InPlayerController) -> void;
+
+private:
+    UPROPERTY(Transient)
+    TArray<TObjectPtr<ACk_ConsoleCommandsHelper_UE>> _ConsoleCommandsHelper;
+};
