ccbabc7df6930532fd304cbf04b3c65859060f6d
Nitrogen-CK <nitrogen@chainkemists.com>
2023-11-10 18:36:47 -0500

    feat: PlayerState now updates the current ping at a given frequency and stores a history of the last few pings

--- Source/CkCore/Public/CkCore/Engine/CkPlayerState.cpp
+++ Source/CkCore/Public/CkCore/Engine/CkPlayerState.cpp
@@ -1,3 +1,212 @@
 #include "CkPlayerState.h"
 
+#include "CkCore/Time/CkTime_Utils.h"
+#include "Net/UnrealNetwork.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+const FCk_Time ACk_PlayerState_UE::_PingFrequency = FCk_Time::OneSecond;
+
+// --------------------------------------------------------------------------------------------------------------------
+
+const FCk_Time FCk_PlayerState_PingRange::_UpdateFrequency = FCk_Time{ 5.0f };
+
+// --------------------------------------------------------------------------------------------------------------------
+
+#if CK_BUILD_TEST
+auto
+    FCk_PlayerState_PingRange_History::
+    Request_AddNewEntry(
+        const EntryType& InEntry)
+    -> void
+{
+    _Entries.Emplace(InEntry);
+
+    if (_Entries.Num() <= _MaxHistorySize)
+    { return; }
+
+    _Entries.RemoveAt(0);
+}
+#endif
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    FCk_PlayerState_PingRange::
+    Request_HandleNewPing(
+        const FCk_Time& InPing,
+        const FCk_Time_Unreal& InCurrentTime)
+    -> void
+{
+    _NextMaxPing = FMath::Max(InPing, _NextMaxPing);
+    _NextMinPing = FMath::Min(InPing, _NextMinPing);
+
+    const auto& UpdateTime = _LastUpdateTime + _UpdateFrequency;
+
+    if (InCurrentTime < UpdateTime)
+    { return; }
+
+    _LastUpdateTime = InCurrentTime.Get_Time();
+
+    _MinPing = _NextMinPing;
+    _MaxPing = _NextMaxPing;
+
+    _NextMinPing = FCk_Time::OneSecond;
+    _NextMaxPing = FCk_Time::Zero;
+
+#if CK_BUILD_TEST
+    const auto PingHistoryEntry = FCk_PlayerState_PingRange_History_Entry{}.Set_MinPing(_MinPing).Set_MaxPing(_MaxPing);
+
+    _PingHistory.Request_AddNewEntry(PingHistoryEntry);
+#endif
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    ACk_PlayerState_UE::
+    BeginPlay()
+    -> void
+{
+    Super::BeginPlay();
+
+    if (NOT HasAuthority())
+    { return; }
+
+    if (ck::Is_NOT_Valid(Owner))
+    { return; }
+    
+    const auto& PlayerController = Cast<APlayerController>(Owner);
+    if (ck::Is_NOT_Valid(PlayerController))
+    { return; }
+
+    const auto& GameInstance = GetGameInstance();
+    if (ck::Is_NOT_Valid(GameInstance))
+    { return; }
+
+    constexpr bool ShouldTimerLoop = true;
+    GameInstance->GetTimerManager().SetTimer
+    (
+        _PingTimerHandle,
+        this,
+        &ThisType::DoRequest_Ping,
+        _PingFrequency.Get_Seconds(),
+        ShouldTimerLoop
+    );
+}
+
+auto
+    ACk_PlayerState_UE::
+    EndPlay(
+        EEndPlayReason::Type InEndPlayReason)
+    -> void
+{
+    const auto& GameInstance = GetGameInstance();
+    if (ck::IsValid(GameInstance))
+    {
+        GameInstance->GetTimerManager().ClearTimer(_PingTimerHandle);
+    }
+
+    Super::EndPlay(InEndPlayReason);
+}
+
+auto
+    ACk_PlayerState_UE::
+    UpdatePing(
+        float InPing)
+    -> void
+{
+    Super::UpdatePing(InPing);
+
+    const auto& CurrentTime = UCk_Utils_Time_UE::Get_WorldTime
+    (
+        FCk_Utils_Time_GetWorldTime_Params
+        {
+            this, GetWorld(),
+            ECk_Time_WorldTimeType::RealTime
+        }
+    );
+
+    _PingRange.Request_HandleNewPing(FCk_Time{InPing}, CurrentTime.Get_WorldTime());
+}
+
+auto
+    ACk_PlayerState_UE::
+    Server_Request_Pong_Implementation()
+    -> void
+{
+    const auto& CurrentTime      = FApp::GetCurrentTime();
+    const auto& TimePassedInSecs = CurrentTime - static_cast<double>(_PingSentTime.Get_Seconds());
+
+    _CurrentPingInMs  = FMath::RoundToInt(static_cast<float>(TimePassedInSecs) * 1000.0f);
+    _PingSentTime = FCk_Time::Zero;
+}
+
+auto
+    ACk_PlayerState_UE::
+    Server_Request_Pong_Validate()
+    -> bool
+{
+    return true;
+}
+
+auto
+    ACk_PlayerState_UE::
+    Client_Request_Ping_Implementation()
+    -> void
+{
+    Server_Request_Pong();
+}
+
+auto
+    ACk_PlayerState_UE::
+    DoRequest_Ping()
+    -> void
+{
+    if (_PingSentTime != FCk_Time::Zero)
+    { return; }
+
+    Client_Request_Ping();
+
+    const auto& CurrentTime = FApp::GetCurrentTime();
+
+    _PingSentTime = FCk_Time{static_cast<float>(CurrentTime)};
+}
+
+auto
+    ACk_PlayerState_UE::
+    Get_MinPing() const
+    -> FCk_Time
+{
+    return _PingRange.Get_MinPing();
+}
+
+auto
+    ACk_PlayerState_UE::
+    Get_MaxPing() const
+    -> FCk_Time
+{
+    return _PingRange.Get_MaxPing();
+}
+
+#if CK_BUILD_TEST
+auto
+    ACk_PlayerState_UE::
+    Get_PingRangeHistoryEntries() const
+    -> TArray<FCk_PlayerState_PingRange_History_Entry>
+{
+    return _PingRange.Get_PingHistory().Get_Entries();
+}
+#endif
+
+auto
+    ACk_PlayerState_UE::
+    GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+    -> void
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+    
+    DOREPLIFETIME_CONDITION(ACk_PlayerState_UE, _CurrentPingInMs, COND_None)
+}
+
 // --------------------------------------------------------------------------------------------------------------------
--- Source/CkCore/Public/CkCore/Engine/CkPlayerState.h
+++ Source/CkCore/Public/CkCore/Engine/CkPlayerState.h
@@ -1,10 +1,90 @@
 #pragma once
 
 #include "CkCore/Macros/CkMacros.h"
+#include "CkCore/Time/CkTime.h"
+#include "CkCore/Build/CkBuild_Macros.h"
+#include "Containers/RingBuffer.h"
 
 #include <GameFramework/PlayerState.h>
 
 #include "CkPlayerState.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+#if CK_BUILD_TEST
+
+struct FCk_PlayerState_PingRange_History_Entry
+{
+    CK_GENERATED_BODY(FCk_PlayerState_PingRange_History_Entry);
+
+private:
+    FCk_Time _MinPing;
+    FCk_Time _MaxPing;
+
+public:
+    CK_PROPERTY(_MinPing);
+    CK_PROPERTY(_MaxPing);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+struct FCk_PlayerState_PingRange_History
+{
+public:
+    CK_GENERATED_BODY(FCk_PlayerState_PingRange_History);
+
+public:
+    using EntryType   = FCk_PlayerState_PingRange_History_Entry;
+    using EntriesType = TArray<EntryType>;
+
+public:
+    auto Request_AddNewEntry(const EntryType& InEntry) -> void;
+
+private:
+    EntriesType _Entries;
+
+    static constexpr int _MaxHistorySize = 5;
+
+public:
+    CK_PROPERTY_GET(_Entries);
+};
+
+#endif
+
+// --------------------------------------------------------------------------------------------------------------------
+
+struct FCk_PlayerState_PingRange
+{
+    CK_GENERATED_BODY(FCk_PlayerState_PingRange);
+
+public:
+    auto Request_HandleNewPing(
+        const FCk_Time& InPing,
+        const FCk_Time_Unreal& InCurrentTime) -> void;
+
+private:
+    FCk_Time _MinPing;
+    FCk_Time _MaxPing;
+
+    FCk_Time _NextMinPing = FCk_Time::OneSecond;
+    FCk_Time _NextMaxPing = FCk_Time::Zero;
+
+    FCk_Time _LastUpdateTime;
+
+    static const FCk_Time _UpdateFrequency;
+
+public:
+    CK_PROPERTY_GET(_MaxPing);
+    CK_PROPERTY_GET(_MinPing);
+
+#if CK_BUILD_TEST
+private:
+    FCk_PlayerState_PingRange_History _PingHistory;
+
+public:
+    CK_PROPERTY_GET(_PingHistory);
+#endif
+};
 
 // --------------------------------------------------------------------------------------------------------------------
 
@@ -15,6 +95,49 @@ class CKCORE_API ACk_PlayerState_UE : public APlayerState
 
 public:
     CK_GENERATED_BODY(ACk_PlayerState_UE);
+
+public:
+    auto BeginPlay() -> void override;
+    auto EndPlay(EEndPlayReason::Type InEndPlayReason) -> void override;
+    auto UpdatePing(float InPing) -> void override;
+
+    auto GetLifetimeReplicatedProps(TArray<FLifetimeProperty>&) const -> void override;
+
+public:
+    UFUNCTION(Server, Reliable, WithValidation)
+    void Server_Request_Pong();
+
+    UFUNCTION(Client, Reliable)
+    void Client_Request_Ping();
+
+private:
+    void DoRequest_Ping();
+
+public:
+    auto Get_MinPing() const -> FCk_Time;
+    auto Get_MaxPing() const -> FCk_Time;
+
+#if CK_BUILD_TEST
+    auto Get_PingRangeHistoryEntries() const -> TArray<FCk_PlayerState_PingRange_History_Entry>;
+#endif
+
+private:
+    UPROPERTY(BlueprintReadOnly, Transient, Replicated, meta = (AllowPrivateAccess = true, Units = "ms"))
+    int32 _CurrentPingInMs = 0;
+
+private:
+    FCk_PlayerState_PingRange _PingRange;
+
+    FTimerHandle _PingTimerHandle;
+
+    FCk_Time _PingSentTime = FCk_Time::Zero;
+
+    static const FCk_Time _PingFrequency;
+
+public:
+    CK_PROPERTY_GET(_CurrentPingInMs);
+    CK_PROPERTY_GET(_PingFrequency);
+    CK_PROPERTY_GET(_PingSentTime);
 };
 
 // --------------------------------------------------------------------------------------------------------------------
