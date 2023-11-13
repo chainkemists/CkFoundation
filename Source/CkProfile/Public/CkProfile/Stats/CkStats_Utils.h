06a780f06f5fdd8319f90831af898b2c3c962175
Nitrogen-CK <nitrogen@chainkemists.com>
2023-11-13 18:36:41 -0500

    feat: Added Stats utils lib with function to get the FPS

---
+++ Source/CkProfile/Public/CkProfile/Stats/CkStats_Utils.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <Kismet/BlueprintFunctionLibrary.h>
+
+#include "CkCore/Macros/CkMacros.h"
+
+#include "CkStats_Utils.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable)
+class CKPROFILE_API UCk_Utils_Stats_UE : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_Stats_UE);
+
+public:
+    UFUNCTION(BlueprintPure,
+              Category = "Ck|Utils|Profile|Stats")
+    static float
+    Get_FPS();
+
+    UFUNCTION(BlueprintPure,
+              Category = "Ck|Utils|Profile|Stats")
+    static bool
+    Get_IsStatEnabled(
+        const FString& InStatName);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
