4998276c8b4094d079c392a0ad5149fa320b16b7
Sulfur-CK <sulfur@chainkemists.com>
2023-12-05 00:05:36 -0800

    feat: added the CkBuildConfig module
    - this is the base module for all other modules that contains the build configurations (moved from CkCore)

--- Source/CkCore/CkCore.build.cs
+++ Source/CkCore/CkCore.build.cs
@@ -1,85 +1,6 @@
 using System;
 using UnrealBuildTool;
 
-public class CkModuleRules : ModuleRules
-{
-    enum BuildConfiguration
-    {
-        MatchWithUnreal,
-        Profile
-    }
-
-    void SetBuildConfiguration()
-    {
-        // override this variable to change the configuration settings on a broad level
-        var BuildConfigurationOverride = BuildConfiguration.MatchWithUnreal;
-
-        switch(BuildConfigurationOverride)
-        {
-            case BuildConfiguration.MatchWithUnreal:
-            {
-                switch(Target.Configuration)
-                {
-                    case UnrealTargetConfiguration.Unknown:
-                        PublicDefinitions.Add("CK_BYPASS_ENSURES=0");
-                        PublicDefinitions.Add("CK_LOG_NO_CONTEXT=0");
-                        PublicDefinitions.Add("CK_DISABLE_STACK_TRACE=0");
-                        PublicDefinitions.Add("CK_MEMORY_TRACKING=0");
-                        break;
-                    case UnrealTargetConfiguration.Debug:
-                        PublicDefinitions.Add("CK_BYPASS_ENSURES=0");
-                        PublicDefinitions.Add("CK_LOG_NO_CONTEXT=0");
-                        PublicDefinitions.Add("CK_DISABLE_STACK_TRACE=0");
-                        PublicDefinitions.Add("CK_MEMORY_TRACKING=0");
-                        break;
-                    case UnrealTargetConfiguration.DebugGame:
-                        PublicDefinitions.Add("CK_BYPASS_ENSURES=0");
-                        PublicDefinitions.Add("CK_LOG_NO_CONTEXT=0");
-                        PublicDefinitions.Add("CK_DISABLE_STACK_TRACE=0");
-                        PublicDefinitions.Add("CK_MEMORY_TRACKING=0");
-                        break;
-                    case UnrealTargetConfiguration.Development:
-                        PublicDefinitions.Add("CK_BYPASS_ENSURES=0");
-                        PublicDefinitions.Add("CK_LOG_NO_CONTEXT=0");
-                        PublicDefinitions.Add("CK_DISABLE_STACK_TRACE=0");
-                        PublicDefinitions.Add("CK_MEMORY_TRACKING=0");
-                        break;
-                    case UnrealTargetConfiguration.Test:
-                        PublicDefinitions.Add("CK_BYPASS_ENSURES=0");
-                        PublicDefinitions.Add("CK_LOG_NO_CONTEXT=0");
-                        PublicDefinitions.Add("CK_DISABLE_STACK_TRACE=0");
-                        PublicDefinitions.Add("CK_MEMORY_TRACKING=0");
-                        break;
-                    case UnrealTargetConfiguration.Shipping:
-                        PublicDefinitions.Add("CK_BYPASS_ENSURES=1");
-                        PublicDefinitions.Add("CK_LOG_NO_CONTEXT=1");
-                        PublicDefinitions.Add("CK_DISABLE_STACK_TRACE=1");
-                        PublicDefinitions.Add("CK_MEMORY_TRACKING=0");
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException();
-                }
-                break;
-            }
-            case BuildConfiguration.Profile:
-                PublicDefinitions.Add("CK_LOG_NO_CONTEXT=1");
-                PublicDefinitions.Add("CK_BYPASS_ENSURES=1");
-                PublicDefinitions.Add("CK_MEMORY_TRACKING=1");
-                break;
-            default:
-                throw new ArgumentOutOfRangeException();
-        }
-    }
-
-    public CkModuleRules(ReadOnlyTargetRules Target) : base(Target)
-    {
-        CppStandard = CppStandardVersion.Cpp20;
-        PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-
-        SetBuildConfiguration();
-    }
-}
-
 public class CkCore : CkModuleRules
 {
     public CkCore(ReadOnlyTargetRules Target) : base(Target)
@@ -104,6 +25,7 @@ public class CkCore : CkModuleRules
                 "Core",
                 "GameplayTags",
 
+                "CkBuildConfig",
                 "CkLog",
                 "CkSettings",
                 "CkThirdParty",
