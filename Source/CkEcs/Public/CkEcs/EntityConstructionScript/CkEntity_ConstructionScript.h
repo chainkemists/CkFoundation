c22c4a489095270762386f9002b1d0fcfc1a6f13
Sulfur-CK <sulfur@chainkemists.com>
2023-12-05 20:36:17 -0800

    refactor: UCk_EntityBridge_ConstructionScript_WithTransform_PDA has been moved to CkEcs

--- Source/CkEcs/Public/CkEcs/EntityConstructionScript/CkEntity_ConstructionScript.h
+++ Source/CkEcs/Public/CkEcs/EntityConstructionScript/CkEntity_ConstructionScript.h
@@ -30,3 +30,21 @@ protected:
 };
 
 // --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(Abstract, BlueprintType, Blueprintable, EditInlineNew)
+class CKECS_API UCk_EntityBridge_ConstructionScript_WithTransform_PDA : public UCk_EntityBridge_ConstructionScript_PDA
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_EntityBridge_ConstructionScript_WithTransform_PDA);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (ExposeOnSpawn, AllowPrivateAccess = true))
+    FTransform _EntityInitialTransform;
+
+public:
+    CK_PROPERTY(_EntityInitialTransform);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
