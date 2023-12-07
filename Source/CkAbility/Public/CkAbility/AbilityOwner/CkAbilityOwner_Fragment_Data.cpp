36c763a0cc53590989cf1bc01f79f7fb9ffab169
Sulfur-CK <sulfur@chainkemists.com>
2023-12-06 19:19:51 -0800

    feat: added Ability Events related objects, structs and delegates

--- Source/CkAbility/Public/CkAbility/AbilityOwner/CkAbilityOwner_Fragment_Data.cpp
+++ Source/CkAbility/Public/CkAbility/AbilityOwner/CkAbilityOwner_Fragment_Data.cpp
@@ -10,6 +10,24 @@ FCk_Request_AbilityOwner_GiveAbility::
 
 // --------------------------------------------------------------------------------------------------------------------
 
+FCk_Request_AbilityOwner_RevokeAbility::
+    FCk_Request_AbilityOwner_RevokeAbility(
+        FGameplayTag InAbilityName)
+    : _SearchPolicy(ECk_AbilityOwner_AbilitySearchPolicy::SearchByName)
+    , _AbilityName(InAbilityName)
+{
+}
+
+FCk_Request_AbilityOwner_RevokeAbility::
+    FCk_Request_AbilityOwner_RevokeAbility(
+        FCk_Handle InAbilityHandle)
+    : _SearchPolicy(ECk_AbilityOwner_AbilitySearchPolicy::SearchByHandle)
+    , _AbilityHandle(InAbilityHandle)
+{
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
 FCk_Request_AbilityOwner_ActivateAbility::
     FCk_Request_AbilityOwner_ActivateAbility(
         FGameplayTag InAbilityName,
