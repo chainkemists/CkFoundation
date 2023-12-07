e36d12e706a5f90452e29c19492bfff4d4c21569
Sulfur-CK <sulfur@chainkemists.com>
2023-12-06 19:15:34 -0800

    chore: added comments for PayloadInFlight which explains a caveat

--- Source/CkSignal/Public/CkSignal/CkSignal_Fragment_Data.h
+++ Source/CkSignal/Public/CkSignal/CkSignal_Fragment_Data.h
@@ -8,7 +8,11 @@
 UENUM(BlueprintType)
 enum class ECk_Signal_BindingPolicy : uint8
 {
+    // Note: you only receive the LAST payload if the Signal was fired multiple times AND you bound AFTER the Signal(s) were fired.
+    // If you would like to receive ALL the payloads, then the Signal Payload needs to be an Array.
     FireIfPayloadInFlightThisFrame,
+    // Note: you only receive the LAST payload if the Signal was fired multiple times AND you bound AFTER the Signal(s) were fired
+    // If you would like to receive ALL the payloads, then the Signal Payload needs to be an Array.
     FireIfPayloadInFlight,
     IgnorePayloadInFlight
 };
