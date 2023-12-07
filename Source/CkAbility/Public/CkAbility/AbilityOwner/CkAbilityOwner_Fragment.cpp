bf6984458d7a8bc8b42cc5510c5117b256bd1695
Sulfur-CK <sulfur@chainkemists.com>
2023-12-06 19:20:54 -0800

    feat, wip: added the AbilityOwner_Events fragment - Added the AbilityOwner_Events Replicated Fragment
    - the Replicated Fragment is not yet complete

--- Source/CkAbility/Public/CkAbility/AbilityOwner/CkAbilityOwner_Fragment.cpp
+++ Source/CkAbility/Public/CkAbility/AbilityOwner/CkAbilityOwner_Fragment.cpp
@@ -1,3 +1,30 @@
 #include "CkAbilityOwner_Fragment.h"
 
+#include <Net/UnrealNetwork.h>
+
 // --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Fragment_AbilityOwner_Events_Rep::
+    GetLifetimeReplicatedProps(
+        TArray<FLifetimeProperty>& OutLifetimeProps) const
+    -> void
+{
+    Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+    DOREPLIFETIME(ThisType, _Events);
+}
+
+auto
+    UCk_Fragment_AbilityOwner_Events_Rep::
+    OnRep_NewEvents()
+    -> void
+{
+    if (ck::Is_NOT_Valid(Get_AssociatedEntity()))
+    { return; }
+
+    for (auto Index = _NextEventToProcess; Index < _Events.Num(); ++Index)
+    {
+        // Fire the event here
+    }
+}
