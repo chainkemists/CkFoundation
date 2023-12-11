6f5cd096c19aa318db90ccae08b5871ba4f8a389
Sulfur-CK <sulfur@chainkemists.com>
2023-12-11 10:44:28 -0800

    feat, wip: added the Ability Fragment and Utilities (no Processors yet) - Added the Ability Subsystem to help root Ability Scripts

---
+++ Source/CkAbility/Public/CkAbility/Ability/CkAbility_Fragment.cpp
@@ -0,0 +1,22 @@
+#include "CkAbility_Fragment.h"
+
+#include "CkAbility/Ability/CkAbility_Utils.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ck::algo
+{
+    auto
+        MatchesAnyAbilityActivationCancelledTags::
+        operator()(
+            const FCk_Handle& InHandle) const
+        -> bool
+    {
+        const auto AbilityName = UCk_Utils_GameplayLabel_UE::Get_Label(InHandle);
+
+        if (NOT UCk_Utils_Ability_UE::Has(InHandle, AbilityName))
+        { return false; }
+
+        return UCk_Utils_Ability_UE::Get_ActivationSettings(InHandle, AbilityName).Get_ActivationCancelledTags().HasAnyExact(_Tags);
+    }
+}
