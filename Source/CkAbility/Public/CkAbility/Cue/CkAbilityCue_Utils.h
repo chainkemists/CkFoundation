ba37f2ca3dbfd70defffd09eb58232ef68defd14
Sulfur-CK <sulfur@chainkemists.com>
2023-12-15 22:58:27 -0800

    feat, wip: added AbilityCue Fragment/Processor and Utils - Added AbilityCue Aggregator
    notes: AbilityCue Aggregators are assets responsible for tracking Ability Cue's in the folder (Package) that they currently reside in using Stably Named GameplayTags. The stably named tags are then used to replicate the firing of an Ability Cue remotely.

---
+++ Source/CkAbility/Public/CkAbility/Cue/CkAbilityCue_Utils.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CkEcs/Handle/CkHandle.h"
+
+#include "CkAbilityCue_Utils.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable)
+class CKABILITY_API UCk_Utils_AbilityCue_UE : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_AbilityCue_UE);
+
+public:
+};
+
+// --------------------------------------------------------------------------------------------------------------------
