c26b0890a2446bc72d9b5409426171095c781d8f
Sulfur-CK <sulfur@chainkemists.com>
2023-12-15 22:58:27 -0800

    feat, wip: added AbilityCue Fragment/Processor and Utils - Added AbilityCue Aggregator
    notes: AbilityCue Aggregators are assets responsible for tracking Ability Cue's in the folder (Package) that they currently reside in using Stably Named GameplayTags. The stably named tags are then used to replicate the firing of an Ability Cue remotely.

---
+++ Source/CkAbility/Public/CkAbility/Cue/CkAbilityCue_Fragment_Data.h
@@ -0,0 +1,32 @@
+#pragma once
+#include "CkCore/Types/DataAsset/CkDataAsset.h"
+
+ #include "CkAbilityCue_Fragment_Data.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+class UCk_EntityBridge_Config_Base_PDA;
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(BlueprintType)
+class CKABILITY_API UCk_AbilityCue_Aggregator_DA : public UCk_DataAsset_PDA
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_AbilityCue_Aggregator_DA);
+
+public:
+    auto PreSave(FObjectPreSaveContext ObjectSaveContext) -> void override;
+    auto PostLoad() -> void override;
+
+private:
+    UPROPERTY(VisibleAnywhere, meta=(AllowPrivateAccess))
+    TArray<TSoftObjectPtr<UCk_EntityBridge_Config_Base_PDA>> _AbilityCues;
+
+public:
+    CK_PROPERTY_GET(_AbilityCues);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
