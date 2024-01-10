a5840af312cc7498109b49f07d105e1998f4e6dc
Nitrogen-CK <nitrogen@chainkemists.com>
2024-01-10 17:30:35 -0500

    feat: Added new function to gather all rendered actors. Future version will defer the work which is why it accepts a delegate as part of the interface

--- Source/CkGraphics/Public/CkGraphics/CkGraphics_Utils.cpp
+++ Source/CkGraphics/Public/CkGraphics/CkGraphics_Utils.cpp
@@ -1,5 +1,10 @@
 #include "CkGraphics_Utils.h"
 
+#include "CkCore/Ensure/CkEnsure.h"
+
+#include <Engine/World.h>
+#include <EngineUtils.h>
+
 // --------------------------------------------------------------------------------------------------------------------
 
 auto
@@ -23,6 +28,32 @@ auto
         IntensifyColor(InColor.B),
         InColor.A
     };
+}
+
+auto
+    UCk_Utils_Graphics_UE::
+    Request_GatherAllRenderedActors(
+        UObject* InWorldContextObject,
+        FCk_Time InTimeTolerance,
+        FCk_Delegate_OnAllRenderedActorsGathered InDelegate)
+    -> void
+{
+    TArray<AActor*> AllRenderedActors;
+
+    CK_ENSURE_IF_NOT(ck::IsValid(InWorldContextObject), TEXT("Invalid WorldContextObject"))
+    { return; }
+
+    // TODO: Modify this to return a cached result
+
+    for (auto ActorItr = TActorIterator<AActor>{InWorldContextObject->GetWorld()}; ActorItr; ++ActorItr)
+    {
+        if (ActorItr->WasRecentlyRendered(InTimeTolerance.Get_Seconds()))
+        {
+            AllRenderedActors.Add(*ActorItr);
+        }
+    }
+
+    std::ignore = InDelegate.ExecuteIfBound(AllRenderedActors);
 }
 
 // --------------------------------------------------------------------------------------------------------------------
