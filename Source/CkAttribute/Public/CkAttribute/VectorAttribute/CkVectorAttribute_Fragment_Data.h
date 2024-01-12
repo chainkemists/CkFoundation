077d3666394c2213dc34d932a988a9332eb30361
Sulfur-CK <sulfur@chainkemists.com>
2024-01-11 21:21:02 -0800

    feat: added Vector Attributes

---
+++ Source/CkAttribute/Public/CkAttribute/VectorAttribute/CkVectorAttribute_Fragment_Data.h
@@ -0,0 +1,249 @@
+#pragma once
+
+#include "CkCore/Enums/CkEnums.h"
+#include "CkEcs/Handle/CkHandle.h"
+#include "CkCore/Macros/CkMacros.h"
+#include "CkProvider/CkProvider_Data.h"
+
+#include "CkVectorAttribute_Fragment_Data.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKATTRIBUTE_API FCk_Fragment_VectorAttribute_ParamsData
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Fragment_VectorAttribute_ParamsData);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true, ForceInlineRow))
+    FGameplayTag _AttributeName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true, ForceInlineRow))
+    FVector _AttributeBaseValue = FVector::ZeroVector;
+
+public:
+    CK_PROPERTY_GET(_AttributeName);
+    CK_PROPERTY_GET(_AttributeBaseValue);
+
+public:
+    CK_DEFINE_CONSTRUCTORS(FCk_Fragment_VectorAttribute_ParamsData, _AttributeName, _AttributeBaseValue);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKATTRIBUTE_API FCk_Fragment_MultipleVectorAttribute_ParamsData
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Fragment_MultipleVectorAttribute_ParamsData);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    TArray<FCk_Fragment_VectorAttribute_ParamsData> _VectorAttributeParams;
+
+public:
+    CK_PROPERTY_GET(_VectorAttributeParams)
+
+public:
+    CK_DEFINE_CONSTRUCTORS(FCk_Fragment_MultipleVectorAttribute_ParamsData, _VectorAttributeParams);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKATTRIBUTE_API FCk_Fragment_VectorAttributeModifier_ParamsData
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Fragment_VectorAttributeModifier_ParamsData);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    FVector _ModifierDelta = FVector::ZeroVector;
+
+    UPROPERTY(BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    FGameplayTag _TargetAttributeName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    ECk_ModifierOperation _ModifierOperation = ECk_ModifierOperation::Additive;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    ECk_ModifierOperation_RevocablePolicy _ModifierOperation_RevokablePolicy = ECk_ModifierOperation_RevocablePolicy::Revocable;
+
+public:
+    CK_PROPERTY_GET(_ModifierDelta);
+    CK_PROPERTY_GET(_TargetAttributeName);
+    CK_PROPERTY_GET(_ModifierOperation);
+    CK_PROPERTY_GET(_ModifierOperation_RevokablePolicy);
+
+public:
+    CK_DEFINE_CONSTRUCTORS(FCk_Fragment_VectorAttributeModifier_ParamsData, _ModifierDelta, _TargetAttributeName,
+        _ModifierOperation, _ModifierOperation_RevokablePolicy);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(Abstract, Blueprintable, BlueprintType, EditInlineNew)
+class CKATTRIBUTE_API UCk_Provider_VectorAttribute_ParamsData_PDA : public UCk_Provider_PDA
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Provider_VectorAttribute_ParamsData_PDA);
+
+public:
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent,
+              Category = "Ck|Provider|VectorAttribute")
+    FCk_Fragment_VectorAttribute_ParamsData
+    Get_Value(
+        FCk_Handle InHandle) const;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable)
+class CKATTRIBUTE_API UCk_Provider_VectorAttribute_ParamsData_Literal_PDA : public UCk_Provider_VectorAttribute_ParamsData_PDA
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Provider_VectorAttribute_ParamsData_Literal_PDA);
+
+private:
+    auto Get_Value_Implementation(
+        FCk_Handle InHandle) const -> FCk_Fragment_VectorAttribute_ParamsData override;
+
+private:
+    UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = true))
+    FCk_Fragment_VectorAttribute_ParamsData _Value;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKATTRIBUTE_API FCk_Provider_VectorAttribute_ParamsData
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Provider_VectorAttribute_ParamsData);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Instanced, meta = (AllowPrivateAccess = true))
+    TObjectPtr<UCk_Provider_VectorAttribute_ParamsData_PDA> _Provider;
+
+public:
+    CK_PROPERTY_GET(_Provider);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(Abstract, Blueprintable, BlueprintType, EditInlineNew)
+class CKATTRIBUTE_API UCk_Provider_MultipleVectorAttribute_ParamsData_PDA : public UCk_Provider_PDA
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Provider_MultipleVectorAttribute_ParamsData_PDA);
+
+public:
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent,
+              Category = "Ck|Provider|VectorAttribute")
+    FCk_Fragment_MultipleVectorAttribute_ParamsData
+    Get_Value(
+        FCk_Handle InHandle) const;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable)
+class CKATTRIBUTE_API UCk_Provider_MultipleVectorAttribute_ParamsData_Literal_PDA
+    : public UCk_Provider_MultipleVectorAttribute_ParamsData_PDA
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Provider_MultipleVectorAttribute_ParamsData_Literal_PDA);
+
+private:
+    auto Get_Value_Implementation(
+        FCk_Handle InHandle) const -> FCk_Fragment_MultipleVectorAttribute_ParamsData override;
+
+private:
+    UPROPERTY(EditAnywhere, meta = (AllowPrivateAccess = true))
+    FCk_Fragment_MultipleVectorAttribute_ParamsData _Value;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKATTRIBUTE_API FCk_Provider_MultipleVectorAttribute_ParamsData
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Provider_MultipleVectorAttribute_ParamsData);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Instanced, meta = (AllowPrivateAccess = true))
+    TObjectPtr<UCk_Provider_MultipleVectorAttribute_ParamsData_PDA> _Provider;
+
+public:
+    CK_PROPERTY_GET(_Provider);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKATTRIBUTE_API FCk_Payload_VectorAttribute_OnValueChanged
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Payload_VectorAttribute_OnValueChanged);
+
+private:
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+    FCk_Handle  _Handle;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+    FVector  _BaseValue = FVector::ZeroVector;
+
+    UPROPERTY(VisibleAnywhere, BlueprintReadOnly, meta = (AllowPrivateAccess = true))
+    FVector  _FinalValue = FVector::ZeroVector;
+
+public:
+    CK_PROPERTY_GET(_Handle);
+    CK_PROPERTY_GET(_BaseValue);
+    CK_PROPERTY_GET(_FinalValue);
+
+public:
+    CK_DEFINE_CONSTRUCTORS(FCk_Payload_VectorAttribute_OnValueChanged, _Handle, _BaseValue, _FinalValue);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+DECLARE_DYNAMIC_DELEGATE_TwoParams(
+    FCk_Delegate_VectorAttribute_OnValueChanged,
+    FCk_Handle, InHandle,
+    FCk_Payload_VectorAttribute_OnValueChanged, InPayload);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(
+    FCk_Delegate_VectorAttribute_OnValueChanged_MC,
+    FCk_Handle, InHandle,
+    FCk_Payload_VectorAttribute_OnValueChanged, InPayload);
+
+// --------------------------------------------------------------------------------------------------------------------
--- Source/CkAttribute/Public/CkAttribute/VectorAttribute/CkVectorAttribute_Utils.h
+++ Source/CkAttribute/Public/CkAttribute/VectorAttribute/CkVectorAttribute_Utils.h
@@ -3,184 +3,184 @@
 #include <GameplayTagContainer.h>
 
 #include "CkAttribute/CkAttribute_Utils.h"
-#include "CkAttribute/ByteAttribute/CkByteAttribute_Fragment.h"
-#include "CkAttribute/ByteAttribute/CkByteAttribute_Fragment_Data.h"
+#include "CkAttribute/VectorAttribute/CkVectorAttribute_Fragment.h"
+#include "CkAttribute/VectorAttribute/CkVectorAttribute_Fragment_Data.h"
 #include "CkEcsBasics/CkEcsBasics_Utils.h"
 #include "CkSignal/CkSignal_Fragment_Data.h"
 
-#include "CkByteAttribute_Utils.generated.h"
+#include "CkVectorAttribute_Utils.generated.h"
 
 // --------------------------------------------------------------------------------------------------------------------
 
 UCLASS(NotBlueprintable)
-class CKATTRIBUTE_API UCk_Utils_ByteAttribute_UE : public UCk_Utils_Ecs_Base_UE
+class CKATTRIBUTE_API UCk_Utils_VectorAttribute_UE : public UCk_Utils_Ecs_Base_UE
 {
     GENERATED_BODY()
 
 public:
-    CK_GENERATED_BODY(UCk_Utils_ByteAttribute_UE);
+    CK_GENERATED_BODY(UCk_Utils_VectorAttribute_UE);
 
 public:
-    friend class UCk_Utils_ByteAttributeModifier_UE;
+    friend class UCk_Utils_VectorAttributeModifier_UE;
 
 private:
-    class ByteAttribute_Utils : public ck::TUtils_Attribute<ck::FFragment_ByteAttribute> {};
-    class RecordOfByteAttributes_Utils : public ck::TUtils_RecordOfEntities<ck::FFragment_RecordOfByteAttributes> {};
+    class VectorAttribute_Utils : public ck::TUtils_Attribute<ck::FFragment_VectorAttribute> {};
+    class RecordOfVectorAttributes_Utils : public ck::TUtils_RecordOfEntities<ck::FFragment_RecordOfVectorAttributes> {};
 
 public:
-    friend class UCk_Utils_ByteAttributes_UE;
+    friend class UCk_Utils_VectorAttributes_UE;
 
 public:
     UFUNCTION(BlueprintCallable,
-              Category = "Ck|Utils|Attribute|Byte",
-              DisplayName="Add New Byte Attribute")
+              Category = "Ck|Utils|Attribute|Vector",
+              DisplayName="Add New Vector Attribute")
     static void
     Add(
         FCk_Handle InHandle,
-        const FCk_Fragment_ByteAttribute_ParamsData& InParams,
+        const FCk_Fragment_VectorAttribute_ParamsData& InParams,
         ECk_Replication InReplicates = ECk_Replication::Replicates);
 
     UFUNCTION(BlueprintCallable,
-              Category = "Ck|Utils|Attribute|Byte",
-              DisplayName="Add Multiple New Byte Attributes")
+              Category = "Ck|Utils|Attribute|Vector",
+              DisplayName="Add Multiple New Vector Attributes")
     static void
     AddMultiple(
         FCk_Handle InHandle,
-        const FCk_Fragment_MultipleByteAttribute_ParamsData& InParams,
+        const FCk_Fragment_MultipleVectorAttribute_ParamsData& InParams,
         ECk_Replication InReplicates = ECk_Replication::Replicates);
 
     UFUNCTION(BlueprintPure,
-              Category = "Ck|Utils|Attribute|Byte",
-              DisplayName="Has Byte Attribute")
+              Category = "Ck|Utils|Attribute|Vector",
+              DisplayName="Has Vector Attribute")
     static bool
     Has(
         FCk_Handle InAttributeOwnerEntity,
         FGameplayTag InAttributeName);
 
     UFUNCTION(BlueprintPure,
-              Category = "Ck|Utils|Attribute|Byte",
-              DisplayName="Has Any Byte Attribute")
+              Category = "Ck|Utils|Attribute|Vector",
+              DisplayName="Has Any Vector Attribute")
     static bool
     Has_Any(
         FCk_Handle InAttributeOwnerEntity);
 
     UFUNCTION(BlueprintPure,
-              Category = "Ck|Utils|Attribute|Byte",
-              DisplayName="Ensure Has Byte Attribute")
+              Category = "Ck|Utils|Attribute|Vector",
+              DisplayName="Ensure Has Vector Attribute")
     static bool
     Ensure(
         FCk_Handle InAttributeOwnerEntity,
         FGameplayTag InAttributeName);
 
     UFUNCTION(BlueprintPure,
-              Category = "Ck|Utils|Attribute|Byte",
-              DisplayName="Ensure Has Any Byte Attribute")
+              Category = "Ck|Utils|Attribute|Vector",
+              DisplayName="Ensure Has Any Vector Attribute")
     static bool
     Ensure_Any(
         FCk_Handle InAttributeOwnerEntity);
 
 public:
     UFUNCTION(BlueprintCallable,
-              Category = "Ck|Utils|Attribute|Byte",
-              DisplayName="For Each Byte Attribute",
+              Category = "Ck|Utils|Attribute|Vector",
+              DisplayName="For Each Vector Attribute",
               meta=(AutoCreateRefTerm="InDelegate"))
     static TArray<FCk_Handle>
-    ForEach_ByteAttribute(
+    ForEach_VectorAttribute(
         FCk_Handle InAttributeOwner,
         const FCk_Lambda_InHandle& InDelegate);
     static auto
-    ForEach_ByteAttribute(
+    ForEach_VectorAttribute(
         const FCk_Handle&                  InAttributeOwner,
         const TFunction<void(FCk_Handle)>& InFunc) -> void;
 
     UFUNCTION(BlueprintCallable,
-              Category = "Ck|Utils|Attribute|Byte",
-              DisplayName="For Each Byte Attribute If",
+              Category = "Ck|Utils|Attribute|Vector",
+              DisplayName="For Each Vector Attribute If",
               meta=(AutoCreateRefTerm="InDelegate"))
     static TArray<FCk_Handle>
-    ForEach_ByteAttribute_If(
+    ForEach_VectorAttribute_If(
         FCk_Handle InAttributeOwner,
         const FCk_Lambda_InHandle& InDelegate,
         const FCk_Predicate_InHandle_OutResult& InPredicate);
     static auto
-    ForEach_ByteAttribute_If(
+    ForEach_VectorAttribute_If(
         FCk_Handle InAttributeOwner,
         const TFunction<void(FCk_Handle)>& InFunc,
         const TFunction<bool(FCk_Handle)>& InPredicate) -> void;
 
 public:
     UFUNCTION(BlueprintPure,
-              Category = "Ck|Utils|Attribute|Byte",
-              DisplayName="Get Byte Attribute Base Value")
-    static uint8
+              Category = "Ck|Utils|Attribute|Vector",
+              DisplayName="Get Vector Attribute Base Value")
+    static FVector
     Get_BaseValue(
         FCk_Handle InAttributeOwnerEntity,
         FGameplayTag InAttributeName);
 
     UFUNCTION(BlueprintPure,
-              Category = "Ck|Utils|Attribute|Byte",
-              DisplayName="Get Byte Attribute Bonus Value")
-    static uint8
+              Category = "Ck|Utils|Attribute|Vector",
+              DisplayName="Get Vector Attribute Bonus Value")
+    static FVector
     Get_BonusValue(
         FCk_Handle InAttributeOwnerEntity,
         FGameplayTag InAttributeName);
 
     UFUNCTION(BlueprintPure,
-              Category = "Ck|Utils|Attribute|Byte",
-              DisplayName="Get Byte Attribute Final Value")
-    static uint8
+              Category = "Ck|Utils|Attribute|Vector",
+              DisplayName="Get Vector Attribute Final Value")
+    static FVector
     Get_FinalValue(
         FCk_Handle InAttributeOwnerEntity,
         FGameplayTag InAttributeName);
 
 public:
     UFUNCTION(BlueprintCallable,
-              Category = "Ck|Utils|Attribute|Byte",
-              DisplayName = "Bind To On Byte Attribute Value Changed")
+              Category = "Ck|Utils|Attribute|Vector",
+              DisplayName = "Bind To On Vector Attribute Value Changed")
     static void
     BindTo_OnValueChanged(
         FCk_Handle InAttributeOwnerEntity,
         FGameplayTag InAttributeName,
         ECk_Signal_BindingPolicy InBehavior,
         ECk_Signal_PostFireBehavior InPostFireBehavior,
-        const FCk_Delegate_ByteAttribute_OnValueChanged& InDelegate);
+        const FCk_Delegate_VectorAttribute_OnValueChanged& InDelegate);
 
     UFUNCTION(BlueprintCallable,
-              Category = "Ck|Utils|Attribute|Byte",
-              DisplayName = "Unbind From On Byte Attribute Value Changed")
+              Category = "Ck|Utils|Attribute|Vector",
+              DisplayName = "Unbind From On Vector Attribute Value Changed")
     static void
     UnbindFrom_OnValueChanged(
         FCk_Handle InAttributeOwnerEntity,
         FGameplayTag InAttributeName,
-        const FCk_Delegate_ByteAttribute_OnValueChanged& InDelegate);
+        const FCk_Delegate_VectorAttribute_OnValueChanged& InDelegate);
 };
 
 // --------------------------------------------------------------------------------------------------------------------
 
 UCLASS(NotBlueprintable)
-class CKATTRIBUTE_API UCk_Utils_ByteAttributeModifier_UE : public UCk_Utils_Ecs_Base_UE
+class CKATTRIBUTE_API UCk_Utils_VectorAttributeModifier_UE : public UCk_Utils_Ecs_Base_UE
 {
     GENERATED_BODY()
 
 public:
-    CK_GENERATED_BODY(UCk_Utils_ByteAttributeModifier_UE);
+    CK_GENERATED_BODY(UCk_Utils_VectorAttributeModifier_UE);
 
 private:
-    class ByteAttributeModifier_Utils : public ck::TUtils_AttributeModifier<ck::FFragment_ByteAttributeModifier> {};
+    class VectorAttributeModifier_Utils : public ck::TUtils_AttributeModifier<ck::FFragment_VectorAttributeModifier> {};
 
 public:
     UFUNCTION(BlueprintCallable,
-              Category = "Ck|Utils|AttributeModifier|Byte",
-              DisplayName="Add Byte Attribute Modifier")
+              Category = "Ck|Utils|AttributeModifier|Vector",
+              DisplayName="Add Vector Attribute Modifier")
     static void
     Add(
         FCk_Handle InAttributeOwnerEntity,
         FGameplayTag InModifierName,
-        const FCk_Fragment_ByteAttributeModifier_ParamsData& InParams);
+        const FCk_Fragment_VectorAttributeModifier_ParamsData& InParams);
 
     UFUNCTION(BlueprintPure,
-              Category = "Ck|Utils|AttributeModifier|Byte",
-              DisplayName="Has Byte Attribute Modifier")
+              Category = "Ck|Utils|AttributeModifier|Vector",
+              DisplayName="Has Vector Attribute Modifier")
     static bool
     Has(
         FCk_Handle InAttributeOwnerEntity,
@@ -188,8 +188,8 @@ public:
         FGameplayTag InModifierName);
 
     UFUNCTION(BlueprintPure,
-              Category = "Ck|Utils|AttributeModifier|Byte",
-              DisplayName="Ensure Has Byte Attribute Modifier")
+              Category = "Ck|Utils|AttributeModifier|Vector",
+              DisplayName="Ensure Has Vector Attribute Modifier")
     static bool
     Ensure(
         FCk_Handle InAttributeOwnerEntity,
@@ -197,8 +197,8 @@ public:
         FGameplayTag InModifierName);
 
     UFUNCTION(BlueprintCallable,
-              Category = "Ck|Utils|AttributeModifier|Byte",
-              DisplayName="Remove Byte Attribute Modifier")
+              Category = "Ck|Utils|AttributeModifier|Vector",
+              DisplayName="Remove Vector Attribute Modifier")
     static void
     Remove(
         FCk_Handle InAttributeOwnerEntity,
