e70b65b449c8f9e2c0bc9c72354fb88534a98e77
Sulfur-CK <sulfur@chainkemists.com>
2024-01-15 14:46:06 -0800

    fix: build-fix for non-unit builds (missing header)

--- Source/CkLog/Public/CkLog/CkLog_Utils.h
+++ Source/CkLog/Public/CkLog/CkLog_Utils.h
@@ -6,6 +6,7 @@
 
 #include <Framework/Notifications/NotificationManager.h>
 #include <Widgets/Notifications/SNotificationList.h>
+#include <Logging/MessageLog.h>
 
 #if WITH_EDITOR
 #if ENGINE_MAJOR_VERSION == 5 && ENGINE_MINOR_VERSION <= 2
