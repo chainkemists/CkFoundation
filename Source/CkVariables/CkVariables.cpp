9be12912bae6b904e6c67aea6edfbeba60efb5b7
Nitrogen-CK <nitrogen@chainkemists.com>
2024-01-15 21:38:57 -0500

    feat: Added new CkVariables module

---
+++ Source/CkVariables/CkVariables.cpp
@@ -0,0 +1,15 @@
+#include "CkVariables.h"
+
+#define LOCTEXT_NAMESPACE "FCkVariablesModule"
+
+void FCkVariablesModule::StartupModule()
+{
+}
+
+void FCkVariablesModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkVariablesModule, CkVariables)
---
+++ Source/CkVariables/Public/CkVariables/CkUnrealVariables_Fragment.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "CkVariables/CkVariables_Fragment.h"
+
+#include <GameplayTagContainer.h>
+#include <InstancedStruct.h>
+
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ck
+{
+    struct CKVARIABLES_API FFragment_Variable_Bool : public TFragment_Variables<bool> {};
+    struct CKVARIABLES_API FFragment_Variable_Int32 : public TFragment_Variables<int32> {};
+    struct CKVARIABLES_API FFragment_Variable_Int64 : public TFragment_Variables<int64> {};
+    struct CKVARIABLES_API FFragment_Variable_Float : public TFragment_Variables<float> {};
+    struct CKVARIABLES_API FFragment_Variable_Name : public TFragment_Variables<FName> {};
+    struct CKVARIABLES_API FFragment_Variable_String : public TFragment_Variables<FString, const FString&> {};
+    struct CKVARIABLES_API FFragment_Variable_Text : public TFragment_Variables<FText> {};
+    struct CKVARIABLES_API FFragment_Variable_Vector : public TFragment_Variables<FVector> {};
+    struct CKVARIABLES_API FFragment_Variable_Vector2D : public TFragment_Variables<FVector2D> {};
+    struct CKVARIABLES_API FFragment_Variable_Rotator : public TFragment_Variables<FRotator> {};
+    struct CKVARIABLES_API FFragment_Variable_Transform : public TFragment_Variables<FTransform, const FTransform&> {};
+
+    struct CKVARIABLES_API FFragment_Variable_InstancedStruct : public TFragment_Variables<FInstancedStruct, const FInstancedStruct&> {};
+
+    struct CKVARIABLES_API FFragment_Variable_UObject : public TFragment_Variables<UObject*> {};
+    struct CKVARIABLES_API FFragment_Variable_UObject_SubclassOf : public TFragment_Variables<TSubclassOf<UObject>> {};
+};
+
+// --------------------------------------------------------------------------------------------------------------------
