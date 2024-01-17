64bf0ba7d8875c87109cf9d5cf081ba97f20056c
Nitrogen-CK <nitrogen@chainkemists.com>
2024-01-16 20:25:15 -0500

    chore: Modifier certain ActorModifier structs to accepts some of their properties as "optional" (by using CK_PROPERTY instead of CK_PROPERTY_GET)

--- Source/CkActor/Public/CkActor/ActorModifier/CkActorModifier_Fragment_Data.cpp
+++ Source/CkActor/Public/CkActor/ActorModifier/CkActorModifier_Fragment_Data.cpp
@@ -15,25 +15,8 @@ FCk_SpawnActor_PostSpawn_Params::
 
 FCk_AddActorComponent_Params::
     FCk_AddActorComponent_Params(
-        bool InIsTickEnabled,
-        FCk_Time InTickInterval,
-        ECk_ActorComponent_AttachmentPolicy InAttachmentType,
-        USceneComponent* InParent,
-        FName InAttachmentSocket)
-    : _IsTickEnabled(InIsTickEnabled)
-    , _TickInterval(InTickInterval)
-    , _AttachmentType(InAttachmentType)
-    , _Parent(InParent)
-    , _AttachmentSocket(InAttachmentSocket)
-{
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-FCk_Request_ActorModifier_AttachActor::
-    FCk_Request_ActorModifier_AttachActor(
-        FCk_Actor_AttachToActor_Params InParams)
-    : _Params(InParams)
+        USceneComponent* InParent)
+    : _Parent(InParent)
 {
 }
 
@@ -47,21 +30,6 @@ FCk_Request_ActorModifier_SpawnActor::
     : _SpawnParams(InSpawnParams)
     , _PostSpawnParams(InPostSpawnParams)
     , _PreFinishSpawnFunc(InPreFinishSpawnFunc)
-{
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-FCk_Request_ActorModifier_AddActorComponent::
-    FCk_Request_ActorModifier_AddActorComponent(
-        TSubclassOf<UActorComponent> InComponentToAdd,
-        bool InIsUnique,
-        FCk_AddActorComponent_Params InComponentParams,
-        InitializerFuncType InInitializerFunc)
-    : _ComponentToAdd(InComponentToAdd)
-    , _IsUnique(InIsUnique)
-    , _ComponentParams(InComponentParams)
-    , _InitializerFunc(InInitializerFunc)
 {
 }
 
