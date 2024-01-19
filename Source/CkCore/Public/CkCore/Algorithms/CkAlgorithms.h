06752d014231e7b7685a730489b295c8f697a8e1
Sulfur-CK <sulfur@chainkemists.com>
2024-01-19 00:55:15 -0800

    feat: added nodiscard to all non-void algorithms

--- Source/CkCore/Public/CkCore/Algorithms/CkAlgorithms.h
+++ Source/CkCore/Public/CkCore/Algorithms/CkAlgorithms.h
@@ -25,18 +25,23 @@ namespace ck::algo
 namespace ck::algo
 {
     template <typename T_Container, typename T_PredicateFunction>
+    [[nodiscard]]
     auto AllOf(const T_Container& InContainer, T_PredicateFunction InFunc) -> bool;
 
     template <typename T_ItrType, typename T_PredicateFunction>
+    [[nodiscard]]
     auto AllOf(T_ItrType InItrBegin, T_ItrType InItrEnd, T_PredicateFunction InFunc) -> bool;
 
     template <typename T_Container, typename T_PredicateFunction>
+    [[nodiscard]]
     auto AnyOf(const T_Container& InContainer, T_PredicateFunction InFunc) -> bool;
 
     template <typename T_ItrType, typename T_PredicateFunction>
+    [[nodiscard]]
     auto AnyOf(T_ItrType InItrBegin, T_ItrType InItrEnd, T_PredicateFunction InFunc) -> bool;
 
     template <typename T_Container, typename T_PredicateFunction>
+    [[nodiscard]]
     auto CountIf(const T_Container& InContainer, T_PredicateFunction InFunc) -> int32;
 
     template <typename T_Container, typename T_UnaryFunction>
@@ -76,9 +81,11 @@ namespace ck::algo
     auto ForEachReverse(T_Array& InArray, T_UnaryFunction InFunc) -> void;
 
     template <class T_ReturnContainer, class T_TransformFunc, class T_Container>
+    [[nodiscard]]
     auto Transform(const T_Container& InContainer, T_TransformFunc InFunc) -> T_ReturnContainer;
 
     template <class T_ReturnContainer, class T_TransformFunc, class T_ItrType>
+    [[nodiscard]]
     auto Transform(T_ItrType InItrBegin, T_ItrType InItrEnd, T_TransformFunc InFunc) -> T_ReturnContainer;
 
     template <class T_ReturnContainer, class T_TransformFunc, class T_Container>
@@ -88,6 +95,7 @@ namespace ck::algo
     auto Transform(T_ItrType InItrBegin, T_ItrType InItrEnd, TToTransform<T_ReturnContainer> InReturnContainer, T_TransformFunc InFunc) -> void;
 
     template <typename T_Container, typename T_PredicateFunction>
+    [[nodiscard]]
     auto Filter(const T_Container& InContainer, T_PredicateFunction InFunc) -> T_Container;
 
     template <typename T_Container, typename T_PredicateFunction>
@@ -149,6 +157,7 @@ namespace ck::algo
         T_TransformFunc InFunc) -> void;
 
     template <class T_ReturnContainer, class T_TransformFunc, class T_ContainerA, typename T_ContainerB, typename T_ContainerC>
+    [[nodiscard]]
     static auto ForEachViewTransform(
         T_ContainerA& InContainerA,
         T_ContainerB& InContainerB,
@@ -168,6 +177,7 @@ namespace ck::algo
 
     template <class T_ReturnContainer, class T_TransformFunc, class T_ContainerA, typename T_ContainerB, typename
               T_ContainerC, typename T_ContainerD>
+    [[nodiscard]]
     static auto ForEachViewTransform(
         T_ContainerA& InContainerA,
         T_ContainerB& InContainerB,
@@ -189,6 +199,7 @@ namespace ck::algo
 
     template <class T_ReturnContainer, class T_TransformFunc, class T_ContainerA, typename T_ContainerB, typename
               T_ContainerC, typename T_ContainerD, typename T_ContainerE>
+    [[nodiscard]]
     static auto ForEachViewTransform(
         T_ContainerA& InContainerA,
         T_ContainerB& InContainerB,
