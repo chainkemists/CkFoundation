121dc0ef2fe1a76d1b63765ddabffb2be5b90a7b
Sulfur-CK <sulfur@chainkemists.com>
2024-01-19 01:16:20 -0800

    feat: added World utils library with functions to get the UWorld(s) in various capacties

---
+++ Source/CkCore/Public/CkCore/World/CkWorld_Utils.cpp
@@ -0,0 +1,161 @@
+#include "CkWorld_Utils.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// --------------------------------------------------------------------------------------------------------------------
+auto
+    UCk_Utils_World_UE::
+    TryGet_GameWorld(
+        const UObject* InObject)
+    -> TOptional<const UWorld*>
+{
+    CK_ENSURE_IF_NOT(ck::IsValid(InObject), TEXT("Object is [{}]"), InObject)
+    { return {}; }
+
+    const auto SuperWorld = TryGet_WorldFromOuterRecursively(InObject->GetOuter());
+    if (ck::IsValid(SuperWorld))
+    {
+        return SuperWorld;
+    }
+
+    return {};
+}
+
+auto
+    UCk_Utils_World_UE::
+    TryGet_WorldFromOuterRecursively(
+        const UObject* InOuter)
+    -> TOptional<const UWorld*>
+{
+    if (ck::Is_NOT_Valid(InOuter))
+    { return {}; }
+
+    if (ck::IsValid(Cast<UWorld>(InOuter)))
+    {
+        return Cast<UWorld>(InOuter);
+    }
+
+    return TryGet_WorldFromOuterRecursively(InOuter->GetOuter());
+}
+
+auto
+    UCk_Utils_World_UE::
+    TryGet_FirstValidWorld(
+        const UObject* InObject,
+        std::function<bool(const UWorld*)> InPredicate)
+    -> TOptional<const UWorld*>
+{
+    const auto& MaybeWorld = TryGet_GameWorld(InObject);
+    if (ck::IsValid(MaybeWorld))
+    { return MaybeWorld; }
+
+    // The following will get the first valid world regardless of the _type_
+    const auto& World = [&]() -> UWorld*
+    {
+        if (ck::Is_NOT_Valid(GEngine))
+        { return {}; }
+
+        for (auto& Context : GEngine->GetWorldContexts())
+        {
+            const auto& ContextWorld = Context.World();
+
+            if (ck::Is_NOT_Valid(ContextWorld))
+            { continue; }
+
+            if (const auto& GameInstance = ContextWorld->GetGameInstance(); ck::IsValid(GameInstance))
+            {
+                const auto GameInstanceWorld = GameInstance->GetWorld();
+
+                if (ck::Is_NOT_Valid(GameInstanceWorld))
+                { continue; }
+
+                if (InPredicate(GameInstanceWorld))
+                { return GameInstanceWorld; }
+
+                return GameInstance->GetWorld();
+            }
+        }
+
+        return {};
+    }();
+
+    return World;
+}
+
+auto
+    UCk_Utils_World_UE::
+    TryGet_MutableGameWorld(
+        const UObject* InObject)
+    -> TOptional<UWorld*>
+{
+    const auto& MaybeWorld = TryGet_GameWorld(InObject);
+
+    if (ck::Is_NOT_Valid(MaybeWorld))
+    { return {}; }
+
+    auto MutableWorld = const_cast<UWorld*>(*MaybeWorld);
+    return MutableWorld;
+}
+
+auto
+    UCk_Utils_World_UE::
+    TryGet_MutableWorldFromOuterRecursively(
+        const UObject* InOuter)
+    -> TOptional<UWorld*>
+{
+    const auto& MaybeWorld = TryGet_WorldFromOuterRecursively(InOuter);
+
+    if (ck::Is_NOT_Valid(MaybeWorld))
+    { return {}; }
+
+    auto MutableWorld = const_cast<UWorld*>(*MaybeWorld);
+    return MutableWorld;
+}
+
+auto
+    UCk_Utils_World_UE::
+    TryGet_MutableFirstValidWorld(
+        const UObject* InObject,
+        std::function<bool(UWorld*)> InPredicate)
+    -> TOptional<UWorld*>
+{
+    const auto& MaybeWorld = TryGet_FirstValidWorld(InObject, [&](const UWorld* InWorld)
+    {
+        auto MutableWorld = const_cast<UWorld*>(InWorld);
+        return InPredicate(MutableWorld);
+    });
+
+    if (ck::Is_NOT_Valid(MaybeWorld))
+    { return {}; }
+
+    auto MutableWorld = const_cast<UWorld*>(*MaybeWorld);
+    return MutableWorld;
+}
+
+auto
+    UCk_Utils_World_UE::
+    Get_AllAvailableGameWorlds()
+    -> TArray<UWorld*>
+{
+    auto Worlds = TArray<UWorld*>{};
+
+    if (ck::Is_NOT_Valid(GEngine))
+    { return {}; }
+
+    for (auto& Context : GEngine->GetWorldContexts())
+    {
+        const auto& ContextWorld = Context.World();
+
+        if (ck::Is_NOT_Valid(ContextWorld))
+        { continue; }
+
+        auto GameInstance = ContextWorld->GetGameInstance();
+
+        if (ck::Is_NOT_Valid(GameInstance))
+        { continue; }
+
+        Worlds.Add(ContextWorld);
+    }
+
+    return Worlds;
+}
---
+++ Source/CkCore/Public/CkCore/World/CkWorld_Utils.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "CkCore/Ensure/CkEnsure.h"
+
+#include "CkCore/Macros/CkMacros.h"
+
+#include "CkWorld_Utils.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable)
+class CKCORE_API UCk_Utils_World_UE : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_World_UE);
+
+public:
+    static auto
+    TryGet_GameWorld(
+        const UObject* InObject) -> TOptional<const UWorld*>;
+
+    static auto
+    TryGet_WorldFromOuterRecursively(
+        const UObject* InOuter) -> TOptional<const UWorld*>;
+
+    static auto
+    TryGet_FirstValidWorld(
+        const UObject* InObject,
+        std::function<bool(const UWorld*)> InPredicate) -> TOptional<const UWorld*>;
+
+    static auto
+    TryGet_MutableGameWorld(
+        const UObject* InObject) -> TOptional<UWorld*>;
+
+    static auto
+    TryGet_MutableWorldFromOuterRecursively(
+        const UObject* InOuter) -> TOptional<UWorld*>;
+
+    static auto
+    TryGet_MutableFirstValidWorld(
+        const UObject* InObject,
+        std::function<bool(UWorld*)> InPredicate) -> TOptional<UWorld*>;
+
+    static auto
+    Get_AllAvailableGameWorlds() -> TArray<UWorld*>;
+};
