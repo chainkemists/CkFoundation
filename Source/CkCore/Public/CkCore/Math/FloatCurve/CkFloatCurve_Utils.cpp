d451f105ff9c93848f34ba81027a3ac6295b28fa
Nitrogen-CK <nitrogen@chainkemists.com>
2024-01-26 18:56:24 -0500

    feat: Added new FloatCurve utils with functions that return the ValueAtTime, TimeRange and ValueRange of an UCurveFloat

--- Source/CkCore/Public/CkCore/Math/FloatCurve/CkFloatCurve_Utils.cpp
+++ Source/CkCore/Public/CkCore/Math/FloatCurve/CkFloatCurve_Utils.cpp
@@ -1,67 +1,95 @@
 #include "CkFloatCurve_Utils.h"
 
+#include "CkCore/Curve/CkCurve_Utils.h"
 #include "CkCore/Ensure/CkEnsure.h"
 
 // --------------------------------------------------------------------------------------------------------------------
 
 auto
     UCk_Utils_FloatCurve_UE::
-    Get_RuntimeFloatCurve_ValueAtTime(
+    Get_ValueAtTime(
+        const UCurveFloat* InFloatCurve,
+        const FCk_Time&    InTime)
+    -> float
+{
+    CK_ENSURE_IF_NOT(ck::IsValid(InFloatCurve, ck::IsValid_Policy_NullptrOnly{}),
+        TEXT("Invalid Float Curve when trying to get Value At Time"))
+    { return {}; }
+
+    return UCk_Utils_Curve_UE::Get_ValueAtTime(InFloatCurve->FloatCurve, InTime);
+}
+
+auto
+    UCk_Utils_FloatCurve_UE::
+    Get_TimeRange(
+        const UCurveFloat* InFloatCurve)
+    -> FCk_FloatRange
+{
+    CK_ENSURE_IF_NOT(ck::IsValid(InFloatCurve, ck::IsValid_Policy_NullptrOnly{}),
+        TEXT("Invalid Float Curve when trying to get Time Range"))
+    { return {}; }
+
+    return UCk_Utils_Curve_UE::Get_TimeRange(InFloatCurve->FloatCurve);
+}
+
+auto
+    UCk_Utils_FloatCurve_UE::
+    Get_ValueRange(
+        const UCurveFloat* InFloatCurve)
+    -> FCk_FloatRange
+{
+    CK_ENSURE_IF_NOT(ck::IsValid(InFloatCurve, ck::IsValid_Policy_NullptrOnly{}),
+        TEXT("Invalid Float Curve when trying to get Value Range"))
+    { return {}; }
+
+    return UCk_Utils_Curve_UE::Get_ValueRange(InFloatCurve->FloatCurve);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Utils_RuntimeFloatCurve_UE::
+    Get_ValueAtTime(
         const FRuntimeFloatCurve& InFloatCurve,
         const FCk_Time& InTime)
     -> float
 {
-    const auto& RichCurve = InFloatCurve.GetRichCurveConst();
-
-    CK_ENSURE_IF_NOT(ck::IsValid(RichCurve, ck::IsValid_Policy_NullptrOnly{}),
-        TEXT("Invalid RichCurve for RuntimeFloatCurve"))
+    CK_ENSURE_IF_NOT(ck::IsValid(InFloatCurve),
+        TEXT("Invalid Runtime Float Curve when trying to get Value At Time"))
     { return {}; }
 
-    const auto& Seconds = InTime.Get_Seconds();
-    return RichCurve->Eval(Seconds);
+    return UCk_Utils_Curve_UE::Get_ValueAtTime(*InFloatCurve.GetRichCurveConst(), InTime);
 }
 
 auto
-    UCk_Utils_FloatCurve_UE::
-    Get_RuntimeFloatCurve_TimeRange(
+    UCk_Utils_RuntimeFloatCurve_UE::
+    Get_TimeRange(
         const FRuntimeFloatCurve& InFloatCurve)
     -> FCk_FloatRange
 {
-    const auto& RichCurve = InFloatCurve.GetRichCurveConst();
-
-    CK_ENSURE_IF_NOT(ck::IsValid(RichCurve, ck::IsValid_Policy_NullptrOnly{}),
-        TEXT("Invalid RichCurve for RuntimeFloatCurve"))
+    CK_ENSURE_IF_NOT(ck::IsValid(InFloatCurve),
+        TEXT("Invalid Runtime Float Curve when trying to get Time Range"))
     { return {}; }
 
-    auto OutMinTime = 0.0f;
-    auto OutMaxTime = 0.0f;
-    RichCurve->GetTimeRange(OutMinTime, OutMaxTime);
-
-    return FCk_FloatRange{ OutMinTime, OutMaxTime };
+    return UCk_Utils_Curve_UE::Get_TimeRange(*InFloatCurve.GetRichCurveConst());
 }
 
 auto
-    UCk_Utils_FloatCurve_UE::
-    Get_RuntimeFloatCurve_ValueRange(
+    UCk_Utils_RuntimeFloatCurve_UE::
+    Get_ValueRange(
         const FRuntimeFloatCurve& InFloatCurve)
     -> FCk_FloatRange
 {
-    const auto& RichCurve = InFloatCurve.GetRichCurveConst();
-
-    CK_ENSURE_IF_NOT(ck::IsValid(RichCurve, ck::IsValid_Policy_NullptrOnly{}),
-        TEXT("Invalid RichCurve for RuntimeFloatCurve"))
+    CK_ENSURE_IF_NOT(ck::IsValid(InFloatCurve),
+        TEXT("Invalid Runtime Float Curve when trying to get Value Range"))
     { return {}; }
 
-    auto OutMinValue = 0.0f;
-    auto OutMaxValue = 0.0f;
-    RichCurve->GetValueRange(OutMinValue, OutMaxValue);
-
-    return FCk_FloatRange{ OutMinValue, OutMaxValue };
+    return UCk_Utils_Curve_UE::Get_ValueRange(*InFloatCurve.GetRichCurveConst());
 }
 
 auto
-    UCk_Utils_FloatCurve_UE::
-    Get_IsValid_RuntimeFloatCurve(
+    UCk_Utils_RuntimeFloatCurve_UE::
+    Get_IsValid(
         const FRuntimeFloatCurve& InFloatCurve)
     -> bool
 {
--- Source/CkCore/Public/CkCore/Math/FloatCurve/CkFloatCurve_Utils.h
+++ Source/CkCore/Public/CkCore/Math/FloatCurve/CkFloatCurve_Utils.h
@@ -20,32 +20,67 @@ public:
 
 public:
     UFUNCTION(BlueprintPure,
-              DisplayName = "[Ck] Get Runtime Float Curve Value At Time",
+              DisplayName = "[Ck] Get Value At Time (FloatCurve)",
               Category = "Ck|Utils|Math|FloatCurve")
     static float
-    Get_RuntimeFloatCurve_ValueAtTime(
+    Get_ValueAtTime(
+        const UCurveFloat* InFloatCurve,
+        const FCk_Time& InTime);
+
+    UFUNCTION(BlueprintPure,
+              DisplayName = "[Ck] Get Time Range (FloatCurve)",
+              Category = "Ck|Utils|Math|FloatCurve")
+    static FCk_FloatRange
+    Get_TimeRange(
+        const UCurveFloat* InFloatCurve);
+
+    UFUNCTION(BlueprintPure,
+              DisplayName = "[Ck] Get Value Range (FloatCurve)",
+              Category = "Ck|Utils|Math|FloatCurve")
+    static FCk_FloatRange
+    Get_ValueRange(
+        const UCurveFloat* InFloatCurve);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable)
+class CKCORE_API UCk_Utils_RuntimeFloatCurve_UE : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_RuntimeFloatCurve_UE);
+
+public:
+    UFUNCTION(BlueprintPure,
+              DisplayName = "[Ck] Get Value At Time (RuntimeFloatCurve)",
+              Category = "Ck|Utils|Math|RuntimeFloatCurve")
+    static float
+    Get_ValueAtTime(
         const FRuntimeFloatCurve& InFloatCurve,
         const FCk_Time& InTime);
 
     UFUNCTION(BlueprintPure,
-              DisplayName = "[Ck] Get Runtime Float Curve Time Range",
-              Category = "Ck|Utils|Math|FloatCurve")
+              DisplayName = "[Ck] Get Time Range (RuntimeFloatCurve)",
+              Category = "Ck|Utils|Math|RuntimeFloatCurve")
     static FCk_FloatRange
-    Get_RuntimeFloatCurve_TimeRange(
+    Get_TimeRange(
         const FRuntimeFloatCurve& InFloatCurve);
 
     UFUNCTION(BlueprintPure,
-              DisplayName = "[Ck] Get Runtime Float Curve Value Range",
-              Category = "Ck|Utils|Math|FloatCurve")
+              DisplayName = "[Ck] Get Value Range (RuntimeFloatCurve)",
+              Category = "Ck|Utils|Math|RuntimeFloatCurve")
     static FCk_FloatRange
-    Get_RuntimeFloatCurve_ValueRange(
+    Get_ValueRange(
         const FRuntimeFloatCurve& InFloatCurve);
 
     UFUNCTION(BlueprintPure,
-              DisplayName = "[Ck] Get Runtime Float Is Valid",
-              Category = "Ck|Utils|Math|FloatCurve")
+              DisplayName = "[Ck] Get Is Valid (RuntimeFloatCurve)",
+              Category = "Ck|Utils|Math|RuntimeFloatCurve",
+              meta = (CompactNodeTitle = "IsValid"))
     static bool
-    Get_IsValid_RuntimeFloatCurve(
+    Get_IsValid(
         const FRuntimeFloatCurve& InFloatCurve);
 };
 
