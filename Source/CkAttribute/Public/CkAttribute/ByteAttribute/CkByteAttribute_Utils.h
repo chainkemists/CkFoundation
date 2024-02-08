3a6bfa4c944c556c4e44fe0255c3e856b34ef4bc
Sulfur-CK <sulfur@chainkemists.com>
2024-02-07 19:23:41 -0800

    refactor: ByteAttribute functions with interfaces in bpfl are now categorized as blueprint internal use only
    notes: this is to make sure that these nodes are not conflated with the higher level interface nodes

--- Source/CkAttribute/Public/CkAttribute/ByteAttribute/CkByteAttribute_Utils.h
+++ Source/CkAttribute/Public/CkAttribute/ByteAttribute/CkByteAttribute_Utils.h
@@ -32,7 +32,7 @@ public:
 
 public:
     UFUNCTION(BlueprintCallable,
-              Category = "Ck|Utils|Attribute|Byte",
+              Category = "Ck|BLUEPRINT_INTERNAL_USE_ONLY",
               DisplayName="[Ck][ByteAttribute] Add New Attribute")
     static FCk_Handle
     Add(
@@ -167,7 +167,7 @@ private:
 
 public:
     UFUNCTION(BlueprintCallable,
-              Category = "Ck|Utils|AttributeModifier|Byte",
+              Category = "Ck|BLUEPRINT_INTERNAL_USE_ONLY",
               DisplayName="[Ck][ByteAttribute] Add Modifier")
     static void
     Add(
