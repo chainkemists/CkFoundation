dfd96b27ceff8270dfdc3339cb10a1ba361abcfc
Sulfur-CK <sulfur@chainkemists.com>
2024-02-07 21:15:49 -0800

    feat: heavily refactored template Attribute base with support for optional min/max including the existing current Base/Final values
    notes: Attribute base now supports adding min/max values to Attributes optionally (e.g. for Arithmetic types). The processors of the templated Attribute base were refactored and put under ck::detail namespace. Additional processors that consolidate the many granular processors were added which are designed to be used by the derived Attribute Processors. This has been tested with FloatAttributes, which along with other Attribute types, is being refactored and submitting in the upcoming commits

--- Source/CkAttribute/Public/CkAttribute/CkAttribute_Fragment.inl.h
+++ Source/CkAttribute/Public/CkAttribute/CkAttribute_Fragment.inl.h
@@ -6,8 +6,8 @@
 
 namespace ck
 {
-    template <typename T_AttributeType>
-    TFragment_Attribute<T_AttributeType>::
+    template <typename T_HandleType, typename T_AttributeType, ECk_MinMaxCurrent T_ComponentTag>
+    TFragment_Attribute<T_HandleType, T_AttributeType, T_ComponentTag>::
         TFragment_Attribute(
             AttributeDataType InBase)
         : _Base(InBase)
@@ -17,8 +17,8 @@ namespace ck
 
     // --------------------------------------------------------------------------------------------------------------------
 
-    template <typename T_DerivedAttribute>
-    TFragment_AttributeModifier<T_DerivedAttribute>::
+    template <typename T_HandleType, typename T_DerivedAttribute>
+    TFragment_AttributeModifier<T_HandleType, T_DerivedAttribute>::
         TFragment_AttributeModifier(
             AttributeDataType InModifierDelta)
         : _ModifierDelta(InModifierDelta)
