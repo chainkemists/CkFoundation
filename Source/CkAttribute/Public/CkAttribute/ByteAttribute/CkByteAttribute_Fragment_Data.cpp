46bb4360e896fc65f0e2ac32d6911365945a543d
Sulfur-CK <sulfur@chainkemists.com>
2024-02-13 22:52:42 -0800

    feat, refactor: Byte Attribute is now using the upgraded Attributes

--- Source/CkAttribute/Public/CkAttribute/ByteAttribute/CkByteAttribute_Fragment_Data.cpp
+++ Source/CkAttribute/Public/CkAttribute/ByteAttribute/CkByteAttribute_Fragment_Data.cpp
@@ -1,5 +1,7 @@
 #include "CkByteAttribute_Fragment_Data.h"
 
+#include "CkAttribute/CkAttribute_Log.h"
+
 // --------------------------------------------------------------------------------------------------------------------
 
 auto
@@ -9,6 +11,34 @@ auto
     -> FCk_Fragment_ByteAttribute_ParamsData
 {
     return {};
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    FCk_Fragment_ByteAttribute_ParamsData::
+    Get_MinValue() const
+    -> uint8
+{
+    ck::attribute::ErrorIf(NOT (_Component == ECk_MinMax::Min || _Component == ECk_MinMax::MinMax),
+        TEXT("Attempting to get a Min value of Attribute [{}] where MinMax is set to [{}]. Please address this."),
+         _Component,
+         _Name);
+
+    return _MinValue;
+}
+
+auto
+    FCk_Fragment_ByteAttribute_ParamsData::
+    Get_MaxValue() const
+    -> uint8
+{
+    ck::attribute::ErrorIf(NOT (_Component == ECk_MinMax::Max || _Component == ECk_MinMax::MinMax),
+        TEXT("Attempting to get a Max value Attribute [{}] where MinMax is set to [{}]. Please address this."),
+         _Component,
+         _Name);
+
+    return _MaxValue;
 }
 
 // --------------------------------------------------------------------------------------------------------------------
