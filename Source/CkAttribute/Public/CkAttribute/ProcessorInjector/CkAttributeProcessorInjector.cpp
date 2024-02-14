6c664a89068694c4160b4f256b9569551ca6ada8
Sulfur-CK <sulfur@chainkemists.com>
2024-02-13 22:52:58 -0800

    feat: added the new Attribute processors to the Attributes Processor Injectors

--- Source/CkAttribute/Public/CkAttribute/ProcessorInjector/CkAttributeProcessorInjector.cpp
+++ Source/CkAttribute/Public/CkAttribute/ProcessorInjector/CkAttributeProcessorInjector.cpp
@@ -1,14 +1,20 @@
 #include "CkAttributeProcessorInjector.h"
 
+#include "CkAttribute/ByteAttribute/CkByteAttribute_Processor.h"
 #include "CkAttribute/FloatAttribute/CkFloatAttribute_Processor.h"
+#include "CkAttribute/VectorAttribute/CkVectorAttribute_Processor.h"
 
 // --------------------------------------------------------------------------------------------------------------------
 
-void
-    UCk_Attribute_ProcessorInjector_Teardown::DoInjectProcessors(
+auto
+    UCk_Attribute_ProcessorInjector_Teardown::
+    DoInjectProcessors(
         EcsWorldType& InWorld)
+    -> void
 {
+    InWorld.Add<ck::FProcessor_ByteAttributeModifier_TeardownAll>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_FloatAttributeModifier_TeardownAll>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_VectorAttributeModifier_TeardownAll>(InWorld.Get_Registry());
 }
 
 // --------------------------------------------------------------------------------------------------------------------
@@ -17,13 +23,23 @@ auto
     UCk_Attribute_ProcessorInjector::
     DoInjectProcessors(
         EcsWorldType& InWorld)
-        -> void
+    -> void
 {
+    InWorld.Add<ck::FProcessor_ByteAttribute_RecomputeAll>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_ByteAttributeModifier_ComputeAll>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_ByteAttribute_MinMaxClamp>(InWorld.Get_Registry());
+
     InWorld.Add<ck::FProcessor_FloatAttribute_RecomputeAll>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_FloatAttributeModifier_ComputeAll>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_FloatAttribute_MinMaxClamp>(InWorld.Get_Registry());
 
+    InWorld.Add<ck::FProcessor_VectorAttribute_RecomputeAll>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_VectorAttributeModifier_ComputeAll>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_VectorAttribute_MinMaxClamp>(InWorld.Get_Registry());
+
+    InWorld.Add<ck::FProcessor_ByteAttribute_FireSignals>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_FloatAttribute_FireSignals>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_VectorAttribute_FireSignals>(InWorld.Get_Registry());
 }
 
 // --------------------------------------------------------------------------------------------------------------------
