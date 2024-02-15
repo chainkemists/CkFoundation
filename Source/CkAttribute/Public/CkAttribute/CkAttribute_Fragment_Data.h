b96ddff735a0e5c8b4f82b5d4e2da0c734f40cb7
Nitrogen-CK <nitrogen@chainkemists.com>
2024-02-14 22:55:35 -0500

    feat: Added ECk_Attribute_ExistMissing and ECk_AttributeModifier_ExistMissing enums

--- Source/CkAttribute/Public/CkAttribute/CkAttribute_Fragment_Data.h
+++ Source/CkAttribute/Public/CkAttribute/CkAttribute_Fragment_Data.h
@@ -6,13 +6,35 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 UENUM(BlueprintType)
-enum class ECk_Attribute_RemoveModifier_Result
+enum class ECk_AttributeModifier_Remove_Result
 {
     Success,
     Failed_MissingAttribute,
     Failed_MissingModifier,
 };
 
-CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_Attribute_RemoveModifier_Result);
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_AttributeModifier_Remove_Result);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UENUM(BlueprintType)
+enum class ECk_Attribute_ExistMissing
+{
+    HasAttribute,
+    MissingAttribute
+};
+
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_Attribute_ExistMissing);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UENUM(BlueprintType)
+enum class ECk_AttributeModifier_ExistMissing
+{
+    HasModifier,
+    MissingModifier
+};
+
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_AttributeModifier_ExistMissing);
 
 // --------------------------------------------------------------------------------------------------------------------
