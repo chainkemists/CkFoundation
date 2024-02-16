7094ebb8841f03f888ab4e2c8243d7a138435327
Nitrogen-CK <nitrogen@chainkemists.com>
2024-02-16 18:21:10 -0500

    feat: Added additional ensures when attempting to add a modifier/bind/unbind to an attribute to ensure the necessary attribute component exists

--- Source/CkAttribute/Public/CkAttribute/ByteAttribute/CkByteAttribute_Utils.cpp
+++ Source/CkAttribute/Public/CkAttribute/ByteAttribute/CkByteAttribute_Utils.cpp
@@ -368,6 +368,12 @@ auto
         const FCk_Delegate_ByteAttribute_OnValueChanged& InDelegate)
     -> FCk_Handle_ByteAttribute
 {
+    CK_ENSURE_IF_NOT(Has_Component(InAttribute, InAttributeComponent),
+        TEXT("Byte Attribute [{}] does NOT have a [{}] component. Cannot BIND to OnValueChanged"),
+        InAttribute,
+        InAttributeComponent)
+    { return InAttribute; }
+
     switch(InAttributeComponent)
     {
         case ECk_MinMaxCurrent::Min:
@@ -398,6 +404,12 @@ auto
         const FCk_Delegate_ByteAttribute_OnValueChanged& InDelegate)
     -> FCk_Handle_ByteAttribute
 {
+    CK_ENSURE_IF_NOT(Has_Component(InAttribute, InAttributeComponent),
+        TEXT("Byte Attribute [{}] does NOT have a [{}] component. Cannot UNBIND from OnValueChanged"),
+        InAttribute,
+        InAttributeComponent)
+    { return InAttribute; }
+
     switch(InAttributeComponent)
     {
         case ECk_MinMaxCurrent::Min:
@@ -434,7 +446,15 @@ auto
     ParamsToUse.Set_TargetAttributeName(UCk_Utils_GameplayLabel_UE::Get_Label(InAttribute));
 
     const auto& LifetimeOwner = UCk_Utils_EntityLifetime_UE::Get_LifetimeOwner(InAttribute);
-    const auto ModifierOperation = ParamsToUse.Get_ModifierOperation();
+    const auto& ModifierOperation = ParamsToUse.Get_ModifierOperation();
+    const auto& AttributeComponent = ParamsToUse.Get_Component();
+
+    CK_ENSURE_IF_NOT(UCk_Utils_ByteAttribute_UE::Has_Component(InAttribute, AttributeComponent),
+        TEXT("Byte Attribute [{}] with Owner [{}] does NOT have a [{}] component. Cannot Add Modifier"),
+        InAttribute,
+        LifetimeOwner,
+        AttributeComponent)
+    { return {}; }
 
     if (ParamsToUse.Get_ModifierDelta() == 0 &&
         (ModifierOperation == ECk_ArithmeticOperations_Basic::Add || ModifierOperation  == ECk_ArithmeticOperations_Basic::Subtract))
@@ -448,7 +468,7 @@ auto
     auto NewModifierEntity = ck::StaticCast<FCk_Handle_ByteAttributeModifier>(NewEntity);
     UCk_Utils_GameplayLabel_UE::Add(NewModifierEntity, InModifierName);
 
-    switch (ParamsToUse.Get_Component())
+    switch (AttributeComponent)
     {
         case ECk_MinMaxCurrent::Min:
         {
