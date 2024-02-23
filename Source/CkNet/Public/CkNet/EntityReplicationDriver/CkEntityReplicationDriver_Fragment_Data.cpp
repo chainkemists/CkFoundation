8030b2e38a67c7fdcaf8d1496f204ccd0203df7e
Sulfur-CK <sulfur@chainkemists.com>
2024-02-23 00:24:14 -0800

    feat: added HandleReplicator custom struct that allows us to replicate Handle IDs
    notes: the HandleReplicator requires the Handle to be replicated (i.e. have an EntityReplicationDriver Replicated Object) without which, errors are issued and the Handle ID cannot be replicated (similar to a non-replicated Actor being RPCed or RepVar'ed)

--- Source/CkNet/Public/CkNet/EntityReplicationDriver/CkEntityReplicationDriver_Fragment_Data.cpp
+++ Source/CkNet/Public/CkNet/EntityReplicationDriver/CkEntityReplicationDriver_Fragment_Data.cpp
@@ -1,3 +1,43 @@
 #include "CkEntityReplicationDriver_Fragment_Data.h"
 
-CK_INTENTIONALLY_EMPTY()
+#include "CkNet/CkNet_Log.h"
+#include "CkNet/CkNet_Utils.h"
+#include "CkNet/EntityReplicationDriver/CkEntityReplicationDriver_Fragment.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    FCk_HandleReplicator::
+    NetSerialize(
+        FArchive& Ar,
+        UPackageMap* Map,
+        bool& bOutSuccess)
+    -> bool
+{
+    auto RepBits = uint16{0};
+    if (Ar.IsSaving())
+    {
+        if (ck::IsValid(_Handle))
+        {
+            UCk_Utils_Ecs_Net_UE::TryUpdateReplicatedFragment<UCk_Fragment_EntityReplicationDriver_Rep>(_Handle,
+            [&](UCk_Fragment_EntityReplicationDriver_Rep* InRepDriver)
+            {
+                Ar << InRepDriver;
+            });
+        }
+    }
+
+    if (Ar.IsLoading())
+    {
+        Ar << _Handle_RepObj;
+        if (ck::IsValid(_Handle_RepObj))
+        {
+            _Handle = _Handle_RepObj->Get_AssociatedEntity();
+        }
+    }
+
+    bOutSuccess = true;
+    return true;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
--- Source/CkNet/Public/CkNet/EntityReplicationDriver/CkEntityReplicationDriver_Fragment_Data.h
+++ Source/CkNet/Public/CkNet/EntityReplicationDriver/CkEntityReplicationDriver_Fragment_Data.h
@@ -9,6 +9,48 @@
 
 // --------------------------------------------------------------------------------------------------------------------
 
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType, meta = (
+    HasNativeMake = "/Script/CkNet.Ck_Utils_EntityReplicationDriver_UE:Make_HandleReplicator",
+    HasNativeBreak = "/Script/CkNet.Ck_Utils_EntityReplicationDriver_UE:Break_HandleReplicator"))
+struct CKNET_API FCk_HandleReplicator
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_HandleReplicator);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, NotReplicated,
+        meta=(AllowPrivateAccess))
+    FCk_Handle _Handle;
+    UPROPERTY()
+    TWeakObjectPtr<class UCk_Fragment_EntityReplicationDriver_Rep> _Handle_RepObj;
+
+public:
+    auto
+    NetSerialize(
+        FArchive& Ar,
+        class UPackageMap* Map,
+        bool& bOutSuccess) -> bool;
+
+public:
+    CK_PROPERTY_GET(_Handle);
+    CK_PROPERTY_GET(_Handle_RepObj);
+
+    CK_DEFINE_CONSTRUCTORS(FCk_HandleReplicator, _Handle, _Handle_RepObj);
+};
+
+template<>
+struct TStructOpsTypeTraits<FCk_HandleReplicator> : public TStructOpsTypeTraitsBase2<FCk_HandleReplicator>
+{
+    enum
+    { WithNetSerializer = true };
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
 USTRUCT(BlueprintType)
 struct CKNET_API FCk_EntityReplicationDriver_ConstructionInfo_ReplicatedActor
 {
