c12d5c59be54d989ad77aa5d3dcf904187ecf3b8
Sulfur-CK <sulfur@chainkemists.com>
2024-02-25 20:46:01 -0800

    feat: added the EntityExtensions module

---
+++ Source/CkEntityExtension/CkEntityExtension.cpp
@@ -0,0 +1,15 @@
+#include "CkEntityExtension.h"
+
+#define LOCTEXT_NAMESPACE "FCkEntityExtensionModule"
+
+void FCkEntityExtensionModule::StartupModule()
+{
+}
+
+void FCkEntityExtensionModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkEntityExtensionModule, CkEntityExtension)
---
+++ Source/CkEntityExtension/CkEntityExtension_Log.cpp
@@ -0,0 +1,12 @@
+#include "CkEntityExtension_Log.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+DEFINE_LOG_CATEGORY(CkEntityExtension);
+
+namespace ck::entity_extension
+{
+	CK_REGISTER_LOG_FUNCTIONS(CkEntityExtension);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
