36dbd4debc794a8db7f68b852722dae393ea49df
Sulfur-CK <sulfur@chainkemists.com>
2024-02-25 20:51:32 -0800

    feat: added the completed first iteration of EntityExtension Fragment and Utilities

---
+++ Source/CkEntityExtension/Public/CkEntityExtension/ProcessorInjector/CkEntityExtension_ProcessorInjector.cpp
@@ -0,0 +1,16 @@
+#include "CkEntityExtension_ProcessorInjector.h"
+
+#include "CkEntityExtension/CkEntityExtension_Processor.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_EntityExtension_ProcessorInjector_UE::
+    DoInjectProcessors(
+        EcsWorldType& InWorld)
+        -> void
+{
+    // no processors yet
+}
+
+// --------------------------------------------------------------------------------------------------------------------
