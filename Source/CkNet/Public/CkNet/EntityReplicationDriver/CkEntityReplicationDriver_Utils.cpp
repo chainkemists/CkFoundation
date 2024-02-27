763a4fb667b56ef869425e76517a7eeaf167969f
Sulfur-CK <sulfur@chainkemists.com>
2024-02-26 19:08:28 -0800

    fix: fixed issue where the Server would not receive the optional payload when building a replicated Entity

--- Source/CkNet/Public/CkNet/EntityReplicationDriver/CkEntityReplicationDriver_Utils.cpp
+++ Source/CkNet/Public/CkNet/EntityReplicationDriver/CkEntityReplicationDriver_Utils.cpp
@@ -82,7 +82,8 @@ auto
     if (Add(NewEntity) == ECk_AddedOrNot::NotAdded)
     { return {}; }
 
-    InConstructionInfo.Get_ConstructionScript()->GetDefaultObject<UCk_Entity_ConstructionScript_PDA>()->Construct(NewEntity, {});
+    InConstructionInfo.Get_ConstructionScript()->GetDefaultObject<UCk_Entity_ConstructionScript_PDA>()->Construct(
+        NewEntity, InConstructionInfo.Get_OptionalParams());
 
     switch(const auto NetMode = UCk_Utils_Net_UE::Get_EntityNetMode(InHandle))
     {
