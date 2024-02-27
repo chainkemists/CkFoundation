98851d116967f801330d46a8eeb74f461f524c86
Sulfur-CK <sulfur@chainkemists.com>
2024-02-26 23:09:41 -0800

    fix: it is now valid to get an Entity from an Actor that is pending kill
    notes: this is to help with teardown of Entities where it's possible we looking for an Entity from an Actor that is yet to be fully destroyed by the engine

--- Source/CkEcs/Public/CkEcs/OwningActor/CkOwningActor_Utils.cpp
+++ Source/CkEcs/Public/CkEcs/OwningActor/CkOwningActor_Utils.cpp
@@ -88,7 +88,8 @@ auto
         const AActor* InActor)
     -> FCk_Handle
 {
-    CK_ENSURE_IF_NOT(ck::IsValid(InActor), TEXT("Cannot get the ECS Handle of Actor because the Actor is invalid!"))
+    CK_ENSURE_IF_NOT(ck::IsValid(InActor, ck::IsValid_Policy_IncludePendingKill{}),
+        TEXT("Cannot get the ECS Handle of Actor because the Actor is invalid!"))
     { return {}; }
 
     const auto& EntityOwningActorComp = InActor->FindComponentByClass<UCk_EntityOwningActor_ActorComponent_UE>();
