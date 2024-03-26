cd50a658f637b317bfb61ed37d5040b6b3fdd081
Sulfur-CK <sulfur@chainkemists.com>
2024-03-26 01:18:50 -0700

    feat: added the DataView module for the CkFoundation plugin

---
+++ Source/CkDataViewer/CkDataViewer.cpp
@@ -0,0 +1,15 @@
+#include "CkDataViewer.h"
+
+#define LOCTEXT_NAMESPACE "FCkDataViewerModule"
+
+void FCkDataViewerModule::StartupModule()
+{
+}
+
+void FCkDataViewerModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkDataViewerModule, CkDataViewer)
