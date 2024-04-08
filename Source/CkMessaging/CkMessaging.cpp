cb790d97f1e775390e0a6c66f8917165fa996de7
Sulfur-CK <sulfur@chainkemists.com>
2024-04-07 23:07:15 -0700

    feat: added the CkMessaging module

---
+++ Source/CkMessaging/CkMessaging.cpp
@@ -0,0 +1,15 @@
+#include "CkMessaging.h"
+
+#define LOCTEXT_NAMESPACE "FCkMessagingModule"
+
+void FCkMessagingModule::StartupModule()
+{
+}
+
+void FCkMessagingModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkMessagingModule, CkMessaging)
