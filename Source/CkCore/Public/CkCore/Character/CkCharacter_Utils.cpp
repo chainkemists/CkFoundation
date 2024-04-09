5e97c016c9f11bf2dd908374ea5203cf4f1419e2
Nitrogen-CK <nitrogen@chainkemists.com>
2024-04-09 14:51:38 -0400

    feat: Added new Character utils with conveniance functions to factor in the Character/Capsule Half-Height

---
+++ Source/CkCore/Public/CkCore/Character/CkCharacter_Utils.cpp
@@ -0,0 +1,61 @@
+#include "CkCharacter_Utils.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Utils_Character_UE::
+    Add_CharacterHalfHeightToLocation(
+        ACharacter* InCharacter,
+        FVector InLocation)
+    -> FVector
+{
+    CK_ENSURE_IF_NOT(ck::IsValid(InCharacter),
+        TEXT("Invalid Character supplied to UCk_Utils_Character_UE::Add_CharacterHalfHeightToLocation"))
+    { return InLocation; }
+
+    return Add_CapsuleHalfHeightToLocation(InCharacter->GetCapsuleComponent(), InLocation);
+}
+
+auto
+    UCk_Utils_Character_UE::
+    Subtract_CharacterHalfHeightFromLocation(
+        ACharacter* InCharacter,
+        FVector InLocation)
+    -> FVector
+{
+    CK_ENSURE_IF_NOT(ck::IsValid(InCharacter),
+        TEXT("Invalid Character supplied to UCk_Utils_Character_UE::Subtract_CharacterHalfHeightFromLocation"))
+    { return InLocation; }
+
+    return Subtract_CapsuleHalfHeightFromLocation(InCharacter->GetCapsuleComponent(), InLocation);
+}
+
+auto
+    UCk_Utils_Character_UE::
+    Add_CapsuleHalfHeightToLocation(
+        UCapsuleComponent* InCapsule,
+        FVector InLocation)
+    -> FVector
+{
+    CK_ENSURE_IF_NOT(ck::IsValid(InCapsule),
+        TEXT("Invalid Capsule supplied to UCk_Utils_Character_UE::Add_CapsuleHalfHeightToLocation"))
+    { return InLocation; }
+
+    return FVector{ InLocation.X, InLocation.Y, InLocation.Z + InCapsule->GetScaledCapsuleHalfHeight() };
+}
+
+auto
+    UCk_Utils_Character_UE::
+    Subtract_CapsuleHalfHeightFromLocation(
+        UCapsuleComponent* InCapsule,
+        FVector InLocation)
+    -> FVector
+{
+    CK_ENSURE_IF_NOT(ck::IsValid(InCapsule),
+        TEXT("Invalid Capsule supplied to UCk_Utils_Character_UE::Subtract_CapsuleHalfHeightFromLocation"))
+    { return InLocation; }
+
+    return FVector{ InLocation.X, InLocation.Y, InLocation.Z - InCapsule->GetScaledCapsuleHalfHeight() };
+}
+
+// --------------------------------------------------------------------------------------------------------------------
---
+++ Source/CkCore/Public/CkCore/Character/CkCharacter_Utils.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "CkCore/Format/CkFormat.h"
+#include "CkCore/Ensure/CkEnsure.h"
+
+#include <GameFramework/Character.h>
+#include <Components/CapsuleComponent.h>
+
+#include "CkCharacter_Utils.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable)
+class CKCORE_API UCk_Utils_Character_UE : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_Character_UE);
+
+public:
+    UFUNCTION(BlueprintPure,
+              DisplayName = "[Ck] Add Character Half-Height To Location",
+              Category = "Ck|Utils|Character")
+    static FVector
+    Add_CharacterHalfHeightToLocation(
+        ACharacter* InCharacter,
+        FVector InLocation);
+
+    UFUNCTION(BlueprintPure,
+              DisplayName = "[Ck] Subtract Character Half-Height From Location",
+              Category = "Ck|Utils|Character")
+    static FVector
+    Subtract_CharacterHalfHeightFromLocation(
+        ACharacter* InCharacter,
+        FVector InLocation);
+
+    UFUNCTION(BlueprintPure,
+              DisplayName = "[Ck] Add Capsule Half-Height To Location",
+              Category = "Ck|Utils|Character")
+    static FVector
+    Add_CapsuleHalfHeightToLocation(
+        UCapsuleComponent* InCapsule,
+        FVector InLocation);
+
+    UFUNCTION(BlueprintPure,
+              DisplayName = "[Ck] Subtract Capsule Half-Height From Location",
+              Category = "Ck|Utils|Character")
+    static FVector
+    Subtract_CapsuleHalfHeightFromLocation(
+        UCapsuleComponent* InCapsule,
+        FVector InLocation);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
