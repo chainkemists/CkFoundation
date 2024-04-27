8ab7e0898ffa9503482906df48f16f656f3afdf0
sulfur-ck <sulfur@chainkemists.com>
2024-04-26 21:11:53 -0700

    feat: added OnPlayerAdded/Removed Blueprint callbacks in Gamestate using the C++ only function overrides

--- Source/CkCore/Public/CkCore/Engine/CkGameState.h
+++ Source/CkCore/Public/CkCore/Engine/CkGameState.h
@@ -15,6 +15,25 @@ class CKCORE_API ACk_GameState_UE : public AGameStateBase
 
 public:
     CK_GENERATED_BODY(ACk_GameState_UE);
+
+protected:
+    UFUNCTION(BlueprintImplementableEvent)
+    void
+    OnPlayerAdded(
+        APlayerState* InPlayerState);
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void
+    OnPlayerRemoved(
+        APlayerState* InPlayerState);
+
+protected:
+    auto
+    AddPlayerState(
+        APlayerState* InPlayerState) -> void override;
+
+    auto
+    RemovePlayerState(APlayerState* InPlayerState) -> void override;
 };
 
 // --------------------------------------------------------------------------------------------------------------------
