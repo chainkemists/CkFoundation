4b98383118e6b854c21dc71bdbec3b40d1072fd2
Nitrogen-CK <nitrogen@chainkemists.com>
2024-04-29 01:13:00 -0400

    feat: UCk_Utils_Curve_UE now has overloads for working with FSimpleCurve in addition to FRichCurve

--- Source/CkCore/Public/CkCore/Curve/CkCurve_Utils.cpp
+++ Source/CkCore/Public/CkCore/Curve/CkCurve_Utils.cpp
@@ -16,6 +16,30 @@ auto
 
 auto
     UCk_Utils_Curve_UE::
+    Get_ValueAtTime(
+        const FSimpleCurve& InSimpleCurve,
+        const FCk_Time& InTime)
+    -> float
+{
+    const auto& Seconds = InTime.Get_Seconds();
+    return InSimpleCurve.Eval(Seconds);
+}
+
+auto
+    UCk_Utils_Curve_UE::
+    Get_TimeRange(
+        const FSimpleCurve& InSimpleCurve)
+    -> FCk_FloatRange
+{
+    auto OutMinTime = 0.0f;
+    auto OutMaxTime = 0.0f;
+    InSimpleCurve.GetTimeRange(OutMinTime, OutMaxTime);
+
+    return FCk_FloatRange{ OutMinTime, OutMaxTime };
+}
+
+auto
+    UCk_Utils_Curve_UE::
     Get_TimeRange(
         const FRichCurve& InRichCurve)
     -> FCk_FloatRange
@@ -36,6 +60,19 @@ auto
     auto OutMinValue = 0.0f;
     auto OutMaxValue = 0.0f;
     InRichCurve.GetValueRange(OutMinValue, OutMaxValue);
+
+    return FCk_FloatRange{ OutMinValue, OutMaxValue };
+}
+
+auto
+    UCk_Utils_Curve_UE::
+    Get_ValueRange(
+        const FSimpleCurve& InSimpleCurve)
+    -> FCk_FloatRange
+{
+    auto OutMinValue = 0.0f;
+    auto OutMaxValue = 0.0f;
+    InSimpleCurve.GetValueRange(OutMinValue, OutMaxValue);
 
     return FCk_FloatRange{ OutMinValue, OutMaxValue };
 }
