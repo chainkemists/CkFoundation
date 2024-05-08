dfa9a52e00f06f307c82630299a57623a5a0e53e
Sulfur-CK <sulfur@chainkemists.com>
2024-05-08 02:32:03 -0700

    feat: added Request Base which all Request structs should inherit from to gain access to the Request Entity
    notes: the Request Entity is an intermediate Entity that is created for each unique Request. It helps bind the incoming delegate (if any) to the Request's signal which is then fired. The interface needs work as currently, there are too many steps involved preparing and firing signals through the Request Entity

---
+++ Source/CkEcs/Public/CkEcs/Request/CkRequest_Data.cpp
@@ -0,0 +1,45 @@
+#include "CkRequest_Data.h"
+
+#include "CkEntityLifetime_Utils.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    FCk_Request_Base::
+    PopulateRequestHandle(
+        FCk_Handle InOwner) const
+    -> FCk_Handle
+{
+    CK_ENSURE_IF_NOT(ck::Is_NOT_Valid(_RequestHandle),
+        TEXT("Cannot Populate RequestHandle with requested Owner [{}]. RequestHandle [{}] is VALID and already populated with Owner [{}]."),
+        InOwner, _RequestHandle, UCk_Utils_EntityLifetime_UE::Get_LifetimeOwner(_RequestHandle))
+    { return {}; }
+
+    _RequestHandle = UCk_Utils_EntityLifetime_UE::Request_CreateEntity(InOwner);
+
+    return _RequestHandle;
+}
+
+auto
+    FCk_Request_Base::
+    GetAndDestroyRequestHandle() const
+    -> FCk_Handle
+{
+    CK_ENSURE_IF_NOT(ck::IsValid(_RequestHandle),
+        TEXT("Cannot GetAndDestroy Request Handle [{}] since it is INVALID"), _RequestHandle)
+    { return {}; }
+
+    UCk_Utils_EntityLifetime_UE::Request_DestroyEntity(_RequestHandle);
+
+    return _RequestHandle;
+}
+
+auto
+    FCk_Request_Base::
+    Get_IsRequestHandleValid() const
+    -> bool
+{
+    return ck::IsValid(_RequestHandle);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
---
+++ Source/CkEcs/Public/CkEcs/Request/CkRequest_Data.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "CkHandle.h"
+
+#include "CkCore/Macros/CkMacros.h"
+
+#include "CkRequest_Data.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKECS_API FCk_Request_Base
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Request_Base);
+
+private:
+    mutable FCk_Handle _RequestHandle;
+
+public:
+    auto
+    PopulateRequestHandle(
+        FCk_Handle InOwner) const -> FCk_Handle;
+
+    auto
+    GetAndDestroyRequestHandle() const -> FCk_Handle;
+
+    auto
+    Get_IsRequestHandleValid() const -> bool;
+
+public:
+    // CK_PROPERTY_GET(_RequestHandle);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
