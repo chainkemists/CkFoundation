47a25002a01dfbe96cfd88f4845f0f82ea7f105c
AlexObatake-CK <phosphorus@chainkemists.com>
2024-05-24 06:11:39 -0700

    fix: Add AI module, add EQS utils to add a helper missing from the base EQS implementation
    There is no BP helper to add a set EQS int

---
+++ Source/CkAi/Public/EQS/CkEqs_Utils.cpp
@@ -0,0 +1,20 @@
+﻿#include "CkEqs_Utils.h"
+
+#include "EnvironmentQuery/EnvQueryInstanceBlueprintWrapper.h"
+#include "EnvironmentQuery/EnvQueryTypes.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto 
+	UCk_Utils_Eqs_UE::
+	SetEqsNamedIntParam(
+		UEnvQueryInstanceBlueprintWrapper* EnvQueryInstance, 
+		FName	 ParamName,
+		int Value)
+	-> void
+{
+	if (!EnvQueryInstance)
+		return;
+	// read int bits as a float, this is jank but is what EQS expects, see FEnvQueryRequest::SetIntParam
+	EnvQueryInstance->SetNamedParam(ParamName, *((float*)&Value));
+}
---
+++ Source/CkAi/Public/EQS/CkEqs_Utils.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include <Kismet/BlueprintFunctionLibrary.h>
+#include <CoreMinimal.h>
+
+#include "CkMacros.h"
+#include "CkEqs_Utils.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+class UEnvQueryInstanceBlueprintWrapper;
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable)
+class CKAI_API UCk_Utils_Eqs_UE : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_Eqs_UE);
+
+public:
+    UFUNCTION(BlueprintCallable,
+              Category = "AI|EQS",
+              DisplayName = "Set EQS Named Integer Param")
+    static void
+    SetEqsNamedIntParam(
+        UEnvQueryInstanceBlueprintWrapper* EnvQueryInstance, 
+        FName ParamName,
+        int Value);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
