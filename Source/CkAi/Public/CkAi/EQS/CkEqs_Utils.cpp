2ad5216c033c9897acb789b685edcbab1ae3c73c
AlexObatake-CK <phosphorus@chainkemists.com>
2024-05-24 06:11:39 -0700

    fix: Add AI module, add EQS utils to add a helper missing from the base EQS implementation
    There is no BP helper to add a set EQS int

---
+++ Source/CkAi/Public/CkAi/EQS/CkEqs_Utils.cpp
@@ -0,0 +1,23 @@
+﻿#include "CkEqs_Utils.h"
+
+#include "CkCore/Validation/CkIsValid.h"
+#include <EnvironmentQuery/EnvQueryInstanceBlueprintWrapper.h>
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto 
+	UCk_Utils_Eqs_UE::
+	SetEqsNamedIntParam(
+		UEnvQueryInstanceBlueprintWrapper* InEnvQueryInstance, 
+		FName InParamName,
+		int32 InValue)
+	-> void
+{
+	if (ck::Is_NOT_Valid(InEnvQueryInstance))
+	{ return; }
+	
+	// read int bits as a float, this is jank but is what EQS expects, see FEnvQueryRequest::SetIntParam
+	InEnvQueryInstance->SetNamedParam(InParamName, *reinterpret_cast<float*>(&InValue));
+}
+
+// --------------------------------------------------------------------------------------------------------------------
