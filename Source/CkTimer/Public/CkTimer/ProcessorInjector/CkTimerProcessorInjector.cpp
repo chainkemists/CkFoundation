c106d92d6e15f2f07c4730d1e6357c3cd76e7676
Sulfur-CK <sulfur@chainkemists.com>
2024-05-27 15:20:10 -0700

    feat: Timer Processor HandleRequests injector split from update
    notes: Request processors can be pumped multiple times while Update processors cannot (should not)

--- Source/CkTimer/Public/CkTimer/ProcessorInjector/CkTimerProcessorInjector.cpp
+++ Source/CkTimer/Public/CkTimer/ProcessorInjector/CkTimerProcessorInjector.cpp
@@ -5,12 +5,20 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 auto
+    UCk_Timer_ProcessorInjector_Requests::
+    DoInjectProcessors(
+        EcsWorldType& InWorld)
+    -> void
+{
+    InWorld.Add<ck::FProcessor_Timer_HandleRequests>(InWorld.Get_Registry());
+}
+
+auto
     UCk_Timer_ProcessorInjector::
     DoInjectProcessors(
         EcsWorldType& InWorld)
         -> void
 {
-    InWorld.Add<ck::FProcessor_Timer_HandleRequests>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_Timer_Update>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_Timer_Update_Countdown>(InWorld.Get_Registry());
 }
