60da95ce6a0cd485cda03a7d58ba5bc03b13decc
Sulfur-CK <sulfur@chainkemists.com>
2024-05-27 15:20:10 -0700

    feat: Timer Processor HandleRequests injector split from update
    notes: Request processors can be pumped multiple times while Update processors cannot (should not)

--- Source/CkTimer/Public/CkTimer/ProcessorInjector/CkTimerProcessorInjector.h
+++ Source/CkTimer/Public/CkTimer/ProcessorInjector/CkTimerProcessorInjector.h
@@ -7,6 +7,19 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 UCLASS(NotBlueprintable, NotBlueprintType)
+class CKTIMER_API UCk_Timer_ProcessorInjector_Requests : public UCk_EcsWorld_ProcessorInjector_Base_UE
+{
+    GENERATED_BODY()
+
+protected:
+    auto
+    DoInjectProcessors(
+        EcsWorldType& InWorld) -> void override;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable, NotBlueprintType)
 class CKTIMER_API UCk_Timer_ProcessorInjector : public UCk_EcsWorld_ProcessorInjector_Base_UE
 {
     GENERATED_BODY()
