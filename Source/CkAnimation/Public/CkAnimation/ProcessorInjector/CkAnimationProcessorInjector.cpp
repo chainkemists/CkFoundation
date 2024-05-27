9b3ffd1ddb2742f98c9cfa84a584a15872fd913c
Sulfur-CK <sulfur@chainkemists.com>
2024-05-27 15:21:33 -0700

    feat: Animation Replicate Processor split from Handle Requests Processor

--- Source/CkAnimation/Public/CkAnimation/ProcessorInjector/CkAnimationProcessorInjector.cpp
+++ Source/CkAnimation/Public/CkAnimation/ProcessorInjector/CkAnimationProcessorInjector.cpp
@@ -11,7 +11,14 @@ auto
         -> void
 {
     InWorld.Add<ck::FProcessor_AnimState_HandleRequests>(InWorld.Get_Registry());
+}
 
+auto
+    UCk_Animation_ProcessorInjector_Replicate::
+    DoInjectProcessors(
+        EcsWorldType& InWorld)
+    -> void
+{
     InWorld.Add<ck::FProcessor_AnimState_Replicate>(InWorld.Get_Registry());
 }
 
