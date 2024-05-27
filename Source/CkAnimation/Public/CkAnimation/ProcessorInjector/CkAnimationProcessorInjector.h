86b278e73d44f6827f1ce92673f78604e414fa2e
Sulfur-CK <sulfur@chainkemists.com>
2024-05-27 15:21:33 -0700

    feat: Animation Replicate Processor split from Handle Requests Processor

--- Source/CkAnimation/Public/CkAnimation/ProcessorInjector/CkAnimationProcessorInjector.h
+++ Source/CkAnimation/Public/CkAnimation/ProcessorInjector/CkAnimationProcessorInjector.h
@@ -18,3 +18,16 @@ protected:
 };
 
 // --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable, NotBlueprintType)
+class CKANIMATION_API UCk_Animation_ProcessorInjector_Replicate : public UCk_EcsWorld_ProcessorInjector_Base_UE
+{
+    GENERATED_BODY()
+
+protected:
+    auto
+    DoInjectProcessors(
+        EcsWorldType& InWorld) -> void override;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
