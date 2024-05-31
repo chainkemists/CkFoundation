3ff60bc07ad5b7a144176d98cf511c4a3163643e
Sulfur-CK <sulfur@chainkemists.com>
2024-05-30 18:28:43 -0700

    feat: AbilityScript Not Activated debug is now gated behind a setting (disabled by default)

--- Source/CkAbility/Public/CkAbility/Settings/CkAbility_Settings.cpp
+++ Source/CkAbility/Public/CkAbility/Settings/CkAbility_Settings.cpp
@@ -20,4 +20,12 @@ auto
     return UCk_Utils_Object_UE::Get_ClassDefaultObject<UCk_Ability_ProjectSettings_UE>()->Get_CueTypes();
 }
 
+auto
+    UCk_Utils_Ability_Settings_UE::
+    Get_AbilityNotActivatedDebug()
+    -> const ECk_EnableDisable
+{
+    return UCk_Utils_Object_UE::Get_ClassDefaultObject<UCk_Ability_ProjectSettings_UE>()->Get_AbilityNotActivatedDebug();
+}
+
 // --------------------------------------------------------------------------------------------------------------------
