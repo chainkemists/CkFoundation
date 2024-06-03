ff52c755d3d40fb8d735736575e9f9d10033fb5b
Sulfur-CK <sulfur@chainkemists.com>
2024-06-02 21:39:56 -0700

    feat: added utils to create an Entity with Transform called the 'TargetPoint' Entity
    notes: this is useful for situations where we want to pass a Target Entity but sometimes the Target is just a static Transform

---
+++ Source/CkTargeting/Public/TargetPoint/CkTargetPoint_Utils.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "CkCore/Macros/CkMacros.h"
+
+#include "CkTargetPoint_Utils.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS()
+class CKTARGETING_API UCk_Utils_TargetPoint_UE : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_TargetPoint_UE);
+
+public:
+    // The TargetPoint is destroyed when the Owner is destroyed
+    UFUNCTION(BlueprintCallable,
+              Category = "Ck|Utils|TargetPoint",
+              DisplayName="[Ck][TargetPoint] Create New TargetPoint")
+    static FCk_Handle_Transform
+    Create(
+        const FCk_Handle& InOwner,
+        const FTransform& InTransform);
+
+    // Transient means that the onus of destroying the TargetPoint is now on the user
+    UFUNCTION(BlueprintCallable,
+              Category = "Ck|Utils|TargetPoint",
+              DisplayName="[Ck][TargetPoint] Create New TargetPoint (Transient)",
+              meta = (WorldContext="InWorldContextObject"))
+    static FCk_Handle_Transform
+    Create_Transient(
+        const FTransform& InTransform,
+        const UObject* InWorldContextObject);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
