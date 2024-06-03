296db2d1e4ab03d4be89fea4ceb94538aee99ee7
Nitrogen-CK <nitrogen@chainkemists.com>
2024-06-03 19:01:01 -0400

    feat: Split up the Targeting processor injectors so that the HandleRequests processors can be "pumped"

--- Source/CkTargeting/Public/ProcessorInjector/CkTargeting_ProcessorInjector.h
+++ Source/CkTargeting/Public/ProcessorInjector/CkTargeting_ProcessorInjector.h
@@ -18,3 +18,16 @@ protected:
 };
 
 // --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable, NotBlueprintType)
+class CKTARGETING_API UCk_Targeting_ProcessorInjector_Requests_UE : public UCk_EcsWorld_ProcessorInjector_Base_UE
+{
+    GENERATED_BODY()
+
+protected:
+    auto
+    DoInjectProcessors(
+        EcsWorldType& InWorld) -> void override;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
