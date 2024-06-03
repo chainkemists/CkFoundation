696de0f4c6c7ca319f7eb098af0c69f28b59ce37
Nitrogen-CK <nitrogen@chainkemists.com>
2024-06-03 19:01:01 -0400

    feat: Split up the Targeting processor injectors so that the HandleRequests processors can be "pumped"

--- Source/CkTargeting/Public/ProcessorInjector/CkTargeting_ProcessorInjector.cpp
+++ Source/CkTargeting/Public/ProcessorInjector/CkTargeting_ProcessorInjector.cpp
@@ -9,14 +9,24 @@ auto
     UCk_Targeting_ProcessorInjector_UE::
     DoInjectProcessors(
         EcsWorldType& InWorld)
-        -> void
+    -> void
 {
     InWorld.Add<ck::FProcessor_Targetable_Setup>(InWorld.Get_Registry());
-    InWorld.Add<ck::FProcessor_Targetable_HandleRequests>(InWorld.Get_Registry());
 
     InWorld.Add<ck::FProcessor_Targeter_Update>(InWorld.Get_Registry());
-    InWorld.Add<ck::FProcessor_Targeter_HandleRequests>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_Targeter_Cleanup>(InWorld.Get_Registry());
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Targeting_ProcessorInjector_Requests_UE::
+    DoInjectProcessors(
+        EcsWorldType& InWorld)
+    -> void
+{
+    InWorld.Add<ck::FProcessor_Targetable_HandleRequests>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_Targeter_HandleRequests>(InWorld.Get_Registry());
 }
 
 // --------------------------------------------------------------------------------------------------------------------
