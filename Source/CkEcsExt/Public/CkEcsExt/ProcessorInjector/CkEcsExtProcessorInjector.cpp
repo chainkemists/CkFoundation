02009b19f5eac4d1748235fc41a74c92d4921839
Nitrogen-CK <nitrogen@chainkemists.com>
2024-06-04 19:29:42 -0400

    feat: Extracted EcsExt HandleRequests processors into their own procerssor injector

--- Source/CkEcsExt/Public/CkEcsExt/ProcessorInjector/CkEcsExtProcessorInjector.cpp
+++ Source/CkEcsExt/Public/CkEcsExt/ProcessorInjector/CkEcsExtProcessorInjector.cpp
@@ -8,17 +8,27 @@ auto
     UCk_EcsExt_ProcessorInjector::
     DoInjectProcessors(
         EcsWorldType& InWorld)
-        -> void
+    -> void
 {
     InWorld.Add<ck::FProcessor_Transform_Setup>(InWorld.Get_Registry());
-    InWorld.Add<ck::FProcessor_Transform_InterpolateToGoal_Location>(InWorld.Get_Registry());
-    InWorld.Add<ck::FProcessor_Transform_InterpolateToGoal_Rotation>(InWorld.Get_Registry());
-    InWorld.Add<ck::FProcessor_Transform_HandleRequests>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_Transform_Update>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_Transform_Actor>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_Transform_FireSignals>(InWorld.Get_Registry());
 
-
     InWorld.Add<ck::FProcessor_Transform_Replicate>(InWorld.Get_Registry());
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_EcsExt_ProcessorInjector_Requests_UE::
+    DoInjectProcessors(
+        EcsWorldType& InWorld)
+    -> void
+{
+    InWorld.Add<ck::FProcessor_Transform_HandleRequests>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_Transform_InterpolateToGoal_Location>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_Transform_InterpolateToGoal_Rotation>(InWorld.Get_Registry());
 }
 
 // --------------------------------------------------------------------------------------------------------------------
