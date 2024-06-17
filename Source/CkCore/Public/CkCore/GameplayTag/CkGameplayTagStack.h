be2d473901b287f955591496fce6df6bb05a6009
Nitrogen-CK <nitrogen@chainkemists.com>
2024-06-17 18:12:14 -0400

    feat: Added GameplayTagStackContainer (inspired from Lyra implementation)

---
+++ Source/CkCore/Public/CkCore/GameplayTag/CkGameplayTagStack.h
@@ -0,0 +1,181 @@
+#pragma once
+
+#include "CkCore/Enums/CkEnums.h"
+#include "CkCore/Macros/CkMacros.h"
+
+#include <GameplayTagContainer.h>
+#include <Net/Serialization/FastArraySerializer.h>
+
+#include "CkGameplayTagStack.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+struct FCk_GameplayTagStackContainer;
+struct FNetDeltaSerializeInfo;
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct FCk_GameplayTagStack : public FFastArraySerializerItem
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_GameplayTagStack);
+
+public:
+    friend FCk_GameplayTagStackContainer;
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+        meta = (AllowPrivateAccess = true))
+    FGameplayTag _Tag;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+        meta = (AllowPrivateAccess = true,  UIMin = 0, ClampMin = 0))
+    int32 _StackCount = 0;
+
+public:
+    CK_PROPERTY_GET(_Tag);
+    CK_PROPERTY_GET(_StackCount);
+
+public:
+    CK_DEFINE_CONSTRUCTORS(FCk_GameplayTagStack, _Tag, _StackCount);
+};
+
+CK_DEFINE_CUSTOM_FORMATTER(FCk_GameplayTagStack, [&]()
+{
+    return ck::Format
+    (
+        TEXT("{}x{}"),
+        InObj.Get_Tag(),
+        InObj.Get_StackCount()
+    );
+});
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType,
+    meta = (HasNativeMake = "/Script/CkCore.Ck_Utils_GameplayTagStack_UE:Make_GameplayTagStackContainer",
+            HasNativeBreak = "/Script/CkCore.Ck_Utils_GameplayTagStack_UE:Break_GameplayTagStackContainer"))
+struct FCk_GameplayTagStackContainer : public FFastArraySerializer
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_GameplayTagStackContainer);
+
+public:
+    using GameplayTagStackType = FCk_GameplayTagStack;
+
+public:
+    auto
+    AddStack(
+        const FGameplayTag& InTag,
+        int32 InStackCount) -> FCk_GameplayTagStackContainer&;
+
+    auto
+    AddStack(
+        const FCk_GameplayTagStack& InStack) -> FCk_GameplayTagStackContainer&;
+
+    auto
+    RemoveStack(
+        const FGameplayTag& InTag,
+        int32 InStackCount) -> FCk_GameplayTagStackContainer&;
+
+    auto
+    RemoveStack(
+        const FCk_GameplayTagStack& InStack) -> FCk_GameplayTagStackContainer&;
+
+    [[nodiscard]]
+    auto
+    Get_StackCount(
+        const FGameplayTag& InTag) const -> int32;
+
+    [[nodiscard]]
+    auto
+    Get_ContainsTag(
+        const FGameplayTag& InTag) const -> bool;
+
+public:
+    auto
+    PreReplicatedRemove(
+        const TArrayView<int32> InRemovedIndices,
+        int32 InFinalSize) -> void;
+
+    auto
+    PostReplicatedAdd(
+        const TArrayView<int32> InAddedIndices,
+        int32 InFinalSize) -> void;
+
+    auto
+    PostReplicatedChange(
+        const TArrayView<int32> InChangedIndices,
+        int32 InFinalSize) -> void;
+
+    auto
+    NetDeltaSerialize(
+        FNetDeltaSerializeInfo& InDeltaParams) -> bool;
+
+private:
+    UPROPERTY()
+    TArray<FCk_GameplayTagStack> _Stacks;
+
+private:
+    TMap<FGameplayTag, int32> _TagToCountMap;
+
+public:
+    CK_PROPERTY_GET(_Stacks);
+    CK_PROPERTY_GET(_TagToCountMap);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+template<>
+struct TStructOpsTypeTraits<FCk_GameplayTagStackContainer> : public TStructOpsTypeTraitsBase2<FCk_GameplayTagStackContainer>
+{
+    enum
+    {
+        WithNetDeltaSerializer = true,
+    };
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+// Algos
+
+namespace ck::algo
+{
+    struct CKCORE_API MatchesGameplayTagStackNameExact
+    {
+    public:
+        auto operator()(const FCk_GameplayTagStack& InStack) const -> bool
+        {
+            return InStack.Get_Tag().MatchesTagExact(_Name);
+        }
+
+    private:
+        FGameplayTag _Name;
+
+    public:
+        CK_DEFINE_CONSTRUCTORS(MatchesGameplayTagStackNameExact, _Name)
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    struct CKCORE_API MatchesGameplayTagStackName
+    {
+    public:
+        auto operator()(const FCk_GameplayTagStack& InStack) const -> bool
+        {
+            return InStack.Get_Tag().MatchesTag(_Name);
+        }
+
+    private:
+        FGameplayTag _Name;
+
+    public:
+        CK_DEFINE_CONSTRUCTORS(MatchesGameplayTagStackName, _Name)
+    };
+}
+
+// --------------------------------------------------------------------------------------------------------------------
--- Source/CkCore/Public/CkCore/GameplayTag/CkGameplayTag_Utils.h
+++ Source/CkCore/Public/CkCore/GameplayTag/CkGameplayTag_Utils.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "CkCore/GameplayTag/CkGameplayTagStack.h"
 #include "CkCore/Enums/CkEnums.h"
 #include "CkCore/Macros/CkMacros.h"
 
@@ -20,7 +21,7 @@ public:
 
 public:
     UFUNCTION(BlueprintPure,
-              Category = "Ck|GameplayTag|Utils",
+              Category = "Ck|Utils|GameplayTag",
               DisplayName = "[Ck] Get Are Tag Requirements Met")
     static bool
     Get_AreTagRequirementsMet(
@@ -28,29 +29,29 @@ public:
         const FGameplayTagContainer& InTagsToTest);
 
     UFUNCTION(BlueprintPure,
-              Category = "Ck|GameplayTag|Utils",
-              DisplayName = "[Ck] Make Literal Gameplay Tag (From String)")
+              Category = "Ck|Utils|GameplayTag",
+              DisplayName = "[Ck] Make Literal GameplayTag (From String)")
     static FGameplayTag
     Make_LiteralGameplayTag_FromString(
         const FString& InTagNameAsString);
 
     UFUNCTION(BlueprintPure,
-              Category = "Ck|GameplayTag|Utils",
-              DisplayName = "[Ck] Try Make Literal Gameplay Tag (From String)")
+              Category = "Ck|Utils|GameplayTag",
+              DisplayName = "[Ck] Try Make Literal GameplayTag (From String)")
     static FGameplayTag
     TryMake_LiteralGameplayTag_FromString(
         const FString& InTagNameAsString);
 
     UFUNCTION(BlueprintPure,
-              Category = "Ck|GameplayTag|Utils",
-              DisplayName = "[Ck] Make Literal Gameplay Tag (From Name)")
+              Category = "Ck|Utils|GameplayTag",
+              DisplayName = "[Ck] Make Literal GameplayTag (From Name)")
     static FGameplayTag
     Make_LiteralGameplayTag_FromName(
         FName InTagNameAsString);
 
     UFUNCTION(BlueprintPure,
-              Category = "Ck|GameplayTag|Utils",
-              DisplayName = "[Ck] Try Make Literal Gameplay Tag (From Name)")
+              Category = "Ck|Utils|GameplayTag",
+              DisplayName = "[Ck] Try Make Literal GameplayTag (From Name)")
     static FGameplayTag
     TryMake_LiteralGameplayTag_FromName(
         FName InTagNameAsString);
@@ -59,6 +60,75 @@ public:
     static auto
     Get_TagsWithCount_FromContainer(
         const FGameplayTagCountContainer& InTagContainer) -> TMap<FGameplayTag, int32>;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+
+UCLASS(NotBlueprintable)
+class CKCORE_API UCk_Utils_GameplayTagStack_UE : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_GameplayTagStack_UE);
+
+private:
+    UFUNCTION(BlueprintCallable,
+              Category = "Ck|Utils|GameplayTagStack",
+              DisplayName = "[Ck] Add Stack to GameplayTagStackContainer")
+    void
+    Request_AddStack(
+        UPARAM(ref) FCk_GameplayTagStackContainer& InStackContainer,
+        const FCk_GameplayTagStack& InStack);
+
+    UFUNCTION(BlueprintCallable,
+              Category = "Ck|Utils|GameplayTagStack",
+              DisplayName = "[Ck] Remove Stack from GameplayTagStackContainer")
+    void
+    Request_RemoveStack(
+        UPARAM(ref) FCk_GameplayTagStackContainer& InStackContainer,
+        const FCk_GameplayTagStack& InStack);
+
+    UFUNCTION(BlueprintCallable,
+              Category = "Ck|Utils|GameplayTagStack",
+              DisplayName = "[Ck] Get Contains Tag in GameplayTagStackContainer")
+    bool
+    Get_ContainsTag(
+        const FCk_GameplayTagStackContainer& InStackContainer,
+        const FGameplayTag& InTag);
+
+    UFUNCTION(BlueprintCallable,
+              Category = "Ck|Utils|GameplayTagStack",
+              DisplayName = "[Ck] Get Stack Count in GameplayTagStackContainer")
+    int32
+    Get_StackCount(
+        const FCk_GameplayTagStackContainer& InStackContainer,
+        const FGameplayTag& InTag);
+
+private:
+    UFUNCTION(BlueprintPure,
+              Category = "Ck|Utils|GameplayTagStack",
+              DisplayName = "[Ck] Make GameplayTagStackContainer",
+              meta = (NativeMakeFunc))
+    static FCk_GameplayTagStackContainer
+    Make_GameplayTagStackContainer(
+        const TArray<FCk_GameplayTagStack>& InStacks);
+
+    UFUNCTION(BlueprintPure,
+              Category = "Ck|Utils|GameplayTagStack",
+              DisplayName = "[Ck] Break GameplayTagStackContainer",
+              meta = (NativeBreakFunc))
+    static TArray<FCk_GameplayTagStack>
+    Break_GameplayTagStackContainer(
+        const FCk_GameplayTagStackContainer& InStackContainer);
+
+    UFUNCTION(BlueprintPure,
+              Category = "Ck|Utils|GameplayTagStack",
+              DisplayName = "[Ck] Break GameplayTagStackContainer (As Map)")
+    static TMap<FGameplayTag, int32>
+    Break_GameplayTagStackContainer_AsMap(
+        const FCk_GameplayTagStackContainer& InStackContainer);
 };
 
 // --------------------------------------------------------------------------------------------------------------------
