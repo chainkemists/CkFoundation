e3f43c665e8a043f2576d089a8705478547181b9
Sulfur-CK <sulfur@chainkemists.com>
2024-06-19 19:18:05 -0700

    fix: fixed issue where multiple Overrides in the same frame resulted in unexpected base value results
    notes: overrides by definition need to stomp any other override that may currently be active and not yet resolved. This was not happening previously resulting in multiple override modifier deltas stacking up resulting in an incorrect overriden value. We now hold the override in the owning Entity and destroy it if a new override is added

--- Source/CkAttribute/Public/CkAttribute/CkAttribute_Utils.inl.h
+++ Source/CkAttribute/Public/CkAttribute/CkAttribute_Utils.inl.h
@@ -184,7 +184,21 @@ namespace ck
         {
             case ECk_ModifierOperation_RevocablePolicy::NotRevocable:
             {
+                if (Utils_ExistingOverrideModifierEntity::Has(LifetimeOwnerAsAttributeEntity))
+                {
+                    if (auto MaybeExistingModifier = Utils_ExistingOverrideModifierEntity::Get_StoredEntity(LifetimeOwnerAsAttributeEntity);
+                        ck::IsValid(MaybeExistingModifier))
+                    {
+                        MaybeExistingModifier.template Try_Remove<typename AttributeModifierFragmentType::FTag_ComputeResult>();
+                        UCk_Utils_EntityLifetime_UE::Request_DestroyEntity(MaybeExistingModifier);
+                    }
+
+                    LifetimeOwnerAsAttributeEntity.template Remove<FExistingOverrideModifierEntity>();
+                }
+
+                Utils_ExistingOverrideModifierEntity::Add(LifetimeOwnerAsAttributeEntity, InHandle);
                 InHandle.template Add<typename AttributeModifierFragmentType::FTag_IsNotRevocableModification>();
+
                 Request_ComputeResult(InHandle);
 
                 break;
