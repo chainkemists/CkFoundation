5a594abcb2b8bc8b5c1d0718562da847fba7578c
AlexObatake-CK <phosphorus@chainkemists.com>
2024-07-03 08:51:16 -0700

    fix: Cancelled by tags on self now only apply to self, and not tags on ability owner
    *  Previously the check on self and owner was the same, and would combine cancelled tags for self and owner
    *  Now there are two separate functors for self and owner which are used at the correct time in the ability owner processor

--- Source/CkAbility/Public/CkAbility/Ability/CkAbility_Fragment.cpp
+++ Source/CkAbility/Public/CkAbility/Ability/CkAbility_Fragment.cpp
@@ -9,7 +9,28 @@
 namespace ck::algo
 {
     auto
-        MatchesAnyAbilityActivationCancelledTags::
+        MatchesAnyAbilityActivationCancelledTagsOnSelf::
+        operator()(
+            const FCk_Handle& InTypeUnsafeHandle) const
+        -> bool
+    {
+        const auto& Handle = UCk_Utils_Ability_UE::CastChecked(InTypeUnsafeHandle);
+
+        const auto CancelledByTags = [&]()
+        {
+            const auto& ActivationSettings = UCk_Utils_Ability_UE::Get_ActivationSettings(Handle);
+            auto Tags = ActivationSettings.Get_ActivationSettingsOnSelf().Get_CancelledByTagsOnSelf();
+
+            return Tags;
+        }();
+
+        return CancelledByTags.HasAnyExact(_Tags);
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    auto
+        MatchesAnyAbilityActivationCancelledTagsOnOwner::
         operator()(
             const FCk_Handle& InTypeUnsafeHandle) const
         -> bool
@@ -20,7 +41,6 @@ namespace ck::algo
         {
             const auto& ActivationSettings = UCk_Utils_Ability_UE::Get_ActivationSettings(Handle);
             auto Tags = ActivationSettings.Get_ActivationSettingsOnOwner().Get_CancelledByTagsOnAbilityOwner();
-            Tags.AppendTags(ActivationSettings.Get_ActivationSettingsOnSelf().Get_CancelledByTagsOnSelf());
 
             return Tags;
         }();
