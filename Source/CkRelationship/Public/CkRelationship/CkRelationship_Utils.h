67151a73f14d11329c0e2df76fe38bbff5f81713
Sulfur-CK <sulfur@chainkemists.com>
2024-07-21 20:07:59 -0700

    feat: added Relationship Utils with utils to allow querying Team Attitude between two Entities

---
+++ Source/CkRelationship/Public/CkRelationship/CkRelationship_Utils.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include "CkCore/Macros/CkMacros.h"
+
+#include "CkNet/CkNet_Utils.h"
+
+#include "CkRelationship/Team/CkTeam_Fragment_Data.h"
+
+#include "CkRelationship_Utils.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UENUM(BlueprintType)
+enum class ECk_RelationshipAttitude : uint8
+{
+    Neutral,
+    Friendly,
+    Hostile
+};
+
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_RelationshipAttitude);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS()
+class CKRELATIONSHIP_API UCk_Utils_Relationship_UE : public UCk_Utils_Ecs_Net_UE
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_Relationship_UE);
+
+public:
+    UFUNCTION(BlueprintPure,
+              Category = "Ck|Utils|Relationship",
+              DisplayName="[Ck][Relationship] Get Attitude Towards")
+    ECk_RelationshipAttitude
+    Get_AttitudeTowards(
+        const FCk_Handle& InFrom,
+        const FCk_Handle& InTo);
+
+    UFUNCTION(BlueprintCallable,
+              Category = "Ck|Utils|Relationship",
+              DisplayName="[Ck][Relationship] Get Attitude Towards")
+    ECk_RelationshipAttitude
+    Get_AttitudeTowards_Exec(
+        const FCk_Handle& InFrom,
+        const FCk_Handle& InTo);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
