043ecdd2c85d109d95d6a995eeb6987082637883
Sulfur-CK <sulfur@chainkemists.com>
2024-07-31 07:04:09 -0700

    feat: added the ResolverDataBundle
    notes: the DataBundle is the heart of the resolution pipeline where it is responsible for resolving all the metadata and modification requests

---
+++ Source/CkResolver/Public/ResolverDataBundle/ProcessorInjector/CkResolverDataBundle_ProcessorInjector.cpp
@@ -0,0 +1,43 @@
+#include "CkResolverDataBundle_ProcessorInjector.h"
+
+#include "ResolverDataBundle/CkResolverDataBundle_Processor.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_ResolverDataBundle_ProcessorInjector_StartNewPhase_UE::
+    DoInjectProcessors(
+        EcsWorldType& InWorld)
+    -> void
+{
+    InWorld.Add<ck::FProcessor_ResolverDataBundle_StartNewPhase>(InWorld.Get_Registry());
+}
+
+auto
+    UCk_ResolverDataBundle_ProcessorInjector_HandleRequests_UE::
+    DoInjectProcessors(
+        EcsWorldType& InWorld)
+        -> void
+{
+    InWorld.Add<ck::FProcessor_ResolverDataBundle_HandleRequests>(InWorld.Get_Registry());
+}
+
+auto
+    UCk_ResolverDataBundle_ProcessorInjector_ResolveOperations_UE::
+    DoInjectProcessors(
+        EcsWorldType& InWorld)
+    -> void
+{
+    InWorld.Add<ck::FProcessor_ResolverDataBundle_ResolveOperations>(InWorld.Get_Registry());
+}
+
+auto
+    UCk_ResolverDataBundle_ProcessorInjector_Calculate_UE::
+    DoInjectProcessors(
+        EcsWorldType& InWorld)
+    -> void
+{
+    InWorld.Add<ck::FProcessor_ResolverDataBundle_Calculate>(InWorld.Get_Registry());
+}
+
+// -------------------------------------------------------------------------------------------------------------------
