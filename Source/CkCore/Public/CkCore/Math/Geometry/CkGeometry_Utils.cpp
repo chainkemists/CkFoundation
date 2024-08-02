8547c379bda91798a7d6a4f2e24c514fabd66241
Nitrogen-CK <nitrogen@chainkemists.com>
2024-08-02 14:57:06 -0400

    feat: Added geometry utils to get/iterate over a Box(2D) vertices or edges easily.

--- Source/CkCore/Public/CkCore/Math/Geometry/CkGeometry_Utils.cpp
+++ Source/CkCore/Public/CkCore/Math/Geometry/CkGeometry_Utils.cpp
@@ -1,5 +1,6 @@
 #include "CkGeometry_Utils.h"
 
+#include "CkCore/Algorithms/CkAlgorithms.h"
 #include "CkCore/Ensure/CkEnsure.h"
 
 #include "GameFramework/HUD.h"
@@ -10,7 +11,7 @@
 
 auto
     UCk_Utils_Geometry_UE::
-    ProjectBox3D_ToScreen(
+    Project_Box_ToScreen(
         APlayerController* InPlayerController,
         const FBox& InBox3D)
     -> FBox2D
@@ -26,24 +27,19 @@ auto
     auto BoundingBoxScreenSpace_Min = FVector2D{ FLT_MAX, FLT_MAX };
     auto BoundingBoxScreenSpace_Max = FVector2D{ -FLT_MAX, -FLT_MAX };
 
-    constexpr auto BoxVerticesCount = 8;
-
-    FVector BoxVertices[BoxVerticesCount];
-    InBox3D.GetVertices(BoxVertices);
-
-    for (const auto& BoxVertex : BoxVertices)
+    ForEach_BoxVertices(InBox3D, [&](const FVector& InBoxVertex)
     {
         constexpr auto PlayerViewportRelative = false;
         auto ScreenLocation = FVector2D{};
 
-        if (NOT InPlayerController->ProjectWorldLocationToScreen(BoxVertex, ScreenLocation, PlayerViewportRelative))
+        if (NOT InPlayerController->ProjectWorldLocationToScreen(InBoxVertex, ScreenLocation, PlayerViewportRelative))
         { FBox2D{}; }
 
         BoundingBoxScreenSpace_Min.X = FMath::Min(ScreenLocation.X, BoundingBoxScreenSpace_Min.X);
         BoundingBoxScreenSpace_Min.Y = FMath::Min(ScreenLocation.Y, BoundingBoxScreenSpace_Min.Y);
         BoundingBoxScreenSpace_Max.X = FMath::Max(ScreenLocation.X, BoundingBoxScreenSpace_Max.X);
         BoundingBoxScreenSpace_Max.Y = FMath::Max(ScreenLocation.Y, BoundingBoxScreenSpace_Max.Y);
-    }
+    });
 
     const auto& ViewportSize = [&]()
     {
@@ -66,6 +62,65 @@ auto
 
 auto
     UCk_Utils_Geometry_UE::
+    Get_Box_Vertices(
+        const FBox& InBox3D)
+    -> TArray<FVector>
+{
+    if (ck::Is_NOT_Valid(InBox3D))
+    { return {}; }
+
+    const auto& Result = [&]()
+    {
+        constexpr auto BoxVerticesCount = 8;
+
+        TArray<FVector> ToRet;
+        ToRet.Reserve(BoxVerticesCount);
+
+
+        FVector BoxVertices[BoxVerticesCount];
+        InBox3D.GetVertices(BoxVertices);
+
+        for (const auto& Vertex : BoxVertices)
+        {
+            ToRet.Add(Vertex);
+        }
+
+        return ToRet;
+    }();
+
+    return Result;
+}
+
+auto
+    UCk_Utils_Geometry_UE::
+    Get_Box_Edges(
+        const FBox& InBox3D)
+    -> TArray<FCk_LineSegment>
+{
+    if (ck::Is_NOT_Valid(InBox3D))
+    { return {}; }
+
+    const auto BoxEdges = TArray
+    {
+        FIntVector2{0, 1}, FIntVector2{0, 2}, FIntVector2{0, 4},
+        FIntVector2{1, 3}, FIntVector2{1, 5}, FIntVector2{2, 3},
+        FIntVector2{2, 6}, FIntVector2{3, 7}, FIntVector2{4, 5},
+        FIntVector2{4, 6}, FIntVector2{5, 7}, FIntVector2{6, 7}
+    };
+
+    const auto& BoxVertices = Get_Box_Vertices(InBox3D);
+
+    return ck::algo::Transform<TArray<FCk_LineSegment>>(BoxEdges, [&](const FIntVector2& InBoxEdge)
+    {
+        const auto& EdgeStart = BoxVertices[InBoxEdge.X];
+        const auto& EdgeEnd = BoxVertices[InBoxEdge.Y];
+
+        return FCk_LineSegment{ EdgeStart, EdgeEnd };
+    });
+}
+
+auto
+    UCk_Utils_Geometry_UE::
     Break_Box_WithCenterAndExtents(
         const FBox& InBox,
         FVector& OutMin,
@@ -81,9 +136,94 @@ auto
     OutIsValidBox = static_cast<bool>(InBox.IsValid);
 }
 
+
 auto
     UCk_Utils_Geometry_UE::
-    Break_Box2D_WithCenterAndExtents(
+    ForEach_BoxEdges(
+        const FBox& InBox,
+        TFunction<void(const FCk_LineSegment&)> InFunc)
+    -> ECk_SucceededFailed
+{
+    if (ck::Is_NOT_Valid(InBox))
+    { return ECk_SucceededFailed::Failed; }
+
+    for (const auto& Edge : Get_Box_Edges(InBox))
+    {
+        InFunc(Edge);
+    }
+
+    return ECk_SucceededFailed::Succeeded;
+}
+
+auto
+    UCk_Utils_Geometry_UE::
+    ForEach_BoxVertices(
+        const FBox& InBox,
+        TFunction<void(const FVector&)> InFunc)
+    -> ECk_SucceededFailed
+{
+    if (ck::Is_NOT_Valid(InBox))
+    { return ECk_SucceededFailed::Failed; }
+
+    for (const auto& BoxVertex : Get_Box_Vertices(InBox))
+    {
+        InFunc(BoxVertex);
+    }
+
+    return ECk_SucceededFailed::Succeeded;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Utils_Geometry2D_UE::
+    Get_Box_Vertices(
+        const FBox2D& InBox2D)
+    -> TArray<FVector2D>
+{
+    if (ck::Is_NOT_Valid(InBox2D))
+    { return {}; }
+
+    const auto BoxVertices = TArray
+    {
+        InBox2D.Min,
+        FVector2D(InBox2D.Max.X, InBox2D.Min.Y),
+        FVector2D(InBox2D.Min.X, InBox2D.Max.Y),
+        InBox2D.Max
+    };
+
+    return BoxVertices;
+}
+
+auto
+    UCk_Utils_Geometry2D_UE::
+    Get_Box_Edges(
+        const FBox2D& InBox2D)
+    -> TArray<FCk_LineSegment2D>
+{
+    if (ck::Is_NOT_Valid(InBox2D))
+    { return {}; }
+
+    const auto BoxEdges = TArray
+    {
+        FIntVector2{0, 1}, FIntVector2{1, 3},
+        FIntVector2{3, 2}, FIntVector2{2, 0}
+    };
+
+    const auto& BoxVertices = Get_Box_Vertices(InBox2D);
+
+    return ck::algo::Transform<TArray<FCk_LineSegment2D>>(BoxEdges, [&](const FIntVector2& InBoxEdge)
+    {
+        const auto& EdgeStart = BoxVertices[InBoxEdge.X];
+        const auto& EdgeEnd = BoxVertices[InBoxEdge.Y];
+
+        return FCk_LineSegment2D{ EdgeStart, EdgeEnd };
+    });
+}
+
+auto
+    UCk_Utils_Geometry2D_UE::
+    Break_Box_WithCenterAndExtents(
         const FBox2D& InBox,
         FVector2D& OutMin,
         FVector2D& OutMax,
@@ -96,6 +236,42 @@ auto
     OutMax = InBox.Max,
     InBox.GetCenterAndExtents(OutCenter, InExtents);
     OutIsValidBox = static_cast<bool>(InBox.bIsValid);
+}
+
+auto
+    UCk_Utils_Geometry2D_UE::
+    ForEach_BoxVertices(
+        const FBox2D& InBox,
+        TFunction<void(const FVector2D&)> InFunc)
+    -> ECk_SucceededFailed
+{
+    if (ck::Is_NOT_Valid(InBox))
+    { return ECk_SucceededFailed::Failed; }
+
+    for (const auto& BoxVertex : Get_Box_Vertices(InBox))
+    {
+        InFunc(BoxVertex);
+    }
+
+    return ECk_SucceededFailed::Succeeded;
+}
+
+auto
+    UCk_Utils_Geometry2D_UE::
+    ForEach_BoxEdges(
+        const FBox2D& InBox,
+        TFunction<void(const FCk_LineSegment2D&)> InFunc)
+    -> ECk_SucceededFailed
+{
+    if (ck::Is_NOT_Valid(InBox))
+    { return ECk_SucceededFailed::Failed; }
+
+    for (const auto& Edge : Get_Box_Edges(InBox))
+    {
+        InFunc(Edge);
+    }
+
+    return ECk_SucceededFailed::Succeeded;
 }
 
 // --------------------------------------------------------------------------------------------------------------------
