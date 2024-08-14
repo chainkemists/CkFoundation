dfddf36ffa13f2b085ca041d35b4311ae98eb1ae
Nitrogen-CK <nitrogen@chainkemists.com>
2024-08-14 18:42:53 -0400

    feat: Added new CkAnimation_Utils that exposes several functions to BP related to AnimMontage

---
+++ Source/CkAnimation/Public/CkAnimation/CkAnimation_Utils.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "CkCore/Enums/CkEnums.h"
+#include "CkCore/Time/CkTime.h"
+
+#include "CkAnimation_Utils.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct FCk_Animation_MontageSection_LengthInfo
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Animation_MontageSection_LengthInfo);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+        meta = (AllowPrivateAccess = true))
+    FCk_Time _StartTime;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+        meta = (AllowPrivateAccess = true))
+    FCk_Time _EndTime;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+        meta = (AllowPrivateAccess = true))
+    FCk_Time _Length;
+
+public:
+    CK_PROPERTY(_StartTime);
+    CK_PROPERTY(_EndTime);
+    CK_PROPERTY(_Length);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable)
+class CKANIMATION_API UCk_Utils_Animation_UE : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_Animation_UE);
+
+public:
+    UFUNCTION(BlueprintPure,
+              DisplayName="[Ck][Animation] Get Montage Section Length (By Index)",
+              Category = "Ck|Utils|Animation")
+    static FCk_Animation_MontageSection_LengthInfo
+    Get_MontageSectionLength_ByIndex(
+        UAnimMontage* InAnimMontage,
+        int32 InSectionIndex);
+
+    UFUNCTION(BlueprintPure,
+              DisplayName="[Ck][Animation] Get Montage Section Length (By Name)",
+              Category = "Ck|Utils|Animation")
+    static FCk_Animation_MontageSection_LengthInfo
+    Get_MontageSectionLength_ByName(
+        UAnimMontage* InAnimMontage,
+        FName InSectionName);
+
+    UFUNCTION(BlueprintPure,
+              DisplayName="[Ck][Animation] Get Montage Section Index (From Position)",
+              Category = "Ck|Utils|Animation")
+    static int32
+    Get_MontageSectionIndex_FromPosition(
+        UAnimMontage* InAnimMontage,
+        float InPosition);
+
+    UFUNCTION(BlueprintPure,
+              DisplayName="[Ck][Animation] Get Is Valid Montage Section Index",
+              Category = "Ck|Utils|Animation")
+    static bool
+    Get_IsValidMontageSectionIndex(
+        UAnimMontage* InAnimMontage,
+        int32 InSectionIndex);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
