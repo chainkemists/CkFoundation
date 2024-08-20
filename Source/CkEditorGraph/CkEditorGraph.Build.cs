f1a54878a48e033ca31ab86e5cc1d05de284da7b
Nitrogen-CK <nitrogen@chainkemists.com>
2024-08-20 16:19:01 -0400

    feat: Added new CkEditorGraph module

---
+++ Source/CkEditorGraph/CkEditorGraph.Build.cs
@@ -0,0 +1,41 @@
+using System.IO;
+using UnrealBuildTool;
+
+public class CkEditorGraph : CkModuleRules
+{
+    public CkEditorGraph(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PrivateIncludePaths.AddRange(new string[] {
+            // ... add other private include paths required here ...
+        });
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "Slate",
+            "SlateCore",
+            "InputCore",
+            "StructUtils",
+            "ToolMenus",
+
+            "CkCore",
+            "CkLog",
+        });
+
+        if (Target.Type == TargetRules.TargetType.Editor)
+        {
+            PrivateDependencyModuleNames.AddRange(
+                new string[]
+                {
+                    "UnrealEd",
+                    "EditorStyle",
+                    "AssetTools",
+                    "KismetCompiler",
+                    "GraphEditor",
+                    "BlueprintGraph"
+                });
+        }
+    }
+}
---
+++ Source/CkEditorGraph/Public/CkEditorGraph/CkEditorGraph_Utils.h
@@ -0,0 +1,174 @@
+#pragma once
+
+#include "CkCore/Macros/CkMacros.h"
+#include "CkCore/Enums/CkEnums.h"
+
+#include <Kismet/BlueprintFunctionLibrary.h>
+#include <EdGraph/EdGraphPin.h>
+#include <EdGraph/EdGraphNode.h>
+#include <Editor/BlueprintGraph/Classes/EdGraphSchema_K2.h>
+#include <K2Node_Event.h>
+#include <KismetCompiler.h>
+#include <CoreMinimal.h>
+#include <functional>
+
+#include "CkEditorGraph_Utils.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UENUM(BlueprintType)
+enum class ECk_EditorGraph_PinDirection : uint8
+{
+    Input,
+    Output
+};
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_EditorGraph_PinDirection);
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(EEdGraphPinDirection);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UENUM(BlueprintType)
+enum class ECk_EditorGraph_DefaultPinType : uint8
+{
+    Then,
+    Exec,
+    Result,
+    Self
+};
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_EditorGraph_DefaultPinType);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UENUM(BlueprintType)
+enum class ECk_EditorGraph_PinLinkType : uint8
+{
+    Move,
+    Copy
+};
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_EditorGraph_PinLinkType);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable)
+class CKEDITORGRAPH_API UCk_Utils_EditorGraph_UE : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_EditorGraph_UE);
+
+public:
+    using GraphPinType           = TOptional<UEdGraphPin*>;
+    using GraphPinListType       = std::initializer_list<GraphPinType>;
+    using GraphPinPairType       = std::pair<GraphPinType, GraphPinType>;
+    using GraphPinPairListType   = std::initializer_list<GraphPinPairType>;
+    using CompilerContextOptType = TOptional<FKismetCompilerContext*>;
+    using MultiplePinsFuncType   = std::function<ECk_SucceededFailed(const UEdGraphSchema_K2*, const GraphPinType&, const GraphPinType&)>;
+
+public:
+    UFUNCTION(BlueprintPure,
+              Category = "Ck|Utils|Editor|Graph",
+              DisplayName = "[Ck] Get Graph Pin Direction (To Unreal)")
+    static TEnumAsByte<enum EEdGraphPinDirection>
+    Get_PinDirection_ToUnreal(
+        ECk_EditorGraph_PinDirection InPinDirection);
+
+    UFUNCTION(BlueprintPure,
+              Category = "Ck|Utils|Editor|Graph",
+              DisplayName = "[Ck] Get Graph Pin Direction (From Unreal)")
+    static ECk_EditorGraph_PinDirection
+    Get_PinDirection_FromUnreal(
+        TEnumAsByte<enum EEdGraphPinDirection> InPinDirection);
+
+public:
+    static auto
+    Get_Pin(
+        FName InPinName,
+        ECk_EditorGraph_PinDirection InExpectedPinDirection,
+        const TArray<UEdGraphPin*>& InPins) -> GraphPinType;
+
+    static auto
+    Get_Pin(
+        FName InPinName,
+        ECk_EditorGraph_PinDirection InExpectedPinDirection,
+        const UEdGraphNode& InGraphNode) -> GraphPinType;
+
+    static auto
+    Get_Pin(
+        const UEdGraphNode& InGraphNode,
+        ECk_EditorGraph_DefaultPinType InPinType) -> GraphPinType;
+
+    static auto
+    Get_Pin_Then(
+        const UEdGraphNode& InGraphNode) -> GraphPinType;
+
+    static auto
+    Get_Pin_Result(
+        const UEdGraphNode& InGraphNode) -> GraphPinType;
+
+    static auto
+    Get_Pin_Exec(
+        const UEdGraphNode& InGraphNode) -> GraphPinType;
+
+    static auto
+    Get_Pin_Self(
+        const UEdGraphNode& InGraphNode) -> GraphPinType;
+
+public:
+    template <typename T_Enum>
+    static auto
+    Get_Pin_EnumValue (
+        FName InPinName,
+        ECk_EditorGraph_PinDirection InExpectedPinDirection,
+        const UEdGraphNode& InGraphNode) -> TOptional<T_Enum>;
+
+public:
+    static auto
+    Request_LinkPins(
+        FKismetCompilerContext& InCompilerContext,
+        GraphPinPairListType InGraphPins,
+        ECk_EditorGraph_PinLinkType  InLinkType) -> ECk_SucceededFailed;
+
+    static auto
+    Request_ValidatePins(
+        GraphPinListType InGraphPins,
+        CompilerContextOptType InCompilerContext = {}) -> ECk_ValidInvalid;
+
+    static auto
+    Request_TryCreateConnection(
+        FKismetCompilerContext& InCompilerContext,
+        GraphPinPairListType InGraphPins) -> ECk_SucceededFailed;
+
+    static auto
+    Request_ForceRefreshNode(
+        UEdGraphNode& InGraphNode) -> void;
+
+private:
+    static auto
+    DoModifyMultiplePins(
+        FKismetCompilerContext& InCompilerContext,
+        GraphPinPairType InGraphPins,
+        MultiplePinsFuncType InFunc) -> ECk_SucceededFailed;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+// Definitions
+
+template <typename T_Enum>
+auto
+    UCk_Utils_EditorGraph_UE::
+    Get_Pin_EnumValue(
+        FName InPinName,
+        ECk_EditorGraph_PinDirection InExpectedPinDirection,
+        const UEdGraphNode& InGraphNode)
+    -> TOptional<T_Enum>
+{
+    const auto& Pin = Get_Pin(InPinName, InExpectedPinDirection, InGraphNode);
+
+    if (ck::Is_NOT_Valid(Pin))
+    { return {}; }
+
+    return ck::Get_EnumValueByName<T_Enum>(FName((*Pin)->DefaultValue));
+}
+
+// --------------------------------------------------------------------------------------------------------------------
