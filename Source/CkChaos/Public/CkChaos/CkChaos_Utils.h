c3f308f5c39d656b6b4ff367714d0cc131e80df3
AlexObatake-CK <phosphorus@chainkemists.com>
2024-08-22 11:23:17 -0700

    feat: Add getter for GC particle index closest to location
    *  Add chaos utils function library
    *  Getter for the index of the closest particle to a given location. Can be used to track when that particle is broken

--- Source/CkChaos/Public/CkChaos/CkChaos_Utils.h
+++ Source/CkChaos/Public/CkChaos/CkChaos_Utils.h
@@ -1,3 +1,22 @@
 #pragma once
 
+#include "CkChaos_Utils.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS()
+class CKCHAOS_API UCk_Utils_Chaos_UE : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintPure,
+        Category = "Ck|Utils|Chaos|GeometryCollection",
+        DisplayName="[Ck][GeometryCollection] Get Closest Particle Index To Location")
+    static int32
+    Get_ClosestParticleIndexToLocation(
+        const UGeometryCollectionComponent* InGeometryCollection,
+        const FVector& InLocation);
+};
+
 // --------------------------------------------------------------------------------------------------------------------
