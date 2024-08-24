6d420a4207915ddba834b299a928adfaa9c42dfd
Sulfur-CK <sulfur@chainkemists.com>
2024-08-24 13:44:26 -0700

    feat: added the Aggro module

---
+++ Source/CkAggro/CkAggro.cpp
@@ -0,0 +1,15 @@
+#include "CkAggro.h"
+
+#define LOCTEXT_NAMESPACE "FCkAggroModule"
+
+void FCkAggroModule::StartupModule()
+{
+}
+
+void FCkAggroModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkAggroModule, CkAggro)
