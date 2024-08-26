c2f0e8856015adab1c55810277e8d8327507ea22
Nitrogen-CK <nitrogen@chainkemists.com>
2024-08-26 16:19:29 -0400

    feat: Added LocalPlayer and GameEngine WorldSetting base classes

---
+++ Source/CkCore/Public/CkCore/Engine/CkGameEngine.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CkCore/Macros/CkMacros.h"
+
+#include <Engine/GameEngine.h>
+
+#include "CkGameEngine.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS()
+class CKCORE_API UCk_GameEngine_UE : public UGameEngine
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_GameEngine_UE);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
---
+++ Source/CkCore/Public/CkCore/Engine/CkLocalPlayer.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "CkCore/Macros/CkMacros.h"
+
+#include <Engine/LocalPlayer.h>
+
+#include "CkLocalPlayer.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS()
+class CKCORE_API UCk_LocalPlayer_UE : public ULocalPlayer
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_LocalPlayer_UE);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
