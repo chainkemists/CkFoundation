429ef660ef572902d285047694870936e86a60de
Nitrogen-CK <nitrogen@chainkemists.com>
2024-08-26 20:09:30 -0400

    fix: Aggro module no longer references itself in its Build.cs

--- Source/CkAggro/CkAggro.Build.cs
+++ Source/CkAggro/CkAggro.Build.cs
@@ -17,7 +17,6 @@ public class CkAggro : CkModuleRules
             "StructUtils",
             "GameplayTags",
 
-            "CkAggro",
             "CkAttribute",
             "CkCore",
             "CkEcs",
