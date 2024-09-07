1b648a5841d6a664fb10a624bc28a86fdd577292
Nitrogen-CK <nitrogen@chainkemists.com>
2024-09-06 23:04:47 -0400

    feat: Exposed option to enable & add  the (development only) cheats from the CheatManager in MP
    By default, will only work for SP

--- Source/CkCore/Public/CkCore/Engine/CkGameMode.cpp
+++ Source/CkCore/Public/CkCore/Engine/CkGameMode.cpp
@@ -1,3 +1,20 @@
 #include "CkGameMode.h"
 
+#include "CkCore/Build/CkBuild_Macros.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    ACk_GameMode_UE::
+    AllowCheats(
+        APlayerController* InPlayerController)
+    -> bool
+{
+#if CK_BUILD_RELEASE
+    return Super::AllowCheats(InPlayerController);
+#else
+    return _AllowMultiplayerCheats ? _AllowMultiplayerCheats : Super::AllowCheats(InPlayerController);
+#endif
+}
+
 // --------------------------------------------------------------------------------------------------------------------
