d8eacd052e4e9ea4b7310de8f52cfe85aae16607
AlexObatake-CK <phosphorus@chainkemists.com>
2024-09-09 14:57:12 -0700

    feat: Timers for WaitFor MoveTo and RotateTowards have a timer name set

---
+++ Source/CkActor/Public/CkActor/CkActor_GameplayTags.cpp
@@ -0,0 +1,8 @@
+﻿#include <NativeGameplayTags.h>
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_Timer_ActorMoveTo, TEXT("Timer.Actor.MoveTo"));
+UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_Timer_ActorWaitRotateTowards, TEXT("Timer.Actor.RotateTowards"));
+
+// --------------------------------------------------------------------------------------------------------------------
