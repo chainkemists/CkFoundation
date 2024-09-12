e08e37e2a28921f7a0279d09d412cfc4f1f0b239
Nitrogen-CK <nitrogen@chainkemists.com>
2024-09-12 18:55:27 -0400

    feat: Added base tickable game world subsystem

--- Source/CkCore/Public/CkCore/Subsystems/GameWorldSubsytem/CkGameWorldSubsystem.cpp
+++ Source/CkCore/Public/CkCore/Subsystems/GameWorldSubsytem/CkGameWorldSubsystem.cpp
@@ -32,6 +32,7 @@ auto
 {
     Super::PostInitialize();
 
+    _AllWorldSubsystemsInitialized = true;
     OnAllWorldSubsystemsInitialized();
 }
 
@@ -63,6 +64,92 @@ auto
     -> bool
 {
     return WorldType == EWorldType::Game || WorldType == EWorldType::PIE;
+}
+
+auto
+    UCk_Game_WorldSubsystem_Base_UE::
+    Get_AreAllWorldSubsystemsInitialized() const
+    -> bool
+{
+    return _AllWorldSubsystemsInitialized;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Game_TickableWorldSubsystem_Base_UE::
+    Deinitialize()
+    -> void
+{
+    OnDeinitialize();
+}
+
+auto
+    UCk_Game_TickableWorldSubsystem_Base_UE::
+    Initialize(
+        FSubsystemCollectionBase& Collection)
+    -> void
+{
+    Super::Initialize(Collection);
+
+    OnInitialize();
+}
+
+auto
+    UCk_Game_TickableWorldSubsystem_Base_UE::
+    PostInitialize()
+    -> void
+{
+    Super::PostInitialize();
+
+    _AllWorldSubsystemsInitialized = true;
+    OnAllWorldSubsystemsInitialized();
+}
+
+auto
+    UCk_Game_TickableWorldSubsystem_Base_UE::
+    ShouldCreateSubsystem(
+        UObject* InOuter) const
+    -> bool
+{
+    if (const auto& ShouldCreateSubsystem = Super::ShouldCreateSubsystem(InOuter);
+        NOT ShouldCreateSubsystem)
+    { return false; }
+
+    if (ck::Is_NOT_Valid(InOuter))
+    { return true; }
+
+    const auto& World = InOuter->GetWorld();
+
+    if (ck::Is_NOT_Valid(World))
+    { return true; }
+
+    return DoesSupportWorldType(World->WorldType);
+}
+
+auto
+    UCk_Game_TickableWorldSubsystem_Base_UE::
+    GetStatId() const
+    -> TStatId
+{
+    RETURN_QUICK_DECLARE_CYCLE_STAT(UCk_Game_TickableWorldSubsystem_Base_UE, STATGROUP_Tickables);
+}
+
+auto
+    UCk_Game_TickableWorldSubsystem_Base_UE::
+    DoesSupportWorldType(
+        const EWorldType::Type WorldType) const
+    -> bool
+{
+    return WorldType == EWorldType::Game || WorldType == EWorldType::PIE;
+}
+
+auto
+    UCk_Game_TickableWorldSubsystem_Base_UE::
+    Get_AreAllWorldSubsystemsInitialized() const
+    -> bool
+{
+    return _AllWorldSubsystemsInitialized;
 }
 
 // --------------------------------------------------------------------------------------------------------------------
--- Source/CkCore/Public/CkCore/Subsystems/GameWorldSubsytem/CkGameWorldSubsystem.h
+++ Source/CkCore/Public/CkCore/Subsystems/GameWorldSubsytem/CkGameWorldSubsystem.h
@@ -29,6 +29,7 @@ public:
 
 protected:
     auto DoesSupportWorldType(const EWorldType::Type WorldType) const -> bool override;
+    auto Get_AreAllWorldSubsystemsInitialized() const -> bool;
 
 public:
     UFUNCTION(BlueprintImplementableEvent)
@@ -39,6 +40,49 @@ public:
 
     UFUNCTION(BlueprintImplementableEvent)
     void OnDeinitialize();
+
+private:
+    bool _AllWorldSubsystemsInitialized = false;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+/*
+ * This particular Subsystem should be derived from IFF we need a TickableWorldSubsystem that is only created when we are
+ * in-game. There are several other worlds created in the Editor that are not in PIE
+ *
+ */
+UCLASS(BlueprintType)
+class CKCORE_API UCk_Game_TickableWorldSubsystem_Base_UE : public UTickableWorldSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Game_TickableWorldSubsystem_Base_UE);
+
+public:
+    auto Deinitialize() -> void override;
+    auto Initialize(FSubsystemCollectionBase& Collection) -> void override;
+    auto PostInitialize() -> void override;
+    auto ShouldCreateSubsystem(UObject* Outer) const -> bool override;
+    auto GetStatId() const -> TStatId override;
+
+protected:
+    auto DoesSupportWorldType(const EWorldType::Type WorldType) const -> bool override;
+    auto Get_AreAllWorldSubsystemsInitialized() const -> bool;
+
+public:
+    UFUNCTION(BlueprintImplementableEvent)
+    void OnInitialize();
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void OnAllWorldSubsystemsInitialized();
+
+    UFUNCTION(BlueprintImplementableEvent)
+    void OnDeinitialize();
+
+private:
+    bool _AllWorldSubsystemsInitialized = false;
 };
 
 // --------------------------------------------------------------------------------------------------------------------
