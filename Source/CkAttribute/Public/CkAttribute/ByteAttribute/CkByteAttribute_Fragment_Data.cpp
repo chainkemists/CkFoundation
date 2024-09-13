7fcf482201773b316a4fe6df3a559335aea1c244
Nitrogen-CK <nitrogen@chainkemists.com>
2024-09-13 15:33:56 -0400

    style: Fixed incorrect formatter arguments inthe error message when attempting to get the min/max of an attribute that is missing this component

--- Source/CkAttribute/Public/CkAttribute/ByteAttribute/CkByteAttribute_Fragment_Data.cpp
+++ Source/CkAttribute/Public/CkAttribute/ByteAttribute/CkByteAttribute_Fragment_Data.cpp
@@ -28,8 +28,8 @@ auto
 {
     ck::attribute::ErrorIf(NOT (_MinMax == ECk_MinMax::Min || _MinMax == ECk_MinMax::MinMax),
         TEXT("Attempting to get a Min value of Attribute [{}] where MinMax is set to [{}]. Please address this."),
-         _MinMax,
-         _Name);
+         _Name,
+         _MinMax);
 
     return _MinValue;
 }
@@ -41,8 +41,8 @@ auto
 {
     ck::attribute::ErrorIf(NOT (_MinMax == ECk_MinMax::Max || _MinMax == ECk_MinMax::MinMax),
         TEXT("Attempting to get a Max value Attribute [{}] where MinMax is set to [{}]. Please address this."),
-         _MinMax,
-         _Name);
+         _Name,
+         _MinMax);
 
     return _MaxValue;
 }
