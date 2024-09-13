cf7c5064c821898432e3b6991d46a363a6d66463
sulfur-ck <sulfur@chainkemists.com>
2024-09-13 09:49:31 -0700

    feat: added new log category for Ability Cue

--- Source/CkAbility/CkAbility_Log.cpp
+++ Source/CkAbility/CkAbility_Log.cpp
@@ -3,10 +3,16 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 DEFINE_LOG_CATEGORY(CkAbility);
+DEFINE_LOG_CATEGORY(CkAbilityCue);
 
 namespace ck::ability
 {
 	CK_REGISTER_LOG_FUNCTIONS(CkAbility);
+}
+
+namespace ck::ability_cue
+{
+	CK_REGISTER_LOG_FUNCTIONS(CkAbilityCue);
 }
 
 // --------------------------------------------------------------------------------------------------------------------
