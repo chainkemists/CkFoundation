1a3731098230283113164ec0d44b3709454c193d
sulfur-ck <sulfur@chainkemists.com>
2024-09-13 09:49:31 -0700

    feat: added new log category for Ability Cue

--- Source/CkAbility/CkAbility_Log.h
+++ Source/CkAbility/CkAbility_Log.h
@@ -5,12 +5,18 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 CKABILITY_API DECLARE_LOG_CATEGORY_EXTERN(CkAbility, Log, All);
+CKABILITY_API DECLARE_LOG_CATEGORY_EXTERN(CkAbilityCue, Log, All);
 
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace ck::ability
 {
     CK_DEFINE_LOG_FUNCTIONS(CkAbility);
+}
+
+namespace ck::ability_cue
+{
+    CK_DEFINE_LOG_FUNCTIONS(CkAbilityCue);
 }
 
 // --------------------------------------------------------------------------------------------------------------------
