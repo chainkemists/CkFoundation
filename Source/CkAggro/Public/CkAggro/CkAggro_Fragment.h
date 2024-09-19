ec37367bf79198ef479842d6e068cb31fb33a2a1
Sulfur-CK <sulfur@chainkemists.com>
2024-09-19 05:47:39 -0700

    feat: added AggroOwner feature and refactored functions to be in the AggroOwner utils
    notes: this is mainly to allow the owner to have data such as Range
    which is then used by the upcoming processors to perform distance and
    line of sight calculations to calculate the final aggro score

--- Source/CkAggro/Public/CkAggro/CkAggro_Fragment.h
+++ Source/CkAggro/Public/CkAggro/CkAggro_Fragment.h
@@ -5,6 +5,7 @@
 #include "CkEcsExt/EntityHolder/CkEntityHolder_Utils.h"
 
 #include "CkRecord/Record/CkRecord_Fragment.h"
+#include "CkRecord/Record/CkRecord_Utils.h"
 
 #include "CkSignal/CkSignal_Macros.h"
 
@@ -16,14 +17,61 @@ class UCk_Entity_ConstructionScript_PDA;
 
 namespace ck
 {
-    CK_DEFINE_RECORD_OF_ENTITIES(FFragment_RecordOfAggro, FCk_Handle_Aggro);
+    CK_DEFINE_RECORD_OF_ENTITIES_AND_UTILS(FUtils_RecordOfAggros, FFragment_RecordOfAggro, FCk_Handle_Aggro);
 
     // --------------------------------------------------------------------------------------------------------------------
 
     CK_DEFINE_ECS_TAG(FTag_Aggro);
+    CK_DEFINE_ECS_TAG(FTag_Aggro_Excluded);
 
 
     CK_DEFINE_ENTITY_HOLDER_AND_UTILS(UAggroedEntity_Utils, AggroedEntity);
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    struct CKAGGRO_API FFragment_AggroOwner_Params
+    {
+    public:
+        CK_GENERATED_BODY(FFragment_AggroOwner_Params);
+
+    public:
+        using ParamsType = FCk_Fragment_AggroOwner_Params;
+
+    private:
+        ParamsType _Params;
+
+    public:
+        CK_PROPERTY_GET(_Params);
+        CK_DEFINE_CONSTRUCTORS(FFragment_AggroOwner_Params, _Params);
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    struct CKAGGRO_API FFragment_AggroOwner_Current
+    {
+    public:
+        CK_GENERATED_BODY(FFragment_AggroOwner_Current);
+
+    private:
+        FCk_Handle_Aggro _BestAggro;
+
+    public:
+        CK_PROPERTY_GET(_BestAggro);
+        CK_DEFINE_CONSTRUCTORS(FFragment_AggroOwner_Current, _BestAggro);
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    struct CKAGGRO_API FFragment_Aggro_Current
+    {
+    private:
+        double _Score = std::numeric_limits<float>::max();
+
+    public:
+        CK_PROPERTY_GET(_Score);
+
+        CK_DEFINE_CONSTRUCTORS(FFragment_Aggro_Current, _Score);
+    };
 
     // --------------------------------------------------------------------------------------------------------------------
 
