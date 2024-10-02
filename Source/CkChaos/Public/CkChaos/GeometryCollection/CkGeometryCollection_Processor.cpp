cdfb6c689b48965c8c68dacb31dc5e138684ddbf
AlexObatake-CK <phosphorus@chainkemists.com>
2024-10-02 09:31:33 -0700

    feat: Radial Strains applied to geometry collections can now have a falloff
    *  Falloff is defined by a float curve with the X axis being sclaed to the radius of the strain
    *  Falloff scales the strain and speed applied
    *  Incremental radius now only processes the particles within the radial shell instead of processing everything within the radius each time. This allows internal strain buildup to work properly

--- Source/CkChaos/Public/CkChaos/GeometryCollection/CkGeometryCollection_Processor.cpp
+++ Source/CkChaos/Public/CkChaos/GeometryCollection/CkGeometryCollection_Processor.cpp
@@ -1,26 +1,29 @@
 #include "CkGeometryCollection_Processor.h"
 
+#include <PhysicsProxy/GeometryCollectionPhysicsProxy.h>
+
 #include "CkChaos/GeometryCollection/CkGeometryCollection_Utils.h"
 #include "CkChaos/Settings/CkChaos_Settings.h"
 
 #include "CkCore/Algorithms/CkAlgorithms.h"
+#include "CkCore/Math/Arithmetic/CkArithmetic_Utils.h"
+#include "CkCore/Math/FloatCurve/CkFloatCurve_Utils.h"
 #include "CkCore/Math/Vector/CkVector_Utils.h"
 
 #include "CkEcsExt/Transform/CkTransform_Utils.h"
 
 #include "GeometryCollection/GeometryCollectionComponent.h"
 
-#include "PhysicsProxy/GeometryCollectionPhysicsProxy.h"
-
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace ck_geometrycollection_processor
 {
     auto
-    Get_ParticlesInRadius(
+    Get_ParticlesInRadialShell(
         const FGeometryCollectionPhysicsProxy* Proxy,
         const FVector& InLocation,
-        const float InRadius)
+        const float InRadiusMin,
+        const float InRadiusMax)
     -> decltype(Proxy->GetUnorderedParticles_Internal())
     {
         using ClusterHandleType = FGeometryCollectionPhysicsProxy::FClusterHandle;
@@ -34,7 +37,11 @@ namespace ck_geometrycollection_processor
             if (ck::Is_NOT_Valid(InValidHandle, ck::IsValid_Policy_NullptrOnly{}))
             { return false; }
 
-            return UCk_Utils_Vector3_UE::Get_IsPointInRadius(InValidHandle->GetX(), InLocation, InRadius);
+            const auto InMinRadius = UCk_Utils_Arithmetic_UE::Get_IsNearlyEqual(InRadiusMin, 0.0f) ? false :
+                UCk_Utils_Vector3_UE::Get_IsPointInRadius(InValidHandle->GetX(), InLocation, InRadiusMin);
+            const auto InMaxRadius = UCk_Utils_Vector3_UE::Get_IsPointInRadius(InValidHandle->GetX(), InLocation, InRadiusMax);
+
+            return InMaxRadius && NOT InMinRadius;
         });
 
         return ParticlesInRadius;
@@ -78,12 +85,19 @@ namespace ck
             InHandle, InParams.Get_Params().Get_GeometryCollection())
         { return; }
 
+        CK_ENSURE_IF_NOT(InRequest.Get_Radius() > 0.0f,
+            TEXT("Unable to ApplyRadialStrain to GeometryCollection [{}] because the radius is zero"),
+            InHandle)
+        { return; }
+
         const auto MaximumRadiusToUse = UCk_Utils_Chaos_Settings_UE::Get_MaximumRadialDamageDeltaRadiusPerFrame();
         auto MaybeNewRequest = InRequest;
 
+        const auto MinRadius = MaybeNewRequest.Get_IncrementalRadius();
+        const auto FinalRadius = InRequest.Get_Radius();
         MaybeNewRequest.Set_IncrementalRadius(MaybeNewRequest.Get_IncrementalRadius() + MaximumRadiusToUse);
-        const auto& ParticlesInRadius = ck_geometrycollection_processor::Get_ParticlesInRadius(Proxy, InRequest.Get_Location(),
-            MaybeNewRequest.Get_IncrementalRadius() < InRequest.Get_Radius() ? MaybeNewRequest.Get_IncrementalRadius() : InRequest.Get_Radius());
+        const auto& CurrentRadius = MaybeNewRequest.Get_IncrementalRadius() < FinalRadius ? MaybeNewRequest.Get_IncrementalRadius() : FinalRadius;
+        const auto& ParticlesInRadius = ck_geometrycollection_processor::Get_ParticlesInRadialShell(Proxy, InRequest.Get_Location(), MinRadius, CurrentRadius);
 
         const auto RbdSolver = Proxy->GetSolver<Chaos::FPhysicsSolver>();
 
@@ -111,27 +125,34 @@ namespace ck
                 }
 
                 auto Direction = Particle->GetTransformPQ().GetLocation() - InRequest.Get_Location();
+                const auto NormalizedDistance = Direction.Length() / FinalRadius;
                 Direction.Normalize();
 
-                if (const auto& Strain = InRequest.Get_ExternalStrain();
+                const auto IsValidCurve = InRequest.Get_NormalizedFalloffCurve().GetRichCurveConst() &&
+                    NOT InRequest.Get_NormalizedFalloffCurve().GetRichCurveConst()->Keys.IsEmpty();
+
+                const auto FalloffScale = IsValidCurve ?
+                    UCk_Utils_RuntimeFloatCurve_UE::Get_ValueAtTime(InRequest.Get_NormalizedFalloffCurve(), FCk_Time(NormalizedDistance)) : 1.f;
+
+                if (const auto& Strain = InRequest.Get_ExternalStrain() * FalloffScale;
                     Strain > 0.0f)
                 {
                     const auto& NewStrain = Strain;
                     RigidClustering.SetExternalStrain(ClusteredParticle, FMath::Max(ClusteredParticle->GetExternalStrain(), NewStrain));
                 }
 
-                if (const auto& Strain = InRequest.Get_InternalStrain();
+                if (const auto& Strain = InRequest.Get_InternalStrain() * FalloffScale;
                     Strain > 0.0f)
                 {
                     const Chaos::FRealSingle NewInternalStrain = ClusteredParticle->GetInternalStrains() - Strain;
-                    RigidClustering.SetInternalStrain(ClusteredParticle, FMath::Max(0, NewInternalStrain));
+                    RigidClustering.SetInternalStrain(ClusteredParticle, FMath::Max(0.0f, NewInternalStrain));
                 }
 
-                if (const auto& Speed = InRequest.Get_LinearSpeed();
+                if (const auto& Speed = InRequest.Get_LinearSpeed() * FalloffScale;
                     Speed > 0.0f)
                 { Particle->SetV(Direction * Speed); }
 
-                if (const auto& Speed = InRequest.Get_AngularSpeed();
+                if (const auto& Speed = InRequest.Get_AngularSpeed() * FalloffScale;
                     Speed > 0.0f)
                 { Particle->SetW(Direction * Speed); }
             });
