da2f270e96751c7f5ff03b80f4a70437724c13b6
Sulfur-CK <sulfur@chainkemists.com>
2024-10-06 15:55:26 -0700

    feat: added BestAggro processor and fragment to track the best aggro Entity

--- Source/CkAggro/Public/CkAggro/CkAggroOwner_Processor.h
+++ Source/CkAggro/Public/CkAggro/CkAggroOwner_Processor.h
@@ -17,6 +17,7 @@ namespace ck
         FProcessor_Aggro_DistanceScore,
         FCk_Handle_AggroOwner,
         FFragment_AggroOwner_Params,
+        FTag_Aggro_Filter_Distance,
         CK_IGNORE_PENDING_KILL>
     {
     public:
@@ -35,7 +36,26 @@ namespace ck
     class CKAGGRO_API FProcessor_Aggro_LineOfSightScore : public ck_exp::TProcessor<
         FProcessor_Aggro_LineOfSightScore,
         FCk_Handle_AggroOwner,
+        FTag_Aggro_Filter_LoS,
+        CK_IGNORE_PENDING_KILL>
+    {
+    public:
+        using TProcessor::TProcessor;
+
+    public:
+        auto
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle) const -> void;
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    class CKAGGRO_API FProcessor_Aggro_UpdateBestAggro : public ck_exp::TProcessor<
+        FProcessor_Aggro_UpdateBestAggro,
+        FCk_Handle_AggroOwner,
         FFragment_AggroOwner_Current,
+        FFragment_AggroOwner_NewBestAggro,
         CK_IGNORE_PENDING_KILL>
     {
     public:
@@ -46,7 +66,8 @@ namespace ck
         ForEachEntity(
             TimeType InDeltaT,
             HandleType InHandle,
-            FFragment_AggroOwner_Current& InCurrent) const -> void;
+            FFragment_AggroOwner_Current& InCurrent,
+            const FFragment_AggroOwner_NewBestAggro& InNewBestAggro) const -> void;
     };
 
     // --------------------------------------------------------------------------------------------------------------------
