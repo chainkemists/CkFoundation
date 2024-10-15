13fb645ed85722876ff76009036f9a5deb9997fe
Nitrogen-CK <nitrogen@chainkemists.com>
2024-10-15 14:09:12 -0400

    chore: Removed Provider structs from various features

--- Source/CkAttribute/Public/CkAttribute/VectorAttribute/CkVectorAttribute_Fragment_Data.cpp
+++ Source/CkAttribute/Public/CkAttribute/VectorAttribute/CkVectorAttribute_Fragment_Data.cpp
@@ -11,17 +11,6 @@ UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_Label_VectorAttribute, TEXT("VectorAttribute")
 // --------------------------------------------------------------------------------------------------------------------
 
 auto
-    UCk_Provider_VectorAttribute_ParamsData_PDA::
-    Get_Value_Implementation(
-        FCk_Handle InHandle) const
-    -> FCk_Fragment_VectorAttribute_ParamsData
-{
-    return {};
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-auto
     FCk_Fragment_VectorAttribute_ParamsData::
     Get_MinValue() const
     -> FVector
@@ -45,39 +34,6 @@ auto
          _MinMax);
 
     return _MaxValue;
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-auto
-    UCk_Provider_VectorAttribute_ParamsData_Literal_PDA::
-    Get_Value_Implementation(
-        FCk_Handle InHandle) const
-    -> FCk_Fragment_VectorAttribute_ParamsData
-{
-    return _Value;
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-auto
-    UCk_Provider_MultipleVectorAttribute_ParamsData_PDA::
-    Get_Value_Implementation(
-        FCk_Handle InHandle) const
-    -> FCk_Fragment_MultipleVectorAttribute_ParamsData
-{
-    return {};
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-auto
-    UCk_Provider_MultipleVectorAttribute_ParamsData_Literal_PDA::
-    Get_Value_Implementation(
-        FCk_Handle InHandle) const
-    -> FCk_Fragment_MultipleVectorAttribute_ParamsData
-{
-    return _Value;
 }
 
 // --------------------------------------------------------------------------------------------------------------------
