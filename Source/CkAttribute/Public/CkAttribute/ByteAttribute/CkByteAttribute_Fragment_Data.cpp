a40ee43caf80f32048ae9ebdd7162163b8af000c
Nitrogen-CK <nitrogen@chainkemists.com>
2024-10-15 14:09:12 -0400

    chore: Removed Provider structs from various features

--- Source/CkAttribute/Public/CkAttribute/ByteAttribute/CkByteAttribute_Fragment_Data.cpp
+++ Source/CkAttribute/Public/CkAttribute/ByteAttribute/CkByteAttribute_Fragment_Data.cpp
@@ -11,17 +11,6 @@ UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_Label_ByteAttribute, TEXT("ByteAttribute"));
 // --------------------------------------------------------------------------------------------------------------------
 
 auto
-    UCk_Provider_ByteAttribute_ParamsData_PDA::
-    Get_Value_Implementation(
-        FCk_Handle InHandle) const
-    -> FCk_Fragment_ByteAttribute_ParamsData
-{
-    return {};
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-auto
     FCk_Fragment_ByteAttribute_ParamsData::
     Get_MinValue() const
     -> uint8
@@ -45,39 +34,6 @@ auto
          _MinMax);
 
     return _MaxValue;
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-auto
-    UCk_Provider_ByteAttribute_ParamsData_Literal_PDA::
-    Get_Value_Implementation(
-        FCk_Handle InHandle) const
-    -> FCk_Fragment_ByteAttribute_ParamsData
-{
-    return _Value;
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-auto
-    UCk_Provider_MultipleByteAttribute_ParamsData_PDA::
-    Get_Value_Implementation(
-        FCk_Handle InHandle) const
-    -> FCk_Fragment_MultipleByteAttribute_ParamsData
-{
-    return {};
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-auto
-    UCk_Provider_MultipleByteAttribute_ParamsData_Literal_PDA::
-    Get_Value_Implementation(
-        FCk_Handle InHandle) const
-    -> FCk_Fragment_MultipleByteAttribute_ParamsData
-{
-    return _Value;
 }
 
 // --------------------------------------------------------------------------------------------------------------------
