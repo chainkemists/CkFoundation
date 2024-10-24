ed0d6a2be043c30a1de5f233c7b340e5f4f4b0e6
AlexObatake-CK <phosphorus@chainkemists.com>
2024-10-24 14:15:47 -0700

    feat: Add basic interaction feature
    *  Interaction is an entity that references a target and instigator, defines rules for how long to interact, and manages events based on finishing interaction
    *  Interaction typically starts an ability through construction script that will complete the interaction based on its completion policy and duration (if relevant)
       *  The ability will call the request to end interaction (if using timer or instant completion) which will then send the signal of interaction finished

---
+++ Source/CkInteraction/Public/CkInteraction/Interaction/CkInteraction_Fragment_Data.h
@@ -0,0 +1,159 @@
+#pragma once
+
+#include "CkEcs/Handle/CkHandle.h"
+#include "CkCore/Macros/CkMacros.h"
+#include "CkCore/Time/CkTime.h"
+
+#include "CkEcs/EntityConstructionScript/CkEntity_ConstructionScript.h"
+#include "CkEcs/Handle/CkHandle_TypeSafe.h"
+#include "CkEcs/Request/CkRequest_Data.h"
+
+#include <GameplayTagContainer.h>
+
+#include "CkInteraction_Fragment_Data.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ck
+{
+    class FProcessor_Interaction_Setup;
+    class FProcessor_Interaction_HandleRequests;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_InteractionDuration_FloatAttribute_Name);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UENUM(BlueprintType)
+enum class ECk_InteractionCompletionPolicy : uint8
+{
+    Instant,
+    Timed,
+	ManuallyCompleted
+};
+
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_InteractionCompletionPolicy);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType, meta=(HasNativeMake, HasNativeBreak))
+struct CKINTERACTION_API FCk_Handle_Interaction : public FCk_Handle_TypeSafe { GENERATED_BODY() CK_GENERATED_BODY_HANDLE_TYPESAFE(FCk_Handle_Interaction); };
+CK_DEFINE_CUSTOM_ISVALID_AND_FORMATTER_HANDLE_TYPESAFE(FCk_Handle_Interaction);
+
+//--------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKINTERACTION_API FCk_Fragment_Interaction_ParamsData
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Fragment_Interaction_ParamsData);
+
+public:
+    friend class ck::FProcessor_Interaction_Setup;
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true, Categories = "InteractionChannel"))
+    FGameplayTag _InteractionChannel;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    FCk_Handle _Source;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    FCk_Handle _Instigator;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    FCk_Handle _Target;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    ECk_InteractionCompletionPolicy _CompletionPolicy = ECk_InteractionCompletionPolicy::Timed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true, EditCondition="_CompletionPolicy==ECk_InteractionCompletionPolicy::Timed", EditConditionHides))
+    FCk_Time _InteractionDuration;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    TSubclassOf<UCk_Entity_ConstructionScript_PDA> _ConstructionScript;
+
+public:
+	CK_PROPERTY_GET(_InteractionChannel)
+	CK_PROPERTY_GET(_Source)
+	CK_PROPERTY_GET(_Instigator)
+	CK_PROPERTY_GET(_Target)
+	CK_PROPERTY_GET(_CompletionPolicy)
+	CK_PROPERTY_GET(_InteractionDuration)
+	CK_PROPERTY_GET(_ConstructionScript)
+
+public:
+	CK_DEFINE_CONSTRUCTORS(FCk_Fragment_Interaction_ParamsData, _InteractionChannel, _Source, _Instigator, _Target, _CompletionPolicy, _InteractionDuration, _ConstructionScript);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKINTERACTION_API FCk_Fragment_MultipleInteraction_ParamsData
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Fragment_MultipleInteraction_ParamsData);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    TArray<FCk_Fragment_Interaction_ParamsData> _InteractionParams;
+
+public:
+    CK_PROPERTY_GET(_InteractionParams)
+
+public:
+    CK_DEFINE_CONSTRUCTORS(FCk_Fragment_MultipleInteraction_ParamsData, _InteractionParams);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKINTERACTION_API FCk_Request_Interaction_EndInteraction : public FCk_Request_Base
+{
+    GENERATED_BODY()
+
+public:
+    friend class ck::FProcessor_Interaction_HandleRequests;
+
+public:
+    CK_GENERATED_BODY(FCk_Request_Interaction_EndInteraction);
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    ECk_SucceededFailed _SuccessFail;
+
+public:
+    CK_PROPERTY_GET(_SuccessFail)
+
+public:
+    CK_DEFINE_CONSTRUCTORS(FCk_Request_Interaction_EndInteraction, _SuccessFail);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+DECLARE_DYNAMIC_DELEGATE_TwoParams(
+    FCk_Delegate_Interaction_OnInteractionFinished,
+    FCk_Handle_Interaction, InInteraction,
+    ECk_SucceededFailed, SucceededFailed);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(
+    FCk_Delegate_Interaction_OnInteractionFinished_MC,
+    FCk_Handle_Interaction, InInteraction,
+    ECk_SucceededFailed, SucceededFailed);
+
+// --------------------------------------------------------------------------------------------------------------------
---
+++ Source/CkInteraction/Public/CkInteraction/Interaction/CkInteraction_Utils.h
@@ -0,0 +1,157 @@
+#pragma once
+
+#include "CkAttribute/FloatAttribute/CkFloatAttribute_Fragment_Data.h"
+
+#include "CkInteraction/Interaction/CkInteraction_Fragment_Data.h"
+#include "CkInteraction/Interaction/CkInteraction_Fragment.h"
+
+#include "CkECS/Handle/CkHandle.h"
+#include "CkNet/CkNet_Utils.h"
+#include "CkSignal/CkSignal_Fragment_Data.h"
+
+#include "CkInteraction_Utils.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS()
+class CKINTERACTION_API UCk_Utils_Interaction_UE : public UCk_Utils_Ecs_Net_UE
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_Interaction_UE);
+    CK_DEFINE_CPP_CASTCHECKED_TYPESAFE(FCk_Handle_Interaction);
+
+public:
+	using RecordOfInteractions_Utils = ck::TUtils_RecordOfEntities<ck::FFragment_RecordOfInteractions>;
+
+public:
+    UFUNCTION(BlueprintCallable,
+              Category = "Ck|Utils|Interaction",
+              DisplayName="Add Interaction")
+    static FCk_Handle_Interaction
+    Add(
+        UPARAM(ref) FCk_Handle InHandle,
+        const FCk_Fragment_Interaction_ParamsData& InParams);
+
+    UFUNCTION(BlueprintPure,
+              Category = "Ck|Utils|Interaction",
+              DisplayName="Has Interaction")
+    static bool
+    Has(
+        const FCk_Handle& InHandle);
+
+private:
+    UFUNCTION(BlueprintCallable,
+        Category = "Ck|Utils|Interaction",
+        DisplayName="[Ck][Interaction] Cast",
+        meta = (ExpandEnumAsExecs = "OutResult"))
+    static FCk_Handle_Interaction
+    DoCast(
+        UPARAM(ref) FCk_Handle& InHandle,
+        ECk_SucceededFailed& OutResult);
+
+    UFUNCTION(BlueprintPure,
+        Category = "Ck|Utils|Interaction",
+        DisplayName="[Ck][Interaction] Handle -> Interaction Handle",
+        meta = (CompactNodeTitle = "<AsInteraction>", BlueprintAutocast))
+    static FCk_Handle_Interaction
+    DoCastChecked(
+        FCk_Handle InHandle);
+
+public:
+	UFUNCTION(BlueprintCallable,
+        Category = "Ck|Utils|Interaction",
+        DisplayName="[Ck][Interaction] Request End Interaction")
+    static FCk_Handle_Interaction
+    Request_EndInteraction(
+        UPARAM(ref) FCk_Handle_Interaction& InInteraction,
+        const FCk_Request_Interaction_EndInteraction& InRequest);
+
+public:
+	UFUNCTION(BlueprintCallable,
+        Category = "Ck|Utils|Interaction",
+        DisplayName = "[Ck][Interaction] Bind To OnInteractionFinished")
+    static FCk_Handle_Interaction
+    BindTo_OnInteractionFinished(
+        UPARAM(ref) FCk_Handle_Interaction& InHandle,
+        ECk_Signal_BindingPolicy InBindingPolicy,
+        ECk_Signal_PostFireBehavior InPostFireBehavior,
+        const FCk_Delegate_Interaction_OnInteractionFinished& InDelegate);
+
+	UFUNCTION(BlueprintCallable,
+        Category = "Ck|Utils|Interaction",
+        DisplayName = "[Ck][Interaction] Unbind From OnInteractionFinished")
+    static FCk_Handle_Interaction
+    UnbindFrom_OnInteractionFinished(
+        UPARAM(ref) FCk_Handle_Interaction& InHandle,
+        const FCk_Delegate_Interaction_OnInteractionFinished& InDelegate);
+
+public:
+	UFUNCTION(BlueprintPure,
+          Category = "Ck|Utils|Interaction",
+          DisplayName="[Ck][Interaction] Try Get Interaction")
+    static FCk_Handle_Interaction
+    TryGet(
+        const FCk_Handle& InInteractionOwner,
+        const FCk_Handle& InSource,
+        const FCk_Handle& InTarget,
+        UPARAM(meta = (Categories = "InteractionChannel")) FGameplayTag InInteractionChannel);
+
+	static auto
+    ForEach(
+        FCk_Handle& InInteractionOwner,
+        const TFunction<void(FCk_Handle_Interaction)>& InFunc) -> void;
+
+public:
+	UFUNCTION(BlueprintPure,
+        Category = "Ck|Utils|Interaction",
+        DisplayName = "[Ck][Interaction] Get Interaction Source")
+    static FCk_Handle
+    Get_InteractionSource(
+        const FCk_Handle_Interaction& InHandle);
+
+    UFUNCTION(BlueprintPure,
+        Category = "Ck|Utils|Interaction",
+        DisplayName = "[Ck][Interaction] Get Interaction Instigator")
+    static FCk_Handle
+    Get_InteractionInstigator(
+        const FCk_Handle_Interaction& InHandle);
+
+	UFUNCTION(BlueprintPure,
+        Category = "Ck|Utils|Interaction",
+        DisplayName = "[Ck][Interaction] Get Interaction Target")
+    static FCk_Handle
+    Get_InteractionTarget(
+        const FCk_Handle_Interaction& InHandle);
+
+	UFUNCTION(BlueprintPure,
+        Category = "Ck|Utils|Interaction",
+        DisplayName = "[Ck][Interaction] Get Interaction Channel")
+    static const FGameplayTag&
+    Get_InteractionChannel(
+        const FCk_Handle_Interaction& InHandle);
+
+	UFUNCTION(BlueprintPure,
+        Category = "Ck|Utils|Interaction",
+        DisplayName = "[Ck][Interaction] Get Interaction Completion Policy")
+    static ECk_InteractionCompletionPolicy
+    Get_InteractionCompletionPolicy(
+        UPARAM(ref) FCk_Handle_Interaction& InHandle);
+
+	UFUNCTION(BlueprintPure,
+        Category = "Ck|Utils|Interaction",
+        DisplayName = "[Ck][Interaction] Get Interaction InteractionDuration")
+    static FCk_Time
+    Get_InteractionInteractionDuration(
+        UPARAM(ref) FCk_Handle_Interaction& InHandle);
+
+	UFUNCTION(BlueprintPure,
+        Category = "Ck|Utils|Interaction",
+        DisplayName = "[Ck][Interaction] Get Interaction InteractionDurationAttribute")
+    static FCk_Handle_FloatAttribute
+    Get_InteractionInteractionDurationAttribute(
+        UPARAM(ref) FCk_Handle_Interaction& InHandle);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
