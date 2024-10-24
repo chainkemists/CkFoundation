38bfcc7e1780d5dc387e38811b85876a8f765f85
AlexObatake-CK <phosphorus@chainkemists.com>
2024-10-24 14:18:28 -0700

    feat: Update ECS Template
    *  Add Typesafe Handle
    *  Add setup and teardown processors
    *  Add an example request
    *  Request processor set up to use example request

--- Source/CkEcsTemplate/Public/CkEcsTemplate/CkEcsTemplate_Processor.cpp
+++ Source/CkEcsTemplate/Public/CkEcsTemplate/CkEcsTemplate_Processor.cpp
@@ -10,6 +10,31 @@
 
 namespace ck
 {
+	auto
+		FProcessor_EcsTemplate_Setup::
+		DoTick(
+			TimeType InDeltaT)
+		-> void
+	{
+		TProcessor::DoTick(InDeltaT);
+
+        _TransientEntity.Clear<MarkedDirtyBy>();
+	}
+
+	auto
+		FProcessor_EcsTemplate_Setup::
+		ForEachEntity(
+			TimeType InDeltaT,
+			HandleType InHandle,
+			const FFragment_EcsTemplate_Params& InParams,
+			FFragment_EcsTemplate_Current& InCurrent) const
+		-> void
+	{
+		// Add setup logic here
+	}
+
+    // --------------------------------------------------------------------------------------------------------------------
+
     auto
         FProcessor_EcsTemplate_HandleRequests::
         DoTick(
@@ -28,11 +53,44 @@ namespace ck
         ForEachEntity(
             TimeType InDeltaT,
             HandleType InHandle,
-            FFragment_EcsTemplate_Current& InComp,
+            const FFragment_EcsTemplate_Params& InParams,
+            FFragment_EcsTemplate_Current& InCurrent,
             FFragment_EcsTemplate_Requests& InRequestsComp) const
         -> void
     {
+    	InHandle.CopyAndRemove(InRequestsComp, [&](FFragment_EcsTemplate_Requests& InRequests)
+        {
+            algo::ForEachRequest(InRequests._Requests, ck::Visitor([&](const auto& InRequest)
+            {
+                DoHandleRequest(InHandle, InParams, InCurrent, InRequest);
+            }));
+        });
+    }
+
+    auto
+	    FProcessor_EcsTemplate_HandleRequests::
+		DoHandleRequest(
+		    HandleType InHandle,
+		    const FFragment_EcsTemplate_Params& InParams,
+            FFragment_EcsTemplate_Current& InCurrent,
+		    const FCk_Request_EcsTemplate_ExampleRequest& InRequest)
+		-> void
+    {
         // Add request handling logic here
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    auto
+	    FProcessor_EcsTemplate_Teardown::
+		ForEachEntity(
+		    TimeType InDeltaT,
+		    HandleType InHandle,
+		    const FFragment_EcsTemplate_Params& InParams,
+		    FFragment_EcsTemplate_Current& InCurrent) const
+		-> void
+    {
+		// Add teardown logic here
     }
 
     // --------------------------------------------------------------------------------------------------------------------
@@ -43,10 +101,10 @@ namespace ck
             TimeType InDeltaT,
             HandleType InHandle,
             FFragment_EcsTemplate_Current& InCurrent,
-            const TObjectPtr<UCk_Fragment_EcsTemplate_Rep>& InComp) const
+            const TObjectPtr<UCk_Fragment_EcsTemplate_Rep>& InRepComp) const
         -> void
     {
-        UCk_Utils_Ecs_Net_UE::TryUpdateReplicatedFragment<UCk_Fragment_EcsTemplate_Rep>(InHandle, [&](UCk_Fragment_EcsTemplate_Rep* InRepComp)
+        UCk_Utils_Ecs_Net_UE::TryUpdateReplicatedFragment<UCk_Fragment_EcsTemplate_Rep>(InHandle, [&](UCk_Fragment_EcsTemplate_Rep* InLocalRepComp)
         {
             // Add replication logic here
         });
