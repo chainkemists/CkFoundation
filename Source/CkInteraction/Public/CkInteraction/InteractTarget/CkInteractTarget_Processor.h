98fb679fa89f641d89938bcbf6a19f375bf484ee
AlexObatake-CK <phosphorus@chainkemists.com>
2024-10-24 14:16:09 -0700

    feat: Add Interact Target feature for entities being interacted with
    *  Initiates (from request) and manages an interaction
    *  Defines rules for when interaction is possible to start
    *  Provides signals for interaction start and finish to expose the interaction handle
    *  Is added via a record so an entity can have multiple interact targets with different interaction channels
    *  Currently does not replicate since cues can handle most of the replication for cosmetics for now
       *  TODO: may eventually need to repplicated so CanInteractWith can account for current interactions on clients for cosmetics
    *  TODO: Does not properly call teardown, will need to investigate later

---
+++ Source/CkInteraction/Public/CkInteraction/InteractTarget/CkInteractTarget_Processor.h
@@ -0,0 +1,136 @@
+#pragma once
+
+#include "CkInteractTarget_Fragment.h"
+#include "CkEcs/EntityLifetime/CkEntityLifetime_Fragment.h"
+
+#include "CkEcs/Processor/CkProcessor.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ck
+{
+	class CKINTERACTION_API FProcessor_InteractTarget_Setup : public ck_exp::TProcessor<
+            FProcessor_InteractTarget_Setup,
+            FCk_Handle_InteractTarget,
+            FFragment_InteractTarget_Params,
+            FFragment_InteractTarget_Current,
+            FTag_InteractTarget_RequiresSetup,
+            CK_IGNORE_PENDING_KILL>
+    {
+    public:
+        using MarkedDirtyBy = FTag_InteractTarget_RequiresSetup;
+
+    public:
+        using TProcessor::TProcessor;
+
+    public:
+        auto
+    	DoTick(
+            TimeType InDeltaT) -> void;
+
+    public:
+        auto
+    	ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_InteractTarget_Params& InParams,
+            FFragment_InteractTarget_Current& InComp) const -> void;
+    };
+
+    class CKINTERACTION_API FProcessor_InteractTarget_HandleRequests : public ck_exp::TProcessor<
+            FProcessor_InteractTarget_HandleRequests,
+            FCk_Handle_InteractTarget,
+            FFragment_InteractTarget_Params,
+            FFragment_InteractTarget_Current,
+            FFragment_InteractTarget_Requests,
+            CK_IGNORE_PENDING_KILL>
+    {
+    public:
+        using MarkedDirtyBy = FFragment_InteractTarget_Requests;
+
+    public:
+        using TProcessor::TProcessor;
+
+    public:
+        auto
+    	DoTick(
+            TimeType InDeltaT) -> void;
+
+    public:
+        auto
+    	ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_InteractTarget_Params& InParams,
+            FFragment_InteractTarget_Current& InComp,
+            FFragment_InteractTarget_Requests& InRequestsComp) const -> void;
+
+    private:
+        auto
+        DoHandleRequest(
+            HandleType InHandle,
+            const FFragment_InteractTarget_Params& InParams,
+            FFragment_InteractTarget_Current& InCurrent,
+            const FCk_Try_InteractTarget_StartInteraction& InRequest) const -> void;
+
+    	auto
+        DoHandleRequest(
+            HandleType InHandle,
+            const FFragment_InteractTarget_Params& InParams,
+            FFragment_InteractTarget_Current& InCurrent,
+            const FCk_Request_InteractTarget_CancelInteraction& InRequest) const -> void;
+
+    private:
+    	auto
+    	OnInteractionFinished(
+    		FCk_Handle_Interaction InteractionHandle,
+    		ECk_SucceededFailed SucceededFailed) const -> void;
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+	class CKINTERACTION_API FProcessor_InteractTarget_Teardown : public ck_exp::TProcessor<
+            FProcessor_InteractTarget_Teardown,
+            FCk_Handle_InteractTarget,
+            FFragment_InteractTarget_Params,
+            FFragment_InteractTarget_Current,
+            CK_IF_INITIATE_CONFIRM_KILL>
+    {
+    public:
+        using TProcessor::TProcessor;
+
+    public:
+        auto
+    	ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_InteractTarget_Params& InParams,
+            FFragment_InteractTarget_Current& InComp) const -> void;
+    };
+
+	// --------------------------------------------------------------------------------------------------------------------
+
+    class CKINTERACTION_API FProcessor_InteractTarget_Replicate : public ck_exp::TProcessor<
+            FProcessor_InteractTarget_Replicate,
+            FCk_Handle_InteractTarget,
+            FFragment_InteractTarget_Current,
+            TObjectPtr<UCk_Fragment_InteractTarget_Rep>,
+            FTag_InteractTarget_Updated,
+            CK_IGNORE_PENDING_KILL>
+    {
+    public:
+        using MarkedDirtyBy = FTag_InteractTarget_Updated;
+
+    public:
+        using TProcessor::TProcessor;
+
+    public:
+        auto ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            FFragment_InteractTarget_Current& InCurrent,
+            const TObjectPtr<UCk_Fragment_InteractTarget_Rep>& InComp) const -> void;
+    };
+}
+
+// --------------------------------------------------------------------------------------------------------------------
