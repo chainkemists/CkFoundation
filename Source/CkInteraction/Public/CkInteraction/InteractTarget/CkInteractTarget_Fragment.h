b6eca10e35245ebe2a0851e581f806149df3308f
AlexObatake-CK <phosphorus@chainkemists.com>
2024-10-24 14:16:09 -0700

    feat: Add Interact Target feature for entities being interacted with
    *  Initiates (from request) and manages an interaction
    *  Defines rules for when interaction is possible to start
    *  Provides signals for interaction start and finish to expose the interaction handle
    *  Is added via a record so an entity can have multiple interact targets with different interaction channels
    *  Currently does not replicate since cues can handle most of the replication for cosmetics for now
       *  TODO: may eventually need to repplicated so CanInteractWith can account for current interactions on clients for cosmetics
    *  TODO: Does not properly call teardown, will need to investigate later

---
+++ Source/CkInteraction/Public/CkInteraction/InteractTarget/CkInteractTarget_Fragment.h
@@ -0,0 +1,129 @@
+#pragma once
+
+#include "CkInteractTarget_Fragment_Data.h"
+
+#include "CkEcs/Fragments/ReplicatedObjects/CkReplicatedObjects_Fragment_Params.h"
+
+#include "CkSignal/CkSignal_Macros.h"
+#include "CkRecord/Record/CkRecord_Utils.h"
+
+#include "CkInteraction/Interaction/CkInteraction_Fragment.h"
+#include "CkInteraction/Interaction/CkInteraction_Fragment_Data.h"
+
+#include "CkInteractTarget_Fragment.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+class UCk_Utils_InteractTarget_UE;
+
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ck
+{
+    CK_DEFINE_ECS_TAG(FTag_InteractTarget_RequiresSetup);
+    CK_DEFINE_ECS_TAG(FTag_InteractTarget_Updated);
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    CK_DEFINE_RECORD_OF_ENTITIES(FFragment_RecordOfInteractTargets, FCk_Handle_InteractTarget);
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    struct CKINTERACTION_API FFragment_InteractTarget_Params
+    {
+    public:
+        CK_GENERATED_BODY(FFragment_InteractTarget_Params);
+
+    public:
+        using ParamsType = FCk_Fragment_InteractTarget_ParamsData;
+
+    private:
+        ParamsType _Params;
+
+    public:
+        CK_PROPERTY_GET(_Params);
+
+    public:
+        CK_DEFINE_CONSTRUCTORS(FFragment_InteractTarget_Params, _Params);
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    struct CKINTERACTION_API FFragment_InteractTarget_Current
+    {
+    public:
+        CK_GENERATED_BODY(FFragment_InteractTarget_Current);
+
+    public:
+        friend class FProcessor_InteractTarget_Setup;
+        friend class FProcessor_InteractTarget_HandleRequests;
+        friend class FProcessor_InteractTarget_Teardown;
+        friend class UCk_Utils_InteractTarget_UE;
+
+    private:
+    	TMap<FCk_Handle_Interaction, UUtils_Signal_Interaction_OnInteractionFinished::ConnectionType> _InteractionFinishedSignals;
+
+    	ECk_EnableDisable _Enabled = ECk_EnableDisable::Enable;
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    struct CKINTERACTION_API FFragment_InteractTarget_Requests
+    {
+    public:
+        CK_GENERATED_BODY(FFragment_InteractTarget_Requests);
+
+    public:
+        friend class FProcessor_InteractTarget_HandleRequests;
+        friend class UCk_Utils_InteractTarget_UE;
+
+    public:
+    	using RequestType = std::variant
+        <
+            FCk_Try_InteractTarget_StartInteraction,
+            FCk_Request_InteractTarget_CancelInteraction
+        >;
+        using RequestList = TArray<RequestType>;
+
+    private:
+        RequestList _Requests;
+
+    public:
+        CK_PROPERTY_GET(_Requests);
+    };
+
+	// --------------------------------------------------------------------------------------------------------------------
+
+	CK_DEFINE_SIGNAL_AND_UTILS_WITH_DELEGATE(
+        CKINTERACTION_API,
+        InteractTarget_OnNewInteraction,
+        FCk_Delegate_InteractTarget_OnNewInteraction_MC,
+        FCk_Handle_InteractTarget,
+        FCk_Handle_Interaction);
+
+	CK_DEFINE_SIGNAL_AND_UTILS_WITH_DELEGATE(
+        CKINTERACTION_API,
+        InteractTarget_OnInteractionFinished,
+        FCk_Delegate_InteractTarget_OnInteractionFinished_MC,
+        FCk_Handle_InteractTarget,
+        FCk_Handle_Interaction,
+        ECk_SucceededFailed);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ck { class FProcessor_InteractTarget_Replicate; }
+
+UCLASS(Blueprintable)
+class CKINTERACTION_API UCk_Fragment_InteractTarget_Rep : public UCk_Ecs_ReplicatedObject_UE
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY_FRAGMENT_REP(UCk_Fragment_InteractTarget_Rep);
+
+public:
+    friend class ck::FProcessor_InteractTarget_Replicate;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
---
+++ Source/CkInteraction/Public/CkInteraction/InteractTarget/CkInteractTarget_Fragment_Data.h
@@ -0,0 +1,194 @@
+#pragma once
+
+#include "CkEcs/Handle/CkHandle.h"
+#include "CkCore/Macros/CkMacros.h"
+
+#include "CkEcs/EntityConstructionScript/CkEntity_ConstructionScript.h"
+#include "CkEcs/Handle/CkHandle_TypeSafe.h"
+#include "CkEcs/Request/CkRequest_Data.h"
+
+#include "CkInteraction/Interaction/CkInteraction_Fragment_Data.h"
+
+#include <GameplayTagContainer.h>
+
+#include "CkInteractTarget_Fragment_Data.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ck
+{
+    class FProcessor_InteractTarget_HandleRequests;
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UENUM(BlueprintType)
+enum class ECk_InteractionCountPerTargetPolicy : uint8
+{
+    SingleConcurrentInteraction,
+    MultipleConcurrentInteraction
+};
+
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_InteractionCountPerTargetPolicy);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UENUM(BlueprintType)
+enum class ECk_CanInteractWithResult : uint8
+{
+    CanInteractWith,
+    TargetDisabled,
+    TargetNotValid,
+    SourceNotValid,
+    ChannelMismatch,
+    AlreadyExists,
+    MultipleInteractionsDisabled,
+};
+
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_CanInteractWithResult);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType, meta=(HasNativeMake, HasNativeBreak))
+struct CKINTERACTION_API FCk_Handle_InteractTarget : public FCk_Handle_TypeSafe { GENERATED_BODY() CK_GENERATED_BODY_HANDLE_TYPESAFE(FCk_Handle_InteractTarget); };
+CK_DEFINE_CUSTOM_ISVALID_AND_FORMATTER_HANDLE_TYPESAFE(FCk_Handle_InteractTarget);
+
+//--------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKINTERACTION_API FCk_Fragment_InteractTarget_ParamsData
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Fragment_InteractTarget_ParamsData);
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true, Categories = "InteractionChannel"))
+    FGameplayTag _InteractionChannel;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    ECk_InteractionCompletionPolicy _CompletionPolicy = ECk_InteractionCompletionPolicy::Timed;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true, EditCondition="_CompletionPolicy==ECk_InteractionCompletionPolicy::Timed", EditConditionHides))
+    FCk_Time _InteractionDuration;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    TSubclassOf<UCk_Entity_ConstructionScript_PDA> _InteractionConstructionScript;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    ECk_InteractionCountPerTargetPolicy _AllowMultipleInteractions = ECk_InteractionCountPerTargetPolicy::MultipleConcurrentInteraction;
+
+public:
+	CK_PROPERTY_GET(_InteractionChannel)
+	CK_PROPERTY_GET(_CompletionPolicy)
+	CK_PROPERTY_GET(_InteractionDuration)
+	CK_PROPERTY_GET(_InteractionConstructionScript)
+	CK_PROPERTY_GET(_AllowMultipleInteractions)
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKINTERACTION_API FCk_Fragment_MultipleInteractTarget_ParamsData
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Fragment_MultipleInteractTarget_ParamsData);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    TArray<FCk_Fragment_InteractTarget_ParamsData> _InteractTargetParams;
+
+public:
+    CK_PROPERTY_GET(_InteractTargetParams)
+
+public:
+    CK_DEFINE_CONSTRUCTORS(FCk_Fragment_MultipleInteractTarget_ParamsData, _InteractTargetParams);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKINTERACTION_API FCk_Try_InteractTarget_StartInteraction : public FCk_Request_Base
+{
+    GENERATED_BODY()
+
+public:
+    friend class ck::FProcessor_InteractTarget_HandleRequests;
+
+public:
+    CK_GENERATED_BODY(FCk_Try_InteractTarget_StartInteraction);
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    FCk_Handle _InteractSource;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    FCk_Handle _InteractInstigator;
+
+public:
+	CK_PROPERTY(_InteractSource)
+	CK_PROPERTY(_InteractInstigator)
+
+public:
+    CK_DEFINE_CONSTRUCTORS(FCk_Try_InteractTarget_StartInteraction, _InteractSource, _InteractInstigator);
+};
+
+USTRUCT(BlueprintType)
+struct CKINTERACTION_API FCk_Request_InteractTarget_CancelInteraction : public FCk_Request_Base
+{
+	GENERATED_BODY()
+
+public:
+	friend class ck::FProcessor_InteractTarget_HandleRequests;
+
+public:
+	CK_GENERATED_BODY(FCk_Request_InteractTarget_CancelInteraction);
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite,
+			  meta = (AllowPrivateAccess = true))
+	FCk_Handle _InteractSource;
+
+public:
+	CK_PROPERTY(_InteractSource)
+
+public:
+	CK_DEFINE_CONSTRUCTORS(FCk_Request_InteractTarget_CancelInteraction, _InteractSource);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+DECLARE_DYNAMIC_DELEGATE_TwoParams(
+    FCk_Delegate_InteractTarget_OnNewInteraction,
+    FCk_Handle_InteractTarget, InTarget,
+    FCk_Handle_Interaction, InInteraction);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(
+    FCk_Delegate_InteractTarget_OnNewInteraction_MC,
+    FCk_Handle_InteractTarget, InTarget,
+    FCk_Handle_Interaction, InInteraction);
+
+DECLARE_DYNAMIC_DELEGATE_ThreeParams(
+    FCk_Delegate_InteractTarget_OnInteractionFinished,
+    FCk_Handle_InteractTarget, InTarget,
+    FCk_Handle_Interaction, InInteraction,
+    ECk_SucceededFailed, SucceededFailed);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_ThreeParams(
+    FCk_Delegate_InteractTarget_OnInteractionFinished_MC,
+    FCk_Handle_InteractTarget, InTarget,
+    FCk_Handle_Interaction, InInteraction,
+    ECk_SucceededFailed, SucceededFailed);
+
+// --------------------------------------------------------------------------------------------------------------------
