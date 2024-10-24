6784ab030128a470b84fb065d2293a7302c35fb6
AlexObatake-CK <phosphorus@chainkemists.com>
2024-10-24 14:18:28 -0700

    feat: Update ECS Template
    *  Add Typesafe Handle
    *  Add setup and teardown processors
    *  Add an example request
    *  Request processor set up to use example request

--- Source/CkEcsTemplate/Public/CkEcsTemplate/CkEcsTemplate_Utils.h
+++ Source/CkEcsTemplate/Public/CkEcsTemplate/CkEcsTemplate_Utils.h
@@ -17,12 +17,13 @@ class CKECSTEMPLATE_API UCk_Utils_EcsTemplate_UE : public UCk_Utils_Ecs_Net_UE
 
 public:
     CK_GENERATED_BODY(UCk_Utils_EcsTemplate_UE);
+    CK_DEFINE_CPP_CASTCHECKED_TYPESAFE(FCk_Handle_EcsTemplate);
 
 public:
     UFUNCTION(BlueprintCallable,
               Category = "Ck|Utils|EcsTemplate",
               DisplayName="Add EcsTemplate")
-    static void
+    static FCk_Handle_EcsTemplate
     Add(
         UPARAM(ref) FCk_Handle InHandle,
         const FCk_Fragment_EcsTemplate_ParamsData& InParams,
@@ -33,14 +34,34 @@ public:
               DisplayName="Has EcsTemplate")
     static bool
     Has(
-        FCk_Handle InHandle);
+        const FCk_Handle& InHandle);
+
+private:
+    UFUNCTION(BlueprintCallable,
+        Category = "Ck|Utils|EcsTemplate",
+        DisplayName="[Ck][EcsTemplate] Cast",
+        meta = (ExpandEnumAsExecs = "OutResult"))
+    static FCk_Handle_EcsTemplate
+    DoCast(
+        UPARAM(ref) FCk_Handle& InHandle,
+        ECk_SucceededFailed& OutResult);
 
     UFUNCTION(BlueprintPure,
-              Category = "Ck|Utils|EcsTemplate",
-              DisplayName="Ensure Has EcsTemplate")
-    static bool
-    Ensure(
+        Category = "Ck|Utils|EcsTemplate",
+        DisplayName="[Ck][EcsTemplate] Handle -> EcsTemplate Handle",
+        meta = (CompactNodeTitle = "<AsEcsTemplate>", BlueprintAutocast))
+    static FCk_Handle_EcsTemplate
+    DoCastChecked(
         FCk_Handle InHandle);
+
+public:
+	UFUNCTION(BlueprintCallable,
+        Category = "Ck|Utils|EcsTemplate",
+        DisplayName="[Ck][EcsTemplate] Request ExampleRequest")
+    static FCk_Handle_EcsTemplate
+    Request_ExampleRequest(
+        UPARAM(ref) FCk_Handle_EcsTemplate& InEcsTemplate,
+        const FCk_Request_EcsTemplate_ExampleRequest& InRequest);
 };
 
 // --------------------------------------------------------------------------------------------------------------------
