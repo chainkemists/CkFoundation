34553ede7cbe86c753d8e2297b098198da547970
AlexObatake-CK <phosphorus@chainkemists.com>
2024-10-24 14:15:47 -0700

    feat: Add basic interaction feature
    *  Interaction is an entity that references a target and instigator, defines rules for how long to interact, and manages events based on finishing interaction
    *  Interaction typically starts an ability through construction script that will complete the interaction based on its completion policy and duration (if relevant)
       *  The ability will call the request to end interaction (if using timer or instant completion) which will then send the signal of interaction finished

---
+++ Source/CkInteraction/CkInteraction.Build.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using UnrealBuildTool;
+
+public class CkInteraction : CkModuleRules
+{
+    public CkInteraction(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PrivateIncludePaths.AddRange(new string[] {
+            // ... add other private include paths required here ...
+        });
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "StructUtils",
+            "GameplayTags",
+
+            "CkCore",
+            "CkEcs",
+            "CkEcsExt",
+            "CkLabel",
+            "CkLog",
+            "CkNet",
+            "CkProvider",
+            "CkRecord",
+            "CkSettings",
+            "CkSignal",
+            "CkAttribute"
+        });
+    }
+}
---
+++ Source/CkInteraction/Public/CkInteraction/Interaction/ProcessorInjector/CkInteraction_ProcessorInjector.cpp
@@ -0,0 +1,18 @@
+#include "CkInteraction_ProcessorInjector.h"
+
+#include "CkInteraction/Interaction/CkInteraction_Processor.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Interaction_ProcessorInjector_UE::
+    DoInjectProcessors(
+        EcsWorldType& InWorld)
+        -> void
+{
+    InWorld.Add<ck::FProcessor_Interaction_Setup>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_Interaction_HandleRequests>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_Interaction_Teardown>(InWorld.Get_Registry());
+}
+
+// --------------------------------------------------------------------------------------------------------------------
