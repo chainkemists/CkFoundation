8cd06b23413e6271e0b3a216437ba08305ad62e0
Sulfur-CK <sulfur@chainkemists.com>
2024-12-01 17:13:51 -0800

    feat: added the Ballistics module

---
+++ Source/CkBallistics/CkBallistics.Build.cs
@@ -0,0 +1,34 @@
+using System.IO;
+using UnrealBuildTool;
+
+public class CkBallistics : CkModuleRules
+{
+    public CkBallistics(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PrivateIncludePaths.AddRange(new string[] {
+            // ... add other private include paths required here ...
+        });
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "StructUtils",
+            "GameplayTags",
+
+            "RMPP", // realistic projectile plugin
+
+            "CkCore",
+            "CkEcs",
+            "CkEcsExt",
+            "CkLabel",
+            "CkLog",
+            "CkNet",
+            "CkProvider",
+            "CkRecord",
+            "CkSettings",
+            "CkSignal",
+        });
+    }
+}
