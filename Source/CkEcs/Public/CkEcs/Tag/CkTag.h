abaa0cb9f2fcd643cbb605abab5fab4fc4f6da7b
Sulfur-CK <sulfur@chainkemists.com>
2024-12-09 17:40:59 -0800

    feat: added macro to easily define a counted Tag

--- Source/CkEcs/Public/CkEcs/Tag/CkTag.h
+++ Source/CkEcs/Public/CkEcs/Tag/CkTag.h
@@ -12,4 +12,8 @@ namespace ck
     struct _TagName_ : public ck::TTag<_TagName_> { };\
     static_assert(std::is_empty_v<_TagName_>, "Tags must not carry any data")
 
+#define CK_DEFINE_ECS_TAG_COUNTED(_TagName_)\
+    struct _TagName_ : public ck::FTag_CountedTag { };\
+    static_assert(sizeof(_TagName_) == sizeof(int32), "Counted Tags must NOT carry any additional data")
+
 // --------------------------------------------------------------------------------------------------------------------
