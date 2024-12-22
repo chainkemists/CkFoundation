82ec017ba27058c64c4d8838399b0783df2c9ef4
Nitrogen-CK <nitrogen@chainkemists.com>
2024-12-21 20:46:29 -0500

    feat: Exposed various getters for IsmProxy to BP through the utils

--- Source/CkIsmRenderer/Public/CkIsmRenderer/ProcessorInjector/CkIsmRenderer_ProcessorInjector.cpp
+++ Source/CkIsmRenderer/Public/CkIsmRenderer/ProcessorInjector/CkIsmRenderer_ProcessorInjector.cpp
@@ -12,8 +12,8 @@ auto
     -> void
 {
     InWorld.Add<ck::FProcessor_IsmRenderer_Setup>(InWorld.Get_Registry());
-
     InWorld.Add<ck::FProcessor_IsmRenderer_ClearInstances>(InWorld.Get_Registry());
+
     InWorld.Add<ck::FProcessor_IsmProxy_Static>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_IsmProxy_Dynamic>(InWorld.Get_Registry());
 }
