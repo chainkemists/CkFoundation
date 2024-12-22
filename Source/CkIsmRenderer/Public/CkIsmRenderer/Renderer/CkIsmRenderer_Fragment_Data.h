e5c98d02712dcb8e59e7286f8f148f96bc9c6e7f
Nitrogen-CK <nitrogen@chainkemists.com>
2024-12-22 00:14:14 -0500

    feat:  Expose Culling info to IsmRenderer Params

--- Source/CkIsmRenderer/Public/CkIsmRenderer/Renderer/CkIsmRenderer_Fragment_Data.h
+++ Source/CkIsmRenderer/Public/CkIsmRenderer/Renderer/CkIsmRenderer_Fragment_Data.h
@@ -15,6 +15,30 @@ CK_DEFINE_CUSTOM_ISVALID_AND_FORMATTER_HANDLE_TYPESAFE(FCk_Handle_IsmRenderer);
 // --------------------------------------------------------------------------------------------------------------------
 
 USTRUCT(BlueprintType)
+struct CKISMRENDERER_API FCk_IsmRenderer_CullingInfo
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_IsmRenderer_CullingInfo);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    float _InstanceCullDistance_Start = 0.0f;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    float _InstanceCullDistance_End = 0.0f;
+
+public:
+    CK_PROPERTY_GET(_InstanceCullDistance_Start);
+    CK_PROPERTY_GET(_InstanceCullDistance_End);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
 struct CKISMRENDERER_API FCk_Fragment_IsmRenderer_ParamsData
 {
     GENERATED_BODY()
@@ -40,6 +64,10 @@ private:
     ECk_EnableDisable _CastShadows = ECk_EnableDisable::Disable;
 
     UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    FCk_IsmRenderer_CullingInfo _CullingInfo;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
               meta = (AllowPrivateAccess = true, UIMin = 0, ClampMin = 0))
     int32 _NumCustomData = 0;
 
@@ -48,6 +76,7 @@ public:
     CK_PROPERTY_GET(_Mesh);
     CK_PROPERTY_GET(_Collision);
     CK_PROPERTY_GET(_CastShadows);
+    CK_PROPERTY_GET(_CullingInfo);
     CK_PROPERTY_GET(_NumCustomData);
 };
 
