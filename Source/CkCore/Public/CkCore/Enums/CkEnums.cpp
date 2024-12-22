17d5435a3d3bb8fd20b6c57c770cc7f27e75b6f1
Nitrogen-CK <nitrogen@chainkemists.com>
2024-12-21 22:13:27 -0500

    style: Formatting

--- Source/CkCore/Public/CkCore/Enums/CkEnums.cpp
+++ Source/CkCore/Public/CkCore/Enums/CkEnums.cpp
@@ -3,44 +3,34 @@
 // --------------------------------------------------------------------------------------------------------------------
 
 auto
-    UCk_Utils_Enum_UE::ConvertToECollisionEnabled(
+    UCk_Utils_Enum_UE::
+    ConvertToECollisionEnabled(
         ECk_Collision InEnum)
-        -> ECollisionEnabled::Type
+    -> ECollisionEnabled::Type
 {
     switch (InEnum)
     {
-        case ECk_Collision::NoCollision:
-            return ECollisionEnabled::NoCollision;
-        case ECk_Collision::QueryOnly:
-            return ECollisionEnabled::QueryOnly;
-        case ECk_Collision::PhysicsOnly:
-            return ECollisionEnabled::PhysicsOnly;
-        case ECk_Collision::QueryAndPhysics:
-            return ECollisionEnabled::QueryAndPhysics;
-        default:
-            return ECollisionEnabled::NoCollision; // Default case
+        case ECk_Collision::NoCollision:     return ECollisionEnabled::NoCollision;
+        case ECk_Collision::QueryOnly:       return ECollisionEnabled::QueryOnly;
+        case ECk_Collision::PhysicsOnly:     return ECollisionEnabled::PhysicsOnly;
+        case ECk_Collision::QueryAndPhysics: return ECollisionEnabled::QueryAndPhysics;
+        default:                             return ECollisionEnabled::NoCollision;
     }
 }
 
 auto
-    UCk_Utils_Enum_UE::ConvertFromECollisionEnabled(
+    UCk_Utils_Enum_UE::
+    ConvertFromECollisionEnabled(
         ECollisionEnabled::Type InEnum)
-        -> ECk_Collision
+    -> ECk_Collision
 {
-    using EMyCollisionEnabled = ECk_Collision;
-
     switch (InEnum)
     {
-        case ECollisionEnabled::NoCollision:
-            return EMyCollisionEnabled::NoCollision;
-        case ECollisionEnabled::QueryOnly:
-            return EMyCollisionEnabled::QueryOnly;
-        case ECollisionEnabled::PhysicsOnly:
-            return EMyCollisionEnabled::PhysicsOnly;
-        case ECollisionEnabled::QueryAndPhysics:
-            return EMyCollisionEnabled::QueryAndPhysics;
-        default:
-            return EMyCollisionEnabled::NoCollision; // Default case
+        case ECollisionEnabled::NoCollision:     return ECk_Collision::NoCollision;
+        case ECollisionEnabled::QueryOnly:       return ECk_Collision::QueryOnly;
+        case ECollisionEnabled::PhysicsOnly:     return ECk_Collision::PhysicsOnly;
+        case ECollisionEnabled::QueryAndPhysics: return ECk_Collision::QueryAndPhysics;
+        default:                                 return ECk_Collision::NoCollision;
     }
 }
 
