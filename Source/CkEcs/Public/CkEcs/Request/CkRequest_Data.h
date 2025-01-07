1742b6dd427aeaa84d38fa34be5787449cea2ecb
Sulfur-CK <sulfur@chainkemists.com>
2025-01-06 21:11:03 -0800

    feat: added C++ variant of the Request_Base for use with C++ only Fragments

--- Source/CkEcs/Public/CkEcs/Request/CkRequest_Data.h
+++ Source/CkEcs/Public/CkEcs/Request/CkRequest_Data.h
@@ -8,6 +8,39 @@
 
 // --------------------------------------------------------------------------------------------------------------------
 
+namespace ck
+{
+    struct CKECS_API FRequest_Base
+    {
+    public:
+        CK_GENERATED_BODY(FRequest_Base);
+
+    private:
+        mutable FCk_Handle _RequestHandle;
+
+    public:
+        auto
+            PopulateRequestHandle(
+                const FCk_Handle& InOwner) const
+                -> FCk_Handle;
+
+        auto
+            GetAndDestroyRequestHandle() const
+                -> FCk_Handle;
+
+        auto
+            Get_IsRequestHandleValid() const
+                -> bool;
+
+        auto
+            Request_TransferHandleToOther(
+                const FRequest_Base& InOther) const
+                -> void;
+    };
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
 USTRUCT(BlueprintType)
 struct CKECS_API FCk_Request_Base
 {
@@ -16,22 +49,32 @@ struct CKECS_API FCk_Request_Base
 public:
     CK_GENERATED_BODY(FCk_Request_Base);
 
+public:
+    auto
+        PopulateRequestHandle(
+            const FCk_Handle& InOwner) const
+            -> FCk_Handle;
+
+    auto
+        GetAndDestroyRequestHandle() const
+            -> FCk_Handle;
+
+    auto
+        Get_IsRequestHandleValid() const
+            -> bool;
+
+    auto
+        Request_TransferHandleToOther(
+            const ck::FRequest_Base& InOther) const
+            -> void;
+
+    auto
+        Request_TransferHandleToOther(
+            const ThisType& InOther) const
+            -> void;
+
 private:
-    mutable FCk_Handle _RequestHandle;
-
-public:
-    auto
-    PopulateRequestHandle(
-        FCk_Handle InOwner) const -> FCk_Handle;
-
-    auto
-    GetAndDestroyRequestHandle() const -> FCk_Handle;
-
-    auto
-    Get_IsRequestHandleValid() const -> bool;
-
-public:
-    // CK_PROPERTY_GET(_RequestHandle);
+    ck::FRequest_Base _RequestBase;
 };
 
 // --------------------------------------------------------------------------------------------------------------------
