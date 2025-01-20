9cdb9fabeecbb103f4f903652e3ddac59fb3d083
AlexObatake-CK <phosphorus@chainkemists.com>
2025-01-20 06:48:40 -0800

    fix: Play Montage task no longer tries to handle cases where mesh can't tick
    *  Before the play montage task could try to force the mesh to tick while active, but this was removed to reduce risk
    *  Failed output pin now has an enum for the error reason

---
+++ Source/CkAnimation/Public/CkAnimation/CkAnimation_Common.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+
+#include "CkCore/Enums/CkEnums.h"
+
+#include "CkAnimation_Common.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UENUM(BlueprintType)
+enum class ECk_PlayMontageFailureReason : uint8
+{
+    SkeletonMismatch,
+    MeshTickDisabled,
+    MeshComponentCannotTickOnServer
+};
+
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_PlayMontageFailureReason);
+
+// --------------------------------------------------------------------------------------------------------------------
