6644462f0a17410ea25fc5d379d08f32f7143acb
Sulfur-CK <sulfur@chainkemists.com>
2025-02-05 13:47:31 -0800

    feat: added formatter pointer forwarder variant which checks for nullptr only

--- Source/CkCore/Public/CkCore/Format/CkFormat.h
+++ Source/CkCore/Public/CkCore/Format/CkFormat.h
@@ -139,7 +139,7 @@ namespace ck
 #define CK_DEFINE_CUSTOM_FORMATTER_PTR_FORWARDER(_Type_, _LambdaToReturnInvalidObj_)\
 namespace ck { namespace ck_format_detail {                                         \
     inline auto&                                                                    \
-    ForwarderForPointers(const _Type_* InObj)                                      \
+    ForwarderForPointers(const _Type_* InObj)                                       \
     {                                                                               \
         if (ck::IsValid(InObj))                                                     \
         { return *InObj; }                                                          \
@@ -148,85 +148,97 @@ namespace ck { namespace ck_format_detail {                                     
     }                                                                               \
 }}
 
+#define CK_DEFINE_CUSTOM_FORMATTER_PTR_FORWARDER_NULLPTR_VALIDITY(_Type_, _LambdaToReturnInvalidObj_) \
+namespace ck { namespace ck_format_detail {                                                           \
+    inline auto&                                                                                      \
+    ForwarderForPointers(const _Type_* InObj)                                                         \
+    {                                                                                                 \
+        if (ck::IsValid(InObj, ck::IsValid_Policy_NullptrOnly{}))                                     \
+        { return *InObj; }                                                                            \
+                                                                                                      \
+        return _LambdaToReturnInvalidObj_();                                                          \
+    }                                                                                                 \
+}}
+
 // --------------------------------------------------------------------------------------------------------------------
 
 #if NOT CK_FORMAT_FORCE_DETAILED
 #define CK_DEFINE_CUSTOM_FORMATTER_WITH_DETAILS_TARGS(_TemplateArgs_, _Type_, _Lambda_, _DetailsLambda_)  \
-namespace fmt {                                                                     \
-                                                                                    \
-template<_TemplateArgs_>                                                            \
-struct formatter<_Type_, TCHAR>                                                     \
-{                                                                                   \
-    bool printDetails = false;                                                      \
-                                                                                    \
-    template <typename ParseContext>                                                \
-    constexpr auto parse(ParseContext& ctx)                                         \
-    {                                                                               \
-        auto it = ctx.begin(), end = ctx.end();                                     \
-        if (it == end) return it;                                                   \
-        if (*it == 'd')                                                             \
-        { printDetails = true; ++it;}                                               \
-        return it;                                                                  \
-    }                                                                               \
-                                                                                    \
-    template<typename FormatContext>                                                \
-    auto format(const _Type_& InObj, FormatContext& ctx)                            \
-    {                                                                               \
-        if (NOT printDetails)                                                       \
-        { return fmt::format_to(ctx.out(), TEXT("{}"), _Lambda_()); }               \
-                                                                                    \
-        return fmt::format_to(ctx.out(), TEXT("{}"), _DetailsLambda_());            \
-    }                                                                               \
-};                                                                                  \
+namespace fmt {                                                                                           \
+                                                                                                          \
+template<_TemplateArgs_>                                                                                  \
+struct formatter<_Type_, TCHAR>                                                                           \
+{                                                                                                         \
+    bool printDetails = false;                                                                            \
+                                                                                                          \
+    template <typename ParseContext>                                                                      \
+    constexpr auto parse(ParseContext& ctx)                                                               \
+    {                                                                                                     \
+        auto it = ctx.begin(), end = ctx.end();                                                           \
+        if (it == end) return it;                                                                         \
+        if (*it == 'd')                                                                                   \
+        { printDetails = true; ++it;}                                                                     \
+        return it;                                                                                        \
+    }                                                                                                     \
+                                                                                                          \
+    template<typename FormatContext>                                                                      \
+    auto format(const _Type_& InObj, FormatContext& ctx)                                                  \
+    {                                                                                                     \
+        if (NOT printDetails)                                                                             \
+        { return fmt::format_to(ctx.out(), TEXT("{}"), _Lambda_()); }                                     \
+                                                                                                          \
+        return fmt::format_to(ctx.out(), TEXT("{}"), _DetailsLambda_());                                  \
+    }                                                                                                     \
+};                                                                                                        \
 }
 #else
 #define CK_DEFINE_CUSTOM_FORMATTER_WITH_DETAILS_TARGS(_TemplateArgs_, _Type_, _Lambda_, _DetailsLambda_)  \
-namespace fmt {                                                                     \
-                                                                                    \
-template<_TemplateArgs_>                                                            \
-struct formatter<_Type_, TCHAR>                                                     \
-{                                                                                   \
-    template <typename ParseContext>                                                \
-    constexpr auto parse(ParseContext& ctx)                                         \
-    {                                                                               \
-        return ctx.begin();                                                         \
-    }                                                                               \
-                                                                                    \
-    template<typename FormatContext>                                                \
-    auto format(const _Type_& InObj, FormatContext& ctx)                            \
-    {                                                                               \
-        return fmt::format_to(ctx.out(), TEXT("{}"), _DetailsLambda_());            \
-    }                                                                               \
-};                                                                                  \
+namespace fmt {                                                                                           \
+                                                                                                          \
+template<_TemplateArgs_>                                                                                  \
+struct formatter<_Type_, TCHAR>                                                                           \
+{                                                                                                         \
+    template <typename ParseContext>                                                                      \
+    constexpr auto parse(ParseContext& ctx)                                                               \
+    {                                                                                                     \
+        return ctx.begin();                                                                               \
+    }                                                                                                     \
+                                                                                                          \
+    template<typename FormatContext>                                                                      \
+    auto format(const _Type_& InObj, FormatContext& ctx)                                                  \
+    {                                                                                                     \
+        return fmt::format_to(ctx.out(), TEXT("{}"), _DetailsLambda_());                                  \
+    }                                                                                                     \
+};                                                                                                        \
 }
 #endif
 
-#define CK_DEFINE_CUSTOM_FORMATTER_T(_Type_, _Lambda_)                              \
+#define CK_DEFINE_CUSTOM_FORMATTER_T(_Type_, _Lambda_)                                                    \
 CK_DEFINE_CUSTOM_FORMATTER_WITH_DETAILS_TARGS(typename T, _Type_, _Lambda_, _Lambda_)
 
 #define CK_DEFINE_CUSTOM_FORMATTER_WITH_DETAILS_T(_Type_, _Lambda_, _DetailsLambda_)\
 CK_DEFINE_CUSTOM_FORMATTER_WITH_DETAILS_TARGS(typename T, _Type_, _Lambda_, _DetailsLambda_)
 
-#define CK_DEFINE_CUSTOM_FORMATTER_WITH_DETAILS(_Type_, _Lambda_, _DetailsLambda_)  \
+#define CK_DEFINE_CUSTOM_FORMATTER_WITH_DETAILS(_Type_, _Lambda_, _DetailsLambda_)                        \
 CK_DEFINE_CUSTOM_FORMATTER_WITH_DETAILS_TARGS(,_Type_, _Lambda_, _DetailsLambda_)
 
-#define CK_DEFINE_CUSTOM_FORMATTER(_Type_, _Lambda_)                                \
+#define CK_DEFINE_CUSTOM_FORMATTER(_Type_, _Lambda_)                                                      \
 CK_DEFINE_CUSTOM_FORMATTER_WITH_DETAILS(_Type_, _Lambda_, _Lambda_)
 
-#define CK_DEFINE_CUSTOM_FORMATTER_ENUM(_Type_)                                     \
-CK_DEFINE_CUSTOM_FORMATTER(_Type_, [&]()                                            \
-{                                                                                   \
-    return UEnum::GetDisplayValueAsText(InObj).ToString();                          \
-});                                                                                 \
-namespace ck                                                                        \
-{                                                                                   \
-                                                                                    \
-template <>                                                                         \
-struct FEnumToString<_Type_>                                                        \
-{                                                                                   \
-    static FName Get() { return TEXT(#_Type_); }                                    \
-    static const TCHAR* Get_AsTChar() { return TEXT(#_Type_); }                     \
-};                                                                                  \
+#define CK_DEFINE_CUSTOM_FORMATTER_ENUM(_Type_)                                                           \
+CK_DEFINE_CUSTOM_FORMATTER(_Type_, [&]()                                                                  \
+{                                                                                                         \
+    return UEnum::GetDisplayValueAsText(InObj).ToString();                                                \
+});                                                                                                       \
+namespace ck                                                                                              \
+{                                                                                                         \
+                                                                                                          \
+template <>                                                                                               \
+struct FEnumToString<_Type_>                                                                              \
+{                                                                                                         \
+    static FName Get() { return TEXT(#_Type_); }                                                          \
+    static const TCHAR* Get_AsTChar() { return TEXT(#_Type_); }                                           \
+};                                                                                                        \
 }
 
 // --------------------------------------------------------------------------------------------------------------------
