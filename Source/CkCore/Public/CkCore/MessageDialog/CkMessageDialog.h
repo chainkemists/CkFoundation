6bd3316a3bfddf7fc97645087a8a895152e7c7e4
Sulfur-CK <sulfur@chainkemists.com>
2025-02-05 16:18:47 -0800

    chore: cleanup of deprecated functions and comments

--- Source/CkCore/Public/CkCore/MessageDialog/CkMessageDialog.h
+++ Source/CkCore/Public/CkCore/MessageDialog/CkMessageDialog.h
@@ -146,80 +146,27 @@ public:
             _AccessibleParams = FAccessibleWidgetData(EAccessibleBehavior::Auto);
         }
 
-        /*********** Functional ***********/
-
-        /** Title to display for the dialog. */
         SLATE_ARGUMENT(FText, Title)
-
-        /** The content to display above the button; icon is optionally created to the left of it.  */
         SLATE_NAMED_SLOT(FArguments, Content)
-
-        /** The buttons that this dialog should have. One or more buttons must be added.*/
         SLATE_ARGUMENT(TArray<FButton>, Buttons)
-
-        /** Event triggered when the dialog is closed, either because one of the buttons is pressed, or the windows is closed. */
         SLATE_EVENT(FSimpleDelegate, OnClosed)
-
-        /** Provides default values for SWindow::FArguments not overriden by SCustomDialog. */
         SLATE_ARGUMENT(SWindow::FArguments, WindowArguments)
-
-        /** Whether to automatically close this window when any button is pressed (default: true) */
         SLATE_ARGUMENT(bool, AutoCloseOnButtonPress)
 
-        /*********** Cosmetic ***********/
-
-        /** Optional icon to display (default: empty, translucent)*/
         SLATE_ATTRIBUTE(const FSlateBrush*, Icon)
-
-        /** When specified, ignore the brushes size and report the DesiredSizeOverride as the desired image size (default: use icon size) */
         SLATE_ATTRIBUTE(TOptional<FVector2D>, IconDesiredSizeOverride)
-
-        /** Alignment of icon (default: HAlign_Left)*/
         SLATE_ARGUMENT(EHorizontalAlignment, HAlignIcon)
-
-        /** Alignment of icon (default: VAlign_Center) */
         SLATE_ARGUMENT(EVerticalAlignment, VAlignIcon)
-
-        /** Custom widget placed before the buttons */
         SLATE_NAMED_SLOT(FArguments, BeforeButtons)
-
-        /** HAlign to use for Button Box slot (default: HAlign_Left) */
         SLATE_ARGUMENT(EHorizontalAlignment, HAlignButtonBox)
-
-        /** VAlign to use for Button Box  slot (default: VAlign_Center) */
         SLATE_ARGUMENT(EVerticalAlignment, VAlignButtonBox)
-
-        /** Padding to apply to the widget embedded in the window, i.e. to all widgets contained in the window (default: {4,4,4,4} )*/
         SLATE_ATTRIBUTE(FMargin, RootPadding)
-
-        /** Padding to apply around the layout holding the buttons (default: {20,16,0,0}) */
         SLATE_ATTRIBUTE(FMargin, ButtonAreaPadding)
-
-        /** Padding to apply to DialogContent - you can use it to move away from the icon (default: {0,0,0,0}) */
         SLATE_ATTRIBUTE(FMargin, ContentAreaPadding)
-
-        /** Should this dialog use a scroll box for over-sized content? (default: true) */
         SLATE_ARGUMENT(bool, UseScrollBox)
-
-        /** Max height for the scroll box (default: 300) */
         SLATE_ARGUMENT(int32, ScrollBoxMaxHeight)
-
-        /** HAlign to use for Content slot (default: HAlign_Left) */
         SLATE_ARGUMENT(EHorizontalAlignment, HAlignContent)
-
-        /** VAlign to use for Content slot (default: VAlign_Center) */
         SLATE_ARGUMENT(EVerticalAlignment, VAlignContent)
-
-        /********** Legacy - do not use **********/
-
-        /** Optional icon to display in the dialog (default: none) */
-        UE_DEPRECATED(5.1, "Use Icon() instead")
-        CKCORE_API FArguments&
-        IconBrush(
-            FName InIconBrush);
-
-        /** Content for the dialog (deprecated - use Content instead)*/
-        SLATE_ARGUMENT_DEPRECATED(TSharedPtr<SWidget>, DialogContent, 5.1, "Use Content() instead.")
 
     SLATE_END_ARGS()
 
