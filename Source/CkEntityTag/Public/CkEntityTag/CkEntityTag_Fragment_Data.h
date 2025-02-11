d868a754c1b2c9745c3031e8ffc05552f35de9d5
Sulfur-CK <sulfur@chainkemists.com>
2025-02-11 06:07:05 -0800

    feat: complete EntityTag feature which allows Entities to be tagged with the registry at runtime using a FGameplayTag
    notes: the feature currently uses the Abilities Stresstest gym for
    testing

---
+++ Source/CkEntityTag/Public/CkEntityTag/CkEntityTag_Fragment_Data.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include "CkCore/Macros/CkMacros.h"
+
+#include "CkEcs/Handle/CkHandle.h"
+#include "CkEcs/Handle/CkHandle_TypeSafe.h"
+
+#include <GameplayTagContainer.h>
+
+#include "CkEntityTag_Fragment_Data.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// Not yet useful for this feature
+//USTRUCT(BlueprintType, meta=(HasNativeMake, HasNativeBreak))
+//struct CKENTITYTAG_API FCk_Handle_EntityTag : public FCk_Handle_TypeSafe { GENERATED_BODY() CK_GENERATED_BODY_HANDLE_TYPESAFE(FCk_Handle_EntityTag); };
+//CK_DEFINE_CUSTOM_ISVALID_AND_FORMATTER_HANDLE_TYPESAFE(FCk_Handle_EntityTag);
+
+//--------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKENTITYTAG_API FCk_Fragment_EntityTag_ParamsData
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Fragment_EntityTag_ParamsData);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true))
+    FGameplayTag _Tag;
+
+public:
+    CK_PROPERTY_GET(_Tag);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
