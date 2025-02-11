7c2db0cce14f4e517b6eb10e0f5e6adfbeb8e8d2
Sulfur-CK <sulfur@chainkemists.com>
2025-02-11 06:05:35 -0800

    feat: added the EntityTag module

---
+++ Source/CkEntityTag/CkEntityTag_Module.cpp
@@ -0,0 +1,15 @@
+#include "CkEntityTag_Module.h"
+
+#define LOCTEXT_NAMESPACE "FCkEntityTagModule"
+
+void FCkEntityTagModule::StartupModule()
+{
+}
+
+void FCkEntityTagModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkEntityTagModule, CkEntityTag)
