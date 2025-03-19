ecc5760e4ec6a3e6673c5cea9e81b90b444d186e
Sulfur-CK <sulfur@chainkemists.com>
2025-03-18 19:43:39 -0700

    refactor: moved the Transform processors to the main injector
    notes: the Transform processors should technically have never been in
    the HandleRequest injector which is usually in the pumps

--- Source/CkEcsExt/Public/CkEcsExt/ProcessorInjector/CkEcsExtProcessorInjector.cpp
+++ Source/CkEcsExt/Public/CkEcsExt/ProcessorInjector/CkEcsExtProcessorInjector.cpp
@@ -11,6 +11,9 @@ auto
     -> void
 {
     InWorld.Add<ck::FProcessor_Transform_SyncFromActor>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_Transform_InterpolateToGoal_Location>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_Transform_InterpolateToGoal_Rotation>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_Transform_HandleRequests>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_Transform_SyncToActor>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_Transform_FireSignals>(InWorld.Get_Registry());
 }
@@ -34,9 +37,7 @@ auto
         EcsWorldType& InWorld)
     -> void
 {
-    InWorld.Add<ck::FProcessor_Transform_InterpolateToGoal_Location>(InWorld.Get_Registry());
-    InWorld.Add<ck::FProcessor_Transform_InterpolateToGoal_Rotation>(InWorld.Get_Registry());
-    InWorld.Add<ck::FProcessor_Transform_HandleRequests>(InWorld.Get_Registry());
+    // Empty for now
 }
 
 // --------------------------------------------------------------------------------------------------------------------
