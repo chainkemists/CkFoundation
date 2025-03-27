7af1768109db33ca91744ba45eb06f1f7c963261
Sulfur-CK <sulfur@chainkemists.com>
2025-03-27 00:24:17 -0700

    feat: added an overload for EntityScript to spawn an Entity with a TSubclassOf

--- Source/CkEcs/Public/CkEcs/EntityScript/CkEntityScript_Utils.cpp
+++ Source/CkEcs/Public/CkEcs/EntityScript/CkEntityScript_Utils.cpp
@@ -8,9 +8,20 @@
 
 auto
     UCk_Utils_EntityScript_UE::
-    Request_SpawnEntity(
+    DoRequest_SpawnEntity(
         FCk_Handle& InLifetimeOwner,
         UCk_EntityScript_UE* InEntityScript)
+    -> FCk_Handle
+{
+    return Request_SpawnEntity(InLifetimeOwner, InEntityScript->GetClass(), InEntityScript);
+}
+
+auto
+    UCk_Utils_EntityScript_UE::
+    Request_SpawnEntity(
+        const FCk_Handle& InLifetimeOwner,
+        const TSubclassOf<UCk_EntityScript_UE>& InEntityScript,
+        UCk_EntityScript_UE* InEntityScriptTemplate)
     -> FCk_Handle
 {
     CK_ENSURE_IF_NOT(ck::IsValid(InEntityScript), TEXT("Invalid EntityScript supplied, cannot request to Spawn Entity"))
@@ -19,9 +30,8 @@ auto
     auto RequestEntity = UCk_Utils_EntityLifetime_UE::Request_CreateEntity(InLifetimeOwner);
     auto NewEntity = UCk_Utils_EntityLifetime_UE::Request_CreateEntity(InLifetimeOwner);
 
-    const auto Request = FCk_Request_EntityScript_SpawnEntity{InEntityScript->GetClass(), NewEntity}
-                            .Set_EntityScriptTemplate(InEntityScript)
-                            .Set_Owner(InLifetimeOwner);
+    const auto Request = FCk_Request_EntityScript_SpawnEntity{NewEntity, InLifetimeOwner, InEntityScript}
+                            .Set_EntityScriptTemplate(InEntityScriptTemplate);
 
     RequestEntity.Add<ck::FFragment_EntityScript_RequestSpawnEntity>(Request);
 
