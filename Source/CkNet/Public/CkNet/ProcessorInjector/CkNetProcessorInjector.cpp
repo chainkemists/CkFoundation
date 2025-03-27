5b244218843fe2985912b435d1a8ac303d7c0c3c
Sulfur-CK <sulfur@chainkemists.com>
2025-03-27 00:30:33 -0700

    feat: added a new injector for the new ReplicationDriver EntityScript processor

--- Source/CkNet/Public/CkNet/ProcessorInjector/CkNetProcessorInjector.cpp
+++ Source/CkNet/Public/CkNet/ProcessorInjector/CkNetProcessorInjector.cpp
@@ -1,5 +1,6 @@
 #include "CkNetProcessorInjector.h"
 
+#include "CkNet/EntityReplicationDriver/CkEntityReplicationDriver_Processor.h"
 #include "CkNet/TimeSync/CkNetTimeSync_Processor.h"
 
 // --------------------------------------------------------------------------------------------------------------------
@@ -13,6 +14,17 @@ auto
     InWorld.Add<ck::FProcessor_NetTimeSync_HandleRequests>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_NetTimeSync_OnNetworkClockSynchronized>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_NetTimeSync_FirstSync>(InWorld.Get_Registry());
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Net_ProcessorInjector_Replicate::
+    DoInjectProcessors(
+        EcsWorldType& InWorld)
+    -> void
+{
+    InWorld.Add<ck::FProcessor_ReplicationDriver_ReplicateEntityScript>(InWorld.Get_Registry());
 }
 
 // --------------------------------------------------------------------------------------------------------------------
