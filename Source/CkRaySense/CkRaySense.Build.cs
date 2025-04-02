19765bc8b56ab75e616d259a681772f54a3ba318
Sulfur-CK <sulfur@chainkemists.com>
2025-04-01 20:12:27 -0700

    feat: added the RaySense module

---
+++ Source/CkRaySense/CkRaySense.Build.cs
@@ -0,0 +1,33 @@
+using System.IO;
+using UnrealBuildTool;
+
+public class CkRaySense : CkModuleRules
+{
+    public CkRaySense(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PrivateIncludePaths.AddRange(new string[] {
+            // ... add other private include paths required here ...
+        });
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "StructUtils",
+            "GameplayTags",
+
+            "CkCore",
+            "CkEcs",
+            "CkEcsExt",
+            "CkLabel",
+            "CkLog",
+            "CkNet",
+            "CkProvider",
+            "CkRecord",
+            "CkSettings",
+            "CkShapes",
+            "CkSignal",
+        });
+    }
+}
---
+++ Source/CkRaySense/CkRaySense_Module.cpp
@@ -0,0 +1,15 @@
+#include "CkRaySense_Module.h"
+
+#define LOCTEXT_NAMESPACE "FCkRaySenseModule"
+
+void FCkRaySenseModule::StartupModule()
+{
+}
+
+void FCkRaySenseModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkRaySenseModule, CkRaySense)
