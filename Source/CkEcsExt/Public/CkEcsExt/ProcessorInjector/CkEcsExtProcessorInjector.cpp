3ca457b2e2e933af5f85ba7200dae4b1012e1f69
Sulfur-CK <sulfur@chainkemists.com>
2025-04-10 09:56:22 -0700

    feat: moved the EntityScript injectors to Ecs since EntityScript is now in CkEcs

--- Source/CkEcsExt/Public/CkEcsExt/ProcessorInjector/CkEcsExtProcessorInjector.cpp
+++ Source/CkEcsExt/Public/CkEcsExt/ProcessorInjector/CkEcsExtProcessorInjector.cpp
@@ -1,7 +1,5 @@
 #include "CkEcsExtProcessorInjector.h"
 
-#include "CkEcs/EntityScript/CkEntityScript_Processor.h"
-
 #include "CkEcsExt/Transform/CkTransform_Processor.h"
 
 // --------------------------------------------------------------------------------------------------------------------
@@ -31,29 +29,6 @@ auto
 {
     InWorld.Add<ck::FProcessor_Transform_Replicate>(InWorld.Get_Registry());
     InWorld.Add<ck::FProcessor_Transform_Cleanup>(InWorld.Get_Registry());
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-auto
-    UCk_EcsExt_ProcessorInjector_Requests_UE::
-    DoInjectProcessors(
-        EcsWorldType& InWorld)
-    -> void
-{
-    InWorld.Add<ck::FProcessor_EntityScript_SpawnEntity_HandleRequests>(InWorld.Get_Registry());
-    InWorld.Add<ck::FProcessor_EntityScript_BeginPlay>(InWorld.Get_Registry());
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-auto
-    UCk_EcsExt_ProcessorInjector_Teardown_UE::
-    DoInjectProcessors(
-        EcsWorldType& InWorld)
-    -> void
-{
-    InWorld.Add<ck::FProcessor_EntityScript_EndPlay>(InWorld.Get_Registry());
 }
 
 // --------------------------------------------------------------------------------------------------------------------
