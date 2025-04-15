4f68390e5c1f89dee507e24d670f02c700fe3ba3
Nitrogen-CK <nitrogen@chainkemists.com>
2025-04-15 17:22:55 -0400

    feat: Added new processor injectors for RaySense requests

--- Source/CkRaySense/Public/CkRaySense/ProcessorInjector/CkRaySense_ProcessorInjector.h
+++ Source/CkRaySense/Public/CkRaySense/ProcessorInjector/CkRaySense_ProcessorInjector.h
@@ -18,3 +18,16 @@ protected:
 };
 
 // --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable, NotBlueprintType)
+class CKRAYSENSE_API UCk_RaySense_ProcessorInjector_Requests_UE : public UCk_EcsWorld_ProcessorInjector_Base_UE
+{
+    GENERATED_BODY()
+
+protected:
+    auto
+    DoInjectProcessors(
+        EcsWorldType& InWorld) -> void override;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
