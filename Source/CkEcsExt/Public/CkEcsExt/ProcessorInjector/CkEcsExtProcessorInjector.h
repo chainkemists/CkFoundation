470e41196b3fb49deb9f9c81d1a4aeacdece0a50
Sulfur-CK <sulfur@chainkemists.com>
2025-04-15 18:32:01 -0700

    refactor: TransformProcessors are now in their own Injectors to help order them more granularly

--- Source/CkEcsExt/Public/CkEcsExt/ProcessorInjector/CkEcsExtProcessorInjector.h
+++ Source/CkEcsExt/Public/CkEcsExt/ProcessorInjector/CkEcsExtProcessorInjector.h
@@ -20,6 +20,45 @@ protected:
 // --------------------------------------------------------------------------------------------------------------------
 
 UCLASS(NotBlueprintable, NotBlueprintType)
+class CKECSEXT_API UCk_Transform_ProcessorInjector_SyncFromAndInterpolate : public UCk_EcsWorld_ProcessorInjector_Base_UE
+{
+    GENERATED_BODY()
+
+protected:
+    auto
+    DoInjectProcessors(
+        EcsWorldType& InWorld) -> void override;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable, NotBlueprintType)
+class CKECSEXT_API UCk_Transform_ProcessorInjector_HandleRequests : public UCk_EcsWorld_ProcessorInjector_Base_UE
+{
+    GENERATED_BODY()
+
+protected:
+    auto
+    DoInjectProcessors(
+        EcsWorldType& InWorld) -> void override;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable, NotBlueprintType)
+class CKECSEXT_API UCk_Transform_ProcessorInjector_Finalize : public UCk_EcsWorld_ProcessorInjector_Base_UE
+{
+    GENERATED_BODY()
+
+protected:
+    auto
+    DoInjectProcessors(
+        EcsWorldType& InWorld) -> void override;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(NotBlueprintable, NotBlueprintType)
 class CKECSEXT_API UCk_EcsExt_ProcessorInjector_Replicate : public UCk_EcsWorld_ProcessorInjector_Base_UE
 {
     GENERATED_BODY()
