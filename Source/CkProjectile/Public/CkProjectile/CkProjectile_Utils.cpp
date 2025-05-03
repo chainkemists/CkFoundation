55001656a395ac7742f60235a00974e06c8d9b2d
Sulfur-CK <sulfur@chainkemists.com>
2025-05-02 17:35:55 -0700

    fix: Projectile Utils now has the Replication parameter

--- Source/CkProjectile/Public/CkProjectile/CkProjectile_Utils.cpp
+++ Source/CkProjectile/Public/CkProjectile/CkProjectile_Utils.cpp
@@ -13,11 +13,12 @@ auto
     UCk_Utils_Projectile_UE::
     Add(
         FCk_Handle& InHandle,
-        const FCk_Fragment_Projectile_ParamsData& InParams)
+        const FCk_Fragment_Projectile_ParamsData& InParams,
+        ECk_Replication InReplicates)
     -> void
 {
-    UCk_Utils_Velocity_UE::Add(InHandle, InParams.Get_VelocityParams());
-    UCk_Utils_Acceleration_UE::Add(InHandle, InParams.Get_AccelerationParams());
+    UCk_Utils_Velocity_UE::Add(InHandle, InParams.Get_VelocityParams(), InReplicates);
+    UCk_Utils_Acceleration_UE::Add(InHandle, InParams.Get_AccelerationParams(), InReplicates);
     UCk_Utils_AutoReorient_UE::Add(InHandle, InParams.Get_AutoReorientParams());
 
     UCk_Utils_EulerIntegrator_UE::Request_Start(InHandle);
