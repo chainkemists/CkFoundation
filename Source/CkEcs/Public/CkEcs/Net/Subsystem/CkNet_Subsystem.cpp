ad94a2948f4b18936ee85f1fa129588ea173e7ba
Sulfur-CK <sulfur@chainkemists.com>
2025-05-02 18:53:20 -0700

    fix: EntityScript entities now have NetParams if they use the TransientEntity as their LifetimeOwner

--- Source/CkEcs/Public/CkEcs/Net/Subsystem/CkNet_Subsystem.cpp
+++ Source/CkEcs/Public/CkEcs/Net/Subsystem/CkNet_Subsystem.cpp
@@ -13,18 +13,26 @@ auto
     -> void
 {
     Super::Initialize(InCollection);
+
     const auto EcsWorldSubsystem = InCollection.InitializeDependency<UCk_EcsWorld_Subsystem_UE>();
     auto TransientEntity = EcsWorldSubsystem->Get_TransientEntity();
 
-    if (GetWorld()->IsNetMode(NM_DedicatedServer) || GetWorld()->IsNetMode(NM_ListenServer) || GetWorld()->IsNetMode(NM_Standalone))
+    if (GetWorld()->IsNetMode(NM_DedicatedServer) || GetWorld()->IsNetMode(NM_ListenServer) ||
+        GetWorld()->IsNetMode(NM_Standalone))
     {
-        UCk_Utils_Net_UE::Add(TransientEntity,
-            FCk_Net_ConnectionSettings{ECk_Replication::DoesNotReplicate, ECk_Net_NetModeType::Host, ECk_Net_EntityNetRole::Authority});
+        UCk_Utils_Net_UE::Add(TransientEntity, FCk_Net_ConnectionSettings{
+            ECk_Replication::DoesNotReplicate,
+            ECk_Net_NetModeType::Host,
+            ECk_Net_EntityNetRole::Authority
+        });
     }
     else
     {
-        UCk_Utils_Net_UE::Add(TransientEntity,
-            FCk_Net_ConnectionSettings{ECk_Replication::DoesNotReplicate, ECk_Net_NetModeType::Client, ECk_Net_EntityNetRole::Authority});
+        UCk_Utils_Net_UE::Add(TransientEntity, FCk_Net_ConnectionSettings{
+            ECk_Replication::DoesNotReplicate,
+            ECk_Net_NetModeType::Client,
+            ECk_Net_EntityNetRole::Authority
+        });
     }
 }
 
