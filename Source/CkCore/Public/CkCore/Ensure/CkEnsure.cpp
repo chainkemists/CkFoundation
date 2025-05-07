08c0f2346ded388f050cea0461bedd7d7d7e2b01
AlexObatake-CK <phosphorus@chainkemists.com>
2025-05-07 13:23:40 -0700

    fix: Split ensure.h between macros and utils
    *  Ensure BPFL and related changes are now part of CkEnsure_Utils to prevent them from being included in everything that needs the ensure macros and reduce header dependencies
    *  Main ensure macro has been reworked to call a function instead of inlining most of the ensure code
       *  This reduces compile time since there is less code to to include and most dependencies can be placed in the cpp file instead of the header

--- Source/CkCore/Public/CkCore/Ensure/CkEnsure.cpp
+++ Source/CkCore/Public/CkCore/Ensure/CkEnsure.cpp
@@ -1,322 +1,147 @@
 #include "CkEnsure.h"
 
-#include "CkCore/Validation/CkIsValid.h"
-#include "CkCore/Debug/CkDebug_Utils.h"
-#include "CkCore/Ensure/CkEnsure_Subsystem.h"
+#include "CkCore/CkCoreLog.h"
+#include "CkCore/EditorOnly/CkEditorOnly_Utils.h"
+#include "CkCore/Enums/CkEnums.h"
+#include "CkCore/Format/CkFormat.h"
+#include "CkCore/MessageDialog/CkMessageDialog_Utils.h"
 #include "CkCore/Settings/CkCore_Settings.h"
+#include "CkCore/Ensure/CkEnsure_Utils.h"
+
+#include <CoreMinimal.h>
+#include <Windows/WindowsPlatformApplicationMisc.h> // required for clipboard copy
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    CkEnsure_Impl(
+        const FString& InMessage,
+        const FString& InExpressionText,
+        const FName& InFile,
+        int32 InLine)
+    -> void
+{
+    UCk_Utils_Ensure_UE::Request_IncrementEnsureCountAtFileAndLine(InFile, InLine);
+
+    if (UCk_Utils_Ensure_UE::Get_IsEnsureIgnored(InFile, InLine))
+    { return; }
+
+    const auto IsMessageOnly = UCk_Utils_Core_UserSettings_UE::Get_EnsureDetailsPolicy() == ECk_EnsureDetails_Policy::MessageOnly;
+
+    const auto& Title = ck::Format_UE(TEXT("Frame#[{}] PIE-ID[{}]"), GFrameCounter, UCk_Utils_EditorOnly_UE::Get_DebugStringForWorld());
+    const auto& StackTraceWith2Skips = IsMessageOnly ?
+        TEXT("[StackTrace DISABLED]") :
+        UCk_Utils_Debug_StackTrace_UE::Get_StackTrace(2);
+    const auto& BpStackTrace = IsMessageOnly ?
+        TEXT("[BP StackTrace DISABLED]") :
+        UCk_Utils_Debug_StackTrace_UE::Get_StackTrace_Blueprint(ck::type_traits::AsString{});
+    const auto& MessagePlusBpCallStack = ck::Format_UE(
+        TEXT("[{}] {}\n{}\n\n == BP CallStack ==\n{}"),
+        GPlayInEditorID - 1 < 0 ? TEXT("Server") : TEXT("Client"),
+        InExpressionText,
+        InMessage,
+        BpStackTrace);
+
+    const auto& MessagePlusBpCallStackStr = FText::FromString(MessagePlusBpCallStack);
+
+    if (UCk_Utils_Core_UserSettings_UE::Get_EnsureDisplayPolicy() == ECk_EnsureDisplay_Policy::StreamerMode)
+    {
+        ck::core::Error(TEXT("{}"), MessagePlusBpCallStack);
+        UCk_Utils_Ensure_UE::Request_IgnoreEnsureAtFileAndLineWithMessage(InFile, MessagePlusBpCallStackStr, InLine);
+        return;
+    }
 
 #if WITH_EDITOR
-#include <Kismet2/KismetDebugUtilities.h>
+// ReSharper disable once CppInconsistentNaming
+    UCk_Utils_EditorOnly_UE::Request_PushNewEditorMessage
+    (
+        FCk_Utils_EditorOnly_PushNewEditorMessage_Params
+        {
+            TEXT("CkEnsures"),
+            FCk_MessageSegments
+            {
+                {
+                    FCk_TokenizedMessage{MessagePlusBpCallStack}.Set_TargetObject(nullptr)
+                }
+            }
+        }
+        .Set_MessageSeverity(ECk_EditorMessage_Severity::Error)
+        .Set_ToastNotificationDisplayPolicy(ECk_EditorMessage_ToastNotification_DisplayPolicy::DoNotDisplay)
+        .Set_MessageLogDisplayPolicy(ECk_EditorMessage_MessageLog_DisplayPolicy::DoNotFocus)
+    );
+    if (UCk_Utils_Core_UserSettings_UE::Get_EnsureDisplayPolicy() == ECk_EnsureDisplay_Policy::LogOnly)
+    { return; }
+#else
+// ReSharper disable once CppInconsistentNaming
+#define _DETAILS_CK_ENSURE_LOG_OR_PUSHMESSAGE(_Category_, _Msg_, _ContextObject_)
+    if (UCk_Utils_Core_UserSettings_UE::Get_EnsureDisplayPolicy() == ECk_EnsureDisplay_Policy::LogOnly)
+    {
+        UCk_Utils_Ensure_UE::Request_IgnoreEnsureAtFileAndLineWithMessage(__FILE__, FText::FromString(MessagePlusBpCallStack), InLine);
+        UE_LOG(CkCore, Error, TEXT("%s"), *MessagePlusBpCallStack);
+        return;
+    }
 #endif
 
-// --------------------------------------------------------------------------------------------------------------------
-
-auto
-    FCk_Ensure_Entry::
-    operator==(
-        const ThisType& InOther) const
-    -> bool
-{
-    return Get_LineNumber() == InOther.Get_LineNumber() && Get_FileName().IsEqual(InOther.Get_FileName());
-}
-
-auto
-    GetTypeHash(
-        const FCk_Ensure_Entry& InA)
-    -> uint8
-{
-    return GetTypeHash(InA.Get_LineNumber()) + GetTypeHash(InA.Get_FileName());
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-auto
-    FCk_Ensure_IgnoredEntry::
-    operator==(
-        const ThisType& InOther) const
-    -> bool
-{
-    return Get_LineNumber() == InOther.Get_LineNumber() && Get_FileName().IsEqual(InOther.Get_FileName());
-}
-
-auto
-    GetTypeHash(
-        const FCk_Ensure_IgnoredEntry& InA)
-    -> uint8
-{
-    return GetTypeHash(InA.Get_LineNumber()) + GetTypeHash(InA.Get_FileName());
-}
-
-// --------------------------------------------------------------------------------------------------------------------
-
-auto
-    UCk_Utils_Ensure_UE::
-    EnsureMsgf(
-        const bool InExpression,
-        const FText InMsg,
-        ECk_ValidInvalid& OutHitStatus,
-        const UObject* InContext)
-    -> void
-{
-    CK_ENSURE_IF_NOT(InExpression, TEXT("{}.{}"), InMsg, ck::Context(InContext))
+    if (UCk_Utils_Core_UserSettings_UE::Get_EnsureDisplayPolicy() == ECk_EnsureDisplay_Policy::MessageLog)
     {
-        OutHitStatus = ECk_ValidInvalid::Invalid;
+        UCk_Utils_Debug_StackTrace_UE::Try_BreakInScript(nullptr, MessagePlusBpCallStackStr);
+        UCk_Utils_Ensure_UE::Request_IgnoreEnsureAtFileAndLineWithMessage(InFile, MessagePlusBpCallStackStr, InLine);
         return;
     }
 
-    OutHitStatus = ECk_ValidInvalid::Valid;
-}
+    const auto& CallstackPlusMessage = ck::Format_UE(
+        TEXT("{}\nExpression: {}\nMessage: {}\n\n == BP CallStack ==\n{}\n == CallStack ==\n{}\n"),
+        Title,
+        InExpressionText,
+        InMessage,
+        BpStackTrace,
+        StackTraceWith2Skips);
+    const auto& DialogMessage = FText::FromString(CallstackPlusMessage);
 
-auto
-    UCk_Utils_Ensure_UE::
-    EnsureMsgf_IsValid(
-        UObject* InObject,
-        const FText InMsg,
-        ECk_ValidInvalid& OutHitStatus,
-        const UObject* InContext)
-    -> void
-{
-    CK_ENSURE_IF_NOT(InObject, TEXT("{}.{}"), InMsg, ck::Context(InContext))
+    using DialogButton = UCk_Utils_MessageDialog_UE::DialogButton;
+    auto Buttons = TArray<DialogButton>{};
+
+    Buttons.Add(DialogButton{FText::FromString(TEXT("Ignore Once")), FSimpleDelegate::CreateLambda([&]()
+    {})}.Set_Color(FLinearColor{0.22f, 0.22f, 0.22f, 1.0f}));
+
+    Buttons.Add(DialogButton{FText::FromString(TEXT("Ignore All")), FSimpleDelegate::CreateLambda([&]()
     {
-        OutHitStatus = ECk_ValidInvalid::Invalid;
-        return;
+        UCk_Utils_Ensure_UE::Request_IgnoreEnsureAtFileAndLineWithMessage(InFile, DialogMessage, InLine);
+    })}.Set_Color(FLinearColor{1.0f, 0.62f, 0.27f, 1.0f}).Set_IsPrimary(true).Set_ShouldFocus(true));
+
+
+    Buttons.Add(DialogButton{FText::FromString(TEXT("Break")), {}}
+    .Set_Color(FLinearColor{0.22f, 0.22f, 0.22f, 1.0f})
+    .Set_EnableDisable(StackTraceWith2Skips.IsEmpty() ? ECk_EnableDisable::Disable : ECk_EnableDisable::Enable));
+
+    if (GIsEditor)
+    {
+    Buttons.Add(DialogButton{FText::FromString(TEXT("Break in BP")), FSimpleDelegate::CreateLambda([&]()
+    {
+        UCk_Utils_Debug_StackTrace_UE::Try_BreakInScript(nullptr);
+    })}.Set_Color(FLinearColor{0.34f, 0.34f, 0.59f, 1.0f})
+    .Set_EnableDisable(BpStackTrace.IsEmpty() ? ECk_EnableDisable::Disable : ECk_EnableDisable::Enable));
     }
 
-    OutHitStatus = ECk_ValidInvalid::Valid;
-}
+    if (GIsEditor)
+    {
+        Buttons.Add(DialogButton{FText::FromString(TEXT("Abort PIE")), FSimpleDelegate::CreateLambda([&]()
+        {
+            UCk_Utils_Ensure_UE::Request_IgnoreAllEnsures();
+            UCk_Utils_EditorOnly_UE::Request_AbortPIE();
+        })}.Set_Color(FLinearColor{1.0f, 0.1f, 0.1f, 1.0f}));
+    }
 
-auto
-    UCk_Utils_Ensure_UE::
-    TriggerEnsure(
-        FText InMsg,
-        const UObject* InContext)
-    -> void
-{
-    CK_ENSURE(false, TEXT("{}.{}"), InMsg, ck::Context(InContext));
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    Get_AllIgnoredEnsures()
-    -> TArray<FCk_Ensure_IgnoredEntry>
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return {}; }
-
-    return EnsureSubsystem->Get_AllIgnoredEnsures();
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    Get_EnsureCount()
-    -> int32
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return {}; }
-
-    return EnsureSubsystem->Get_EnsureCount();
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    Get_UniqueEnsureCount()
-    -> int32
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return {}; }
-
-    return EnsureSubsystem->Get_UniqueEnsureCount();
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    Request_ClearAllIgnoredEnsures()
-    -> void
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return; }
-
-    EnsureSubsystem->Request_ClearAllIgnoredEnsures();
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    BindTo_OnEnsureIgnored(const FCk_Delegate_OnEnsureIgnored& InDelegate)
-    -> void
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return; }
-
-    EnsureSubsystem->BindTo_OnEnsureIgnored(InDelegate);
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    UnbindFrom_OnEnsureIgnored(const FCk_Delegate_OnEnsureIgnored& InDelegate)
-    -> void
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return; }
-
-    EnsureSubsystem->UnbindFrom_OnEnsureIgnored(InDelegate);
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    BindTo_OnEnsureCountChanged(
-        const FCk_Delegate_OnEnsureCountChanged& InDelegate)
-    -> void
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return; }
-
-    EnsureSubsystem->BindTo_OnEnsureCountChanged(InDelegate);
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    UnbindFrom_OnEnsureCountChanged(
-        const FCk_Delegate_OnEnsureCountChanged& InDelegate)
-    -> void
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return; }
-
-    EnsureSubsystem->UnbindFrom_OnEnsureCountChanged(InDelegate);
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    Request_IgnoreEnsureAtFileAndLine(
-        FName InFile,
-        int32 InLine)
-    -> void
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return; }
-
-    EnsureSubsystem->Request_IgnoreEnsureAtFileAndLine(InFile, InLine);
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    Request_IgnoreEnsureAtFileAndLineWithMessage(
-        FName        InFile,
-        const FText& InMessage,
-        int32        InLine)
-    -> void
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return; }
-
-    EnsureSubsystem->Request_IgnoreEnsureAtFileAndLineWithMessage(InFile, InMessage, InLine);
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    Get_IsEnsureIgnored(
-        FName InFile,
-        int32 InLine)
-    -> bool
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return {}; }
-
-    return EnsureSubsystem->Get_IsEnsureIgnored(InFile, InLine);
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    Request_IgnoreEnsure_WithCallstack(
-        const FString& InCallstack)
-    -> void
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return; }
-
-    EnsureSubsystem->Request_IgnoreEnsure_WithCallstack(InCallstack);
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    Request_IgnoreAllEnsures()
-    -> void
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return; }
-
-    EnsureSubsystem->Request_IgnoreAllEnsures();
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    Get_IsEnsureIgnored_WithCallstack(
-        const FString& InCallstack)
-    -> bool
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return {}; }
-
-    return EnsureSubsystem->Get_IsEnsureIgnored_WithCallstack(InCallstack);
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    Request_IncrementEnsureCountAtFileAndLine(
-        FName InFile,
-        int32 InLine)
-    -> void
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return; }
-
-    EnsureSubsystem->Request_IncrementEnsureCountAtFileAndLine(InFile, InLine);
-}
-
-auto
-    UCk_Utils_Ensure_UE::
-    Request_IncrementEnsureCountWithCallstack(
-        const FString& InCallstack)
-    -> void
-{
-    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
-
-    if (ck::Is_NOT_Valid(EnsureSubsystem))
-    { return; }
-
-    EnsureSubsystem->Request_IncrementEnsureCountWithCallstack(InCallstack);
+    auto ButtonIndex = UCk_Utils_MessageDialog_UE::CustomDialog(DialogMessage, FText::FromString(Title), Buttons);
+    if (ButtonIndex == 2)
+    {
+        ensureAlwaysMsgf(false, TEXT("[DEBUG BREAK HIT]"));
+        if (NOT BpStackTrace.IsEmpty())
+        {
+            UCk_Utils_Debug_StackTrace_UE::Try_BreakInScript(nullptr);
+        }
+    }
 }
 
 // --------------------------------------------------------------------------------------------------------------------
-
-#undef CK_ENSURE_BP
