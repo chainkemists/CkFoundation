c90564d4f78b015a69688be05d48532b01da5f43
AlexObatake-CK <phosphorus@chainkemists.com>
2025-05-07 13:23:40 -0700

    fix: Split ensure.h between macros and utils
    *  Ensure BPFL and related changes are now part of CkEnsure_Utils to prevent them from being included in everything that needs the ensure macros and reduce header dependencies
    *  Main ensure macro has been reworked to call a function instead of inlining most of the ensure code
       *  This reduces compile time since there is less code to to include and most dependencies can be placed in the cpp file instead of the header

---
+++ Source/CkCore/Public/CkCore/Ensure/CkEnsure_Utils.cpp
@@ -0,0 +1,323 @@
+﻿#include "CkEnsure_Utils.h"
+
+#include "CkEnsure.h"
+#include "CkCore/Validation/CkIsValid.h"
+#include "CkCore/Debug/CkDebug_Utils.h"
+#include "CkCore/Ensure/CkEnsure_Subsystem.h"
+#include "CkCore/Settings/CkCore_Settings.h"
+
+#if WITH_EDITOR
+#include <Kismet2/KismetDebugUtilities.h>
+#endif
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    FCk_Ensure_Entry::
+    operator==(
+        const ThisType& InOther) const
+    -> bool
+{
+    return Get_LineNumber() == InOther.Get_LineNumber() && Get_FileName().IsEqual(InOther.Get_FileName());
+}
+
+auto
+    GetTypeHash(
+        const FCk_Ensure_Entry& InA)
+    -> uint8
+{
+    return GetTypeHash(InA.Get_LineNumber()) + GetTypeHash(InA.Get_FileName());
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    FCk_Ensure_IgnoredEntry::
+    operator==(
+        const ThisType& InOther) const
+    -> bool
+{
+    return Get_LineNumber() == InOther.Get_LineNumber() && Get_FileName().IsEqual(InOther.Get_FileName());
+}
+
+auto
+    GetTypeHash(
+        const FCk_Ensure_IgnoredEntry& InA)
+    -> uint8
+{
+    return GetTypeHash(InA.Get_LineNumber()) + GetTypeHash(InA.Get_FileName());
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_Utils_Ensure_UE::
+    EnsureMsgf(
+        const bool InExpression,
+        const FText InMsg,
+        ECk_ValidInvalid& OutHitStatus,
+        const UObject* InContext)
+    -> void
+{
+    CK_ENSURE_IF_NOT(InExpression, TEXT("{}.{}"), InMsg, ck::Context(InContext))
+    {
+        OutHitStatus = ECk_ValidInvalid::Invalid;
+        return;
+    }
+
+    OutHitStatus = ECk_ValidInvalid::Valid;
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    EnsureMsgf_IsValid(
+        UObject* InObject,
+        const FText InMsg,
+        ECk_ValidInvalid& OutHitStatus,
+        const UObject* InContext)
+    -> void
+{
+    CK_ENSURE_IF_NOT(InObject, TEXT("{}.{}"), InMsg, ck::Context(InContext))
+    {
+        OutHitStatus = ECk_ValidInvalid::Invalid;
+        return;
+    }
+
+    OutHitStatus = ECk_ValidInvalid::Valid;
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    TriggerEnsure(
+        FText InMsg,
+        const UObject* InContext)
+    -> void
+{
+    CK_ENSURE(false, TEXT("{}.{}"), InMsg, ck::Context(InContext));
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    Get_AllIgnoredEnsures()
+    -> TArray<FCk_Ensure_IgnoredEntry>
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return {}; }
+
+    return EnsureSubsystem->Get_AllIgnoredEnsures();
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    Get_EnsureCount()
+    -> int32
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return {}; }
+
+    return EnsureSubsystem->Get_EnsureCount();
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    Get_UniqueEnsureCount()
+    -> int32
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return {}; }
+
+    return EnsureSubsystem->Get_UniqueEnsureCount();
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    Request_ClearAllIgnoredEnsures()
+    -> void
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return; }
+
+    EnsureSubsystem->Request_ClearAllIgnoredEnsures();
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    BindTo_OnEnsureIgnored(const FCk_Delegate_OnEnsureIgnored& InDelegate)
+    -> void
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return; }
+
+    EnsureSubsystem->BindTo_OnEnsureIgnored(InDelegate);
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    UnbindFrom_OnEnsureIgnored(const FCk_Delegate_OnEnsureIgnored& InDelegate)
+    -> void
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return; }
+
+    EnsureSubsystem->UnbindFrom_OnEnsureIgnored(InDelegate);
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    BindTo_OnEnsureCountChanged(
+        const FCk_Delegate_OnEnsureCountChanged& InDelegate)
+    -> void
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return; }
+
+    EnsureSubsystem->BindTo_OnEnsureCountChanged(InDelegate);
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    UnbindFrom_OnEnsureCountChanged(
+        const FCk_Delegate_OnEnsureCountChanged& InDelegate)
+    -> void
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return; }
+
+    EnsureSubsystem->UnbindFrom_OnEnsureCountChanged(InDelegate);
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    Request_IgnoreEnsureAtFileAndLine(
+        FName InFile,
+        int32 InLine)
+    -> void
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return; }
+
+    EnsureSubsystem->Request_IgnoreEnsureAtFileAndLine(InFile, InLine);
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    Request_IgnoreEnsureAtFileAndLineWithMessage(
+        FName        InFile,
+        const FText& InMessage,
+        int32        InLine)
+    -> void
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return; }
+
+    EnsureSubsystem->Request_IgnoreEnsureAtFileAndLineWithMessage(InFile, InMessage, InLine);
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    Get_IsEnsureIgnored(
+        FName InFile,
+        int32 InLine)
+    -> bool
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return {}; }
+
+    return EnsureSubsystem->Get_IsEnsureIgnored(InFile, InLine);
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    Request_IgnoreEnsure_WithCallstack(
+        const FString& InCallstack)
+    -> void
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return; }
+
+    EnsureSubsystem->Request_IgnoreEnsure_WithCallstack(InCallstack);
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    Request_IgnoreAllEnsures()
+    -> void
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return; }
+
+    EnsureSubsystem->Request_IgnoreAllEnsures();
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    Get_IsEnsureIgnored_WithCallstack(
+        const FString& InCallstack)
+    -> bool
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return {}; }
+
+    return EnsureSubsystem->Get_IsEnsureIgnored_WithCallstack(InCallstack);
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    Request_IncrementEnsureCountAtFileAndLine(
+        FName InFile,
+        int32 InLine)
+    -> void
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return; }
+
+    EnsureSubsystem->Request_IncrementEnsureCountAtFileAndLine(InFile, InLine);
+}
+
+auto
+    UCk_Utils_Ensure_UE::
+    Request_IncrementEnsureCountWithCallstack(
+        const FString& InCallstack)
+    -> void
+{
+    const auto EnsureSubsystem = UCk_Ensure_Subsystem_UE::Get_Instance();
+
+    if (ck::Is_NOT_Valid(EnsureSubsystem))
+    { return; }
+
+    EnsureSubsystem->Request_IncrementEnsureCountWithCallstack(InCallstack);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+
+#undef CK_ENSURE_BP
---
+++ Source/CkCore/Public/CkCore/Ensure/CkEnsure_Utils.h
@@ -0,0 +1,248 @@
+﻿#pragma once
+
+#include "CkCore/Format/CkFormat.h"
+
+#include "CkCore/Enums/CkEnums.h"
+
+#include "CkEnsure_Utils.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKCORE_API FCk_Ensure_Entry
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Ensure_Entry);
+
+public:
+    auto operator==(const ThisType& InOther) const -> bool;
+    CK_DECL_AND_DEF_OPERATOR_NOT_EQUAL(ThisType);
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FName _FileName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 _LineNumber = 0;
+
+public:
+    CK_PROPERTY_GET(_FileName);
+    CK_PROPERTY_GET(_LineNumber);
+
+public:
+    CK_DEFINE_CONSTRUCTORS(FCk_Ensure_Entry, _FileName, _LineNumber);
+};
+
+auto CKCORE_API GetTypeHash(const FCk_Ensure_Entry& InA) -> uint8;
+
+
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKCORE_API FCk_Ensure_IgnoredEntry
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Ensure_IgnoredEntry);
+
+public:
+    auto operator==(const ThisType& InOther) const -> bool;
+    CK_DECL_AND_DEF_OPERATOR_NOT_EQUAL(ThisType);
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FName _FileName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 _LineNumber = 0;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FText _Message;
+
+public:
+    CK_PROPERTY_GET(_FileName);
+    CK_PROPERTY_GET(_LineNumber);
+    CK_PROPERTY(_Message);
+
+public:
+    CK_DEFINE_CONSTRUCTORS(FCk_Ensure_IgnoredEntry, _FileName, _LineNumber);
+};
+
+auto CKCORE_API GetTypeHash(const FCk_Ensure_IgnoredEntry& InA) -> uint8;
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKCORE_API FCk_Payload_OnEnsureIgnored
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Payload_OnEnsureIgnored);
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FCk_Ensure_IgnoredEntry _IgnoredEnsure;
+
+public:
+    CK_PROPERTY_GET(_IgnoredEnsure);
+
+public:
+    CK_DEFINE_CONSTRUCTORS(FCk_Payload_OnEnsureIgnored, _IgnoredEnsure);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+DECLARE_DYNAMIC_DELEGATE_OneParam(
+    FCk_Delegate_OnEnsureIgnored,
+    const FCk_Payload_OnEnsureIgnored&, InPayload);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(
+    FCk_Delegate_OnEnsureIgnored_MC,
+    const FCk_Payload_OnEnsureIgnored&, InPayload);
+
+DECLARE_DYNAMIC_DELEGATE_TwoParams(
+    FCk_Delegate_OnEnsureCountChanged,
+    int32, InNewTotalCount,
+    int32, InNewUniqueCount);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(
+    FCk_Delegate_OnEnsureCountChanged_MC,
+    int32, InNewTotalCount,
+    int32, InNewUniqueCount);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS()
+class CKCORE_API UCk_Utils_Ensure_UE
+    : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable,
+              Category = "Ck|Utils|Ensure",
+              DisplayName = "[Ck] Ensure",
+              meta     = (DevelopmentOnly, ExpandEnumAsExecs = "OutHitStatus", DefaultToSelf = "InContext", HidePin = "InContext"))
+    static void
+    EnsureMsgf(
+        bool InExpression,
+        FText InMsg,
+        ECk_ValidInvalid& OutHitStatus,
+        const UObject* InContext = nullptr);
+
+    UFUNCTION(BlueprintCallable,
+              Category = "Ck|Utils|Ensure",
+              DisplayName = "[Ck] Ensure IsValid",
+              meta     = (DevelopmentOnly, ExpandEnumAsExecs = "OutHitStatus", DefaultToSelf = "InContext", HidePin = "InContext"))
+    static void
+    EnsureMsgf_IsValid(
+        UObject* InObject,
+        FText InMsg,
+        ECk_ValidInvalid& OutHitStatus,
+        const UObject* InContext = nullptr);
+
+    UFUNCTION(BlueprintCallable,
+              DisplayName = "[Ck] Trigger Ensure",
+              Category = "Ck|Utils|Ensure",
+              meta     = (DevelopmentOnly, DefaultToSelf = "InContext", HidePin = "InContext"))
+    static void
+    TriggerEnsure(
+        FText InMsg,
+        const UObject* InContext = nullptr);
+
+    UFUNCTION(BlueprintPure,
+              DisplayName = "[Ck] Get All Ignored Ensures",
+              Category = "Ck|Utils|Ensure")
+    static TArray<FCk_Ensure_IgnoredEntry>
+    Get_AllIgnoredEnsures();
+
+    UFUNCTION(BlueprintPure,
+              DisplayName = "[Ck] Get Ensure Count",
+              Category = "Ck|Utils|Ensure")
+    static int32
+    Get_EnsureCount();
+
+    UFUNCTION(BlueprintPure,
+              DisplayName = "[Ck] Get Unique Ensure Count",
+              Category = "Ck|Utils|Ensure")
+    static int32
+    Get_UniqueEnsureCount();
+
+    UFUNCTION(BlueprintCallable,
+              DisplayName = "[Ck] Request Clear All Ignored Ensures",
+              Category = "Ck|Utils|Ensure")
+    static void
+    Request_ClearAllIgnoredEnsures();
+
+    UFUNCTION(BlueprintCallable,
+              DisplayName = "[Ck] Bind To OnEnsureIgnored",
+              Category = "Ck|Utils|Ensure")
+    static void
+    BindTo_OnEnsureIgnored(
+        const FCk_Delegate_OnEnsureIgnored& InDelegate);
+
+    UFUNCTION(BlueprintCallable,
+              DisplayName = "[Ck] Unbind From OnEnsureIgnored",
+              Category = "Ck|Utils|Ensure")
+    static void
+    UnbindFrom_OnEnsureIgnored(
+        const FCk_Delegate_OnEnsureIgnored& InDelegate);
+
+    UFUNCTION(BlueprintCallable,
+              DisplayName = "[Ck] Bind To OnEnsureCountChanged",
+              Category = "Ck|Utils|Ensure")
+    static void
+    BindTo_OnEnsureCountChanged(
+        const FCk_Delegate_OnEnsureCountChanged& InDelegate);
+
+    UFUNCTION(BlueprintCallable,
+              DisplayName = "[Ck] Unbind From OnEnsureCountChanged",
+              Category = "Ck|Utils|Ensure")
+    static void
+    UnbindFrom_OnEnsureCountChanged(
+        const FCk_Delegate_OnEnsureCountChanged& InDelegate);
+
+public:
+    static auto
+    Get_IsEnsureIgnored(
+        FName InFile,
+        int32 InLine) -> bool;
+
+    static auto
+    Get_IsEnsureIgnored_WithCallstack(
+        const FString& InCallstack) -> bool;
+
+    static auto
+    Request_IncrementEnsureCountAtFileAndLine(
+        FName InFile,
+        int32 InLine) -> void;
+
+    static auto
+    Request_IncrementEnsureCountWithCallstack(
+        const FString& InCallstack) -> void;
+
+    static auto
+    Request_IgnoreEnsureAtFileAndLineWithMessage(
+        FName InFile,
+        const FText& InMessage,
+        int32 InLine) -> void;
+
+    static auto
+    Request_IgnoreEnsureAtFileAndLine(
+        FName InFile,
+        int32 InLine) -> void;
+
+    static auto
+    Request_IgnoreEnsure_WithCallstack(
+        const FString& InCallstack) -> void;
+
+    static auto
+    Request_IgnoreAllEnsures() -> void;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
