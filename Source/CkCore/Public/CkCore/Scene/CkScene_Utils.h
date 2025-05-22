a1bf5568e21e2f616b57d703ff209740fed9d25a
Nitrogen-CK <nitrogen@chainkemists.com>
2025-05-21 20:51:26 -0400

    feat: Exposed an interface to define a custom ActorComponent visualizer in BP

---
+++ Source/CkCore/Public/CkCore/Scene/CkScene_Utils.h
@@ -0,0 +1,108 @@
+#pragma once
+
+#include "CkCore/Macros/CkMacros.h"
+
+#include "CkScene_Utils.generated.h"
+
+// ----------------------------------------------------------------------------------------------------------------
+
+UCLASS(BlueprintType)
+class CKCORE_API UCk_Utils_Scene_UE : public UBlueprintFunctionLibrary
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_Utils_Scene_UE);
+
+private:
+    UFUNCTION(BlueprintCallable,
+              DisplayName = "[Ck] Draw Point (Visualizer)",
+              Category = "Ck|Utils|Scene")
+    static void
+    DrawPoint(
+        const FCk_Handle_PrimitiveDrawInterface& InDrawHandle,
+        FVector InPosition,
+        FLinearColor InColor,
+        float InPointSize,
+        TEnumAsByte<ESceneDepthPriorityGroup> InDepthPriorityGroup);
+
+    UFUNCTION(BlueprintCallable,
+              DisplayName = "[Ck] Draw Line (Visualizer)",
+              Category = "Ck|Utils|Scene")
+    static void
+    DrawLine(
+        const FCk_Handle_PrimitiveDrawInterface& InDrawHandle,
+        FVector InStart,
+        FVector InEnd,
+        FLinearColor InColor,
+        TEnumAsByte<ESceneDepthPriorityGroup> InDepthPriorityGroup,
+        float InThickness = 0.0f,
+        float InDepthBias = 0.0f,
+        bool InIsScreenSpace = false);
+
+    UFUNCTION(BlueprintCallable,
+              DisplayName = "[Ck] Draw Box (Visualizer)",
+              Category = "Ck|Utils|Scene")
+    static void
+    DrawBox(
+        const FCk_Handle_PrimitiveDrawInterface& InDrawHandle,
+        FVector InOrigin,
+        FRotator InRotation,
+        FVector InExtents,
+        FLinearColor InColor,
+        TEnumAsByte<ESceneDepthPriorityGroup> InDepthPriorityGroup,
+        float InThickness = 0.0f,
+        float InDepthBias = 0.0f,
+        bool InIsScreenSpace = false);
+
+    UFUNCTION(BlueprintCallable,
+              DisplayName = "[Ck] Draw Sphere (Visualizer)",
+              Category = "Ck|Utils|Scene")
+    static void
+    DrawSphere(
+        const FCk_Handle_PrimitiveDrawInterface& InDrawHandle,
+        FVector InOrigin,
+        float InRadius,
+        int32 InNumSides,
+        FLinearColor InColor,
+        TEnumAsByte<ESceneDepthPriorityGroup> InDepthPriorityGroup,
+        float InThickness = 0.0f,
+        float InDepthBias = 0.0f,
+        bool InIsScreenSpace = false);
+
+    UFUNCTION(BlueprintCallable,
+              DisplayName = "[Ck] Draw Capsule (Visualizer)",
+              Category = "Ck|Utils|Scene")
+    static void
+    DrawCapsule(
+        const FCk_Handle_PrimitiveDrawInterface& InDrawHandle,
+        FVector InOrigin,
+        FRotator InRotation,
+        float InRadius,
+        float InHalfHeight,
+        int32 InNumSides,
+        FLinearColor InColor,
+        TEnumAsByte<ESceneDepthPriorityGroup> InDepthPriorityGroup,
+        float InThickness = 0.0f,
+        float InDepthBias = 0.0f,
+        bool InIsScreenSpace = false);
+
+    UFUNCTION(BlueprintCallable,
+              DisplayName = "[Ck] Draw Cylinder (Visualizer)",
+              Category = "Ck|Utils|Scene")
+    static void
+    DrawCylinder(
+        const FCk_Handle_PrimitiveDrawInterface& InDrawHandle,
+        FVector InOrigin,
+        FRotator InRotation,
+        float InRadius,
+        float InHalfHeight,
+        int32 InNumSides,
+        FLinearColor InColor,
+        TEnumAsByte<ESceneDepthPriorityGroup> InDepthPriorityGroup,
+        float InThickness = 0.0f,
+        float InDepthBias = 0.0f,
+        bool InIsScreenSpace = false);
+};
+
+// ----------------------------------------------------------------------------------------------------------------;
---
+++ Source/CkCoreEditor/CkCoreEditor.Build.cs
@@ -0,0 +1,38 @@
+using System.IO;
+using UnrealBuildTool;
+
+public class CkCoreEditor : CkModuleRules
+{
+    public CkCoreEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseSharedPCHs;
+        PrivatePCHHeaderFile = "../CkEcs_PCH.h";
+        PrivateIncludePaths.AddRange(new string[] {
+            // ... add other private include paths required here ...
+        });
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "Slate",
+            "SlateCore",
+            "InputCore",
+            "StructUtils",
+            "ToolMenus",
+            "GameplayTags",
+            "UnrealEd",
+            "EditorStyle",
+            "AssetTools",
+            "KismetCompiler",
+            "GraphEditor",
+            "BlueprintGraph",
+            "StructUtilsEngine",
+            "ComponentVisualizers",
+
+            "CkCore",
+            "CkLog",
+        });
+    }
+}
