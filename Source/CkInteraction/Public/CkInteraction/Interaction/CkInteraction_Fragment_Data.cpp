15b05b78eeb62fcbc85501ddbe8ea9b97d7f8ca2
Sulfur-CK <sulfur@chainkemists.com>
2025-06-02 06:02:52 -0700

    feat: added default and InteractionSuccessful gameplay tags as part of the framework

--- Source/CkInteraction/Public/CkInteraction/Interaction/CkInteraction_Fragment_Data.cpp
+++ Source/CkInteraction/Public/CkInteraction/Interaction/CkInteraction_Fragment_Data.cpp
@@ -6,4 +6,7 @@ UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_Label_InteractionChannel, TEXT("InteractionCha
 UE_DEFINE_GAMEPLAY_TAG(TAG_InteractionTime_FloatAttribute_Name, TEXT("FloatAttribute.Interaction.Time"));
 UE_DEFINE_GAMEPLAY_TAG(TAG_InteractionTimeRefill_FloatAttribute_Name, TEXT("FloatAttribute.Interaction.Time.Refill"));
 
+UE_DEFINE_GAMEPLAY_TAG(TAG_InteractionChannel_Default, "InteractionChannel.Default");
+UE_DEFINE_GAMEPLAY_TAG(TAG_InteractionChannel_InteractionSuccessful, "InteractionChannel.InteractionSuccessful");
+
 // --------------------------------------------------------------------------------------------------------------------
