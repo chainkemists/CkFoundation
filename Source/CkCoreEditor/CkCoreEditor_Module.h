cf24bb41542c7394531b85b4b7eaa6805f831dd0
Nitrogen-CK <nitrogen@chainkemists.com>
2025-07-11 19:56:06 -0400

    fix: Actor Component visualizer registration is now more robust. It will run when assets are opened and if the asset is the BP of an actor, will attempt to register a visualizer for each of its components

--- Source/CkCoreEditor/CkCoreEditor_Module.h
+++ Source/CkCoreEditor/CkCoreEditor_Module.h
@@ -19,16 +19,23 @@ protected:
     auto OnAssetAdded(const FAssetData& InAssetData) -> void;
     auto OnAssetRenamed(const FAssetData& InAssetData, const FString&) -> void;
     auto OnAssetLoaded(UObject* InObject) -> void;
+    auto OnAssetOpened(UObject* Asset, IAssetEditorInstance*) -> void;
     auto OnFilesLoaded() -> void;
     auto OnMapOpened(const FString& InFilename, bool InAsTemplate) -> void;
     auto OnNewCurrentLevel() -> void;
 
 private:
+    auto RegisterNativeComponentVisualizers() -> void;
     auto TryUpdateVisualizer(const FAssetData& InAssetData) -> void;
     auto TryUpdateVisualizer(const UObject* InObject) -> void;
     auto TryUpdateVisualizerForClass(const UClass* InComponentClass) -> void;
-    auto RegisterNativeComponentVisualizers() -> void;
+    auto TryUpdateVisualizerForComponentBlueprint(const UBlueprint* Blueprint) -> void;
     auto ScanWorldObjectsForVisualizers() -> void;
+    auto ScanActorBlueprintForComponentVisualizers(const UBlueprint* ActorBlueprint) -> void;
+    auto ScanSCSNodeForComponentVisualizers(const USCS_Node* Node) -> void;
+    auto ScanActorComponentsForVisualizers(const AActor* Actor) -> void;
+    auto ProcessComponentForVisualizers(const UActorComponent* Component) -> void;
+    auto ProcessBlueprintForVisualizers(const UBlueprint* Blueprint) -> void;
 
 private:
     TSharedPtr<class FCk_ActorComponent_Visualizer> _Visualizer;
@@ -39,6 +46,7 @@ private:
     FDelegateHandle _AssetUpdated_DelegateHandle;
     FDelegateHandle _AssetAdded_DelegateHandle;
     FDelegateHandle _AssetLoaded_DelegateHandle;
+    FDelegateHandle _AssetOpened_DelegateHandle;
     FDelegateHandle _AssetRenamed_DelegateHandle;
     FDelegateHandle _MapOpened_DelegateHandle;
     FDelegateHandle _NewCurrentLevel_DelegateHandle;
