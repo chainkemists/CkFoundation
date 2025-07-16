2d9dd78cbab7f0261c12bd385d7648808fe42499
AlexObatake <aobatake1515@gmail.com>
2025-07-15 17:04:16 -0700

    fix: Add BP Pure to some character utils
    It seems like they were meant to be pure based on how they were used in some BPs (ex. MoveTo task)

--- Source/CkCore/Public/CkCore/Character/CkCharacter_Utils.h
+++ Source/CkCore/Public/CkCore/Character/CkCharacter_Utils.h
@@ -19,7 +19,7 @@ public:
     CK_GENERATED_BODY(UCk_Utils_Character_UE);
 
 public:
-    UFUNCTION(BlueprintCallable,
+    UFUNCTION(BlueprintCallable, BlueprintPure,
               DisplayName = "[Ck] Add Character Half-Height To Location",
               Category = "Ck|Utils|Character")
     static FVector
@@ -27,7 +27,7 @@ public:
         ACharacter* InCharacter,
         FVector InLocation);
 
-    UFUNCTION(BlueprintCallable,
+    UFUNCTION(BlueprintCallable, BlueprintPure,
               DisplayName = "[Ck] Subtract Character Half-Height From Location",
               Category = "Ck|Utils|Character")
     static FVector
@@ -35,7 +35,7 @@ public:
         ACharacter* InCharacter,
         FVector InLocation);
 
-    UFUNCTION(BlueprintCallable,
+    UFUNCTION(BlueprintCallable, BlueprintPure,
               DisplayName = "[Ck] Add Capsule Half-Height To Location",
               Category = "Ck|Utils|Character")
     static FVector
@@ -43,7 +43,7 @@ public:
         UCapsuleComponent* InCapsule,
         FVector InLocation);
 
-    UFUNCTION(BlueprintCallable,
+    UFUNCTION(BlueprintCallable, BlueprintPure,
               DisplayName = "[Ck] Subtract Capsule Half-Height From Location",
               Category = "Ck|Utils|Character")
     static FVector
