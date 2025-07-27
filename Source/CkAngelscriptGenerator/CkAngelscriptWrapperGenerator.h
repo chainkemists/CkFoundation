63174e4ba5625700a3f0aa5579e2210da92c70d1
AlexObatake <aobatake1515@gmail.com>
2025-07-26 22:35:10 -0700

    feat: Fix generating Angelscript function utils for functions that use script mixin meta data
    *  Using `Meta = (ScriptMixin = "FCk_Handle")` will auto-generate mixins for functions with that type as the first param.
    *  We need to generate the utils for these functions differently since the generated angelscript uses the mixin format
    *  This means there are two ways to call mixin-enabled utils, either like a method or by using the utils from the namespace

--- Source/CkAngelscriptGenerator/CkAngelscriptWrapperGenerator.h
+++ Source/CkAngelscriptGenerator/CkAngelscriptWrapperGenerator.h
@@ -13,6 +13,7 @@ public:
 private:
     static void GenerateWrapperForClass(UClass* Class);
     static FString GenerateWrapperFunction(UFunction* Function, const FString& ClassName, bool IsEditorOnly);
+    static FString GenerateWrapperFunctionForMixin(UFunction* Function, const FString& ClassName, bool IsEditorOnly);
     static FString ConvertToAngelscriptType(const FString& UnrealType);
     static FString ConvertClassNameToNamespace(const FString& ClassName);
     static FString GetAngelscriptParameterDeclaration(FProperty* Property);
@@ -23,6 +24,7 @@ private:
     static bool IsClassInEditorModule(UClass* Class);
     static bool HasInterfaceTypes(UFunction* Function);
     static bool IsInterfaceProperty(FProperty* Property);
+    static bool IsScriptMixin(UFunction* Function, const FString& MixinMetadata);
     static bool IsEditorOnlyFunction(UFunction* Function);
     static FString GetDefaultValueForProperty(FProperty* Property);
     static FString ConvertDefaultValueToAngelscript(const FString& CppDefaultValue, FProperty* Property);
