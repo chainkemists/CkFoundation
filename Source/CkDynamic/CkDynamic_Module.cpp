b30783ea00ccd3bf01e3879114ca2046f3b9fd5a
Sulfur-CK <sulfur@chainkemists.com>
2025-07-31 21:14:20 -0700

    feat: added the CkDynamic module which will house the DynamicFragment feature

---
+++ Source/CkDynamic/CkDynamic_Module.cpp
@@ -0,0 +1,15 @@
+#include "CkDynamic_Module.h"
+
+#define LOCTEXT_NAMESPACE "FCkDynamicModule"
+
+void FCkDynamicModule::StartupModule()
+{
+}
+
+void FCkDynamicModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkDynamicModule, CkDynamic)
