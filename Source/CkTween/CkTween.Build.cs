1e2d659db6583a6b40a4c45b4b96125543158b08
Sulfur-CK <sulfur@chainkemists.com>
2025-08-01 19:38:51 -0700

    feat: added the Tween module

---
+++ Source/CkTween/CkTween.Build.cs
@@ -0,0 +1,30 @@
+using System.IO;
+using UnrealBuildTool;
+
+public class CkTween : CkModuleRules
+{
+    public CkTween(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PrivateIncludePaths.AddRange(new string[] {
+            // ... add other private include paths required here ...
+        });
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+                        "GameplayTags",
+
+            "CkCore",
+            "CkEcs",
+            "CkEcsExt",
+            "CkLabel",
+            "CkLog",
+
+            "CkProvider",
+            "CkRecord",
+            "CkSettings",
+        });
+    }
+}
---
+++ Source/CkTween/CkTween_Module.cpp
@@ -0,0 +1,15 @@
+#include "CkTween_Module.h"
+
+#define LOCTEXT_NAMESPACE "FCkTweenModule"
+
+void FCkTweenModule::StartupModule()
+{
+}
+
+void FCkTweenModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkTweenModule, CkTween)
