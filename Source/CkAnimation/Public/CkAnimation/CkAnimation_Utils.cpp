2416ce91c7386396dacfde1c00cdcb1ccd08b4ad
AlexObatake <aobatake1515@gmail.com>
2025-08-08 00:59:32 -0700

    feat: Add Request_PlayMontage as an exposed wrapper for UPlayMontageCallbackProxy
    *  This is mostly to expose a convenient way to play montage in AngelScript since there is no access to the play montage task

--- Source/CkAnimation/Public/CkAnimation/CkAnimation_Utils.cpp
+++ Source/CkAnimation/Public/CkAnimation/CkAnimation_Utils.cpp
@@ -1,5 +1,7 @@
 #include "CkAnimation_Utils.h"
 
+
+#include "CkCore/Component/CkActorComponent_Utils.h"
 #include "CkCore/Ensure/CkEnsure.h"
 
 // --------------------------------------------------------------------------------------------------------------------
@@ -95,6 +97,53 @@ auto
     { return false; }
 
     return InMontage->GetSkeleton() == InSkeletalMeshComponent->GetSkinnedAsset()->GetSkeleton();
+}
+
+
+auto
+	UCk_Utils_Animation_UE::
+	Request_PlayMontage(
+		USkeletalMeshComponent* InSkeletalMeshComponent,
+		UAnimMontage* MontageToPlay,
+		float PlayRate,
+		float StartingPosition,
+		FName StartingSection,
+		bool ShouldStopAllMontages)
+	-> UPlayMontageCallbackProxy*
+{
+	CK_ENSURE_IF_NOT(ck::IsValid(MontageToPlay),
+		TEXT("Montage to play is NOT valid!"))
+    { return {}; }
+
+	CK_ENSURE_IF_NOT(ck::IsValid(InSkeletalMeshComponent),
+		TEXT("Skeletal Mesh is NOT valid! Montage [{}]"), MontageToPlay)
+    { return {}; }
+
+	CK_ENSURE_IF_NOT(PlayRate > 0,
+		TEXT("Invalid playrate [{}]! Needs to be positive"), PlayRate)
+    { return {}; }
+
+	const auto& AnimInstance = InSkeletalMeshComponent->GetAnimInstance();
+	CK_ENSURE_IF_NOT(ck::IsValid(AnimInstance),
+		TEXT("Anim Instance NOT valid! Skeletal Mesh [{}]"), InSkeletalMeshComponent)
+    { return {}; }
+
+	CK_ENSURE_IF_NOT(DoesMontageMatchMeshSkeleton(MontageToPlay, InSkeletalMeshComponent),
+		TEXT("Montage [{}] does NOT match Skeletal Mesh [{}]!"), MontageToPlay, InSkeletalMeshComponent)
+    { return {}; }
+
+	const auto& NetMode = UCk_Utils_Net_UE::Get_NetMode(InSkeletalMeshComponent);
+	const auto& AllowTickOnDedicatedServer = UCk_Utils_ActorComponent_UE::Get_AllowTickOnDedicatedServer(InSkeletalMeshComponent);
+
+	CK_ENSURE_IF_NOT(NetMode != ECk_Net_NetModeType::Host || AllowTickOnDedicatedServer,
+		TEXT("Trying to run montage on Mesh Component trying to tick on server while disallowed! Montage [{}] Skeletal Mesh [{}]!"), MontageToPlay, InSkeletalMeshComponent)
+    { return {}; }
+
+	CK_ENSURE_IF_NOT(InSkeletalMeshComponent->IsComponentTickEnabled(),
+		TEXT("Trying to run montage on Mesh Component [{}] with tick NOT enabled!"), InSkeletalMeshComponent)
+    { return {}; }
+
+	return UPlayMontageCallbackProxy::CreateProxyObjectForPlayMontage(InSkeletalMeshComponent, MontageToPlay, PlayRate, StartingPosition, StartingSection, ShouldStopAllMontages);
 }
 
 // --------------------------------------------------------------------------------------------------------------------
