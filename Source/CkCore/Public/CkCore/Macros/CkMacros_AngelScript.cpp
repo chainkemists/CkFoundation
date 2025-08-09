1f2cc9cf1359544c445ecf6f49530287f33c3dd8
Nitrogen-CK <nitrogen@chainkemists.com>
2025-08-09 18:21:35 -0400

    feat: CK_DEFINE_CONSTRUCTOR macros now attempt to register the ctor for AngelScript

---
+++ Source/CkCore/Public/CkCore/Macros/CkMacros_AngelScript.cpp
@@ -0,0 +1,68 @@
+#include "CkMacros_AngelScript.h"
+
+#include "AngelscriptCodeModule.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+#if WITH_ANGELSCRIPT_CK
+
+auto
+    FCkAngelScriptCtorFunctionRegistration::
+    EnsureCallbackRegistered()
+    -> void
+{
+    static auto CallbackRegistered = false;
+    if (CallbackRegistered)
+    { return; }
+
+    _PreCompileDelegateHandle = FAngelscriptCodeModule::GetPreCompile().AddStatic([]
+    {
+        RegisterAllCtorFunctions();
+        Get_AllCtorFunctions().Reset();
+    });
+    CallbackRegistered = true;
+}
+
+auto
+    FCkAngelScriptCtorFunctionRegistration::
+    RegisterAllCtorFunctions()
+    -> void
+{
+    for (const auto& RegFunc : Get_AllCtorFunctions())
+    {
+        RegFunc();
+    }
+}
+
+auto
+    FCkAngelScriptCtorFunctionRegistration::
+    Get_AllCtorFunctions()
+    -> TArray<FCtorFunction>&
+{
+    static TArray<FCtorFunction> CtorFunctions;
+    return CtorFunctions;
+}
+
+auto
+    FCkAngelScriptCtorFunctionRegistration::
+    RegisterCtorFunction(
+        const FCtorFunction& InCtorFunction)
+    -> void
+{
+    Get_AllCtorFunctions().Add(InCtorFunction);
+
+    // If AngelScript is already initialized, register immediately
+    if (FAngelscriptManager::IsInitialized())
+    {
+        InCtorFunction();
+    }
+    else
+    {
+        // Otherwise, ensure we have a callback set up
+        EnsureCallbackRegistered();
+    }
+}
+
+#endif
+
+// --------------------------------------------------------------------------------------------------------------------
