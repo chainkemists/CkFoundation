e3c900e67143bea5a6e5f4742da031470d75196e
Nitrogen-CK <nitrogen@chainkemists.com>
2025-08-14 23:50:00 -0400

    feat: Moved various angel script binding macros from TimeUtils.cpp to CkMacros_AngelScript file

--- Source/CkCore/Public/CkCore/Macros/CkMacros_AngelScript.h
+++ Source/CkCore/Public/CkCore/Macros/CkMacros_AngelScript.h
@@ -6,6 +6,54 @@
 #if WITH_ANGELSCRIPT_CK
 
 // --------------------------------------------------------------------------------------------------------------------
+
+#define CK_ANGELSCRIPT_BIND_OPERATOR_EQUALS(_Type_)                                                                                        \
+AS_FORCE_LINK const FAngelscriptBinds::FBind opEquals_##_Type_ (FAngelscriptBinds::EOrder::Early, []                                    \
+{                                                                                                                                       \
+    const FBindFlags Flags;                                                                                                             \
+    auto Bind = FAngelscriptBinds::ValueClass<_Type_>(#_Type_, Flags);                                                                  \
+    Bind.Method("bool opEquals(const "#_Type_"& Other) const",                                                                          \
+        METHODPR_TRIVIAL(bool, _Type_, operator==, (const _Type_&) const));                                                             \
+});
+
+#define CK_ANGELSCRIPT_BIND_OPERATOR_COMPARISON(_Type_)                                                                                    \
+AS_FORCE_LINK const FAngelscriptBinds::FBind opCmp_##_Type_ (FAngelscriptBinds::EOrder::Early, []                                       \
+{                                                                                                                                       \
+    const FBindFlags Flags;                                                                                                             \
+    auto Bind = FAngelscriptBinds::ValueClass<_Type_>(#_Type_, Flags);                                                                  \
+    Bind.Method("int opCmp(const "#_Type_"& Other) const",                                                                              \
+        METHODPR_TRIVIAL(int, _Type_, OpCmp, (const _Type_&) const));                                                                   \
+});
+
+#define CK_ANGELSCRIPT_BIND_OPERATOR_ASSIGNMENT(_Type_, _Operator_, _AS_Op_Name_)                                                          \
+AS_FORCE_LINK const FAngelscriptBinds::FBind _AS_Op_Name_##_##_Type_ (FAngelscriptBinds::EOrder::Early, []                              \
+{                                                                                                                                       \
+    const FBindFlags Flags;                                                                                                             \
+    auto Bind = FAngelscriptBinds::ValueClass<_Type_>(#_Type_, Flags);                                                                  \
+    Bind.Method(#_Type_" "#_AS_Op_Name_"(const "#_Type_"& Other) const",                                                                \
+        METHODPR_TRIVIAL(_Type_, _Type_, operator _Operator_, (const _Type_&) const));                                                  \
+});
+
+#define CK_ANGELSCRIPT_BIND_OPERATOR_UNARY(_Type_, _Ret_Type_, _Ret_Type__AsParam_, _Operator_, _AS_Op_Name_)                       \
+AS_FORCE_LINK const FAngelscriptBinds::FBind _AS_Op_Name_##_##_Type_##_Ret_Type_ (FAngelscriptBinds::EOrder::Early, []           \
+{                                                                                                                                \
+    const FBindFlags Flags;                                                                                                      \
+    auto Bind = FAngelscriptBinds::ValueClass<_Type_>(#_Type_, Flags);                                                           \
+    Bind.Method(#_Ret_Type_" "#_AS_Op_Name_"() const",                                                                           \
+        METHODPR_TRIVIAL(_Ret_Type__AsParam_, _Type_, operator _Operator_, () const));                                           \
+});
+
+// The "_Ret_Type__AsParam_" is needed because float in the bind needs to be "float32" for some weird reason, but it returns a float in c++
+#define CK_ANGELSCRIPT_BIND_OPERATOR_BINARY(_Type_, _Other_Type_, _Other_Type_AsParam_, _Ret_Type_, _Ret_Type__AsParam_, _Operator_, _AS_Op_Name_)      \
+AS_FORCE_LINK const FAngelscriptBinds::FBind _AS_Op_Name_##_##_Type_##_Other_Type_##_Ret_Type_ (FAngelscriptBinds::EOrder::Early, []                 \
+{                                                                                                                                                    \
+    const FBindFlags Flags;                                                                                                                          \
+    auto Bind = FAngelscriptBinds::ValueClass<_Type_>(#_Type_, Flags);                                                                               \
+    Bind.Method(#_Ret_Type_" "#_AS_Op_Name_"("#_Other_Type_AsParam_" Other) const",                                                                  \
+        METHODPR_TRIVIAL(_Ret_Type__AsParam_, _Type_, operator _Operator_, (_Other_Type_AsParam_) const));                                           \
+});
+
+// --------------------------------------------------------------------------------------------------------------------
 // AngelScript Constructor Registration Macros
 
 // Helper macro to generate lambda parameter list
