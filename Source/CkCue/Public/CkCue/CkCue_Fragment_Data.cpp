0f23fe0f536bdc48b2c356ef163f13b115e6ca62
Nitrogen-CK <nitrogen@chainkemists.com>
2025-08-27 16:50:53 -0400

    style: Various formatting fixes

--- Source/CkCue/Public/CkCue/CkCue_Fragment_Data.cpp
+++ Source/CkCue/Public/CkCue/CkCue_Fragment_Data.cpp
@@ -1,14 +1,3 @@
 #include "CkCue_Fragment_Data.h"
 
 // --------------------------------------------------------------------------------------------------------------------
-
-FCk_Request_Cue_Execute::
-    FCk_Request_Cue_Execute(
-        const FGameplayTag& InCueName,
-        const FInstancedStruct& InSpawnParams)
-    : _CueName(InCueName)
-    , _SpawnParams(InSpawnParams)
-{
-}
-
-// --------------------------------------------------------------------------------------------------------------------
