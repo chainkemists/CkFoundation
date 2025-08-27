cd8edfc98b53734c8ef39e1a28259189a0da36aa
Nitrogen-CK <nitrogen@chainkemists.com>
2025-08-27 16:48:30 -0400

    feat: Cue EntityScript now correctly handles all values of _LifetimeBehavior

--- Source/CkCue/Public/CkCue/CkCueBase_EntityScript.cpp
+++ Source/CkCue/Public/CkCue/CkCueBase_EntityScript.cpp
@@ -5,23 +5,48 @@
 
 // --------------------------------------------------------------------------------------------------------------------
 
+UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_Label_Timer_CueLifetime, TEXT("Timer.Cue.Lifetime"));
+UE_DEFINE_GAMEPLAY_TAG_STATIC(TAG_Label_Cue, TEXT("Cue"));
+
+// --------------------------------------------------------------------------------------------------------------------
+
 auto
     UCk_CueBase_EntityScript::
-    Construct(FCk_Handle& InHandle, const FInstancedStruct& InSpawnParams) -> ECk_EntityScript_ConstructionFlow
+    Construct(
+        FCk_Handle& InHandle,
+        const FInstancedStruct& InSpawnParams)
+    -> ECk_EntityScript_ConstructionFlow
 {
-    if (_LifetimeBehavior == ECk_Cue_LifetimeBehavior::Timed)
+    switch (_LifetimeBehavior)
     {
-        auto TimerParams = FCk_Fragment_Timer_ParamsData{_LifetimeDuration}
-            .Set_TimerName(FGameplayTag::RequestGameplayTag("Cue.Lifetime"))
-            .Set_Behavior(ECk_Timer_Behavior::StopOnDone)
-            .Set_StartingState(ECk_Timer_State::Running);
+        case ECk_Cue_LifetimeBehavior::AfterOneFrame:
+        {
+            UCk_Utils_EntityLifetime_UE::Request_DestroyEntity(InHandle);
+            break;
+        }
+        case ECk_Cue_LifetimeBehavior::Persistent:
+        {
+            break;
+        }
+        case ECk_Cue_LifetimeBehavior::Timed:
+        {
+            if (_LifetimeBehavior == ECk_Cue_LifetimeBehavior::Timed)
+            {
+                const auto TimerParams = FCk_Fragment_Timer_ParamsData{_LifetimeDuration}
+                    .Set_TimerName(TAG_Label_Timer_CueLifetime)
+                    .Set_Behavior(ECk_Timer_Behavior::StopOnDone)
+                    .Set_StartingState(ECk_Timer_State::Running);
 
-        auto LifetimeTimer = UCk_Utils_Timer_UE::Add(_AssociatedEntity, TimerParams);
+                auto LifetimeTimer = UCk_Utils_Timer_UE::Add(_AssociatedEntity, TimerParams);
 
-        // Bind to timer completion to self-destruct
-        auto OnDoneDelegate = FCk_Delegate_Timer();
-        OnDoneDelegate.BindUFunction(this, FName("OnLifetimeExpired"));
-        UCk_Utils_Timer_UE::BindTo_OnDone(LifetimeTimer, ECk_Signal_BindingPolicy::IgnorePayloadInFlight, OnDoneDelegate);
+                // Bind to timer completion to self-destruct
+                auto OnDoneDelegate = FCk_Delegate_Timer();
+                OnDoneDelegate.BindDynamic(this, &UCk_CueBase_EntityScript::OnLifetimeExpired);
+                UCk_Utils_Timer_UE::BindTo_OnDone(LifetimeTimer, ECk_Signal_BindingPolicy::IgnorePayloadInFlight, OnDoneDelegate);
+            }
+
+            break;
+        }
     }
 
     return Super::Construct(InHandle, InSpawnParams);
@@ -32,7 +57,8 @@ auto
     OnLifetimeExpired(
         FCk_Handle_Timer InTimer,
         FCk_Chrono InChrono,
-        FCk_Time InDeltaT) -> void
+        FCk_Time InDeltaT)
+    -> void
 {
     UCk_Utils_EntityLifetime_UE::Request_DestroyEntity(_AssociatedEntity);
 }
@@ -45,21 +71,22 @@ auto
 auto
     UCk_CueBase_EntityScript::
     GetAssetRegistryTags(
-        TArray<FAssetRegistryTag>& OutTags) const -> void
+        FAssetRegistryTagsContext Context) const
+    -> void
 {
-    Super::GetAssetRegistryTags(OutTags);
+    Super::GetAssetRegistryTags(Context);
 
     // Tag this asset as a cue for efficient discovery without loading
-    OutTags.Add(FAssetRegistryTag("IsCueAsset", "true", FAssetRegistryTag::TT_Hidden));
+    Context.AddTag(FAssetRegistryTag("IsCueAsset", "true", FAssetRegistryTag::TT_Hidden));
 
     // Add the cue name as a searchable tag for even faster lookup
     if (ck::IsValid(_CueName))
     {
-        OutTags.Add(FAssetRegistryTag("CueName", _CueName.ToString(), FAssetRegistryTag::TT_Hidden));
+        Context.AddTag(FAssetRegistryTag("CueName", _CueName.ToString(), FAssetRegistryTag::TT_Hidden));
     }
 
     // Add cue type information for specialized subsystems
-    OutTags.Add(FAssetRegistryTag("CueBaseClass", GetClass()->GetName(), FAssetRegistryTag::TT_Hidden));
+    Context.AddTag(FAssetRegistryTag("CueBaseClass", GetClass()->GetName(), FAssetRegistryTag::TT_Hidden));
 }
 #endif
 
