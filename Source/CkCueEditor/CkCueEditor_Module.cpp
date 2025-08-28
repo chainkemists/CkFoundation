bb3868b5faa542b5217c9c52b37ad570cd753174
Sulfur-CK <sulfur@chainkemists.com>
2025-08-27 19:29:11 -0700

    fix: fixed issue where the CueToolbox was no longer registered

--- Source/CkCueEditor/CkCueEditor_Module.cpp
+++ Source/CkCueEditor/CkCueEditor_Module.cpp
@@ -1,21 +1,104 @@
 #include "CkCueEditor_Module.h"
 
+#include "CkCueEditor/Toolbox/CkCueToolbox.h"
+#include "CkCueEditor/Toolbox/CkCueToolboxCommands.h"
+#include "CkCueEditor/Toolbox/CkCueToolboxStyle.h"
+
+#include <LevelEditor.h>
+#include <WorkspaceMenuStructure.h>
+#include <WorkspaceMenuStructureModule.h>
+
 #define LOCTEXT_NAMESPACE "FCkCueEditorModule"
 
 auto
-    FCkCueEditorModule::
+    FCk_CueEditorModule::
     StartupModule()
     -> void
 {
+    FCkCueToolboxStyle::Initialize();
+    FCkCueToolboxCommands::Register();
+    DoRegisterTabSpawner();
+
+    auto& LevelEditorModule = FModuleManager::LoadModuleChecked<FLevelEditorModule>("LevelEditor");
+    auto ToolbarExtender = MakeShareable(new FExtender);
+
+    ToolbarExtender.Object->AddToolBarExtension(
+        "Settings",
+        EExtensionHook::After,
+        nullptr,
+        FToolBarExtensionDelegate::CreateLambda([](FToolBarBuilder& Builder)
+        {
+            Builder.BeginSection("CkCue");
+            {
+                Builder.AddToolBarButton(
+                    FCkCueToolboxCommands::Get().OpenCueToolbox,
+                    NAME_None,
+                    FText::FromString(TEXT("Cue Toolbox")),
+                    FText::FromString(TEXT("Open the Cue Toolbox")),
+                    FSlateIcon("CkCueToolboxStyle", "CkCueToolbox.Icon")
+                );
+            }
+            Builder.EndSection();
+        })
+    );
+
+    LevelEditorModule.GetToolBarExtensibilityManager()->AddExtender(ToolbarExtender);
 }
 
 auto
-    FCkCueEditorModule::
+    FCk_CueEditorModule::
     ShutdownModule()
     -> void
 {
+    DoUnregisterTabSpawner();
+    FCkCueToolboxCommands::Unregister();
+    FCkCueToolboxStyle::Shutdown();
 }
+
+auto
+    FCk_CueEditorModule::
+    DoRegisterTabSpawner()
+    -> void
+{
+    auto& GlobalTabManager = FGlobalTabmanager::Get();
+
+    GlobalTabManager->RegisterNomadTabSpawner(
+        TabName,
+        FOnSpawnTab::CreateRaw(this, &FCk_CueEditorModule::DoOnSpawnTab)
+    )
+    .SetDisplayName(FText::FromString(TabDisplayName))
+    .SetTooltipText(FText::FromString(TEXT("Open the Cue Toolbox for discovering and managing gameplay cues")))
+    .SetGroup(WorkspaceMenu::GetMenuStructure().GetDeveloperToolsDebugCategory())
+    .SetIcon(FSlateIcon("CkCueToolboxStyle", "CkCueToolbox.Icon"));
+}
+
+auto
+    FCk_CueEditorModule::
+    DoUnregisterTabSpawner()
+    -> void
+{
+    if (FSlateApplication::IsInitialized())
+    {
+        FGlobalTabmanager::Get()->UnregisterNomadTabSpawner(TabName);
+    }
+}
+
+auto
+    FCk_CueEditorModule::
+    DoOnSpawnTab(const FSpawnTabArgs& Args)
+    -> TSharedRef<SDockTab>
+{
+    return SNew(SDockTab)
+        .TabRole(ETabRole::NomadTab)
+        .Label(FText::FromString(TabDisplayName))
+        .ToolTipText(FText::FromString(TEXT("Cue Toolbox - Discover, validate, and execute gameplay cues")))
+        [
+            SNew(SCkCueToolbox)
+        ];
+}
+
+// --------------------------------------------------------------------------------------------------------------------
 
 #undef LOCTEXT_NAMESPACE
 
-IMPLEMENT_MODULE(FCkCueEditorModule, CkCueEditor)
+IMPLEMENT_MODULE(FCk_CueEditorModule, CkCueEditor)
