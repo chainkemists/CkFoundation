04422337462f07a362c582ba9a0a626aa89cd001
Sulfur-CK <sulfur@chainkemists.com>
2025-08-27 19:29:11 -0700

    fix: fixed issue where the CueToolbox was no longer registered

--- Source/CkCueEditor/CkCueEditor_Module.h
+++ Source/CkCueEditor/CkCueEditor_Module.h
@@ -3,11 +3,25 @@
 #include "CoreMinimal.h"
 #include "Modules/ModuleManager.h"
 
-class FCkCueEditorModule : public IModuleInterface
+// --------------------------------------------------------------------------------------------------------------------
+
+class FCk_CueEditorModule : public IModuleInterface
 {
 public:
+    static constexpr auto ModuleName = "CkCueToolbox";
 
-	/** IModuleInterface implementation */
-	virtual void StartupModule() override;
-	virtual void ShutdownModule() override;
+public:
+    auto StartupModule() -> void override;
+    auto ShutdownModule() -> void override;
+
+private:
+    auto DoRegisterTabSpawner() -> void;
+    auto DoUnregisterTabSpawner() -> void;
+    auto DoOnSpawnTab(const FSpawnTabArgs& Args) -> TSharedRef<SDockTab>;
+
+private:
+    static constexpr auto TabName = "CkCueToolbox";
+    static constexpr auto TabDisplayName = "Cue Toolbox";
 };
+
+// --------------------------------------------------------------------------------------------------------------------
