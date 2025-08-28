149fde9f7b5824ee6da4668b2f9e7bf1e2249671
Sulfur-CK <sulfur@chainkemists.com>
2025-08-27 19:47:43 -0700

    fix: AudioTrack requests now wait for a pending transform update

--- Source/CkAudio/Public/CkAudio/AudioTrack/CkAudioTrack_Processor.h
+++ Source/CkAudio/Public/CkAudio/AudioTrack/CkAudioTrack_Processor.h
@@ -42,6 +42,7 @@ namespace ck
             FFragment_AudioTrack_Current,
             FFragment_AudioTrack_Requests,
             TExclude<FTag_AudioTrack_NeedsSetup>,
+            TExclude<FTag_Transform_Updated>,
             CK_IGNORE_PENDING_KILL>
     {
     public:
