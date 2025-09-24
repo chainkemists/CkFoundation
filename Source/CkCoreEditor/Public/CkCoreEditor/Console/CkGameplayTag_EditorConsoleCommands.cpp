7a249b2c5036ef6f7d1e466db7d119f8fa6aa76d
AlexObatake <aobatake1515@gmail.com>
2025-09-24 13:06:49 -0700

    feat: Add console command to add Gameplay Tags
    *  Simple command to add a tag to default gameplay tags
    *  Can be run through editor console (doesn't need to be in PIE)
    *  Works with period or underscore separated names

---
+++ Source/CkCoreEditor/Public/CkCoreEditor/Console/CkGameplayTag_EditorConsoleCommands.cpp
@@ -0,0 +1,38 @@
+﻿#include "CkGameplayTag_EditorConsoleCommands.h"
+
+#include "CkCoreEditor/CkCoreEditor_Log.h"
+
+#include "HAL/IConsoleManager.h"
+
+// ----------------------------------------------------------------------------------------------------------------
+
+namespace ck
+{
+    static FAutoConsoleCommand AddGameplayTagCmd(
+        TEXT("AddGameplayTag"),
+        TEXT("AddGameplayTag <TagName> [OptionalDescription]\nAdds a gameplay tag. Can use '.' or '_' as separators."),
+        FConsoleCommandWithArgsDelegate::CreateLambda([](const TArray<FString>& Args)
+        {
+            if (Args.Num() < 1)
+            {
+                core_editor::Warning(TEXT("Usage: AddGameplayTag <TagName> [OptionalDescription]"));
+                return;
+            }
+
+            auto TagName = Args[0];
+            const auto& Description = Args.Num() > 1 ? Args[1] : TEXT("Added via AddGameplayTag console command");
+
+            // if no period separators, assume this is using Angelscript tag name format which separates with underscores instead
+            if (NOT TagName.Contains(TEXT(".")))
+            {
+                TagName = TagName.Replace(TEXT("_"), TEXT("."));
+            }
+
+            core_editor::Display(TEXT("Adding gameplay tag: [{}] (Description: [{}])"), TagName, Description);
+
+            UCk_Utils_GameplayTag_UE::ResolveGameplayTag(FName(TagName), Description);
+        })
+    );
+}
+
+// ----------------------------------------------------------------------------------------------------------------
