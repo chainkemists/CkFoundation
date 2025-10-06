f5f5d5a361f90db96ac97806fd88fb97b01641ef
Nitrogen-CK <nitrogen@chainkemists.com>
2025-10-06 15:07:59 -0400

    feat: Added TFuncArgsAreAllRefOrPointerOrSmallTrivial compile-time check
    Implements trait to detect function signatures with expensive by-value arguments.
    Enforces that arguments are passed by reference/pointer or are small trivial types (<= 16 bytes).

--- Source/CkCore/Public/CkCore/TypeTraits/CkTypeTraits.h
+++ Source/CkCore/Public/CkCore/TypeTraits/CkTypeTraits.h
@@ -98,6 +98,8 @@ namespace ck::type_traits
         }
     };
 
+    // --------------------------------------------------------------------------------------------------------------------
+
     template <typename T>
     using AddConstUnlessAlready = std::conditional_t<
         std::is_const_v<std::remove_pointer_t<T>>,
@@ -111,6 +113,61 @@ namespace ck::type_traits
         T,           // Pass by value if copyable
         const T&     // Pass by const reference if not copyable
     >;
+
+    // Helper trait to check if a single type is acceptable
+    template<typename T>
+    struct TIsRefOrPointerOrSmallTrivial
+    {
+        // Remove any const qualifiers for size checking
+        using DecayedType = typename TRemoveConst<T>::Type;
+
+        enum { Value =
+            TIsReferenceType<T>::Value ||
+            TIsPointer<T>::Value ||
+            (TIsTrivial<DecayedType>::Value && sizeof(DecayedType) <= 16)
+        };
+    };
+
+    // Primary template for function signature checking
+    template<typename Signature>
+    struct TFuncArgsAreAllRefOrPointerOrSmallTrivial;
+
+    // Specialization for no arguments
+    template<typename RetType>
+    struct TFuncArgsAreAllRefOrPointerOrSmallTrivial<RetType()>
+    {
+        enum { Value = true };
+    };
+
+    // Specialization for one or more arguments (recursive)
+    template<typename RetType, typename FirstArg, typename... RestArgs>
+    struct TFuncArgsAreAllRefOrPointerOrSmallTrivial<RetType(FirstArg, RestArgs...)>
+    {
+        enum { Value =
+            TIsRefOrPointerOrSmallTrivial<FirstArg>::Value &&
+            TFuncArgsAreAllRefOrPointerOrSmallTrivial<RetType(RestArgs...)>::Value
+        };
+    };
+
+    // Support for const member functions
+    template<typename RetType, typename... Args>
+    struct TFuncArgsAreAllRefOrPointerOrSmallTrivial<RetType(Args...) const>
+        : TFuncArgsAreAllRefOrPointerOrSmallTrivial<RetType(Args...)>
+    {
+    };
+
+    // Support for member function pointers
+    template<typename Class, typename RetType, typename... Args>
+    struct TFuncArgsAreAllRefOrPointerOrSmallTrivial<RetType(Class::*)(Args...)>
+        : TFuncArgsAreAllRefOrPointerOrSmallTrivial<RetType(Args...)>
+    {
+    };
+
+    template<typename Class, typename RetType, typename... Args>
+    struct TFuncArgsAreAllRefOrPointerOrSmallTrivial<RetType(Class::*)(Args...) const>
+        : TFuncArgsAreAllRefOrPointerOrSmallTrivial<RetType(Args...)>
+    {
+    };
 }
 
 // --------------------------------------------------------------------------------------------------------------------
