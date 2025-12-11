f1cffe723a73942ea2ade6aa8efa4392a7085a1f
Saad K <saadrustam@gmail.com>
2025-12-11 14:47:41 -0800

    feat: added function to get all components in an Actor's root component hierarchy

--- Source/CkCore/Public/CkCore/Component/CkActorComponent_Utils.cpp
+++ Source/CkCore/Public/CkCore/Component/CkActorComponent_Utils.cpp
@@ -4,6 +4,51 @@
 
 auto
     UCk_Utils_ActorComponent_UE::
+    Get_AllComponentsInHierarchy(
+        const AActor* InActor)
+    -> TArray<UActorComponent*>
+{
+    auto OutComponents = TArray<UActorComponent*>{};
+
+    if (ck::Is_NOT_Valid(InActor))
+    { return OutComponents; }
+
+    auto Root = InActor->GetRootComponent();
+    if (ck::Is_NOT_Valid(Root))
+    { return OutComponents; }
+
+    auto Stack = TArray<USceneComponent*>{};
+    Stack.Add(Root);
+
+    while (Stack.Num() > 0)
+    {
+        const auto Current = Stack.Pop();
+
+        CK_ENSURE_IF_NOT(ck::IsValid(Current),
+            TEXT("Current component [{}] was expected to be valid during hierarchical traversal of Actor [{}]"),
+            InActor)
+        { continue; }
+
+        OutComponents.Add(Current);
+
+        TArray<USceneComponent*> Children;
+
+        Current->GetChildrenComponents(true, Children);
+
+        for (auto* Child : Children)
+        {
+            if (ck::IsValid(Child))
+            {
+                Stack.Add(Child);
+            }
+        }
+    }
+
+    return OutComponents;
+}
+
+auto
+    UCk_Utils_ActorComponent_UE::
     Get_AllowTickOnDedicatedServer(
         const UActorComponent* InActorComponent)
     -> bool
