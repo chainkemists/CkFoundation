ed178fb8256b1c0703d10696e355b5a02f3863db
Saad K <saadrustam@gmail.com>
2025-12-17 01:59:58 -0800

    feat: add StateTree utilities and processor injector

---
+++ Source/CkStateTree/Public/CkStateTree/ProcessorInjector/CkStateTree_ProcessorInjector.cpp
@@ -0,0 +1,20 @@
+#include "CkStateTree_ProcessorInjector.h"
+
+#include "CkStateTree/CkStateTree_Processor.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+auto
+    UCk_StateTree_ProcessorInjector_UE::
+    DoInjectProcessors(
+        EcsWorldType& InWorld)
+        -> void
+{
+    InWorld.Add<ck::FProcessor_StateTree_Setup>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_StateTree_HandleRequests>(InWorld.Get_Registry());
+    InWorld.Add<ck::FProcessor_StateTree_EndPlay>(InWorld.Get_Registry());
+
+    InWorld.Add<ck::FProcessor_StateTree_Replicate>(InWorld.Get_Registry());
+}
+
+// --------------------------------------------------------------------------------------------------------------------
