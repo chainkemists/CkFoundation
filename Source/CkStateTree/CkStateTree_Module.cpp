aafefcb326e3c6a2b31ab83a67e6b778df401595
Saad K <saadrustam@gmail.com>
2025-12-17 01:19:47 -0800

    feat: add CkStateTree module infrastructure

---
+++ Source/CkStateTree/CkStateTree_Module.cpp
@@ -0,0 +1,15 @@
+#include "CkStateTree_Module.h"
+
+#define LOCTEXT_NAMESPACE "FCkStateTreeModule"
+
+void FCkStateTreeModule::StartupModule()
+{
+}
+
+void FCkStateTreeModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkStateTreeModule, CkStateTree)
