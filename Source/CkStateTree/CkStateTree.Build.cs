68ce2cfba1626b087c3225a818ccbccd192dc237
Saad K <saadrustam@gmail.com>
2025-12-17 01:19:47 -0800

    feat: add CkStateTree module infrastructure

---
+++ Source/CkStateTree/CkStateTree.Build.cs
@@ -0,0 +1,31 @@
+using System.IO;
+using UnrealBuildTool;
+
+public class CkStateTree : CkModuleRules
+{
+    public CkStateTree(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PrivateIncludePaths.AddRange(new string[] {
+            // ... add other private include paths required here ...
+        });
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "GameplayTags",
+            "StateTreeModule",
+
+            "CkCore",
+            "CkEcs",
+            "CkEcsExt",
+            "CkLabel",
+            "CkLog",
+
+            "CkProvider",
+            "CkRecord",
+            "CkSettings",
+        });
+    }
+}
