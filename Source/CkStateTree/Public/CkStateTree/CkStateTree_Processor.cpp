903039e3d0b2d7a9bc1b224772146bd13416f998
Saad K <saadrustam@gmail.com>
2025-12-17 01:52:55 -0800

    feat: add StateTree ECS processors for setup, requests, endplay, and replication

---
+++ Source/CkStateTree/Public/CkStateTree/CkStateTree_Processor.cpp
@@ -0,0 +1,119 @@
+#include "CkStateTree_Processor.h"
+
+#include "CkCore/Algorithms/CkAlgorithms.h"
+
+#include "CkStateTree/CkStateTree_Log.h"
+
+#include "CkEcs/Net/CkNet_Utils.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ck
+{
+    auto
+        FProcessor_StateTree_Setup::
+        DoTick(
+            TimeType InDeltaT)
+        -> void
+    {
+        TProcessor::DoTick(InDeltaT);
+
+        _TransientEntity.Clear<MarkedDirtyBy>();
+    }
+
+    auto
+        FProcessor_StateTree_Setup::
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_StateTree_Params& InParams,
+            FFragment_StateTree_Current& InCurrent) const
+        -> void
+    {
+        // Add setup logic here
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    auto
+        FProcessor_StateTree_HandleRequests::
+        DoTick(
+            TimeType InDeltaT)
+        -> void
+    {
+        _TransientEntity.Clear<FTag_StateTree_Updated>();
+
+        TProcessor::DoTick(InDeltaT);
+
+        _TransientEntity.Clear<MarkedDirtyBy>();
+    }
+
+    auto
+        FProcessor_StateTree_HandleRequests::
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_StateTree_Params& InParams,
+            FFragment_StateTree_Current& InCurrent,
+            FFragment_StateTree_Requests& InRequestsComp) const
+        -> void
+    {
+        InHandle.CopyAndRemove(InRequestsComp, [&](FFragment_StateTree_Requests& InRequests)
+        {
+            algo::ForEachRequest(InRequests._Requests, ck::Visitor([&](const auto& InRequest)
+            {
+                DoHandleRequest(InHandle, InParams, InCurrent, InRequest);
+
+                if (InRequest.Get_IsRequestHandleValid())
+                {
+                    InRequest.GetAndDestroyRequestHandle();
+                }
+            }));
+        });
+    }
+
+    auto
+        FProcessor_StateTree_HandleRequests::
+        DoHandleRequest(
+            HandleType InHandle,
+            const FFragment_StateTree_Params& InParams,
+            FFragment_StateTree_Current& InCurrent,
+            const FCk_Request_StateTree_ExampleRequest& InRequest)
+        -> void
+    {
+        // Add request handling logic here
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    auto
+        FProcessor_StateTree_EndPlay::
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_StateTree_Params& InParams,
+            FFragment_StateTree_Current& InCurrent) const
+        -> void
+    {
+        // Add teardown logic here
+    }
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    auto
+        FProcessor_StateTree_Replicate::
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            FFragment_StateTree_Current& InCurrent,
+            const TObjectPtr<UCk_Fragment_StateTree_Rep>& InRepComp) const
+        -> void
+    {
+        UCk_Utils_Net_UE::TryUpdateReplicatedFragment<UCk_Fragment_StateTree_Rep>(InHandle, [&](UCk_Fragment_StateTree_Rep* InLocalRepComp)
+        {
+            // Add replication logic here
+        });
+    }
+}
+
+// --------------------------------------------------------------------------------------------------------------------
---
+++ Source/CkStateTree/Public/CkStateTree/CkStateTree_Processor.h
@@ -0,0 +1,125 @@
+#pragma once
+
+#include "CkStateTree_Fragment.h"
+#include "CkEcs/EntityLifetime/CkEntityLifetime_Fragment.h"
+
+#include "CkEcs/Processor/CkProcessor.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ck
+{
+    class CKSTATETREE_API FProcessor_StateTree_Setup : public ck_exp::TProcessor<
+            FProcessor_StateTree_Setup,
+            FCk_Handle_StateTree,
+            FFragment_StateTree_Params,
+            FFragment_StateTree_Current,
+            FTag_StateTree_RequiresSetup,
+            CK_IGNORE_PENDING_KILL>
+    {
+    public:
+        using MarkedDirtyBy = FTag_StateTree_RequiresSetup;
+
+    public:
+        using TProcessor::TProcessor;
+
+    public:
+        auto
+        DoTick(
+            TimeType InDeltaT) -> void;
+
+    public:
+        auto
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_StateTree_Params& InParams,
+            FFragment_StateTree_Current& InCurrent) const -> void;
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    class CKSTATETREE_API FProcessor_StateTree_HandleRequests : public ck_exp::TProcessor<
+            FProcessor_StateTree_HandleRequests,
+            FCk_Handle_StateTree,
+            FFragment_StateTree_Params,
+            FFragment_StateTree_Current,
+            FFragment_StateTree_Requests,
+            CK_IGNORE_PENDING_KILL>
+    {
+    public:
+        using MarkedDirtyBy = FFragment_StateTree_Requests;
+
+    public:
+        using TProcessor::TProcessor;
+
+    public:
+        auto
+        DoTick(
+            TimeType InDeltaT) -> void;
+
+    public:
+        auto
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_StateTree_Params& InParams,
+            FFragment_StateTree_Current& InCurrent,
+            FFragment_StateTree_Requests& InRequestsComp) const -> void;
+
+    private:
+        static auto
+        DoHandleRequest(
+            HandleType InHandle,
+            const FFragment_StateTree_Params& InParams,
+            FFragment_StateTree_Current& InCurrent,
+            const FCk_Request_StateTree_ExampleRequest& InRequest) -> void;
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    class CKSTATETREE_API FProcessor_StateTree_EndPlay : public ck_exp::TProcessor<
+            FProcessor_StateTree_EndPlay,
+            FCk_Handle_StateTree,
+            FFragment_StateTree_Params,
+            FFragment_StateTree_Current,
+            CK_IF_END_PLAY>
+    {
+    public:
+        using TProcessor::TProcessor;
+
+    public:
+        auto
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_StateTree_Params& InParams,
+            FFragment_StateTree_Current& InCurrent) const -> void;
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    class CKSTATETREE_API FProcessor_StateTree_Replicate : public ck_exp::TProcessor<
+            FProcessor_StateTree_Replicate,
+            FCk_Handle_StateTree,
+            FFragment_StateTree_Current,
+            TObjectPtr<UCk_Fragment_StateTree_Rep>,
+            FTag_StateTree_Updated,
+            CK_IGNORE_PENDING_KILL>
+    {
+    public:
+        using MarkedDirtyBy = FTag_StateTree_Updated;
+
+    public:
+        using TProcessor::TProcessor;
+
+    public:
+        auto ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            FFragment_StateTree_Current& InCurrent,
+            const TObjectPtr<UCk_Fragment_StateTree_Rep>& InRepComp) const -> void;
+    };
+}
+
+// --------------------------------------------------------------------------------------------------------------------
