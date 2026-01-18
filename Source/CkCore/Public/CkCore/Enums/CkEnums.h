68f1f2c829bb097eeaf676e53ee4ef2707fca3e4
Saad K <saadrustam@gmail.com>
2026-01-17 18:19:52 -0800

    refactor: SanityCheck enum is Checked by default
    default as UnChecked results in too many hidden errors that are harder to resolve

--- Source/CkCore/Public/CkCore/Enums/CkEnums.h
+++ Source/CkCore/Public/CkCore/Enums/CkEnums.h
@@ -742,8 +742,8 @@ CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_Collision);
 UENUM(BlueprintType)
 enum class ECk_SanityCheck : uint8
 {
-    UnChecked,
-    Checked
+    Checked,
+    UnChecked
 };
 
 CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_SanityCheck);
