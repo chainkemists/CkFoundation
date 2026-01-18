f947e3c580462823349777aba3c6f4e5f2191ee7
Saad K <saadrustam@gmail.com>
2026-01-17 18:22:56 -0800

    fix: fixed issue where the AngelScript debugger would trigger an ensure
    the AngelScript debugger gets Handle information from all sources where the Handle is identified as a mixin, which is the case with OwningActor and Label where some getters ensure

--- Source/CkEcs/Public/CkEcs/OwningActor/CkOwningActor_Utils.cpp
+++ Source/CkEcs/Public/CkEcs/OwningActor/CkOwningActor_Utils.cpp
@@ -9,6 +9,10 @@
 
 #include "CkEcs/Handle/CkHandle.h"
 
+#if WITH_ANGELSCRIPT_CK
+#include <AngelscriptCode/Private/Debugging/AngelscriptDebugServer.h>
+#endif
+
 // --------------------------------------------------------------------------------------------------------------------
 
 auto
@@ -66,6 +70,17 @@ auto
         const FCk_Handle& InHandle)
     -> AActor*
 {
+#if WITH_AS_DEBUGSERVER
+    // do NOT ensure if we are paused in the AngelScript debugger
+    auto& Manager = FAngelscriptManager::Get();
+    if (Manager.DebugServer != nullptr &&
+        Manager.DebugServer->bIsDebugging &&
+        Manager.DebugServer->bIsPaused)
+    {
+        if (NOT Has(InHandle))
+        { return {}; }
+    }
+#endif
     if (NOT Ensure(InHandle))
     { return {}; }
 
@@ -104,6 +119,18 @@ auto
         const FCk_Handle& InHandle)
     -> FCk_EntityOwningActor_BasicDetails
 {
+#if WITH_AS_DEBUGSERVER
+    // do NOT ensure if we are paused in the AngelScript debugger
+    auto& Manager = FAngelscriptManager::Get();
+    if (Manager.DebugServer != nullptr &&
+        Manager.DebugServer->bIsDebugging &&
+        Manager.DebugServer->bIsPaused)
+    {
+        if (NOT Has(InHandle))
+        { return {}; }
+    }
+#endif
+
     if (NOT Ensure(InHandle))
     { return {}; }
 
--- Source/CkLabel/Public/CkLabel/CkLabel_Utils.cpp
+++ Source/CkLabel/Public/CkLabel/CkLabel_Utils.cpp
@@ -5,6 +5,10 @@
 
 #include <GameplayTagsManager.h>
 
+#if WITH_ANGELSCRIPT_CK
+#include <AngelscriptCode/Private/Debugging/AngelscriptDebugServer.h>
+#endif
+
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace ck_label
@@ -92,6 +96,17 @@ auto
         const FCk_Handle& InHandle)
     -> FGameplayTag
 {
+#if WITH_AS_DEBUGSERVER
+    // do NOT ensure if we are paused in the AngelScript debugger
+    auto& Manager = FAngelscriptManager::Get();
+    if (Manager.DebugServer != nullptr &&
+        Manager.DebugServer->bIsDebugging &&
+        Manager.DebugServer->bIsPaused)
+    {
+        if (NOT Has(InHandle))
+        { return {}; }
+    }
+#endif
     if (NOT Ensure(InHandle))
     { return {}; }
 
@@ -104,6 +119,18 @@ auto
         const FCk_Handle& InHandle)
     -> bool
 {
+#if WITH_AS_DEBUGSERVER
+    // do NOT ensure if we are paused in the AngelScript debugger
+    auto& Manager = FAngelscriptManager::Get();
+    if (Manager.DebugServer != nullptr &&
+        Manager.DebugServer->bIsDebugging &&
+        Manager.DebugServer->bIsPaused)
+    {
+        if (NOT Has(InHandle))
+        { return {}; }
+    }
+#endif
+
     CK_ENSURE_IF_NOT(Has(InHandle), TEXT("Handle [{}] does NOT have Gameplay Label"), InHandle)
     { return false; }
 
