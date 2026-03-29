15adf688525a63e93b1c47917b9885ee71c19fc0
Adam Letourneau <adam.letourneau1993@gmail.com>
2026-03-29 18:10:52 -0400

    feat: add native gameplay tag definitions for UI.ExtensionPoint and UI.Layer
    Define tags in C++ via UE_DEFINE_GAMEPLAY_TAG so they are always available
    without requiring DefaultGameplayTags.ini entries. Includes four default
    layer tags: Game, GameMenu, Menu, Modal.

---
+++ Source/CkUI/Public/CkUI/CkUI_GameplayTags.cpp
@@ -0,0 +1,21 @@
+// Copyright 2025 CkFoundation. All Rights Reserved.
+
+#include "CkUI/CkUI_GameplayTags.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+// Extension Point Tags
+// --------------------------------------------------------------------------------------------------------------------
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_UI_ExtensionPoint, TEXT("UI.ExtensionPoint"));
+
+// --------------------------------------------------------------------------------------------------------------------
+// Layer Tags
+// --------------------------------------------------------------------------------------------------------------------
+
+UE_DEFINE_GAMEPLAY_TAG(TAG_UI_Layer, TEXT("UI.Layer"));
+UE_DEFINE_GAMEPLAY_TAG(TAG_UI_Layer_Game, TEXT("UI.Layer.Game"));
+UE_DEFINE_GAMEPLAY_TAG(TAG_UI_Layer_GameMenu, TEXT("UI.Layer.GameMenu"));
+UE_DEFINE_GAMEPLAY_TAG(TAG_UI_Layer_Menu, TEXT("UI.Layer.Menu"));
+UE_DEFINE_GAMEPLAY_TAG(TAG_UI_Layer_Modal, TEXT("UI.Layer.Modal"));
+
+// --------------------------------------------------------------------------------------------------------------------
---
+++ Source/CkUI/Public/CkUI/CkUI_GameplayTags.h
@@ -0,0 +1,23 @@
+// Copyright 2025 CkFoundation. All Rights Reserved.
+
+#pragma once
+
+#include <NativeGameplayTags.h>
+
+// --------------------------------------------------------------------------------------------------------------------
+// Extension Point Tags
+// --------------------------------------------------------------------------------------------------------------------
+
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_UI_ExtensionPoint);
+
+// --------------------------------------------------------------------------------------------------------------------
+// Layer Tags
+// --------------------------------------------------------------------------------------------------------------------
+
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_UI_Layer);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_UI_Layer_Game);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_UI_Layer_GameMenu);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_UI_Layer_Menu);
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(TAG_UI_Layer_Modal);
+
+// --------------------------------------------------------------------------------------------------------------------
