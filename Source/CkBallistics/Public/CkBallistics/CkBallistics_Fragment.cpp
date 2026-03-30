7221a4ab594366ca17f8177984e3866766c49fb5
Neil Koo <neil.j.koo@gmail.com>
2026-03-30 19:18:13 -0400

    chore: remove CkBallistics module and RMPP plugin dependency

--- Source/CkBallistics/Public/CkBallistics/CkBallistics_Fragment.cpp
+++
@@ -1,9 +0,0 @@
-#include "CkBallistics_Fragment.h"
-
-#include "CkEcs/Handle/CkDebugCallstack_Macros.h"
-
-// --------------------------------------------------------------------------------------------------------------------
-
-CK_ECS_DEFINE_CALLSTACK_ANGELSCRIPT_UTILS(CKBALLISTICS_API, ballistics, ck::FFragment_Ballistics_Requests);
-
-// --------------------------------------------------------------------------------------------------------------------
