f31a1bbedabe07311d8a4e9085f423e3fea446a9
Adam Letourneau <adam.letourneau1993@gmail.com>
2026-03-30 02:05:58 -0400

    refactor: remove old UI context system (subsystem, utils, interface, FCk_UI_Context)
    Delete UCk_UI_Context_Subsystem_UE, UCk_Utils_UI_Context_UE,
    ICk_UI_ContextReceiver interface, and FCk_UI_Context struct.
    Add ECk_UI_ForEachWidgetResult enum for widget tree traversal.
    All context injection now goes through the ECS ContextReceiver.

--- Source/CkUI/Public/CkUI/Interfaces/CkUI_Interfaces.h
+++ Source/CkUI/Public/CkUI/Interfaces/CkUI_Interfaces.h
@@ -2,54 +2,10 @@
 
 #pragma once
 
-#include "CkUI/Types/CkUI_Types.h"
-
 #include <GameplayTagContainer.h>
 #include <UObject/Interface.h>
 
 #include "CkUI_Interfaces.generated.h"
-
-// --------------------------------------------------------------------------------------------------------------------
-// ICk_UI_ContextReceiver
-// --------------------------------------------------------------------------------------------------------------------
-
-/**
- * Interface for widgets that can receive context injection.
- *
- * Context is managed by the Context Subsystem's registry. Widgets implementing
- * this interface will be notified when context is injected or cleared.
- *
- * To query context, use UCk_Utils_UI_Context_UE::Get_ContextForWidget() or
- * UCk_UI_Context_Subsystem_UE::Get_ContextForWidget() rather than storing context locally.
- */
-UINTERFACE(MinimalAPI, Blueprintable, BlueprintType)
-class UCk_UI_ContextReceiver : public UInterface { GENERATED_BODY() };
-class CKUI_API ICk_UI_ContextReceiver
-{
-    GENERATED_BODY()
-
-public:
-    /**
-     * Called when context is injected into this widget.
-     * The widget should use this to react to the new context (e.g., update displays).
-     */
-    UFUNCTION(BlueprintNativeEvent, Category = "Ck|UI|Context")
-    void OnContextInjected(const FCk_UI_Context& InContext);
-
-    /**
-     * Called when context is cleared from this widget.
-     * The widget should use this to reset any context-dependent state.
-     */
-    UFUNCTION(BlueprintNativeEvent, Category = "Ck|UI|Context")
-    void OnContextCleared();
-
-    /**
-     * Returns whether this widget should inherit context from its parent widget.
-     * Default is true - override to prevent automatic context propagation.
-     */
-    UFUNCTION(BlueprintNativeEvent, Category = "Ck|UI|Context")
-    bool Get_ShouldInheritContextFromParent() const;
-};
 
 // --------------------------------------------------------------------------------------------------------------------
 // ICk_UI_LayerParticipant
--- Source/CkUI/Public/CkUI/Types/CkUI_Types.h
+++ Source/CkUI/Public/CkUI/Types/CkUI_Types.h
@@ -3,7 +3,6 @@
 #pragma once
 
 #include "CkCore/Macros/CkMacros.h"
-#include "CkEcs/Handle/CkHandle.h"
 
 #include <GameplayTagContainer.h>
 
@@ -100,67 +99,15 @@ enum class ECk_UI_NewInputMode : uint8
 CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_UI_NewInputMode);
 
 // --------------------------------------------------------------------------------------------------------------------
-// Context
-// --------------------------------------------------------------------------------------------------------------------
 
-/**
- * Context data that can be injected into widgets.
- * Provides a flexible way to pass entity, actor, or arbitrary object references.
- */
-USTRUCT(BlueprintType)
-struct CKUI_API FCk_UI_Context
+UENUM(BlueprintType)
+enum class ECk_UI_ForEachWidgetResult : uint8
 {
-    GENERATED_BODY()
-
-public:
-    CK_GENERATED_BODY(FCk_UI_Context);
-
-    FCk_UI_Context() = default;
-
-    static auto MakeFromEntity(FCk_Handle InEntity) -> FCk_UI_Context;
-    static auto MakeFromActor(AActor* InActor) -> FCk_UI_Context;
-    static auto MakeFromObject(UObject* InObject) -> FCk_UI_Context;
-
-    auto IsValid() const -> bool;
-    auto HasEntity() const -> bool;
-    auto HasActor() const -> bool;
-    auto HasPayload() const -> bool;
-
-    auto Reset() -> void;
-
-    auto operator==(const ThisType& Other) const -> bool;
-    CK_DECL_AND_DEF_OPERATOR_NOT_EQUAL(ThisType);
-
-
-private:
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
-    FCk_Handle _Entity;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
-    TWeakObjectPtr<AActor> _Actor;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
-    TWeakObjectPtr<UObject> _Payload;
-
-public:
-    CK_PROPERTY_GET(_Entity);
-    CK_PROPERTY_GET(_Actor);
-    CK_PROPERTY_GET(_Payload);
+    Continue,
+    SkipSubtree
 };
 
-CK_DEFINE_CUSTOM_FORMATTER_INLINE(FCk_UI_Context, [](const FCk_UI_Context& InContext)
-{
-    return ck::Format(TEXT("Entity:[{}] | Actor:[{}] | Payload:[{}]"),
-        InContext.Get_Entity(),
-        InContext.Get_Actor().Get(),
-        InContext.Get_Payload().Get());
-});
-
-CK_DEFINE_CUSTOM_IS_VALID_INLINE(FCk_UI_Context, IsValid_Policy_Default,
-[=](const FCk_UI_Context& InContext)
-{
-    return InContext.IsValid();
-});
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_UI_ForEachWidgetResult);
 
 // --------------------------------------------------------------------------------------------------------------------
 
