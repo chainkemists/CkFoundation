935c79bc9203c712893ba4a2defc7d372c2d5c73
Sulfur-CK <sulfur@chainkemists.com>
2026-04-03 01:49:46 -0700

    feat: add EntityScript_WithActor base class to replace EntityBridge
    Introduce UCk_EntityScript_WithActor_UE in CkEcsExt that automatically
    adds OwningActor and Transform features during construction, providing
    a simpler alternative to EntityBridge for actor-entity association.
    
    Also consolidate the duplicated actor-entity link setup logic into
    UCk_Utils_OwningActor_UE::SetupActorEntityLink.

---
+++ Source/CkEcsExt/Public/CkEcsExt/EntityScript/CkEntityScript_WithActor.h
@@ -0,0 +1,62 @@
+#pragma once
+
+#include "CkEcs/EntityScript/CkEntityScript.h"
+
+#include "CkEntityScript_WithActor.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UCLASS(Abstract, Blueprintable, BlueprintType)
+class CKECSEXT_API UCk_EntityScript_WithActor_UE : public UCk_EntityScript_UE
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(UCk_EntityScript_WithActor_UE);
+
+public:
+    [[nodiscard]]
+    auto
+    Construct(
+        FCk_Handle& InHandle,
+        const FInstancedStruct& InSpawnParams) -> ECk_EntityScript_ConstructionFlow override;
+
+    auto
+    EndPlay() -> void override;
+
+protected:
+    [[nodiscard]]
+    virtual auto
+    ConstructWithActor(
+        FCk_Handle& InHandle,
+        AActor* InOwningActor) -> ECk_EntityScript_ConstructionFlow;
+
+protected:
+    UFUNCTION(BlueprintImplementableEvent,
+        Category = "Ck|EntityScript|WithActor",
+        DisplayName = "ConstructionScript (WithActor)")
+    ECk_EntityScript_ConstructionFlow
+    DoConstructWithActor(
+        UPARAM(ref) FCk_Handle& InHandle,
+        AActor* InOwningActor);
+
+    UFUNCTION(BlueprintPure,
+        Category = "Ck|EntityScript|WithActor",
+        DisplayName = "[Ck][EntityScript] Get Owning Actor",
+        meta = (CompactNodeTitle="OwningActor", HideSelfPin = true))
+    AActor*
+    DoGet_OwningActor() const;
+
+protected:
+    auto
+    ShowReplicationInEditor() const -> bool override;
+
+private:
+    UPROPERTY(Transient)
+    TObjectPtr<AActor> _OwningActor;
+
+public:
+    CK_PROPERTY_GET(_OwningActor);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
