b013d7a896fe208c90be559d791ae2fc35fa99f6
Adam Letourneau <adam.letourneau1993@gmail.com>
2026-04-03 19:32:43 -0400

    refactor: split ProbeTrace processors into dedicated files with debug draw support
    Move FProcessor_ProbeTrace from CkProbe_Processor into new CkProbeTrace_Processor
    files, renamed to FProcessor_ProbeTrace_RayCast. Add FProcessor_ProbeTrace_ShapeCast
    for persistent shape traces. Add FProcessor_ProbeTrace_DebugDraw_RayCast/ShapeCast
    processors that draw traces when FTag_ProbeTrace_DebugDraw is present.

---
+++ Source/CkSpatialQuery/Public/CkSpatialQuery/Probe/CkProbeTrace_Processor.h
@@ -0,0 +1,113 @@
+#pragma once
+
+#include "CkSpatialQuery/Probe/CkProbe_Fragment.h"
+
+#include "CkEcs/Processor/CkProcessor.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+// ReSharper disable once CppInconsistentNaming
+namespace JPH { class PhysicsSystem; }
+
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace ck
+{
+    class CKSPATIALQUERY_API FProcessor_ProbeTrace_RayCast : public ck_exp::TProcessor<
+        FProcessor_ProbeTrace_RayCast,
+        FCk_Handle_ProbeTrace,
+        FFragment_ProbeTrace_RayCast,
+        CK_IGNORE_PENDING_KILL>
+    {
+    public:
+        using MarkedDirtyBy = FFragment_Probe_Requests;
+
+        FProcessor_ProbeTrace_RayCast(
+            const RegistryType& InRegistry,
+            const TWeakPtr<JPH::PhysicsSystem>& InPhysicsSystem);
+
+        auto
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_ProbeTrace_RayCast& InRequest) const -> void;
+
+    private:
+        TWeakPtr<JPH::PhysicsSystem> _PhysicsSystem;
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    class CKSPATIALQUERY_API FProcessor_ProbeTrace_ShapeCast : public ck_exp::TProcessor<
+        FProcessor_ProbeTrace_ShapeCast,
+        FCk_Handle_ProbeTrace,
+        FFragment_ProbeTrace_ShapeCast,
+        CK_IGNORE_PENDING_KILL>
+    {
+    public:
+        using MarkedDirtyBy = FFragment_Probe_Requests;
+
+        FProcessor_ProbeTrace_ShapeCast(
+            const RegistryType& InRegistry,
+            const TWeakPtr<JPH::PhysicsSystem>& InPhysicsSystem);
+
+        auto
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_ProbeTrace_ShapeCast& InRequest) const -> void;
+
+    private:
+        TWeakPtr<JPH::PhysicsSystem> _PhysicsSystem;
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    class CKSPATIALQUERY_API FProcessor_ProbeTrace_DebugDraw_RayCast : public ck_exp::TProcessor<
+        FProcessor_ProbeTrace_DebugDraw_RayCast,
+        FCk_Handle_ProbeTrace,
+        FTag_ProbeTrace_DebugDraw,
+        FFragment_ProbeTrace_RayCast,
+        CK_IGNORE_PENDING_KILL>
+    {
+    public:
+        FProcessor_ProbeTrace_DebugDraw_RayCast(
+            const RegistryType& InRegistry,
+            const TWeakPtr<JPH::PhysicsSystem>& InPhysicsSystem);
+
+        auto
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_ProbeTrace_RayCast& InRequest) const -> void;
+
+    private:
+        TWeakPtr<JPH::PhysicsSystem> _PhysicsSystem;
+    };
+
+    // --------------------------------------------------------------------------------------------------------------------
+
+    class CKSPATIALQUERY_API FProcessor_ProbeTrace_DebugDraw_ShapeCast : public ck_exp::TProcessor<
+        FProcessor_ProbeTrace_DebugDraw_ShapeCast,
+        FCk_Handle_ProbeTrace,
+        FTag_ProbeTrace_DebugDraw,
+        FFragment_ProbeTrace_ShapeCast,
+        CK_IGNORE_PENDING_KILL>
+    {
+    public:
+        FProcessor_ProbeTrace_DebugDraw_ShapeCast(
+            const RegistryType& InRegistry,
+            const TWeakPtr<JPH::PhysicsSystem>& InPhysicsSystem);
+
+        auto
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_ProbeTrace_ShapeCast& InRequest) const -> void;
+
+    private:
+        TWeakPtr<JPH::PhysicsSystem> _PhysicsSystem;
+    };
+}
+
+// --------------------------------------------------------------------------------------------------------------------
