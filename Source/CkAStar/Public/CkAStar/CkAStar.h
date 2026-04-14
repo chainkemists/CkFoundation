602fb1e96d19e3d0be02171a112695478f26214f
Sulfur-CK <sulfur@chainkemists.com>
2026-04-13 22:12:21 -0700

    chore: add CkAStar umbrella include

---
+++ Source/CkAStar/Public/CkAStar/CkAStar.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "CkAStar/Algorithm/CkAStar_GraphConcept.h"
+#include "CkAStar/Algorithm/CkAStar_Types.h"
+#include "CkAStar/Algorithm/CkAStar_Search.h"
+#include "CkAStar/Algorithm/CkAStar_PathCache.h"
+#include "CkAStar/Algorithm/CkAStar_PrecomputedTable.h"
