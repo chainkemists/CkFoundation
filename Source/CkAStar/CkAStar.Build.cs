71d252f7714206ef17d5e7c72f085b7da4626c3e
Sulfur-CK <sulfur@chainkemists.com>
2026-04-13 22:07:30 -0700

    feat: add CkAStar module scaffolding — pure algorithm library for general-purpose A* search

---
+++ Source/CkAStar/CkAStar.Build.cs
@@ -0,0 +1,20 @@
+using System.IO;
+using UnrealBuildTool;
+
+public class CkAStar : CkModuleRules
+{
+	public CkAStar(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PrivateIncludePaths.AddRange(new string[] {
+			// ... add other private include paths required here ...
+		});
+
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"CoreUObject",
+
+			"CkCore",
+		});
+	}
+}
---
+++ Source/CkAStar/CkAStar_Module.cpp
@@ -0,0 +1,15 @@
+#include "CkAStar_Module.h"
+
+#define LOCTEXT_NAMESPACE "FCkAStarModule"
+
+void FCkAStarModule::StartupModule()
+{
+}
+
+void FCkAStarModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkAStarModule, CkAStar)
