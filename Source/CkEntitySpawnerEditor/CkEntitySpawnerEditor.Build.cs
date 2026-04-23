8adf4d70558450ded211354f61d3baa026e56cb3
Saad K <saadrustam@gmail.com>
2026-04-23 00:13:42 -0700

    feat(EntitySpawnerEditor): drag-and-drop EntityScripts into the viewport via UActorFactory, replace AInfo billboard with plugin EntityIcon.png, and expose icon scale/offset as user settings

---
+++ Source/CkEntitySpawnerEditor/CkEntitySpawnerEditor.Build.cs
@@ -0,0 +1,23 @@
+using UnrealBuildTool;
+
+public class CkEntitySpawnerEditor : CkModuleRules
+{
+    public CkEntitySpawnerEditor(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "UnrealEd",
+
+            "ImageWrapper",
+            "Projects",
+
+            "CkCore",
+            "CkEcs",
+            "CkEntitySpawner",
+            "CkLog",
+        });
+    }
+}
---
+++ Source/CkEntitySpawnerEditor/CkEntitySpawnerEditor_Log.cpp
@@ -0,0 +1,12 @@
+#include "CkEntitySpawnerEditor_Log.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+DEFINE_LOG_CATEGORY(CkEntitySpawnerEditor);
+
+namespace ck::entity_spawner_editor
+{
+    CK_REGISTER_LOG_FUNCTIONS(CkEntitySpawnerEditor);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
