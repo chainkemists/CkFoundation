45671687adf4017ab80fae4d1565e0d16ff9f008
Sulfur-CK <sulfur@chainkemists.com>
2026-05-06 19:25:49 -0700

    feat(CkIskmRenderer): add log category

---
+++ Source/CkIskmRenderer/CkIskmRenderer_Log.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "CkCore/Log/CkLog.h"
+
+CKISKMRENDERER_API DECLARE_LOG_CATEGORY_EXTERN(CkIskmRenderer, Log, All);
+
+namespace ck::iskm
+{
+    CK_DEFINE_LOG_FUNCTIONS(CkIskmRenderer);
+}
