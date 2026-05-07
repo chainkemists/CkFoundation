25ec113c3c896473a57e14abd870fff8bae5c293
Sulfur-CK <sulfur@chainkemists.com>
2026-05-06 19:23:20 -0700

    feat(CkIskmRenderer): add module entry

---
+++ Source/CkIskmRenderer/CkIskmRenderer_Module.cpp
@@ -0,0 +1,15 @@
+#include "CkIskmRenderer_Module.h"
+
+#define LOCTEXT_NAMESPACE "FCkIskmRendererModule"
+
+void FCkIskmRendererModule::StartupModule()
+{
+}
+
+void FCkIskmRendererModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkIskmRendererModule, CkIskmRenderer)
