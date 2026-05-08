b2ce44ed86dc54a073763e0e0efb79f59660a2e6
Sulfur-CK <sulfur@chainkemists.com>
2026-05-08 00:12:26 -0700

    feat(CkIskmRenderer): renderer fragments and tags

---
+++ Source/CkIskmRenderer/Public/CkIskmRenderer/Renderer/CkIskmRenderer_Fragment.cpp
@@ -0,0 +1,11 @@
+#include "CkIskmRenderer/Renderer/CkIskmRenderer_Fragment.h"
+
+#include "CkIskmRenderer/AnimCollection/CkIskmAnimCollection_Fragment_Data.h"
+
+namespace ck
+{
+    FFragment_IskmRenderer_Params::FFragment_IskmRenderer_Params(UCk_IskmRenderer_Data* InRendererData)
+        : _RendererData(InRendererData)
+    {
+    }
+}
---
+++ Source/CkIskmRenderer/Public/CkIskmRenderer/Renderer/CkIskmRenderer_Fragment.h
@@ -0,0 +1,49 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/WeakObjectPtr.h"
+
+#include "CkCore/Macros/CkMacros.h"
+#include "CkEcs/EntityId/CkEntityId.h"
+
+class UCk_IskmAnimCollection_Data;
+class UCk_IskmRenderer_Data;
+class ACk_IskmRenderer_Actor_UE;
+
+namespace ck
+{
+    CK_DEFINE_ECS_TAG(FTag_IskmRenderer_NeedsSetup);
+    CK_DEFINE_ECS_TAG(FTag_IskmRenderer_PendingAsyncLoad);
+
+    struct CKISKMRENDERER_API FFragment_IskmRenderer_Params
+    {
+    public:
+        CK_GENERATED_BODY(FFragment_IskmRenderer_Params);
+
+    public:
+        FFragment_IskmRenderer_Params() = default;
+        explicit FFragment_IskmRenderer_Params(UCk_IskmRenderer_Data* InRendererData);
+
+    private:
+        TWeakObjectPtr<UCk_IskmRenderer_Data> _RendererData;
+
+    public:
+        CK_PROPERTY_GET(_RendererData);
+    };
+
+    struct CKISKMRENDERER_API FFragment_IskmRenderer_Current
+    {
+    public:
+        CK_GENERATED_BODY(FFragment_IskmRenderer_Current);
+
+    public:
+        friend class FProcessor_IskmRenderer_Setup;
+        friend class UCk_Utils_IskmRenderer_UE;
+
+    private:
+        TWeakObjectPtr<ACk_IskmRenderer_Actor_UE> _RendererActor;
+
+    public:
+        CK_PROPERTY_GET(_RendererActor);
+    };
+}
