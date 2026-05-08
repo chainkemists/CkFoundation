8d9c10c5ff596829e5e6fe51f5c7beb7a1d22fc7
Sulfur-CK <sulfur@chainkemists.com>
2026-05-08 00:16:05 -0700

    feat(CkIskmRenderer): renderer setup processor

---
+++ Source/CkIskmRenderer/Public/CkIskmRenderer/Renderer/CkIskmRenderer_Processor.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "CkEcs/Processor/CkProcessor.h"
+#include "CkEcs/ProcessorGroup/CkProcessorGroup.h"
+
+#include "CkIskmRenderer/Renderer/CkIskmRenderer_Fragment.h"
+#include "CkIskmRenderer/Renderer/CkIskmRenderer_Fragment_Data.h"
+
+namespace ck
+{
+    class CKISKMRENDERER_API FProcessor_IskmRenderer_Setup : public ck_exp::TProcessor<
+        FProcessor_IskmRenderer_Setup,
+        FCk_Handle_IskmRenderer,
+        TReadOnly<FFragment_IskmRenderer_Params>,
+        FFragment_IskmRenderer_Current,
+        FTag_IskmRenderer_NeedsSetup,
+        CK_IGNORE_PENDING_KILL>
+    {
+    public:
+        using Group = FGroup_Gameplay_Rendering;
+        using MarkedDirtyBy = FTag_IskmRenderer_NeedsSetup;
+
+    public:
+        using TProcessor::TProcessor;
+
+    public:
+        auto
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            const FFragment_IskmRenderer_Params& InParams,
+            FFragment_IskmRenderer_Current& InCurrent) const -> void;
+    };
+}
