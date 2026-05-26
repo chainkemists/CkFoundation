8536fe85ca6e0afd0e3caab33e29051780054084
Sulfur-CK <sulfur@chainkemists.com>
2026-05-25 23:48:45 -0700

    feat(CkEntityTag): add FProcessor_EntityTag_HandleRequests

--- Source/CkEntityTag/Public/CkEntityTag/CkEntityTag_Processor.cpp
+++ Source/CkEntityTag/Public/CkEntityTag/CkEntityTag_Processor.cpp
@@ -3,13 +3,57 @@
 #include "CkCore/Algorithms/CkAlgorithms.h"
 
 #include "CkEntityTag/CkEntityTag_Log.h"
+#include "CkEntityTag/CkEntityTag_Utils.h"
 
 #include "CkEcs/Net/CkNet_Utils.h"
+#include "CkEcs/Scheduler/CkProcessorRegistration.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+CK_REGISTER_PROCESSOR(ck::FProcessor_EntityTag_HandleRequests);
 
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace ck
 {
-}
+    auto
+        FProcessor_EntityTag_HandleRequests::
+        ForEachEntity(
+            TimeType,
+            HandleType InHandle,
+            FFragment_EntityTag_Requests& InRequestsComp) const
+        -> void
+    {
+        InHandle.CopyAndRemove(InRequestsComp, [&](const FFragment_EntityTag_Requests& InRequests)
+        {
+            ck::algo::ForEachRequest(InRequests.Get_Requests(),
+                ck::Visitor([&](const auto& InEntry)
+                {
+                    DoHandleRequest(InHandle, InEntry);
+                }),
+                ck::policy::DontResetContainer{});
+        });
+    }
 
-// --------------------------------------------------------------------------------------------------------------------
+    // ----
+
+    auto
+        FProcessor_EntityTag_HandleRequests::
+        DoHandleRequest(
+            HandleType& InHandle,
+            const FCk_Request_EntityTag_Add& InRequest)
+        -> void
+    {
+        UCk_Utils_EntityTag_UE::DoApply_Add(InHandle, InRequest.Get_Tag());
+    }
+
+    auto
+        FProcessor_EntityTag_HandleRequests::
+        DoHandleRequest(
+            HandleType& InHandle,
+            const FCk_Request_EntityTag_TryRemove& InRequest)
+        -> void
+    {
+        UCk_Utils_EntityTag_UE::DoApply_TryRemove(InHandle, InRequest.Get_Tag());
+    }
+}
--- Source/CkEntityTag/Public/CkEntityTag/CkEntityTag_Processor.h
+++ Source/CkEntityTag/Public/CkEntityTag/CkEntityTag_Processor.h
@@ -2,13 +2,39 @@
 
 #include "CkEntityTag_Fragment.h"
 #include "CkEcs/EntityLifetime/CkEntityLifetime_Fragment.h"
-
 #include "CkEcs/Processor/CkProcessor.h"
+#include "CkEcs/Scheduler/CkProcessorGroups.h"
 
 // --------------------------------------------------------------------------------------------------------------------
 
 namespace ck
 {
-}
+    class CKENTITYTAG_API FProcessor_EntityTag_HandleRequests : public ck_exp::TProcessor<
+            FProcessor_EntityTag_HandleRequests,
+            FCk_Handle,
+            FFragment_EntityTag_Requests,
+            CK_IGNORE_PENDING_KILL>
+    {
+    public:
+        using MarkedDirtyBy = FFragment_EntityTag_Requests;
+        using TProcessor::TProcessor;
 
-// --------------------------------------------------------------------------------------------------------------------
+    public:
+        auto
+        ForEachEntity(
+            TimeType InDeltaT,
+            HandleType InHandle,
+            FFragment_EntityTag_Requests& InRequestsComp) const -> void;
+
+    private:
+        static auto
+        DoHandleRequest(
+            HandleType& InHandle,
+            const FCk_Request_EntityTag_Add& InRequest) -> void;
+
+        static auto
+        DoHandleRequest(
+            HandleType& InHandle,
+            const FCk_Request_EntityTag_TryRemove& InRequest) -> void;
+    };
+}
