5ea0bbb18ac93756516b01b80338f529117c5a63
Sulfur-CK <sulfur@chainkemists.com>
2026-05-26 00:48:45 -0700

    feat(CkEntityTagQuery): handle, enums, requirement, result, requests, delegate

---
+++ Source/CkEntityTag/Public/CkEntityTag/Query/CkEntityTagQuery_Fragment_Data.cpp
@@ -0,0 +1,68 @@
+#include "CkEntityTagQuery_Fragment_Data.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+FCk_EntityTagQuery_Requirement::FCk_EntityTagQuery_Requirement(
+    FName InTag,
+    ECk_EntityTagQuery_CountMode InMode,
+    int32 InCount,
+    int32 InMaxAllowedEnsure)
+    : _Tag(InTag)
+    , _Mode(InMode)
+    , _Count(InCount)
+    , _MaxAllowedEnsure(InMaxAllowedEnsure)
+{}
+
+// ----
+
+auto
+    FCk_EntityTagQuery_Requirement::
+    Single(
+        FName InTag)
+    -> FCk_EntityTagQuery_Requirement
+{
+    return FCk_EntityTagQuery_Requirement{InTag, ECk_EntityTagQuery_CountMode::SingleOnly, 1, 0};
+}
+
+auto
+    FCk_EntityTagQuery_Requirement::
+    Of(
+        FName InTag,
+        int32 InCount)
+    -> FCk_EntityTagQuery_Requirement
+{
+    return FCk_EntityTagQuery_Requirement{InTag, ECk_EntityTagQuery_CountMode::Count, InCount, 0};
+}
+
+auto
+    FCk_EntityTagQuery_Requirement::
+    All(
+        FName InTag)
+    -> FCk_EntityTagQuery_Requirement
+{
+    return FCk_EntityTagQuery_Requirement{InTag, ECk_EntityTagQuery_CountMode::All, 0, 0};
+}
+
+// ----
+
+auto
+    FCk_EntityTagQuery_Requirement::
+    WithEnsure(
+        int32 InMaxAllowed) &
+    -> FCk_EntityTagQuery_Requirement&
+{
+    _MaxAllowedEnsure = InMaxAllowed;
+    return *this;
+}
+
+auto
+    FCk_EntityTagQuery_Requirement::
+    WithEnsure(
+        int32 InMaxAllowed) &&
+    -> FCk_EntityTagQuery_Requirement
+{
+    _MaxAllowedEnsure = InMaxAllowed;
+    return MoveTemp(*this);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
---
+++ Source/CkEntityTag/Public/CkEntityTag/Query/CkEntityTagQuery_Fragment_Data.h
@@ -0,0 +1,156 @@
+#pragma once
+
+#include "CkCore/Macros/CkMacros.h"
+
+#include "CkEcs/Handle/CkHandle.h"
+#include "CkEcs/Handle/CkHandle_TypeSafe.h"
+#include "CkEcs/Request/CkRequest_Data.h"
+
+#include <GameplayTagContainer.h>
+
+#include "CkEntityTagQuery_Fragment_Data.generated.h"
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKENTITYTAG_API FCk_Handle_EntityTagQuery : public FCk_Handle_TypeSafe
+{
+    GENERATED_BODY()
+    CK_GENERATED_BODY_HANDLE_TYPESAFE(FCk_Handle_EntityTagQuery);
+};
+
+CK_DEFINE_CUSTOM_ISVALID_AND_FORMATTER_HANDLE_TYPESAFE(FCk_Handle_EntityTagQuery);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+UENUM(BlueprintType)
+enum class ECk_EntityTagQuery_CountMode : uint8
+{
+    SingleOnly,
+    Count,
+    All
+};
+
+CK_DEFINE_CUSTOM_FORMATTER_ENUM(ECk_EntityTagQuery_CountMode);
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKENTITYTAG_API FCk_EntityTagQuery_Requirement
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_EntityTagQuery_Requirement);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true, Categories="EntityTag"))
+    FName _Tag;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+    ECk_EntityTagQuery_CountMode _Mode = ECk_EntityTagQuery_CountMode::Count;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true,
+                     EditCondition = "_Mode == ECk_EntityTagQuery_CountMode::Count"))
+    int32 _Count = 1;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+    int32 _MaxAllowedEnsure = 0;
+
+public:
+    CK_PROPERTY_GET(_Tag);
+    CK_PROPERTY_GET(_Mode);
+    CK_PROPERTY_GET(_Count);
+    CK_PROPERTY_GET(_MaxAllowedEnsure);
+
+    // Hand-rolled (not CK_DEFINE_CONSTRUCTORS) so we can also expose named factories.
+    FCk_EntityTagQuery_Requirement() = default;
+    FCk_EntityTagQuery_Requirement(
+        FName InTag,
+        ECk_EntityTagQuery_CountMode InMode,
+        int32 InCount,
+        int32 InMaxAllowedEnsure);
+
+    static FCk_EntityTagQuery_Requirement Single(FName InTag);
+    static FCk_EntityTagQuery_Requirement Of(FName InTag, int32 InCount);
+    static FCk_EntityTagQuery_Requirement All(FName InTag);
+
+    // Chainable mutator: `Of(n"A", 3).WithEnsure(5)`
+    FCk_EntityTagQuery_Requirement& WithEnsure(int32 InMaxAllowed) &;
+    FCk_EntityTagQuery_Requirement  WithEnsure(int32 InMaxAllowed) &&;
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKENTITYTAG_API FCk_EntityTagQuery_Result
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_EntityTagQuery_Result);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+    FName _Tag;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+    TArray<FCk_Handle> _Handles;
+
+public:
+    CK_PROPERTY_GET(_Tag);
+    CK_PROPERTY_GET(_Handles);
+    CK_DEFINE_CONSTRUCTORS(FCk_EntityTagQuery_Result, _Tag, _Handles);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKENTITYTAG_API FCk_Request_EntityTagQuery_AddRequirement : public FCk_Request_Base
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Request_EntityTagQuery_AddRequirement);
+    CK_REQUEST_DEFINE_DEBUG_NAME(FCk_Request_EntityTagQuery_AddRequirement);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+    FCk_EntityTagQuery_Requirement _Requirement;
+
+public:
+    CK_PROPERTY_GET(_Requirement);
+    CK_DEFINE_CONSTRUCTORS(FCk_Request_EntityTagQuery_AddRequirement, _Requirement);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+USTRUCT(BlueprintType)
+struct CKENTITYTAG_API FCk_Request_EntityTagQuery_RemoveRequirement : public FCk_Request_Base
+{
+    GENERATED_BODY()
+
+public:
+    CK_GENERATED_BODY(FCk_Request_EntityTagQuery_RemoveRequirement);
+    CK_REQUEST_DEFINE_DEBUG_NAME(FCk_Request_EntityTagQuery_RemoveRequirement);
+
+private:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite,
+              meta = (AllowPrivateAccess = true, Categories="EntityTag"))
+    FName _Tag;
+
+public:
+    CK_PROPERTY_GET(_Tag);
+    CK_DEFINE_CONSTRUCTORS(FCk_Request_EntityTagQuery_RemoveRequirement, _Tag);
+};
+
+// --------------------------------------------------------------------------------------------------------------------
+
+DECLARE_DYNAMIC_DELEGATE_TwoParams(
+    FCk_Delegate_EntityTagQuery_OnSatisfied,
+    FCk_Handle_EntityTagQuery, InQuery,
+    const TArray<FCk_EntityTagQuery_Result>&, InResults);
+
+// --------------------------------------------------------------------------------------------------------------------
