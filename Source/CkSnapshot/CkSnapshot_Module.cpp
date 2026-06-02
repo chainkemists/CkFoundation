b695063810f333baeaaeadfcc22553e6af67ac67
Saad K <saadrustam@gmail.com>
2026-06-02 14:33:05 -0700

    feat(CkSnapshot): module init + log category

---
+++ Source/CkSnapshot/CkSnapshot_Module.cpp
@@ -0,0 +1,15 @@
+#include "CkSnapshot_Module.h"
+
+#define LOCTEXT_NAMESPACE "FCkSnapshotModule"
+
+void FCkSnapshotModule::StartupModule()
+{
+}
+
+void FCkSnapshotModule::ShutdownModule()
+{
+}
+
+#undef LOCTEXT_NAMESPACE
+
+IMPLEMENT_MODULE(FCkSnapshotModule, CkSnapshot)
