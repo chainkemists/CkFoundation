748a9ddd5c57b7794d7751bcb9ac016736810ed0
Saad K <saadrustam@gmail.com>
2026-06-02 15:18:25 -0700

    feat(CkSnapshot): FragmentRegistry + CK_REGISTER_SNAPSHOTABLE macro
    Process-wide singleton. Macro drops one line in each owning *_Fragment.cpp;
    static_assert fires with an actionable message if the fragment satisfies
    neither tier.

---
+++ Source/CkSnapshot/Public/CkSnapshot/Context/CkSnapshot_FragmentRegistry.cpp
@@ -0,0 +1,32 @@
+#include "CkSnapshot_FragmentRegistry.h"
+
+#include "CkSnapshot_Log.h"
+
+namespace ck
+{
+    auto FCk_Snapshot_FragmentRegistry::Get() -> FCk_Snapshot_FragmentRegistry&
+    {
+        static FCk_Snapshot_FragmentRegistry Instance;
+        return Instance;
+    }
+
+    auto FCk_Snapshot_FragmentRegistry::Register(FCk_Snapshot_RegisteredFragment InEntry) -> void
+    {
+        _Entries.Emplace(MoveTemp(InEntry));
+    }
+
+    auto FCk_Snapshot_FragmentRegistry::Get_All() const -> const TArray<FCk_Snapshot_RegisteredFragment>&
+    {
+        return _Entries;
+    }
+
+    auto FCk_Snapshot_FragmentRegistry::Find_ByDisplayName(const FString& InName) const -> const FCk_Snapshot_RegisteredFragment*
+    {
+        return _Entries.FindByPredicate([&](const auto& E) { return E._DisplayName == InName; });
+    }
+
+    auto FCk_Snapshot_FragmentRegistry::Find_ByEnttHash(uint32 InHash) const -> const FCk_Snapshot_RegisteredFragment*
+    {
+        return _Entries.FindByPredicate([&](const auto& E) { return E._EnttTypeHash == InHash; });
+    }
+}
