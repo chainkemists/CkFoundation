ad045415b3a07323418e738125f60403f325a693
Adam Letourneau <adam.letourneau1993@gmail.com>
2026-06-09 16:11:05 -0400

    chore: Remove deprecated CkTemplate and CkEcsTemplate modules

--- Source/CkEcsTemplate/Public/CkEcsTemplate/CkEcsTemplate_Fragment.cpp
+++
@@ -1,9 +0,0 @@
-#include "CkEcsTemplate_Fragment.h"
-
-#include "CkEcs/Handle/CkDebugCallstack_Macros.h"
-
-// --------------------------------------------------------------------------------------------------------------------
-
-CK_ECS_DEFINE_CALLSTACK_ANGELSCRIPT_UTILS(CKECSTEMPLATE_API, ecstemplate, ck::FFragment_EcsTemplate_Requests);
-
-// --------------------------------------------------------------------------------------------------------------------
